28420d2896b7b8cc90e027bfc095227f60d74709
Eddy Ashton <v-edasht@microsoft.com>
2019-05-07 17:13:54 +0100

    Split into methods

--- src/clients/logging_client.cpp
+++ src/clients/logging_client.cpp
@@ -15,13 +15,69 @@ class LoggingClient
 {
   unique_ptr<RpcTlsClient> rpc_client;
 
+  static std::string make_message(const std::string& body)
+  {
+    stringstream ss;
+    timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    ss << "[LEVEL] ";
+    // ISO 8601, plus 6 digits of us, aka
+    // YYYY-mm-ddTHH:MM:SS.uuuuuu
+    ss << put_time(std::localtime(&ts.tv_sec), "%FT%T.") << setfill('0')
+       << setw(6) << (ts.tv_nsec / 1000);
+    ss << " " << body;
+    return ss.str();
+  }
+
+  void record_log_message(size_t id, const std::string& msg, bool print_status)
+  {
+    json params;
+    params["id"] = id;
+    params["msg"] = msg;
+
+    const auto response_bytes = rpc_client->call("LOG_record", params);
+    const json response = json::from_msgpack(response_bytes);
+
+    const auto error_it = response.find("error");
+    if (error_it != response.end())
+    {
+      throw std::runtime_error(
+        "LOG_record returned error: " + error_it->dump());
+    }
+
+    if (print_status)
+    {
+      cout << "Sent: " << params.dump() << endl;
+      cout << "Received: " << response.dump() << endl;
+    }
+  }
+
+  void get_log_message(size_t id)
+  {
+    json params;
+    params["id"] = id;
+
+    const auto response_bytes = rpc_client->call("LOG_get", params);
+    const json response = json::from_msgpack(response_bytes);
+
+    const auto error_it = response.find("error");
+    if (error_it != response.end())
+    {
+      throw std::runtime_error("LOG_get returned error: " + error_it->dump());
+    }
+
+    cout << "Sent: " << params.dump() << endl;
+    cout << "Received: " << response.dump() << endl;
+  }
+
 public:
   LoggingClient(
     const string& host, const string& port, const shared_ptr<tls::Cert>& cert) :
     rpc_client(make_unique<RpcTlsClient>(host, port, "users", nullptr, cert))
   {}
 
-  void run(size_t num_transactions, size_t initial_record_id)
+  void run(
+    size_t num_transactions, size_t initial_record_id, const std::string& body)
   {
     const size_t final_record_id = initial_record_id + num_transactions - 1;
     for (auto record_id = initial_record_id; record_id <= final_record_id;
@@ -31,60 +87,15 @@ public:
       bool const print_status =
         record_id == initial_record_id || record_id == final_record_id;
 
-      stringstream ss;
-      timespec ts;
-      timespec_get(&ts, TIME_UTC);
-      ss << "[LEVEL] ";
-      // ISO 8601, plus 6 digits of us, aka
-      // YYYY-mm-ddTHH:MM:SS.uuuuuu
-      ss << put_time(std::localtime(&ts.tv_sec), "%FT%T.") << setfill('0')
-         << setw(6) << (ts.tv_nsec / 1000);
-      ss << " Sample log message, for the purpose of testing";
-      const auto record_msg = ss.str();
+      const auto record_msg = make_message(body);
 
       // First transaction - record a log message
-      {
-        json params;
-        params["id"] = record_id;
-        params["msg"] = record_msg;
-
-        const auto response_bytes = rpc_client->call("LOG_record", params);
-        const json response = json::from_msgpack(response_bytes);
-
-        const auto error_it = response.find("error");
-        if (error_it != response.end())
-        {
-          throw std::runtime_error(
-            "LOG_record returned error: " + error_it->dump());
-        }
-
-        if (print_status)
-        {
-          cout << "Sent: " << params.dump() << endl;
-          cout << "Received: " << response.dump() << endl;
-        }
-      }
+      record_log_message(record_id, record_msg, print_status);
 
       if (print_status)
       {
         // Second transaction - get a log message
-        {
-          json params;
-          params["id"] = record_id;
-
-          const auto response_bytes = rpc_client->call("LOG_get", params);
-          const json response = json::from_msgpack(response_bytes);
-
-          const auto error_it = response.find("error");
-          if (error_it != response.end())
-          {
-            throw std::runtime_error(
-              "LOG_get returned error: " + error_it->dump());
-          }
-
-          cout << "Sent: " << params.dump() << endl;
-          cout << "Received: " << response.dump() << endl;
-        }
+        get_log_message(record_id);
       }
     }
   }
@@ -96,6 +107,7 @@ int main(int argc, char** argv)
   string port;
   size_t num_messages = 1;
   size_t msg_id = 42;
+  std::string msg_body = "Sample log message, for the purposes of testing";
   string cert_file, key_file, ca_file;
 
   {
@@ -116,6 +128,8 @@ int main(int argc, char** argv)
       "--msg-count", num_messages, "Number of log messages to record", true);
     cli_app.add_option(
       "--msg-id", msg_id, "ID to be used for first log message", true);
+    cli_app.add_option(
+      "--msg-body", msg_body, "Body of log sent with each message");
 
     CLI11_PARSE(cli_app, argc, argv);
   }
@@ -131,7 +145,7 @@ int main(int argc, char** argv)
 
   try
   {
-    client.run(num_messages, msg_id);
+    client.run(num_messages, msg_id, msg_body);
   }
   catch (const char* e)
   {
