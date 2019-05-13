6fa54c4ab58cb05fc07b4a83214f2fdc834e035b
Amaury Chamayou <amchamay@microsoft.com>
2019-05-13 11:27:07 +0100

    Formatting

--- src/node/test/merkle_mem.cpp
+++ src/node/test/merkle_mem.cpp
@@ -1,13 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
-#include "../history.h"
 #include "../../ds/logger.h"
+#include "../history.h"
 
 #include <algorithm>
 #include <random>
-
-#include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/time.h>
 
 extern "C"
 {
@@ -22,11 +21,11 @@ static constexpr size_t flushes_without_retract = 10;
 
 static size_t get_maxrss()
 {
-    rusage r;
-    auto rc = getrusage(RUSAGE_SELF, &r);
-    if (rc != 0)
-        throw std::logic_error("getrusage failed");
-    return r.ru_maxrss;
+  rusage r;
+  auto rc = getrusage(RUSAGE_SELF, &r);
+  if (rc != 0)
+    throw std::logic_error("getrusage failed");
+  return r.ru_maxrss;
 }
 
 static crypto::Sha256Hash random_hash(std::random_device& rdev)
@@ -61,8 +60,7 @@ static int append_flush_and_retract()
       }
     }
     if (index % (appends / 10) == 0)
-        LOG_INFO << "MAX RSS: " << get_maxrss() << "Kb" << std::endl;
-
+      LOG_INFO << "MAX RSS: " << get_maxrss() << "Kb" << std::endl;
   }
   LOG_INFO << "MAX RSS: " << get_maxrss() << "Kb" << std::endl;
 
