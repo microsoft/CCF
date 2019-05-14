61185b1da36f68f3d71022cba90b6d2c6d0b407c
Eddy Ashton <v-edasht@microsoft.com>
2019-05-14 16:56:19 +0100

    Broken WIP

--- src/apps/logging/json_parse.cpp
+++ src/apps/logging/json_parse.cpp
@@ -10,35 +10,31 @@ struct JsonField
   using Target = TTarget;
 };
 
-template <size_t N>
-auto json_find_fields(
-  const nlohmann::json& j,
-  const std::array<std::string, N>& fields,
-  bool required) -> std::array<nlohmann::json::const_iterator, N>
-{
-  if (!j.is_object())
-  {
-    throw std::invalid_argument("Expected an object, received " + j.dump());
-  }
+// template <typename Field, typename... Rest>
+// auto json_find_fields(const nlohmann::json& j)
+// {
+//   const auto it = j.find(Field::name);
+//   if (Field::required && it == j.end())
+//   {
+//     throw std::invalid_argument(
+//       "Missing required field '" + Field::name + "' in object: " + j.dump());
+//   }
 
-  std::array<nlohmann::json::const_iterator, N> found = {};
+//   if constexpr (sizeof...(Rest) == 0)
+//   {
+//     return std::make_tuple(it);
+//   }
+//   else
+//   {
+//     return std::tuple_cat(it, json_find_fields<Rest...>(j));
+//   }
+// }
 
-  for (size_t i = 0; i < fields.size(); ++i)
-  {
-    const auto& field = fields[i];
-    const auto it = j.find(field);
-    if (required && it == j.end())
-    {
-      throw std::invalid_argument(
-        "Object is missing required field '" + field +
-        "'. Object is: " + j.dump());
-    }
-
-    found[i] = it;
-  }
-
-  return found;
-}
+// template <typename T>
+// void parse_with_validation(const nlohmann::json& j, T& t)
+// {
+//   using Spec = JsonSpec<T>;
+// }
 
 template <typename T>
 struct JsonSpec;
@@ -49,27 +45,71 @@ struct RecordParams
   std::string msg;
 };
 
+struct TAG_id
+{
+  static constexpr auto name = "id";
+};
+struct MSG_id
+{
+  static constexpr auto name = "msg";
+};
+
+#define ADD_JSON_TRANSLATORS(C, attr...) \
+  inline void from_json(const nlohmann::json& j, C& c) \
+  { \
+    _JSON_N(attr)(FROM, attr) \
+  } \
+  inline void to_json(nlohmann::json& j, const C& c) \
+  { \
+    _JSON_N(attr)(TO, attr) \
+  }
+
+#define DECLARE_REQUIRED_JSON_FIELDS(TYPE, FIELD_A, FIELD_B)
+DECLARE_REQUIRED_JSON_FIELDS(RecordParams, id, msg)
+
+enum ErrorCodes : int16_t
+{
+#define XX(Name, Value) Name = Value,
+  XX_ERROR_CODES
+#undef XX
+};
+
+using FIELD_id = JsonField<TAG_id, true, decltype(RecordParams::id)>;
+
 template <>
 struct JsonSpec<RecordParams>
 {
-  struct TAG_id
-  {
-    static constexpr auto name = "id";
-  };
-  struct MSG_id
-  {
-    static constexpr auto name = "msg";
-  };
-
   using RequiredFields = std::tuple<
     JsonField<TAG_id, true, size_t>,
     JsonField<MSG_id, true, std::string>>;
 };
 
-template <typename T>
-void parse_with_validation(const nlohmann::json& j, T& t)
+inline void to_json(nlohmann::json& j, const RecordParams& rp)
 {
-  using Spec = JsonSpec<T>;
+  j["id"] = rp.id;
+  j["msg"] = rp.msg;
+
+  //
+
+  j[FIELD_id::name] = rp.id;
+}
+
+inline void from_json(const nlohmann::json& j, RecordParams& rp)
+{
+  rp.id = j["id"];
+  rp.msg = j["msg"];
+
+  //
+
+  const auto it = j.find(FIELD_id::name);
+  if (it == j.end())
+  {
+    throw std::invalid_argument(
+      "Missing required field '" + std::string(FIELD_id::name) +
+      "' in object: " + j.dump());
+  }
+
+  rp.id = it->get<decltype(rp.id)>();
 }
 
 int main(int argc, char** argv)
@@ -78,8 +118,8 @@ int main(int argc, char** argv)
     nlohmann::json a;
     a["hello"] = "world";
 
-    const auto found =
-      json_find_fields(a, std::array<std::string, 1>{"hello"}, true);
+    // const auto found =
+    //   json_find_fields<JsonSpec<RecordParams>::RequiredFields>(a);
   }
   return 0;
 }
