41197b55ee6fbd3cdb930d3b2a557709ab01bed8
Olga Vrousgou <olga.vrousgou@microsoft.com>
2019-05-15 18:09:44 +0100

    Adding css to all html files

--- tests/coverage/style_html.py
+++ tests/coverage/style_html.py
@@ -1,17 +1,18 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 import argparse
+import glob
 
 """
-copies the css into the html file
+copies the css into the html files
 """
 
 style_file = "coverage/style.css"
-html_file = "coverage/index.html"
 
-
-with open(html_file, "a") as html:
-    with open(style_file) as style:
-        html.write('<style type="text/css">')
-        html.write(style.read())
-        html.write("</style>")
+with open(style_file) as style:
+    css = style.read()
+    for filename in glob.iglob("coverage/**/*.html", recursive=True):
+        with open(filename, "a") as html:
+            html.write('<style type="text/css">')
+            html.write(css)
+            html.write("</style>")
