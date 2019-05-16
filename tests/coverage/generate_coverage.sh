a1c273356e4a1914c50253bbcdfc39b826148158
Olga Vrousgou <olga.vrousgou@microsoft.com>
2019-05-16 09:18:05 +0100

    fixing shellcheck comments

--- tests/coverage/generate_coverage.sh
+++ tests/coverage/generate_coverage.sh
@@ -2,13 +2,19 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-echo *profraw | xargs -n1 > prof_files
-llvm-profdata-7 merge -sparse -f prof_files -o coverage.profdata
-objects=""; 
-for f in *_test; do 
-    objects=$objects" -object $f" 
+for f in *profraw; do
+    echo "$f" >> prof_files
 done
-llvm-cov-7 show -instr-profile coverage.profdata -output-dir=coverage -format=html ds_test "$objects" -Xdemangler c++filt -Xdemangler -n -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)"
-llvm-cov-7 export -instr-profile coverage.profdata -format=text ds_test "$objects" -Xdemangler c++filt -Xdemangler -n -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)" -summary-only > coverage.json
+
+llvm-profdata-7 merge -sparse -f prof_files -o coverage.profdata
+
+objects=()
+for f in *_test; do
+    objects+=( -object "$f")
+done
+
+llvm-cov-7 show -instr-profile coverage.profdata -output-dir=coverage -format=html ds_test "${objects[@]}" -Xdemangler c++filt -Xdemangler -n -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)"
+llvm-cov-7 export -instr-profile coverage.profdata -format=text ds_test "${objects[@]}" -Xdemangler c++filt -Xdemangler -n -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)" -summary-only > coverage.json
+
 python3.7 ../tests/coverage/cobertura_generator.py
 python3.7 ../tests/coverage/style_html.py
