9c04a996e2f51c45bb9e770fe48d4cd838ca7a7b
Olga Vrousgou <olga.vrousgou@microsoft.com>
2019-05-17 09:09:47 +0100

    Adding copyright

--- tests/coverage/add_perf_summary.py
+++ tests/coverage/add_perf_summary.py
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
 import time
 import json
 
@@ -8,4 +10,11 @@ with open("coverage.json", "r") as file:
     lines_valid = str(data["totals"]["lines"]["count"])
 
 with open("perf_summary.csv", "a") as f:
-    f.write(timestamp + "," + lines_valid + ",Unit_Test_Coverage,0,0,0," + lines_covered + ",0,0,0,0")
+    f.write(
+        timestamp
+        + ","
+        + lines_valid
+        + ",Unit_Test_Coverage,0,0,0,"
+        + lines_covered
+        + ",0,0,0,0"
+    )
