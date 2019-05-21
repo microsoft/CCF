d079ecb3e42cf266e5647f75c18589cab602d78c
Julien Maffre <jumaffre@microsoft.com>
2019-05-21 17:02:21 +0100

    Cleanup

--- tests/coverage/generate_coverage.sh
+++ tests/coverage/generate_coverage.sh
@@ -2,27 +2,34 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
+set -e
+
 for f in *profraw; do
     echo "$f" >> prof_files
 done
 
-llvm-profdata-7 merge -sparse -f prof_files -o coverage.profdata
-
+# Generate html coverage report for individual unit test
 objects=()
 for f in *_test; do
     objects+=( -object "$f")
     llvm-cov-7 show -instr-profile "$f".profdata -output-dir=cov_"$f" -format=html "$f" -Xdemangler c++filt -Xdemangler -n -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)"
 done
 
+# Merge coverage report for all unit tests
+llvm-profdata-7 merge -sparse -f prof_files -o coverage.profdata
+
+# Generate combined coverage report
 llvm-cov-7 show -instr-profile coverage.profdata -output-dir=coverage -format=html "${objects[@]}" -Xdemangler c++filt -Xdemangler -n -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)"
 llvm-cov-7 export -instr-profile coverage.profdata -format=text "${objects[@]}" -Xdemangler c++filt -Xdemangler -n -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)" -summary-only > coverage.json
 
-llvm-cov-7 show -instr-profile coverage.profdata "${objects[@]}" -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)" > coverage2.txt
+# Generate and upload combined coverage report for Codecov
+llvm-cov-7 show -instr-profile coverage.profdata "${objects[@]}" -ignore-filename-regex="(boost|openenclave|3rdparty|/test/)" > codecov.txt
+bash <(curl -s https://codecov.io/bash) -t "${CODECOV_TOKEN}" -f codecov.txt
 
-bash <(curl -s https://codecov.io/bash) -t ${CODECOV_TOKEN} -f coverage2.txt || echo "Codecov did not collect coverage reports"
-
+# Generate html for Azure Devops
 mv cov_* coverage/
-
 python3.7 ../tests/coverage/cobertura_generator.py
 python3.7 ../tests/coverage/style_html.py
+
+# Add coverage results to perf summary file
 python3.7 ../tests/coverage/add_perf_summary.py
