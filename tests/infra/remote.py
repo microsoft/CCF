dcafc101b447613c192b9909846ac025f0d92cc9
Amaury Chamayou <amchamay@microsoft.com>
2019-05-24 21:52:14 +0100

    Coverage for *all* the nodes

--- tests/infra/remote.py
+++ tests/infra/remote.py
@@ -456,7 +456,7 @@ class CCFRemote(object):
         env = {}
         self.profraw = None
         if enclave_type == "virtual":
-            self.profraw = f"{os.path.basename(lib_path)}.profraw"
+            self.profraw = f"{node_id}_{os.path.basename(lib_path)}.profraw"
             env["LLVM_PROFILE_FILE"] = self.profraw
 
         oe_log_level = CCF_TO_OE_LOG_LEVEL.get(log_level)
