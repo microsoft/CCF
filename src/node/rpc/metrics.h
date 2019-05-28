7e6877141e22170f583d3b7645c2d3429527fd45
Eddy Ashton <v-edasht@microsoft.com>
2019-05-28 14:04:21 +0100

    Formatting

--- src/node/rpc/metrics.h
+++ src/node/rpc/metrics.h
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
-#include "serialization.h"
 #include "ds/histogram.h"
 #include "ds/logger.h"
+#include "serialization.h"
 
 #include <nlohmann/json.hpp>
 
