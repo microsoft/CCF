c22133fb6da45a3ad2003ba619090a2e28fb8493
rschust <roy.schuster@hotmail.com>
2019-06-11 16:41:19 +0100

    Update addnode.py

--- tests/addnode.py
+++ tests/addnode.py
@@ -11,28 +11,33 @@ import time
 from loguru import logger as LOG
 
 
-def create_and_add_node(network, args, node_id, member_host, member_port, should_succeed=True):
+def create_and_add_node(
+    network, args, node_id, member_host, member_port, should_succeed=True
+):
     dict_args = vars(args)
     forwarded_args = {
         arg: dict_args[arg] for arg in infra.ccf.Network.node_args_to_forward
     }
     node_status = args.node_status or "pending"
     new_node = network.create_node(node_id, "localhost")
-    new_node.start(lib_name="libloggingenc",
-                   node_status=node_status, **forwarded_args)
+    new_node.start(lib_name="libloggingenc", node_status=node_status, **forwarded_args)
     new_node_info = new_node.remote.info()
-    new_cert_path = "{}/{}".format(new_node.remote.remote.root,
-                                   getattr(new_node.remote, "pem"))
+    new_cert_path = "{}/{}".format(
+        new_node.remote.remote.root, getattr(new_node.remote, "pem")
+    )
 
     if should_succeed:
-        new_quote_path = "{}/{}".format(new_node.remote.remote.root,
-                                        getattr(new_node.remote, "quote"))
+        new_quote_path = "{}/{}".format(
+            new_node.remote.remote.root, getattr(new_node.remote, "quote")
+        )
     else:
         invalid_node = network.create_node(node_id + 1, "localhost")
-        invalid_node.start(lib_name="libluagenericenc",
-                           node_status=node_status, **forwarded_args)
-        new_quote_path = "{}/{}".format(invalid_node.remote.remote.root,
-                                        getattr(invalid_node.remote, "quote"))
+        invalid_node.start(
+            lib_name="libluagenericenc", node_status=node_status, **forwarded_args
+        )
+        new_quote_path = "{}/{}".format(
+            invalid_node.remote.remote.root, getattr(invalid_node.remote, "quote")
+        )
         invalid_node.stop()
 
     result = infra.proc.ccall(
@@ -83,13 +88,16 @@ def run(args):
 
         # add a valid node
         new_node_info = create_and_add_node(
-            network, args, 2, primary.host, primary.tls_port)
+            network, args, 2, primary.host, primary.tls_port
+        )
         # add an invalid node
         new_node_info = create_and_add_node(
-            network, args, 3, primary.host, primary.tls_port, False)
+            network, args, 3, primary.host, primary.tls_port, False
+        )
 
 
 if __name__ == "__main__":
+
     def add(parser):
         parser.add_argument(
             "-p",
@@ -97,6 +105,9 @@ if __name__ == "__main__":
             help="The enclave package to load (e.g., libsimplebank)",
             required=True,
         )
+
     args = e2e_args.cli_args(add)
-    args.package = args.app_script and "libluagenericenc" or "libloggingenc" or "libunsignedenc"
+    args.package = (
+        args.app_script and "libluagenericenc" or "libloggingenc" or "libunsignedenc"
+    )
     run(args)
