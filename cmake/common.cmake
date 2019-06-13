d87e644fb5cab7429e5c013a210246264253984c
Eddy Ashton <v-edasht@microsoft.com>
2019-06-13 13:37:53 +0100

    Move BUILD_END_TO_END_TESTS to common

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -86,6 +86,7 @@ endif()
 
 option(SAN "Enable Address and Undefined Behavior Sanitizers" OFF)
 option(DISABLE_QUOTE_VERIFICATION "Disable quote verification" OFF)
+option(BUILD_END_TO_END_TESTS "Build end to end tests" ON)
 
 option(PBFT "Enable PBFT" OFF)
 if (PBFT)
