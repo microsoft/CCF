0cad061ad8c78c61734ee55005a1e55cc74a4534
Eddy Ashton <v-edasht@microsoft.com>
2019-07-05 16:18:29 +0100

    Manual schema to match manual conversion

--- src/node/clientsignatures.h
+++ src/node/clientsignatures.h
@@ -22,7 +22,43 @@ namespace ccf
   // this maps client-id to latest SignedReq
   using ClientSignatures = Store::Map<CallerId, SignedReq>;
 
-  DECLARE_JSON_TYPE_WITH_OPTIONAL_FIELDS(SignedReq)
-  DECLARE_JSON_REQUIRED_FIELDS(SignedReq, sig);
-  DECLARE_JSON_OPTIONAL_FIELDS(SignedReq, req);
+  inline void to_json(nlohmann::json& j, const SignedReq& sr)
+  {
+    if (!sr.sig.empty())
+    {
+      j["sig"] = sr.sig;
+    }
+    if (!sr.req.empty())
+    {
+      j["req"] = nlohmann::json::from_msgpack(sr.req);
+    }
+  }
+
+  inline void from_json(const nlohmann::json& j, SignedReq& sr)
+  {
+    auto sig_it = j.find("req");
+    if (sig_it != j.end())
+    {
+      assign_j(sr.sig, j["sig"]);
+    }
+    auto req_it = j.find("req");
+    if (req_it != j.end())
+    {
+      assign_j(sr.req, nlohmann::json::to_msgpack(req_it.value()));
+    }
+  }
+
+  inline void fill_json_schema(nlohmann::json& j, const SignedReq& sr)
+  {
+    j["type"] = "object";
+
+    j["properties"]["req"] = nlohmann::json();
+
+    auto sig_schema = nlohmann::json::object();
+    sig_schema["type"] = "array";
+    sig_schema["items"] = ::ds::json::schema_element<uint8_t>();
+    j["properties"]["sig"] = sig_schema;
+
+    j["required"].push_back("req");
+  }
 }
