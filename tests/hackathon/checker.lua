fbdf1e59e62c0dd7384d4ab1751fa02673df9761
Olga Vrousgou <olga.vrousgou@microsoft.com>
2019-07-11 07:48:19 +0100

    adding copyright notice

--- tests/hackathon/checker.lua
+++ tests/hackathon/checker.lua
@@ -1,3 +1,6 @@
+-- Copyright (c) Microsoft Corporation. All rights reserved.
+-- Licensed under the Apache 2.0 License.
+
 if (tx_type == 2 or tx_type == 3) and tonumber(amt) > 200000 then 
 	return true 
 else 
