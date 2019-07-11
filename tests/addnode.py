6bb1098c9fad66a6ca3078e2ffa50196adc83a6a
rschust <roy.schuster@hotmail.com>
2019-07-11 15:37:13 +0100

    Renamed the node_id member to local_node_id
    We now distinguish the local node id (the one being used by the python environment) from the actual node id. This allows setting up the nodes using an identifier which is not related to the actual node id, as we cannot determine the id that will be generated on the KV, yet still have the actual node id once it is up and running.

--- tests/addnode.py
+++ tests/addnode.py
@@ -59,9 +59,8 @@ def run(args):
 
         # add a valid node
         res = network.create_and_add_node("libloggingenc", args)
-        assert res[0] == True
+        assert res[0]
         new_node = res[1]
-        new_node_id = res[2]
 
         # attempt to add a node having the host and port fields
         # similar to a the ones of an existing node
@@ -80,7 +79,7 @@ def run(args):
         network.wait_for_node_commit_sync()
 
         # retire a node
-        retire_node(primary, new_node_id)
+        retire_node(primary, new_node.node_id)
 
 
 if __name__ == "__main__":
