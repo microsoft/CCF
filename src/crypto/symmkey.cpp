518dffa7af7a22694a9c135c6be853f6358925b3
Eddy Ashton <v-edasht@microsoft.com>
2019-07-22 20:53:14 +0100

    Better errors, truncate to useable key

--- src/crypto/symmkey.cpp
+++ src/crypto/symmkey.cpp
@@ -2,24 +2,45 @@
 // Licensed under the Apache 2.0 License.
 #include "symmkey.h"
 
+#include "ds/logger.h"
 #include "error.h"
 
 #include <mbedtls/aes.h>
+#include <mbedtls/error.h>
 #include <mbedtls/gcm.h>
 
 namespace crypto
 {
+  inline std::string str_err(int err)
+  {
+    constexpr size_t len = 100;
+    char buf[len];
+    mbedtls_strerror(err, buf, len);
+    return std::string(buf);
+  }
+
   KeyAesGcm::KeyAesGcm(CBuffer rawKey)
   {
     ctx = new mbedtls_gcm_context;
     mbedtls_gcm_init(reinterpret_cast<mbedtls_gcm_context*>(ctx));
 
-    if (mbedtls_gcm_setkey(
-          reinterpret_cast<mbedtls_gcm_context*>(ctx),
-          MBEDTLS_CIPHER_ID_AES,
-          rawKey.p,
-          static_cast<unsigned int>(rawKey.rawSize() * 8)))
-      throw crypto_error("Failed to set AES GCM key");
+    static constexpr auto n_bits = 256;
+    const auto n = static_cast<unsigned int>(rawKey.rawSize() * 8);
+    if (n < n_bits)
+    {
+      LOG_FATAL_FMT("Need at least {} bits, only have {}", n_bits, n);
+    }
+
+    int rc = mbedtls_gcm_setkey(
+      reinterpret_cast<mbedtls_gcm_context*>(ctx),
+      MBEDTLS_CIPHER_ID_AES,
+      rawKey.p,
+      n_bits);
+
+    if (rc != 0)
+    {
+      LOG_FATAL_FMT(str_err(rc));
+    }
   }
 
   KeyAesGcm::KeyAesGcm(KeyAesGcm&& that)
@@ -45,19 +66,23 @@ namespace crypto
     uint8_t* cipher,
     uint8_t tag[GCM_SIZE_TAG]) const
   {
-    if (mbedtls_gcm_crypt_and_tag(
-          reinterpret_cast<mbedtls_gcm_context*>(ctx),
-          MBEDTLS_GCM_ENCRYPT,
-          plain.n,
-          iv.p,
-          iv.n,
-          aad.p,
-          aad.n,
-          plain.p,
-          cipher,
-          GCM_SIZE_TAG,
-          tag))
-      throw crypto_error("AES GCM encryption failed.");
+    int rc = mbedtls_gcm_crypt_and_tag(
+      reinterpret_cast<mbedtls_gcm_context*>(ctx),
+      MBEDTLS_GCM_ENCRYPT,
+      plain.n,
+      iv.p,
+      iv.n,
+      aad.p,
+      aad.n,
+      plain.p,
+      cipher,
+      GCM_SIZE_TAG,
+      tag);
+
+    if (rc != 0)
+    {
+      LOG_FATAL_FMT(str_err(rc));
+    }
   }
 
   bool KeyAesGcm::decrypt(
