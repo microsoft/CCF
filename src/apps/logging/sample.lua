c4eac67162e31634fcdf053c8738541754d99365
Olga Vrousgou <olga.vrousgou@microsoft.com>
2019-07-26 12:54:51 +0100

    updating sample app to work with new changes on master

--- src/apps/logging/sample.lua
+++ src/apps/logging/sample.lua
@@ -3,39 +3,13 @@
 
 return {
   __environment = [[
-    env = {
-      error_codes = {
-        PARSE_ERROR = -32700,
-        INVALID_REQUEST = -32600,
-        METHOD_NOT_FOUND = -32601,
-        INVALID_PARAMS = -32602,
-        INTERNAL_ERROR = -32603,
-
-        TX_NOT_LEADER = -32001,
-        TX_FAILED_TO_REPLICATE = -32002,
-        SCRIPT_ERROR = -32003,
-        INSUFFICIENT_RIGHTS = -32004,
-        TX_LEADER_UNKNOWN = -32005,
-        RPC_NOT_SIGNED = -32006,
-        INVALID_CLIENT_SIGNATURE = -32007,
-        INVALID_CALLER_ID = -32008,
-        CODE_ID_NOT_FOUND = -32009,
-        CODE_ID_RETIRED = -32010,
-        RPC_NOT_FORWARDED = -32011,
-        QUOTE_NOT_VERIFIED = -32012,
-
-        UNKNOWN_ID = -32050,
-        MESSAGE_EMPTY = -32051,
-      },
-
-      transaction_type = {
+      env.transaction_type = {
         PAYMENT = 1,
         TRANSFER = 2,
         CASH_OUT = 3,
         DEBIT = 4,
         CASH_IN = 5
       }
-    }
 
     function env.jsucc(result)
       return {result = result}
