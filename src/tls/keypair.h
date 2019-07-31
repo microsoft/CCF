19ecb7b610b6c5a279c3a9b9d612076850f3c23b
Eddy Ashton <v-edasht@microsoft.com>
2019-07-31 12:00:11 +0100

    Feedback, name and document signature size

--- src/tls/keypair.h
+++ src/tls/keypair.h
@@ -223,7 +223,11 @@ namespace tls
 
   struct RecoverableSignature
   {
-    std::array<uint8_t, 64> raw;
+    // Signature consists of 32 byte R, 32 byte S, and recovery id. Some formats
+    // concatenate all 3 into 65 bytes. We stick with libsecp256k1 and separate
+    // 64 bytes of (R, S) from recovery_id.
+    static constexpr size_t RS_Size = 64;
+    std::array<uint8_t, RS_Size> raw;
     int recovery_id;
   };
 
