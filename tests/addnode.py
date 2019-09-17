295dd70177f5a4f066cb60c1ed42a68044cc50e7
Julien Maffre <42961061+jumaffre@users.noreply.github.com>
2019-09-17 10:05:04 +0100

    Fix issue and add e2e test (#377)

--- tests/addnode.py
+++ tests/addnode.py
@@ -20,10 +20,23 @@ def run(args):
     ) as network:
         primary, others = network.start_and_join(args)
 
+        LOG.debug("Adding a node conflicting with an existing node")
+        local_node_id = network.get_next_local_node_id()
+        conflicting_node = network.create_node(local_node_id, "localhost")
+
+        # Node ip/port of new node conflicts with existing active node
+        conflicting_node.host = primary.host
+        conflicting_node.node_port = primary.node_port
+
+        assert (
+            network.add_node(conflicting_node, args.package, None, args) == False
+        ), "Node with conflicting node ports should not be able to join the network"
+        assert conflicting_node.is_joined() == False
+
         # TODO: For now, node is added straight away, without validation by
         # the consortium. See https://github.com/microsoft/CCF/issues/293
         LOG.debug("Add a valid node")
-        new_node = network.create_and_add_node("libloggingenc", args)
+        new_node = network.create_and_add_node(args.package, "localhost", args)
 
         with primary.management_client() as mc:
             check_commit = infra.ccf.Checker(mc)
@@ -33,10 +46,14 @@ def run(args):
                     c.rpc("LOG_record", {"id": 42, "msg": "Hello world"}), result=True
                 )
 
-        LOG.debug("Add an invalid node (code id not known)")
-        assert (
-            network.create_and_add_node("libluagenericenc", args) == None
-        ), "Adding node with unknown code id should fail"
+        if args.enclave_type == "debug":
+            LOG.debug("Add an invalid node (unknown code id)")
+            assert (
+                network.create_and_add_node("libluagenericenc", "localhost", args)
+                == None
+            ), "Adding node with unknown code id should fail"
+        else:
+            LOG.warning("Skipping unknown code id test with virtual enclave")
 
         LOG.debug("Retire node")
         network.retire_node(primary, 0)
@@ -53,10 +70,5 @@ if __name__ == "__main__":
         )
 
     args = e2e_args.cli_args(add)
-
-    if args.enclave_type != "debug":
-        LOG.error("This test can only run in real enclaves, skipping")
-        sys.exit(0)
-
     args.package = args.app_script and "libluagenericenc" or "libloggingenc"
     run(args)
