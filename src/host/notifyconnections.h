ee29f51cd41e0fd0cee99fdf59251ebc0cf61238
Eddy Ashton <v-edasht@microsoft.com>
2019-09-18 11:16:59 +0100

    Drop old notifications, write HTTP to given address

--- src/host/notifyconnections.h
+++ src/host/notifyconnections.h
@@ -12,55 +12,14 @@ namespace asynchost
   class NotifyConnections
   {
   private:
-    TCP notify_client;
-    bool is_setup = false;
-
-    class ClientBehaviour : public TCPBehaviour
-    {
-    private:
-      NotifyConnections& parent;
-
-    public:
-      ClientBehaviour(NotifyConnections& parent) : parent(parent) {}
-
-      void on_resolve_failed()
-      {
-        LOG_DEBUG_FMT("notify client resolve failed");
-        reconnect();
-      }
-
-      void on_connect_failed()
-      {
-        LOG_DEBUG_FMT("notify client connect failed");
-        reconnect();
-      }
-
-      void on_disconnect()
-      {
-        LOG_DEBUG_FMT("notify client disconnect");
-        reconnect();
-      }
-
-      void reconnect()
-      {
-        parent.notify_client->reconnect();
-      }
-    };
+    std::string listen_address;
 
   public:
     NotifyConnections(const std::string& host, const std::string& service)
     {
       if (!host.empty())
       {
-        LOG_INFO_FMT(
-          "Notifications client connecting to: {}:{}", host, service);
-
-        notify_client->set_behaviour(std::make_unique<ClientBehaviour>(*this));
-        if (!notify_client->connect(host, service))
-        {
-          LOG_FATAL_FMT("Notifications client failed initial connect");
-        }
-        is_setup = true;
+        listen_address = fmt::format("{}:{}", host, service);
       }
     }
 
@@ -74,26 +33,29 @@ namespace asynchost
           auto [msg] =
             ringbuffer::read_message<AdminMessage::notification>(data, size);
 
-          if (is_setup)
-            notify_client->write(msg.size(), msg.data());
-
           CURL* curl = curl_easy_init();
           CURLcode res;
           if (curl)
           {
             std::string s((char const*)data, size);
-            curl_easy_setopt(curl, CURLOPT_URL, "0.0.0.0:4242");
+            curl_easy_setopt(curl, CURLOPT_URL, listen_address.c_str());
             curl_easy_setopt(curl, CURLOPT_POST, 1L);
             curl_easy_setopt(curl, CURLOPT_POSTFIELDS, s.c_str());
 
             res = curl_easy_perform(curl);
             /* Check for errors */
             if (res != CURLE_OK)
+            {
               LOG_FAIL_FMT(
                 "curl_easy_perform() failed: {}", curl_easy_strerror(res));
+            }
 
             /* always cleanup */
             curl_easy_cleanup(curl);
+          }
+          else
+          {
+            LOG_FAIL_FMT("curl_easy_init failed");
           }
         });
     }
