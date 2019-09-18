0172dd04bdd978920baf7f494544dc94a62503eb
Eddy Ashton <v-edasht@microsoft.com>
2019-09-18 15:34:50 +0100

    Properly poll for curl multi responses, +cleanup

--- src/host/notifyconnections.h
+++ src/host/notifyconnections.h
@@ -18,13 +18,15 @@ namespace asynchost
 
     std::string notify_destination = {};
 
+    // To ensure we cleanup all open handles, keep track of them locally
+    std::set<CURL*> easy_handles;
+
     void send_notification(const std::vector<uint8_t>& body)
     {
       if (multi_handle)
       {
         CURL* curl = curl_easy_init();
-
-        curl_multi_add_handle(multi_handle, curl);
+        easy_handles.insert(curl);
 
         curl_easy_setopt(curl, CURLOPT_URL, notify_destination.c_str());
         curl_easy_setopt(curl, CURLOPT_POST, 1L);
@@ -32,6 +34,8 @@ namespace asynchost
         curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, body.data());
 
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        curl_multi_add_handle(multi_handle, curl);
       }
     };
 
@@ -41,6 +45,14 @@ namespace asynchost
       const std::string& host,
       const std::string& service)
     {
+      auto init_res = curl_global_init(CURL_GLOBAL_ALL);
+      if (init_res != 0)
+      {
+        LOG_FATAL_FMT(
+          "libcurl global initialisation failed: {}",
+          curl_easy_strerror(init_res));
+      }
+
       if (!host.empty())
       {
         notify_destination = fmt::format("{}:{}", host, service);
@@ -53,8 +65,14 @@ namespace asynchost
 
     ~NotifyConnectionsImpl()
     {
-      // TODO: Remove and cleanup all remaining handles,
+      // TODO: Remove and cleanup all remaining handles
+      for (auto easy_handle : easy_handles)
+      {
+        curl_multi_remove_handle(multi_handle, easy_handle);
+        curl_easy_cleanup(easy_handle);
+      }
       curl_multi_cleanup(multi_handle);
+      easy_handles.clear();
     }
 
     void every()
@@ -62,7 +80,54 @@ namespace asynchost
       int still_running = 0;
       curl_multi_perform(multi_handle, &still_running);
 
-      // TODO: Get info on remaining handles, remove those which are done
+      CURLMsg* message;
+      int pending;
+      while ((message = curl_multi_info_read(multi_handle, &pending)))
+      {
+        switch (message->msg)
+        {
+          case CURLMSG_DONE:
+          {
+            CURL* easy_handle = message->easy_handle;
+
+            long response_code;
+            auto res = curl_easy_getinfo(
+              easy_handle, CURLINFO_RESPONSE_CODE, &response_code);
+            if (res == CURLE_OK)
+            {
+              LOG_DEBUG_FMT(
+                "Notification completed with response code {}", response_code);
+            }
+            else
+            {
+              LOG_FAIL_FMT(
+                "Unable to retrieve response code for completed notification: "
+                "{}",
+                curl_easy_strerror(res));
+            }
+
+            // Cleanup completed handles - NB: message contents are invalidated!
+            auto mres = curl_multi_remove_handle(multi_handle, easy_handle);
+            if (mres != CURLM_OK)
+            {
+              LOG_FAIL_FMT(
+                "Failed to remove curl handle from multi-handle: {}",
+                curl_multi_strerror(mres));
+            }
+
+            curl_easy_cleanup(easy_handle);
+            easy_handles.erase(easy_handle);
+            break;
+          }
+          default:
+          {
+            LOG_FAIL_FMT(
+              "Unhandled case while processing curl info message: {}",
+              message->msg);
+            break;
+          }
+        }
+      }
     }
 
     void register_message_handlers(
