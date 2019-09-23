6dd5abfbdd6982a90b9dccb722b41875e86e46be
Alex Shamis <alexsha@microsoft.com>
2019-09-23 22:33:33 +0100

    fix issue where pbft globals were added to both the host and the enclave

---
+++ src/consensus/pbft/pbftglobals.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "libbyz/ITimer.h"
+#include "libbyz/Statistics.h"
+#include "node/nodetonode.h"
+
+#include <signal.h>
+
+namespace pbft
+{
+  char* AbstractPbftConfig::service_mem = 0;
+};
+
+std::vector<ITimer*> ITimer::timers;
+Time ITimer::min_deadline = Long_max;
+Time ITimer::_relative_current_time = 0;
+
+long long clock_mhz = 1;
+
+Statistics stats;
