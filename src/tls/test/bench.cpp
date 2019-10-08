873f832f043a58ee7d8ca0cad1d14c9132ebe9e5
Eddy Ashton <v-edasht@microsoft.com>
2019-10-08 10:34:55 +0100

    tls_bench uses mbedtls verification when appropriate

--- src/tls/test/bench.cpp
+++ src/tls/test/bench.cpp
@@ -66,13 +66,14 @@ static void benchmark_verify(picobench::state& s)
 
   auto signature = kp->sign(contents);
   auto public_key = kp->public_key_pem();
-  auto pubk = tls::make_public_key(public_key);
+  auto pubk = tls::make_public_key(
+    public_key, Curve == tls::CurveImpl::secp256k1_bitcoin);
 
   s.start_timer();
   for (auto _ : s)
   {
     (void)_;
-    auto verified = pubk->verify(contents, signature);
+    auto verified = kp->verify(contents, signature);
     do_not_optimize(verified);
     clobber_memory();
   }
