b5da1fb882e2c6f05340862be3eb6f123b0a954f
Eddy Ashton <edashton@microsoft.com>
2019-11-04 11:43:59 +0000

    Reparse where necessary

--- src/consensus/pbft/pbftconfig.h
+++ src/consensus/pbft/pbftconfig.h
@@ -115,9 +115,13 @@ namespace pbft
 
       auto frontend = handler.value();
 
+      // TODO: Should serialise context directly, rather than reconstructing
       const enclave::SessionContext session(
         enclave::InvalidSessionId, request.caller_id, request.caller_cert);
       auto ctx = enclave::make_rpc_context(session, request.request);
+      ctx.actor = (ccf::ActorsType)request.actor;
+      const auto n = ctx.method.find_last_of('/');
+      ctx.method = ctx.method.substr(n + 1, ctx.method.size());
 
       auto rep = frontend->process_pbft(ctx);
 
