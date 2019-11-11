1d6b870d56a99cc3f5a588c3c14fae9949756c31
Julien Maffre <42961061+jumaffre@users.noreply.github.com>
2019-11-11 11:11:04 +0000

    Cleanup ccf.py (#544)

--- tests/infra/ccf.py
+++ tests/infra/ccf.py
@@ -66,6 +66,49 @@ class Network:
         for host in hosts:
             self.create_node(host)
 
+    def _get_next_local_node_id(self):
+        if len(self.nodes):
+            return self.nodes[-1].node_id + 1
+        return self.node_offset
+
+    def create_node(self, host):
+        node_id = self._get_next_local_node_id()
+        debug = (
+            (str(node_id) in self.dbg_nodes) if self.dbg_nodes is not None else False
+        )
+        perf = (
+            (str(node_id) in self.perf_nodes) if self.perf_nodes is not None else False
+        )
+        node = infra.node.Node(node_id, host, debug, perf)
+        self.nodes.append(node)
+        return node
+
+    def _add_node(self, node, lib_name, args, target_node=None):
+        forwarded_args = {
+            arg: getattr(args, arg) for arg in infra.ccf.Network.node_args_to_forward
+        }
+
+        # Contact primary if no target node is set
+        if target_node is None:
+            target_node, _ = self.find_primary()
+
+        node.join(
+            lib_name=lib_name,
+            workspace=args.workspace,
+            label=args.label,
+            target_rpc_address=f"{target_node.host}:{target_node.rpc_port}",
+            **forwarded_args,
+        )
+
+        # If the network is opening, node are trusted without consortium approval
+        if self.status == ServiceStatus.OPENING and not args.pbft:
+            try:
+                node.wait_for_node_to_join()
+            except TimeoutError:
+                LOG.error(f"New node {node.node_id} failed to join the network")
+                raise
+            node.network_state = infra.node.NodeNetworkState.joined
+
     def _start_all_nodes(
         self, args, recovery=False, ledger_file=None, sealed_secrets=None
     ):
@@ -163,147 +206,10 @@ class Network:
         self.wait_for_all_nodes_to_catch_up(primary)
         LOG.success("All nodes joined recovered public network")
 
-    def create_node(self, host):
-        node_id = self.get_next_local_node_id()
-        debug = (
-            (str(node_id) in self.dbg_nodes) if self.dbg_nodes is not None else False
-        )
-        perf = (
-            (str(node_id) in self.perf_nodes) if self.perf_nodes is not None else False
-        )
-        node = infra.node.Node(node_id, host, debug, perf)
-        self.nodes.append(node)
-        return node
-
-    def _add_node(self, node, lib_name, args, target_node=None):
-        forwarded_args = {
-            arg: getattr(args, arg) for arg in infra.ccf.Network.node_args_to_forward
-        }
-
-        # Contact primary if no target node is set
-        if target_node is None:
-            target_node, _ = self.find_primary()
-
-        node.join(
-            lib_name=lib_name,
-            workspace=args.workspace,
-            label=args.label,
-            target_rpc_address=f"{target_node.host}:{target_node.rpc_port}",
-            **forwarded_args,
-        )
-
-        # If the network is opening, node are trusted without consortium approval
-        if self.status == ServiceStatus.OPENING and not args.pbft:
-            try:
-                node.wait_for_node_to_join()
-            except TimeoutError:
-                LOG.error(f"New node {node.node_id} failed to join the network")
-                raise
-            node.network_state = infra.node.NodeNetworkState.joined
-
-    def create_users(self, users):
-        users = ["user{}".format(u) for u in users]
-        for u in users:
-            infra.proc.ccall("./keygenerator", "--name={}".format(u)).check_returncode()
-
-    def wait_for_state(self, node, state, timeout=3):
-        for _ in range(timeout):
-            try:
-                with node.node_client(format="json") as c:
-                    id = c.request("getSignedIndex", {})
-                    r = c.response(id).result
-                    if r["state"] == state:
-                        break
-            except ConnectionRefusedError:
-                pass
-            time.sleep(1)
-        else:
-            raise TimeoutError(
-                f"Timed out waiting for public ledger to be read on node {node.node_id}"
-            )
-        if state == "partOfNetwork":
-            self.status = ServiceStatus.OPEN
-
-    def wait_for_all_nodes_to_be_trusted(self, timeout=3):
-        primary, _ = self.find_primary(timeout)
-        for n in self.nodes:
-            self.consortium.wait_for_node_to_exist_in_store(
-                primary, n.node_id, infra.node.NodeStatus.TRUSTED
-            )
-
-    def accept_recovery(self, node, sealed_secrets):
-        result = self.propose(
-            1, node, None, None, "accept_recovery", f"--sealed-secrets={sealed_secrets}"
-        )
-        self.vote_using_majority(node, result[1]["id"])
-
-    def wait_for_all_nodes_to_be_trusted(self, timeout=3):
-        primary, term = self.find_primary()
-        for n in self.nodes:
-            self.consortium.wait_for_node_to_exist_in_store(
-                primary, n.node_id, infra.node.NodeStatus.TRUSTED
-            )
-
     def stop_all_nodes(self):
         for node in self.nodes:
             node.stop()
         LOG.info("All remotes stopped...")
-
-    def get_members(self):
-        return self.consortium.members
-
-    def get_joined_nodes(self):
-        return [node for node in self.nodes if node.is_joined()]
-
-    def get_node_by_id(self, node_id):
-        return next((node for node in self.nodes if node.node_id == node_id), None)
-
-    def find_primary(self, timeout=3):
-        """
-        Find the identity of the primary in the network and return its identity
-        and the current term.
-        """
-        primary_id = None
-        term = None
-
-        for _ in range(timeout):
-            for node in self.get_joined_nodes():
-                with node.node_client() as c:
-                    id = c.request("getPrimaryInfo", {})
-                    res = c.response(id)
-                    if res.error is None:
-                        primary_id = res.result["primary_id"]
-                        term = res.term
-                        break
-                    else:
-                        assert (
-                            res.error["code"]
-                            == infra.jsonrpc.ErrorCode.TX_PRIMARY_UNKNOWN
-                        ), "RPC error code is not TX_NOT_PRIMARY"
-            if primary_id is not None:
-                break
-            time.sleep(1)
-
-        assert primary_id is not None, "No primary found"
-        return (self.get_node_by_id(primary_id), term)
-
-    def find_backups(self, primary=None, timeout=3):
-        if primary is None:
-            primary, term = self.find_primary(timeout)
-        return [n for n in self.get_joined_nodes() if n != primary]
-
-    def find_any_backup(self, primary=None, timeout=3):
-        return random.choice(self.find_backups(primary=primary, timeout=timeout))
-
-    def find_nodes(self, timeout=3):
-        primary, term = self.find_primary(timeout)
-        backups = self.find_backups(primary=primary, timeout=timeout)
-        return primary, backups
-
-    def find_primary_and_any_backup(self, timeout=3):
-        primary, backups = self.find_nodes(timeout)
-        backup = random.choice(backups)
-        return primary, backup
 
     def create_and_add_pending_node(self, lib_name, host, args, target_node=None):
         """
@@ -359,6 +265,92 @@ class Network:
 
         return new_node
 
+    def create_users(self, users):
+        users = ["user{}".format(u) for u in users]
+        for u in users:
+            infra.proc.ccall("./keygenerator", "--name={}".format(u)).check_returncode()
+
+    def get_members(self):
+        return self.consortium.members
+
+    def get_joined_nodes(self):
+        return [node for node in self.nodes if node.is_joined()]
+
+    def wait_for_state(self, node, state, timeout=3):
+        for _ in range(timeout):
+            try:
+                with node.node_client(format="json") as c:
+                    id = c.request("getSignedIndex", {})
+                    r = c.response(id).result
+                    if r["state"] == state:
+                        break
+            except ConnectionRefusedError:
+                pass
+            time.sleep(1)
+        else:
+            raise TimeoutError(
+                f"Timed out waiting for public ledger to be read on node {node.node_id}"
+            )
+        if state == "partOfNetwork":
+            self.status = ServiceStatus.OPEN
+
+    def wait_for_all_nodes_to_be_trusted(self, timeout=3):
+        primary, term = self.find_primary()
+        for n in self.nodes:
+            self.consortium.wait_for_node_to_exist_in_store(
+                primary, n.node_id, infra.node.NodeStatus.TRUSTED
+            )
+
+    def _get_node_by_id(self, node_id):
+        return next((node for node in self.nodes if node.node_id == node_id), None)
+
+    def find_primary(self, timeout=3):
+        """
+        Find the identity of the primary in the network and return its identity
+        and the current term.
+        """
+        primary_id = None
+        term = None
+
+        for _ in range(timeout):
+            for node in self.get_joined_nodes():
+                with node.node_client() as c:
+                    id = c.request("getPrimaryInfo", {})
+                    res = c.response(id)
+                    if res.error is None:
+                        primary_id = res.result["primary_id"]
+                        term = res.term
+                        break
+                    else:
+                        assert (
+                            res.error["code"]
+                            == infra.jsonrpc.ErrorCode.TX_PRIMARY_UNKNOWN
+                        ), "RPC error code is not TX_NOT_PRIMARY"
+            if primary_id is not None:
+                break
+            time.sleep(1)
+
+        assert primary_id is not None, "No primary found"
+        return (self._get_node_by_id(primary_id), term)
+
+    def find_backups(self, primary=None, timeout=3):
+        if primary is None:
+            primary, term = self.find_primary(timeout)
+        return [n for n in self.get_joined_nodes() if n != primary]
+
+    def find_any_backup(self, primary=None, timeout=3):
+        return random.choice(self.find_backups(primary=primary, timeout=timeout))
+
+    def find_nodes(self, timeout=3):
+        primary, term = self.find_primary(timeout)
+        backups = self.find_backups(primary=primary, timeout=timeout)
+        return primary, backups
+
+    def find_primary_and_any_backup(self, timeout=3):
+        primary, backups = self.find_nodes(timeout)
+        backup = random.choice(backups)
+        return primary, backup
+
     def wait_for_all_nodes_to_catch_up(self, primary, timeout=3):
         """
         Wait for all nodes to have joined the network and globally replicated
@@ -406,11 +398,6 @@ class Network:
                 break
             time.sleep(1)
         assert [commits[0]] * len(commits) == commits, "All nodes at the same commit"
-
-    def get_next_local_node_id(self):
-        if len(self.nodes):
-            return self.nodes[-1].node_id + 1
-        return self.node_offset
 
 
 @contextmanager
