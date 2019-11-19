ce19fb23a25b0f209a990bb20b092fd2799c4110
Eddy Ashton <edashton@microsoft.com>
2019-11-19 11:07:41 +0000

    Parametrisable decorators, standard enforcement pattern

--- tests/e2e_logging.py
+++ tests/e2e_logging.py
@@ -18,7 +18,7 @@ import e2e_args
 from loguru import logger as LOG
 
 
-@reqs.lua_logging_app
+@reqs.installed_package("libluagenericenc")
 def test_update_lua(network, args):
     if args.package == "libluagenericenc":
         LOG.info("Updating Lua application")
@@ -65,8 +65,10 @@ def test_update_lua(network, args):
 
 
 @reqs.supports_methods("mkSign", "LOG_record", "LOG_get")
-@reqs.at_least_2_nodes
+@reqs.at_least_n_nodes(2)
 def test(network, args, notifications_queue=None):
+    """This is the docstring for the test function"""
+
     LOG.info("Running transactions against logging app")
     primary, backup = network.find_primary_and_any_backup()
 
@@ -130,8 +132,8 @@ def run(args):
             hosts, args.build_dir, args.debug_nodes, args.perf_nodes, pdb=args.pdb
         ) as network:
             network.start_and_join(args)
-            test(network, args, notifications_queue)
-            test_update_lua(network, args)
+            network = test(network, args, notifications_queue)
+            network = test_update_lua(network, args)
 
 
 if __name__ == "__main__":
