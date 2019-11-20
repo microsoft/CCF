c3a43f70315be14dbed920c0709d9845da0b7e1c
Amaury Chamayou <amchamay@microsoft.com>
2019-11-20 10:43:16 +0000

    License

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
 find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h)
 
 find_library(MBEDTLS_LIBRARY mbedtls)
