b816666f0f2a19cc3162213b7c3f23f065d898cd
Amaury Chamayou <amchamay@microsoft.com>
2019-12-10 19:08:08 +0000

    Update documentation (#627)

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -52,6 +52,8 @@ extensions = [
     "sphinx-jsonschema",
 ]
 
+autosectionlabel_prefix_document = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
