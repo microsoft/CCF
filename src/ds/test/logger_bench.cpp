25c2c20a7f96c982257437d8f6bfb387ca5dbef8
Eddy Ashton <edashton@microsoft.com>
2020-01-07 17:20:40 +0000

    Honest logger benchmark

--- src/ds/test/logger_bench.cpp
+++ src/ds/test/logger_bench.cpp
@@ -15,7 +15,7 @@ enum LoggerKind
   All = 0xffff,
 };
 
-template <LoggerKind LK>
+template <LoggerKind LK, bool Absorb = true>
 static void prepare_loggers()
 {
   logger::config::loggers().clear();
@@ -31,75 +31,100 @@ static void prepare_loggers()
     logger::config::loggers().emplace_back(
       std::make_unique<logger::JsonLogger>("./custom_json_logger"));
   }
+
+  if constexpr (Absorb)
+  {
+    // Swallow all output for duration of benchmarks
+    for (auto& logger : logger::config::loggers())
+    {
+      logger->get_stream().setstate(std::ios_base::badbit);
+    }
+  }
 }
 
-template <LoggerKind LK>
+static void reset_loggers()
+{
+  logger::config::loggers().clear();
+
+  logger::config::loggers().emplace_back(
+    std::make_unique<logger::ConsoleLogger>());
+
+  std::cout.clear();
+}
+
+template <LoggerKind LK, bool Absorb = true>
 static void log_accepted(picobench::state& s)
 {
-  prepare_loggers<LK>();
-
-  // Swallow the output instead of printing to stdout.
-  std::cout.setstate(std::ios_base::badbit);
+  prepare_loggers<LK, Absorb>();
 
   logger::config::level() = logger::DBG;
-  picobench::scope scope(s);
-
-  for (size_t i = 0; i < s.iterations(); ++i)
   {
-    LOG_DEBUG << "test" << std::endl;
+    picobench::scope scope(s);
+
+    for (size_t i = 0; i < s.iterations(); ++i)
+    {
+      LOG_DEBUG << "test" << std::endl;
+    }
   }
 
-  std::cout.clear();
+  reset_loggers();
 }
 
-template <LoggerKind LK>
+template <LoggerKind LK, bool Absorb = true>
 static void log_accepted_fmt(picobench::state& s)
 {
-  prepare_loggers<LK>();
-
-  // Swallow the output instead of printing to stdout.
-  std::cout.setstate(std::ios_base::badbit);
+  prepare_loggers<LK, Absorb>();
 
   logger::config::level() = logger::DBG;
-  picobench::scope scope(s);
-
-  for (size_t i = 0; i < s.iterations(); ++i)
   {
-    LOG_DEBUG_FMT("test");
+    picobench::scope scope(s);
+
+    for (size_t i = 0; i < s.iterations(); ++i)
+    {
+      LOG_DEBUG_FMT("test");
+    }
   }
 
-  std::cout.clear();
+  reset_loggers();
 }
 
-template <LoggerKind LK>
+template <LoggerKind LK, bool Absorb = true>
 static void log_rejected(picobench::state& s)
 {
-  prepare_loggers<LK>();
+  prepare_loggers<LK, Absorb>();
 
   logger::config::level() = logger::FAIL;
-  picobench::scope scope(s);
-
-  for (size_t i = 0; i < s.iterations(); ++i)
   {
-    LOG_DEBUG << "test" << std::endl;
+    picobench::scope scope(s);
+
+    for (size_t i = 0; i < s.iterations(); ++i)
+    {
+      LOG_DEBUG << "test" << std::endl;
+    }
   }
+
+  reset_loggers();
 }
 
-template <LoggerKind LK>
+template <LoggerKind LK, bool Absorb = true>
 static void log_rejected_fmt(picobench::state& s)
 {
-  prepare_loggers<LK>();
+  prepare_loggers<LK, Absorb>();
 
   logger::config::level() = logger::FAIL;
-  picobench::scope scope(s);
-
-  for (size_t i = 0; i < s.iterations(); ++i)
   {
-    LOG_DEBUG_FMT("test");
+    picobench::scope scope(s);
+
+    for (size_t i = 0; i < s.iterations(); ++i)
+    {
+      LOG_DEBUG_FMT("test");
+    }
   }
+
+  reset_loggers();
 }
 
-const std::vector<int> sizes = {100000};
+const std::vector<int> sizes = {1000};
 
 PICOBENCH_SUITE("logger");
 auto console_accept = log_accepted<LoggerKind::Console>;
@@ -119,3 +144,6 @@ auto json_reject = log_rejected<LoggerKind::JSON>;
 PICOBENCH(json_reject).iterations(sizes).samples(10);
 auto json_reject_fmt = log_rejected_fmt<LoggerKind::JSON>;
 PICOBENCH(json_reject_fmt).iterations(sizes).samples(10);
+
+auto log_all = log_accepted<LoggerKind::All, false>;
+PICOBENCH(log_all).iterations(sizes).samples(10);
