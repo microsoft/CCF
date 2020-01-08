615bff6793b608469a9d588f9963f84c4e65b608
g-ongetta <georgeongetta@gmail.com>
2020-01-08 13:18:22 +0000

    NewOrder transaction code written

--- samples/apps/tpcc/app/tpcc.cpp
+++ samples/apps/tpcc/app/tpcc.cpp
@@ -11,6 +11,10 @@ namespace ccfapp
 namespace tpcc
 {
 
+  struct Procs {
+    static constexpr auto TPCC_NEW_ORDER = "TPCC_new_order";
+  };
+
   class Tpcc : public ccf::UserRpcFrontend
   {
   private:
@@ -37,7 +41,225 @@ namespace tpcc
       items(tables.create<ItemId, Item>("items")),
       stocks(tables.create<StockId, Stock>("stocks"))
     {
+      auto newOrder = [this](Store::Tx& tx, const nlohmann::json& params) {
+        uint64_t w_id = params["w_id"];
+        uint64_t d_id = params["d_id"];
+        uint64_t c_id = params["c_id"];
+        std::string o_entry_d = params["o_entry_d"];
+        std::vector<uint64_t> i_ids = params["i_ids"];
+        std::vector<uint64_t> i_w_ids = params["i_w_ids"];
+        std::vector<uint64_t> i_qtys = params["i_qtys"];
+
+        // Output data defined as per TPCC 2.4.3.3
+        OutputData output_data;
+        output_data.w_id = w_id;
+        output_data.d_id = d_id;
+        output_data.c_id = c_id;
+        output_data.o_entry_d = o_entry_d;
+
+        // Get district information
+        auto districts_view = tx.get_view(districts);
+
+        DistrictId district_key = {d_id, w_id};
+        auto d_result = districts_view->get(district_key);
+
+        if (!d_result.has_value())
+        {
+          //TODO: Error
+        }
+
+        District d = d_result.value();
+        double d_tax = d.tax;
+        uint64_t d_next_o_id = d.next_o_id;
+
+        output_data.d_tax = d_tax;
+        output_data.o_id = d_next_o_id;
+
+        // Update the district's next order number
+        d.next_o_id += 1;
+        districts_view->put(district_key, d);
       
+        // Get warehouse information
+        auto warehouses_view = tx.get_view(warehouses);
+
+        WarehouseId warehouse_key = w_id;
+        auto w_result = warehouses_view->get(warehouse_key);
+        
+        if (!w_result.has_value())
+        {
+          //TODO: Error
+        }
+
+        Warehouse w = w_result.value();
+        double w_tax = w.tax;
+
+        output_data.w_tax = w_tax;
+
+        // Get customer information
+        auto customers_view = tx.get_view(customers);
+
+        CustomerId customer_key = {c_id, w_id, d_id};
+        auto c_result = customers_view->get(customer_key);
+
+        if (!c_result.has_value())
+        {
+          //TODO: Error
+        }
+
+        Customer c = c_result.value();
+        double c_discount = c.discount;
+        std::string c_last = c.last;
+        std::string c_credit = c.credit;
+
+        output_data.c_last = c_last;
+        output_data.c_credit = c_credit;
+        output_data.c_discount = c_discount;
+
+        // Insert NewOrder entry
+        auto neworders_view = tx.get_view(neworders);
+
+        NewOrderId neworder_key = {d_next_o_id, w_id, d_id};
+        NewOrder no = {0};
+        neworders_view->put(neworder_key, no);
+
+        // Insert Order entry
+        auto orders_view = tx.get_view(orders);
+
+        uint8_t all_local = 0; //TODO: set this appropriately
+        uint64_t ol_cnt = i_ids.size();
+
+        output_data.o_ol_cnt = ol_cnt;
+
+        OrderId order_key = {d_next_o_id, w_id, d_id};
+        Order order = {
+          c_id,
+          o_entry_d,
+          0, // carrier_id unused for benchmark purposes
+          ol_cnt,
+          all_local
+        };
+
+        // TODO: order is inserted at the end, check this
+
+        // Insert Order Line and Stock Information
+        auto items_view = tx.get_view(items);
+        auto stocks_view = tx.get_view(stocks);
+        auto orderlines_view = tx.get_view(orderlines);
+
+        uint64_t total = 0;
+
+        std::vector<ItemOutputData> item_output_data;
+        item_output_data.reserve(ol_cnt);
+
+        for (size_t i = 1; i <= ol_cnt; i++)
+        {
+          uint64_t i_id = i_ids.at(i);
+          uint64_t i_w_id = i_w_ids.at(i);
+          uint64_t ol_quantity = i_qtys.at(i);
+
+          // Stores required output data for item as per TPCC 2.4.3.3
+          ItemOutputData item_data;
+          item_data.ol_supply_w_id = i_w_id;
+          item_data.ol_i_id = i_id;
+          item_data.ol_quantity = ol_quantity;
+
+          // Find the ITEM
+          auto i_result = items_view->get(i_id);
+
+          if (!i_result.has_value())
+          {
+            // 'not-found' signal, item was not found in store
+            // TODO: rollback transaction
+          }
+
+          Item item = i_result.value();
+          double i_price = item.price;
+          std::string i_name = item.name;
+          std::string i_data = item.data;
+
+          item_data.i_name = i_name;
+          item_data.i_price = i_price;
+
+          // Find the STOCK
+          StockId stock_key = {i_w_id, i_id};
+          auto s_result = stocks_view->get(stock_key);
+
+          if (!s_result.has_value())
+          {
+            //TODO: Error
+          }
+
+          Stock stock = s_result.value();
+
+          // Update stock information
+          if (stock.quantity >= ol_quantity + 10)
+          {
+            stock.quantity -= ol_quantity;
+          }
+          else
+          {
+            stock.quantity = stock.quantity - ol_quantity + 91;
+          }
+
+          stock.ytd += ol_quantity;
+          stock.order_cnt += 1;
+
+          if (i_w_id != w_id)
+          {
+            stock.remote_cnt += 1;
+          }
+
+          stocks_view->put(stock_key, stock);
+          item_data.s_quantity = stock.quantity;
+
+          // Check the data for the 'brand-generic' field
+          char brand_generic;
+          if (i_data.find("ORIGINAL") != std::string::npos 
+              && stock.data.find("ORIGINAL") != std::string::npos)
+          {
+            brand_generic = 'B';
+          }
+          else
+          {
+            brand_generic = 'G';
+          }
+
+          item_data.brand_generic = brand_generic;
+
+          // Insert the OrderLine entry
+          uint64_t ol_amount = ol_quantity * i_price;
+
+          item_data.ol_amount = ol_amount;
+          total += ol_amount;
+
+          OrderLineId orderline_key = {d_next_o_id, w_id, d_id, i};
+          OrderLine orderline = {
+            i_id,
+            i_w_id,
+            "",
+            (uint8_t) stock.quantity,
+            (double) ol_amount,
+            stock.dist_xx[d_id]
+          };
+
+          orderlines_view->put(orderline_key, orderline);
+          item_output_data.push_back(item_data);
+        }
+
+        total *= (1 - c_discount) * (1 + w_tax + d_tax);
+
+        orders_view->put(order_key, order);
+
+        output_data.item_data = item_output_data;
+        output_data.total_amount = total;
+        output_data.status_msg = "Success";
+
+        // TODO: should OutputData be printed as per TPCC spec?
+
+        return jsonrpc::success(true);
+      };
+
+      install(Procs::TPCC_NEW_ORDER, newOrder, Write);
     }
   };
 
--- samples/apps/tpcc/app/tpcc_entities.h
+++ samples/apps/tpcc/app/tpcc_entities.h
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
 #include <string>
+#include <functional>
 #include <msgpack.hpp>
+#include "crypto/hash.h"
 
 namespace ccfapp
 {
@@ -25,7 +27,18 @@ namespace tpcc
         MSGPACK_DEFINE(name, street_1, street_2, city, state, zip, tax, ytd);
     };
 
-    using DistrictId = std::tuple<uint64_t, uint64_t>;
+    struct DistrictId
+    {
+        uint64_t id;
+        uint64_t w_id;
+
+        bool operator==(const DistrictId& other) const
+        {
+            return id == other.id && w_id == other.w_id;
+        }
+
+        MSGPACK_DEFINE(id, w_id);
+    };
 
     struct District
     {
@@ -38,13 +51,25 @@ namespace tpcc
         std::string zip;
         double tax;
         double ytd;
-        std::string next_o_id;
+        uint64_t next_o_id;
 
         MSGPACK_DEFINE(name, street_1, street_2, city, state, zip, tax, ytd,
             next_o_id);
     };
 
-    using CustomerId = std::tuple<uint64_t, uint64_t, uint64_t>;
+    struct CustomerId
+    {
+        uint64_t id;
+        uint64_t w_id;
+        uint64_t d_id;
+
+        bool operator==(const CustomerId& other) const
+        {
+            return id == other.id && w_id == other.w_id && d_id == other.d_id;
+        }
+
+        MSGPACK_DEFINE(id, w_id, d_id);
+    };
 
     struct Customer
     {
@@ -78,54 +103,95 @@ namespace tpcc
     struct History
     {
         // Primary key: none
-        std::string c_id;
-        std::string c_d_id;
-        std::string c_w_id;
-        std::string d_id;
-        std::string w_id;
-        std::string date;
+        uint64_t c_id;
+        uint64_t c_d_id;
+        uint64_t c_w_id;
+        uint64_t d_id;
+        uint64_t w_id;
+        uint64_t date;
         double amount;
         std::string data;
 
         MSGPACK_DEFINE(c_id, c_d_id, c_w_id, d_id, w_id, date, amount, data);
     };
 
-    using NewOrderId = std::tuple<uint64_t, uint64_t, uint64_t>;
+    struct NewOrderId 
+    {
+        uint64_t o_id;
+        uint64_t w_id;
+        uint64_t d_id;
+
+        bool operator==(const NewOrderId& other) const
+        {
+            return o_id == other.o_id && w_id == other.w_id && d_id == other.d_id;
+        }
+
+        MSGPACK_DEFINE(o_id, w_id, d_id);
+    };
 
     struct NewOrder
     {
         // Primary key: (w_id, d_id, o_id)
+        uint8_t flag;
 
+        MSGPACK_DEFINE(flag);
     };
 
-    using OrderId = std::tuple<uint64_t, uint64_t, uint64_t>;
+    struct OrderId 
+    {
+        uint64_t id;
+        uint64_t w_id;
+        uint64_t d_id;
+
+        bool operator==(const OrderId& other) const
+        {
+            return id == other.id && w_id == other.w_id && d_id == other.d_id;
+        }
+
+        MSGPACK_DEFINE(id, w_id, d_id);
+    };
 
     struct Order
     {
         // Primary key: (w_id, d_id, id)
-        std::string c_id;
+        uint64_t c_id;
         std::string entry_d;
-        std::string carrier_id;
-        uint8_t ol_cnt;
-        uint8_t all_local;  
+        uint8_t carrier_id;
+        uint64_t ol_cnt;
+        uint8_t all_local;
 
         MSGPACK_DEFINE(c_id, entry_d, carrier_id, ol_cnt, all_local);
     };
 
-    using OrderLineId = std::tuple<uint64_t, uint64_t, uint64_t, uint64_t>;
+    struct OrderLineId 
+    {
+        uint64_t o_id;
+        uint64_t w_id;
+        uint64_t d_id;
+        uint64_t number;
+
+        bool operator==(const OrderLineId& other) const
+        {
+            return o_id == other.o_id
+                && w_id == other.w_id
+                && d_id == other.d_id
+                && number == other.number;
+        }
+
+        MSGPACK_DEFINE(o_id, w_id, d_id, number);
+    };
 
     struct OrderLine
     {
-        // Primary key: (w_id, d_id, o_id, id)
-        std::string i_id;
-        std::string supply_w_id;
-        std::string delivery_id;
+        // Primary key: (w_id, d_id, o_id, number)
+        uint64_t i_id;
+        uint64_t supply_w_id;
         std::string delivery_d;
         uint8_t quantity;
         double amount;
         std::string dist_info;
 
-        MSGPACK_DEFINE(i_id, supply_w_id, delivery_id, delivery_d, quantity,
+        MSGPACK_DEFINE(i_id, supply_w_id, delivery_d, quantity,
             amount, dist_info);
     };
 
@@ -142,31 +208,130 @@ namespace tpcc
         MSGPACK_DEFINE(im_id, name, price, data);
     };
 
-    using StockId = std::tuple<uint64_t, uint64_t>;
+    struct StockId 
+    {
+        uint64_t w_id;
+        uint64_t i_id;
+
+        bool operator==(const StockId& other) const
+        {
+            return i_id == other.i_id && w_id == other.w_id;
+        }
+
+        MSGPACK_DEFINE(i_id, w_id);
+    };
 
     struct Stock
     {
         // Primary key: (w_id, i_id)
         int16_t quantity;
-        std::string dist_01;
-        std::string dist_02;
-        std::string dist_03;
-        std::string dist_04;
-        std::string dist_05;
-        std::string dist_06;
-        std::string dist_07;
-        std::string dist_08;
-        std::string dist_09;
-        std::string dist_10;
+        std::string dist_xx[10];
+        // std::string dist_01;
+        // std::string dist_02;
+        // std::string dist_03;
+        // std::string dist_04;
+        // std::string dist_05;
+        // std::string dist_06;
+        // std::string dist_07;
+        // std::string dist_08;
+        // std::string dist_09;
+        // std::string dist_10;
         uint32_t ytd;
         uint16_t order_cnt;
         uint16_t remote_cnt;
         std::string data;
 
-        MSGPACK_DEFINE(quantity, dist_01, dist_02, dist_03, dist_04, dist_05,
-            dist_06, dist_07, dist_08, dist_09, dist_10, ytd, order_cnt,
-            remote_cnt, data);
+        MSGPACK_DEFINE(quantity, dist_xx, ytd, order_cnt, remote_cnt, data);
+        // MSGPACK_DEFINE(quantity, dist_01, dist_02, dist_03, dist_04, dist_05,
+        //     dist_06, dist_07, dist_08, dist_09, dist_10, ytd, order_cnt,
+        //     remote_cnt, data);
+    };
+
+    struct ItemOutputData
+    {
+        uint64_t ol_supply_w_id;
+        uint64_t ol_i_id;
+        uint64_t ol_quantity;
+        uint64_t ol_amount;
+
+        std::string i_name;
+        double i_price;
+
+        uint64_t s_quantity;
+
+        char brand_generic;
+    };
+
+    struct OutputData
+    {
+        std::vector<ItemOutputData> item_data;
+
+        uint64_t w_id;
+        double w_tax;
+
+        uint64_t d_id;
+        double d_tax;
+
+        uint64_t o_id;
+        uint64_t o_ol_cnt;
+        std::string o_entry_d;
+
+        uint64_t c_id;
+        std::string c_last;
+        std::string c_credit;
+        double c_discount;
+
+        uint64_t total_amount;
+        std::string status_msg;
     };
 
 } // namespace tpcc
-} // namespace ccf
+} // namespace ccfapp
+
+namespace std
+{
+    template <> struct hash<ccfapp::tpcc::DistrictId> {
+        std::size_t operator()(const ccfapp::tpcc::DistrictId& k) const {
+            return hash<uint64_t>()(k.id) ^ (hash<uint64_t>()(k.w_id) << 1);
+        }
+    };
+
+    template <> struct hash<ccfapp::tpcc::CustomerId> {
+        std::size_t operator()(const ccfapp::tpcc::CustomerId& k) const {
+            return ((hash<uint64_t>()(k.id)
+                 ^ (hash<uint64_t>()(k.w_id) << 1)) >> 1)
+                 ^ (hash<uint64_t>()(k.d_id) << 1);
+        }
+    };
+
+    template <> struct hash<ccfapp::tpcc::NewOrderId> {
+        std::size_t operator()(const ccfapp::tpcc::NewOrderId& k) const {
+            return ((hash<uint64_t>()(k.o_id)
+                 ^ (hash<uint64_t>()(k.w_id) << 1)) >> 1)
+                 ^ (hash<uint64_t>()(k.d_id) << 1);
+        }
+    };
+
+    template <> struct hash<ccfapp::tpcc::OrderId> {
+        std::size_t operator()(const ccfapp::tpcc::OrderId& k) const {
+            return ((hash<uint64_t>()(k.id)
+                 ^ (hash<uint64_t>()(k.w_id) << 1)) >> 1)
+                 ^ (hash<uint64_t>()(k.d_id) << 1);
+        }
+    };
+
+    template <> struct hash<ccfapp::tpcc::OrderLineId> {
+        std::size_t operator()(const ccfapp::tpcc::OrderLineId& k) const {
+            return (((hash<uint64_t>()(k.number)
+                 ^ (hash<uint64_t>()(k.o_id) << 1)) >> 1)
+                 ^ (hash<uint64_t>()(k.w_id) << 1))
+                 ^ (hash<uint64_t>()(k.d_id) << 1);
+        }
+    };
+
+    template <> struct hash<ccfapp::tpcc::StockId> {
+        std::size_t operator()(const ccfapp::tpcc::StockId& k) const {
+            return hash<uint64_t>()(k.w_id) ^ (hash<uint64_t>()(k.i_id) << 1);
+        }
+    };
+}
