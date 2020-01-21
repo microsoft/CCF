c737c78b4ad3d9b4b8d6cf240273fe3ef7f9fd3e
Amaury Chamayou <amchamay@microsoft.com>
2020-01-21 15:23:24 +0000

    Format things

--- src/apps/jsgeneric/jsgeneric.cpp
+++ src/apps/jsgeneric/jsgeneric.cpp
@@ -15,62 +15,65 @@ namespace ccfapp
 
   using LogTable = ccf::Store::Map<int32_t, std::string>;
 
-  static JSValue js_print(JSContext *ctx, JSValueConst this_val,
-                                int argc, JSValueConst *argv)
+  static JSValue js_print(
+    JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv)
   {
-      int i;
-      const char *str;
-      auto level = logger::INFO;
+    int i;
+    const char* str;
+    auto level = logger::INFO;
 
-      if (logger::config::ok(level))
+    if (logger::config::ok(level))
+    {
+      auto os = logger::LogLine(level, __FILE__, __LINE__);
+      for (i = 0; i < argc; i++)
       {
-        auto os = logger::LogLine(level, __FILE__, __LINE__);
-        for(i = 0; i < argc; i++) {
-          if (i != 0)
-            os << ' ';    
-            str = JS_ToCString(ctx, argv[i]);
-            if (!str)
-                return JS_EXCEPTION;
-            os << str;
-            JS_FreeCString(ctx, str);
-        }
-        os << std::endl;
-        auto _ = logger::Out() == os;
+        if (i != 0)
+          os << ' ';
+        str = JS_ToCString(ctx, argv[i]);
+        if (!str)
+          return JS_EXCEPTION;
+        os << str;
+        JS_FreeCString(ctx, str);
       }
-      return JS_UNDEFINED;
+      os << std::endl;
+      auto _ = logger::Out() == os;
+    }
+    return JS_UNDEFINED;
   }
 
-  void js_dump_error(JSContext *ctx)
+  void js_dump_error(JSContext* ctx)
   {
-      JSValue exception_val = JS_GetException(ctx);
+    JSValue exception_val = JS_GetException(ctx);
 
     JSValue val;
-    const char *stack;
+    const char* stack;
     bool is_error;
-    
+
     is_error = JS_IsError(ctx, exception_val);
     if (!is_error)
       LOG_INFO_FMT("Throw: ");
-    js_print(ctx, JS_NULL, 1, (JSValueConst *)&exception_val);
-    if (is_error) {
-        val = JS_GetPropertyStr(ctx, exception_val, "stack");
-        if (!JS_IsUndefined(val)) {
-            stack = JS_ToCString(ctx, val);
-            LOG_INFO_FMT("{}", stack);
+    js_print(ctx, JS_NULL, 1, (JSValueConst*)&exception_val);
+    if (is_error)
+    {
+      val = JS_GetPropertyStr(ctx, exception_val, "stack");
+      if (!JS_IsUndefined(val))
+      {
+        stack = JS_ToCString(ctx, val);
+        LOG_INFO_FMT("{}", stack);
 
-            JS_FreeCString(ctx, stack);
-        }
-        JS_FreeValue(ctx, val);
+        JS_FreeCString(ctx, stack);
+      }
+      JS_FreeValue(ctx, val);
     }
 
-      JS_FreeValue(ctx, exception_val);
+    JS_FreeValue(ctx, exception_val);
   }
 
-  static JSValue js_get(JSContext *ctx, JSValueConst this_val,
-                        int argc, JSValueConst *argv)
+  static JSValue js_get(
+    JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv)
   {
-    auto log_table_view = (LogTable::TxView *) JS_GetContextOpaque(ctx);
-    if(!JS_IsInteger(argv[0]))
+    auto log_table_view = (LogTable::TxView*)JS_GetContextOpaque(ctx);
+    if (!JS_IsInteger(argv[0]))
       return JS_EXCEPTION;
     int32_t i = JS_VALUE_GET_INT(argv[0]);
     auto str = log_table_view->get(i);
@@ -80,14 +83,14 @@ namespace ccfapp
       return JS_EXCEPTION;
   }
 
-  static JSValue js_put(JSContext *ctx, JSValueConst this_val,
-                        int argc, JSValueConst *argv)
+  static JSValue js_put(
+    JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv)
   {
-    auto log_table_view = (LogTable::TxView *) JS_GetContextOpaque(ctx);
-    if(!JS_IsInteger(argv[0]))
+    auto log_table_view = (LogTable::TxView*)JS_GetContextOpaque(ctx);
+    if (!JS_IsInteger(argv[0]))
       return JS_EXCEPTION;
     int32_t i = JS_VALUE_GET_INT(argv[0]);
-    auto v = JS_ToCString(ctx, argv[1]); //TODO: error checking?
+    auto v = JS_ToCString(ctx, argv[1]); // TODO: error checking?
     std::string s(v);
     JS_FreeCString(ctx, v);
     if (!log_table_view->put(i, s))
@@ -144,25 +147,32 @@ namespace ccfapp
           JS_FreeRuntime(rt);
           throw std::runtime_error("Failed to initialise QuickJS context");
         }
-       
+
         // TODO: load modules from module table here?
         auto ltv = args.tx.get_view(log_table);
-        JS_SetContextOpaque(ctx, (void *) ltv);
+        JS_SetContextOpaque(ctx, (void*)ltv);
 
         auto global_obj = JS_GetGlobalObject(ctx);
 
         auto console = JS_NewObject(ctx);
-        JS_SetPropertyStr(ctx, console, "log", JS_NewCFunction(ctx, ccfapp::js_print, "log", 1));
+        JS_SetPropertyStr(
+          ctx,
+          console,
+          "log",
+          JS_NewCFunction(ctx, ccfapp::js_print, "log", 1));
         JS_SetPropertyStr(ctx, global_obj, "console", console);
 
         auto log = JS_NewObject(ctx);
-        JS_SetPropertyStr(ctx, log, "get", JS_NewCFunction(ctx, ccfapp::js_get, "get", 1));
-        JS_SetPropertyStr(ctx, log, "put", JS_NewCFunction(ctx, ccfapp::js_put, "put", 2));
+        JS_SetPropertyStr(
+          ctx, log, "get", JS_NewCFunction(ctx, ccfapp::js_get, "get", 1));
+        JS_SetPropertyStr(
+          ctx, log, "put", JS_NewCFunction(ctx, ccfapp::js_put, "put", 2));
         auto tables_ = JS_NewObject(ctx);
-        JS_SetPropertyStr(ctx, tables_, "log", log); 
+        JS_SetPropertyStr(ctx, tables_, "log", log);
         JS_SetPropertyStr(ctx, global_obj, "tables", tables_);
 
-        auto args_str = JS_NewStringLen(ctx, (const char *) args.rpc_ctx->raw.data(), args.rpc_ctx->raw.size());
+        auto args_str = JS_NewStringLen(
+          ctx, (const char*)args.rpc_ctx->raw.data(), args.rpc_ctx->raw.size());
         JS_SetPropertyStr(ctx, global_obj, "args", args_str);
         JS_FreeValue(ctx, global_obj);
 
@@ -174,11 +184,13 @@ namespace ccfapp
         // TODO: support pre-compiled byte-code
         std::string code = handler_script.value().text.value();
         auto path = fmt::format("app_scripts::{}", args.method);
-        JSValue val = JS_Eval(ctx, code.c_str(), code.size(), path.c_str(), JS_EVAL_TYPE_GLOBAL);
+        JSValue val = JS_Eval(
+          ctx, code.c_str(), code.size(), path.c_str(), JS_EVAL_TYPE_GLOBAL);
 
         auto status = true;
 
-        if (JS_IsException(val)) {
+        if (JS_IsException(val))
+        {
           js_dump_error(ctx);
           int err_code = jsonrpc::CCFErrorCodes::SCRIPT_ERROR;
           std::string msg = "";
@@ -203,7 +215,6 @@ namespace ccfapp
         return;
       };
 
-
       // TODO: https://github.com/microsoft/CCF/issues/409
       set_default(default_handler, Write);
     }
@@ -216,7 +227,7 @@ namespace ccfapp
 
       auto scripts = tx.get_view(this->network.app_scripts);
       scripts->foreach([&out](const auto& key, const auto&) {
-          out.methods.push_back(key);
+        out.methods.push_back(key);
         return true;
       });
     }
