951390865126c96a4fd2fd70fc3b452361368d8e
Alex <shamis@gmail.com>
2020-01-28 13:00:54 +0000

    reuse vector

--- src/consensus/pbft/pbft.h
+++ src/consensus/pbft/pbft.h
@@ -45,6 +45,8 @@ namespace pbft
     {
       message_receiver_base = receiver;
     }
+    
+    std::vector<uint8_t> serialized_msg;
 
     int Send(Message* msg, IPrincipal& principal) override
     {
@@ -61,7 +63,10 @@ namespace pbft
       PbftHeader hdr = {PbftMsgType::pbft_message, id};
 
       // TODO: Encrypt msg here
-      std::vector<uint8_t> serialized_msg(sizeof(PbftHeader) + msg->size());
+      auto msg_size = (sizeof(PbftHeader) + msg->size());
+      if (serialized_msg.size() < msg_size) {
+        serialized_msg.resize(msg_size);
+      }
       auto data_ = serialized_msg.data();
       auto space = serialized_msg.size();
       serialized::write<PbftHeader>(data_, space, hdr);
