9f65d22fb6dbf70e3f90c268b129e64233dd5fbf
Amaury Chamayou <amchamay@microsoft.com>
2020-01-28 18:09:42 +0000

    Add cmake-format check (#736)

--- cmake/secp256k1.cmake
+++ cmake/secp256k1.cmake
@@ -1,40 +1,25 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-if ("sgx" IN_LIST TARGET)
-    add_library(secp256k1.enclave STATIC
-        ${CCF_DIR}/3rdparty/secp256k1/src/secp256k1.c
-    )
-    target_include_directories(secp256k1.enclave PUBLIC
-        ${CCF_DIR}/3rdparty/secp256k1
-    )
-    target_compile_options(secp256k1.enclave PRIVATE
-        -fvisibility=hidden -nostdinc
-    )
-    target_compile_definitions(secp256k1.enclave PRIVATE
-        HAVE_CONFIG_H
-        SECP256K1_BUILD
-    )
-    target_link_libraries(secp256k1.enclave PRIVATE
-        openenclave::oelibc
-    )
-    set_property(TARGET secp256k1.enclave
-        PROPERTY POSITION_INDEPENDENT_CODE ON
-    )
+if("sgx" IN_LIST TARGET)
+  add_library(
+    secp256k1.enclave STATIC ${CCF_DIR}/3rdparty/secp256k1/src/secp256k1.c
+  )
+  target_include_directories(
+    secp256k1.enclave PUBLIC ${CCF_DIR}/3rdparty/secp256k1
+  )
+  target_compile_options(
+    secp256k1.enclave PRIVATE -fvisibility=hidden -nostdinc
+  )
+  target_compile_definitions(
+    secp256k1.enclave PRIVATE HAVE_CONFIG_H SECP256K1_BUILD
+  )
+  target_link_libraries(secp256k1.enclave PRIVATE openenclave::oelibc)
+  set_property(TARGET secp256k1.enclave PROPERTY POSITION_INDEPENDENT_CODE ON)
 endif()
 
-add_library(secp256k1.host STATIC
-    ${CCF_DIR}/3rdparty/secp256k1/src/secp256k1.c
-)
-target_include_directories(secp256k1.host PUBLIC
-    ${CCF_DIR}/3rdparty/secp256k1
-)
-target_compile_options(secp256k1.host PRIVATE
-    -fvisibility=hidden
-)
-target_compile_definitions(secp256k1.host PRIVATE
-    HAVE_CONFIG_H SECP256K1_BUILD
-)
-set_property(TARGET secp256k1.host
-    PROPERTY POSITION_INDEPENDENT_CODE ON
-)
+add_library(secp256k1.host STATIC ${CCF_DIR}/3rdparty/secp256k1/src/secp256k1.c)
+target_include_directories(secp256k1.host PUBLIC ${CCF_DIR}/3rdparty/secp256k1)
+target_compile_options(secp256k1.host PRIVATE -fvisibility=hidden)
+target_compile_definitions(secp256k1.host PRIVATE HAVE_CONFIG_H SECP256K1_BUILD)
+set_property(TARGET secp256k1.host PROPERTY POSITION_INDEPENDENT_CODE ON)
