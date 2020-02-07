fd593ab075fadc795da3fa474426a6d1e6b8b2a0
Amaury Chamayou <amchamay@microsoft.com>
2020-02-07 16:18:54 +0000

    .

--- tests/infra/libbyz/node.py
+++ tests/infra/libbyz/node.py
@@ -65,12 +65,14 @@ class RemoteNode(Node):
     def run(self):
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
         def user():
-            u = os.getenv('USER')
-            if u.startswith('ci_'):
-                return 'ci'
+            u = os.getenv("USER")
+            if u.startswith("ci_"):
+                return "ci"
             else:
                 return u
+
         self.client.connect(self.public_ip, username=user())
         cmd = " ".join(self.cmd)
         logger.info(cmd)
