1621086fef85a3b05d648a43732fde9221c5b19a
Julien Maffre <jumaffre@microsoft.com>
2020-02-07 08:56:07 +0000

    cmake format

--- cmake/sss.cmake
+++ cmake/sss.cmake
@@ -7,18 +7,24 @@ set(SSS_PREFIX
 )
 message(STATUS "Using sss at ${SSS_PREFIX}")
 
-set(SSS_SRC
-    ${SSS_PREFIX}/sss.c
-    ${SSS_PREFIX}/hazmat.c
-    ${SSS_PREFIX}/tweetnacl.c
+set(SSS_SRC ${SSS_PREFIX}/sss.c ${SSS_PREFIX}/hazmat.c
+            ${SSS_PREFIX}/tweetnacl.c
 )
 
 if("sgx" IN_LIST TARGET)
   add_library(sss.enclave STATIC ${SSS_SRC})
   set_property(TARGET sss.enclave PROPERTY POSITION_INDEPENDENT_CODE ON)
-  install(TARGETS sss.enclave EXPORT ccf DESTINATION lib)
+  install(
+    TARGETS sss.enclave
+    EXPORT ccf
+    DESTINATION lib
+  )
 endif()
 
 add_library(sss.host STATIC ${SSS_SRC})
 set_property(TARGET sss.host PROPERTY POSITION_INDEPENDENT_CODE ON)
-install(TARGETS sss.host EXPORT ccf DESTINATION lib)
+install(
+  TARGETS sss.host
+  EXPORT ccf
+  DESTINATION lib
+)
