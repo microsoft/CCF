6357d8c41554ebf59b9a460ec649481a01e9a985
Eddy Ashton <edashton@microsoft.com>
2020-02-10 16:21:05 +0000

    Move HttpRpcContext to its own file

---
+++ src/enclave/http_rpc_context.h
@@ -0,0 +1,136 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache 2.0 License.
+#pragma once
+
+#include "httpparser.h"
+#include "httpsig.h"
+#include "rpccontext.h"
+
+namespace enclave
+{
+  class HttpRpcContext : public RpcContext
+  {
+  private:
+    uint64_t seq_no = {};
+    nlohmann::json params = nlohmann::json::object();
+    std::string entire_path = {};
+
+  public:
+    // TODO: This is a temporary bodge. Shouldn't be public?
+    std::string_view remaining_path = {};
+
+    HttpRpcContext(
+      const SessionContext& s,
+      http_method verb,
+      const std::string& path,
+      const std::string& query,
+      const http::HeaderMap& headers,
+      const std::vector<uint8_t>& body) :
+      RpcContext(s),
+      entire_path(path)
+    {
+      remaining_path = entire_path;
+
+      auto signed_req = http::HttpSignatureVerifier::parse(
+        std::string(http_method_str(verb)), path, query, headers, body);
+      if (signed_req.has_value())
+      {
+        signed_request = signed_req;
+      }
+
+      if (verb == HTTP_POST)
+      {
+        std::optional<jsonrpc::Pack> pack;
+
+        auto [success, contents] = jsonrpc::unpack_rpc(body, pack);
+        if (!success)
+        {
+          throw std::logic_error("Unable to unpack body.");
+        }
+
+        // Currently contents must either be a naked json payload, or a JSON-RPC
+        // object. We don't check this object for validity, we just extract its
+        // params field
+        const auto params_it = contents.find(jsonrpc::PARAMS);
+        if (params_it != contents.end())
+        {
+          params = *params_it;
+        }
+        else
+        {
+          params = contents;
+        }
+      }
+      else if (verb == HTTP_GET)
+      {
+        // TODO: Construct params by parsing query
+      }
+    }
+
+    virtual const nlohmann::json& get_params() const override
+    {
+      return params;
+    }
+
+    virtual std::string get_method() const override
+    {
+      return std::string(remaining_path);
+    }
+
+    virtual std::string get_whole_method() const override
+    {
+      return entire_path;
+    }
+
+    // TODO: These are still returning a JSON-RPC response body
+    virtual std::vector<uint8_t> serialise_response() const override
+    {
+      nlohmann::json full_response;
+
+      if (response_is_error())
+      {
+        const auto error = get_response_error();
+        full_response = jsonrpc::error_response(
+          seq_no, jsonrpc::Error(error->code, error->msg));
+      }
+      else
+      {
+        const auto payload = get_response_result();
+        full_response = jsonrpc::result_response(seq_no, *payload);
+      }
+
+      for (const auto& [k, v] : headers)
+      {
+        const auto it = full_response.find(k);
+        if (it == full_response.end())
+        {
+          full_response[k] = v;
+        }
+        else
+        {
+          LOG_DEBUG_FMT(
+            "Ignoring response headers with key '{}' - already present in "
+            "response object",
+            k);
+        }
+      }
+
+      return jsonrpc::pack(full_response, jsonrpc::Pack::Text);
+    }
+
+    virtual std::vector<uint8_t> result_response(
+      const nlohmann::json& result) const override
+    {
+      return jsonrpc::pack(
+        jsonrpc::result_response(seq_no, result), jsonrpc::Pack::Text);
+    }
+
+    std::vector<uint8_t> error_response(
+      int error, const std::string& msg) const override
+    {
+      nlohmann::json error_element = jsonrpc::Error(error, msg);
+      return jsonrpc::pack(
+        jsonrpc::error_response(seq_no, error_element), jsonrpc::Pack::Text);
+    }
+  };
+}
