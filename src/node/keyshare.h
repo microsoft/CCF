f297a1b7003b2b0722486bb4980f6ef8d05b3ff3
Julien Maffre <jumaffre@microsoft.com>
2020-02-13 16:02:29 +0000

    Cleanup

--- src/node/keyshare.h
+++ src/node/keyshare.h
@@ -78,20 +78,4 @@ namespace ccf
       return restored;
     }
   };
-
-  // uint8_t data[sss_MLEN], restored[sss_MLEN];
-  // sss_Share shares[5];
-  // size_t idx;
-  // int tmp;
-
-  // // Read a message to be shared
-  // strncpy(data, "Tyler Durden isn't real.", sizeof(data));
-
-  // // Split the secret into 5 shares (with a recombination theshold of 4)
-  // sss_create_shares(shares, data, 5, 4);
-
-  // // Combine some of the shares to restore the original secret
-  // tmp = sss_combine_shares(restored, shares, 4);
-  // assert(tmp == 0);
-  // assert(memcmp(restored, data, sss_MLEN) == 0);
 }
