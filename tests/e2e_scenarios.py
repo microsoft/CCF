8aed845c9a696582e9b7f767b63f3dac15f58475
Amaury Chamayou <amchamay@microsoft.com>
2020-02-19 15:16:18 +0000

    Remove stream logic

--- tests/e2e_scenarios.py
+++ tests/e2e_scenarios.py
@@ -40,14 +40,14 @@ def run(args):
 
             check = infra.checker.Checker()
             check_commit = infra.checker.Checker(mc)
-            with primary.user_client(format="json") as uc:
+            with primary.user_client() as uc:
                 check_commit(uc.do("mkSign", params={}), result=True)
 
             for connection in scenario["connections"]:
                 with (
-                    primary.user_client(format="json")
+                    primary.user_client()
                     if not connection.get("on_backup")
-                    else random.choice(backups).user_client(format="json")
+                    else random.choice(backups).user_client()
                 ) as client:
                     txs = connection.get("transactions", [])
 
