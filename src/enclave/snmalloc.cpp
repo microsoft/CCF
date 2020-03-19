947362abfa18e71a37dde5bed13849fa9ded9ddc
Alex <shamis@gmail.com>
2020-03-19 18:28:53 +0000

    .

--- src/enclave/snmalloc.cpp
+++ src/enclave/snmalloc.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache 2.0 License.
+
 #define NO_BOOTSTRAP_ALLOCATOR
 
 #include "snmalloc/src/override/malloc.cc"
