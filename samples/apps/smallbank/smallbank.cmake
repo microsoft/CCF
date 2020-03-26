d4bbd787de64f952c4d878e2b5584785a60e8bc3
Eddy Ashton <edashton@microsoft.com>
2020-03-26 14:27:33 +0000

    cmake-format

--- samples/apps/smallbank/smallbank.cmake
+++ samples/apps/smallbank/smallbank.cmake
@@ -61,12 +61,8 @@ if(BUILD_TESTS)
       LABEL SB
       CONSENSUS ${CONSENSUS}
       ADDITIONAL_ARGS
-        --transactions
-        ${SMALL_BANK_ITERATIONS}
-        --max-writes-ahead
-        1000
-        --metrics-file
-        small_bank_${CONSENSUS}_metrics.json
+        --transactions ${SMALL_BANK_ITERATIONS} --max-writes-ahead 1000
+        --metrics-file small_bank_${CONSENSUS}_metrics.json
     )
 
     add_perf_test(
