3640e832f90dbb40723b4c67c6cb3c594fe30070
Olga Vrousgou <olgavrou@gmail.com>
2020-04-08 16:59:37 +0100

    formatting

--- src/clients/sig_rpc_tls_client.h
+++ src/clients/sig_rpc_tls_client.h
@@ -20,8 +20,10 @@ public:
   {}
 
   PreparedRpc gen_request(
-    const std::string& method, const CBuffer params,
-    const std::string& content_type = http::headervalues::contenttype::MSGPACK) override
+    const std::string& method,
+    const CBuffer params,
+    const std::string& content_type =
+      http::headervalues::contenttype::MSGPACK) override
   {
     return {gen_request_internal(method, params, key_pair, content_type),
             next_send_id++};
