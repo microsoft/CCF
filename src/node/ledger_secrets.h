d545a2ff2c8f2e8ba132dc9166a52e423f1066fd
Julien Maffre <jumaffre@microsoft.com>
2020-04-20 14:37:40 +0100

    Small tweaks

--- src/node/ledger_secrets.h
+++ src/node/ledger_secrets.h
@@ -46,10 +46,8 @@ namespace ccf
   private:
     std::shared_ptr<AbstractSeal> seal;
 
-    // TODO: We could use a better data structure here to access the latest and
-    // penultimate elements, e.g. a doubly linked-list
     kv::Version penultimate_version = kv::NoVersion;
-    kv::Version latest_version;
+    kv::Version latest_version = 0;
 
     void add_secret(kv::Version v, LedgerSecret&& secret, bool force_seal)
     {
@@ -80,16 +78,9 @@ namespace ccf
 
       secrets_map.emplace(v, std::move(secret));
 
-      // TODO: Let's assume that secrets are added in order (i.e. the latest are
-      // always added latest). Not sure if this is true during recovery?
       if (v > latest_version)
       {
         latest_version = v;
-      }
-      else
-      {
-        // If we are recovering secrets
-        // Nothing
       }
 
       LOG_FAIL_FMT("Penultimate: {}", penultimate_version);
@@ -97,9 +88,9 @@ namespace ccf
     }
 
   public:
-    // Map of secrets that are valid from a specific version to the version of
-    // the next entry in the map. The last entry in the map is valid for all
-    // subsequent versions.
+    // Map of ledger secrets that are valid from a specific version to the
+    // version of the next entry in the map. The last entry in the map is valid
+    // for all subsequent versions.
     std::map<kv::Version, LedgerSecret> secrets_map;
 
     LedgerSecrets() = default;
@@ -121,13 +112,11 @@ namespace ccf
 
     LedgerSecret get_latest()
     {
-      LOG_FAIL_FMT("Returning latest ls at {}", latest_version);
       return secrets_map.find(latest_version)->second;
     }
 
     std::optional<LedgerSecret> get_penultimate()
     {
-      LOG_FAIL_FMT("Returning penultimate ls at {}", penultimate_version);
       if (penultimate_version == kv::NoVersion)
       {
         return {};
@@ -178,30 +167,6 @@ namespace ccf
         add_secret(v, LedgerSecret(s.value()), false);
 
         restored_versions.push_back(v);
-      }
-
-      return restored_versions;
-    }
-
-    std::vector<kv::Version> restore(LedgerSecrets&& ledger_secrets)
-    {
-      std::vector<kv::Version> restored_versions;
-
-      for (auto it = ledger_secrets.secrets_map.begin();
-           it != ledger_secrets.secrets_map.end();)
-      {
-        auto it_ = ledger_secrets.secrets_map.extract(it++);
-        if (secrets_map.find(it_.key()) != secrets_map.end())
-        {
-          throw std::logic_error(fmt::format(
-            "Ledger secret at version {} cannot be restored as they already "
-            "exist",
-            it_.key()));
-        }
-        restored_versions.emplace_back(it_.key());
-
-        LOG_FAIL_FMT("Restoring secrets from shares at {}", it_.key());
-        secrets_map.insert(std::move(it_));
       }
 
       return restored_versions;
