04d3ced7331e6844fc83a251464c2b8a14c268a8
Julien Maffre <jumaffre@microsoft.com>
2020-05-11 09:25:17 +0100

    Bitcoin by default

--- src/tls/curve.h
+++ src/tls/curve.h
@@ -22,7 +22,9 @@ namespace tls
     secp256k1_mbedtls = 3,
     secp256k1_bitcoin = 4,
 
-    service_identity_curve_choice = secp384r1,
+    // service_identity_curve_choice = secp384r1,
+    service_identity_curve_choice = secp256k1_bitcoin,
+
   };
   // SNIPPET_END: supported_curves
 
