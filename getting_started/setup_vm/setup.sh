37840445343aa779435478c7a26782c037b59069
Amaury Chamayou <amchamay@microsoft.com>
2020-05-14 15:51:07 +0100

    Fix scripts

--- getting_started/setup_vm/setup.sh
+++ getting_started/setup_vm/setup.sh
@@ -6,4 +6,4 @@ set -ex
 sudo add-apt-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt install ansible -y
-ansible-playbook ccf-dependencies.yml
+ansible-playbook driver.yml app-dev.yml
