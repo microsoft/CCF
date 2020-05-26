78ad85e2233a2ac4dc954e1fa150b255ea619976
Eddy Ashton <edashton@microsoft.com>
2020-05-26 18:20:16 +0100

    Fix early termination, expand tests

--- src/kv/tx_view.h
+++ src/kv/tx_view.h
@@ -226,29 +226,42 @@ namespace kv
      * whether the iteration should continue (true) or stop (false)
      */
     template <class F>
-    bool foreach(F&& f)
+    void foreach(F&& f)
     {
       // Record a global read dependency.
       tx_changes.read_version = tx_changes.start_version;
       auto& w = tx_changes.writes;
+      bool should_continue = true;
 
-      tx_changes.state.foreach([&w, &f](const K& k, const VersionV<V>& v) {
-        auto write = w.find(k);
+      tx_changes.state.foreach(
+        [&w, &f, &should_continue](const K& k, const VersionV<V>& v) {
+          auto write = w.find(k);
 
-        if ((write == w.end()) && !is_deleted(v.version))
-          return f(k, v.value);
-        return true;
-      });
+          if ((write == w.end()) && !is_deleted(v.version))
+          {
+            should_continue = f(k, v.value);
+          }
 
-      for (auto write = tx_changes.writes.begin();
-           write != tx_changes.writes.end();
-           ++write)
+          return should_continue;
+        });
+
+      if (should_continue)
       {
-        if (write->second.has_value())
-          if (!f(write->first, write->second.value()))
-            return false;
+        for (auto write = tx_changes.writes.begin();
+             write != tx_changes.writes.end();
+             ++write)
+        {
+          if (write->second.has_value())
+          {
+            should_continue = f(write->first, write->second.value());
+          }
+
+          if (!should_continue)
+          {
+            break;
+          }
+        }
       }
-      return true;
     }
   };
 }
