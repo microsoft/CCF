fdc94ccbceed4fa12ab8362e3f445ff008911b6f
Amaury Chamayou <amchamay@microsoft.com>
2020-06-03 13:29:39 +0100

    Formatting

--- tests/suspend_nodes.py
+++ tests/suspend_nodes.py
@@ -43,7 +43,7 @@ def update_view_info(network, view_info):
 def get_node_local_commit(node):
     with node.node_client() as c:
         r = c.get("commit")
-        return r.result['seqno'], r.global_commit
+        return r.result["seqno"], r.global_commit
 
 
 def wait_for_late_joiner(old_node, late_joiner, timeout=30):
