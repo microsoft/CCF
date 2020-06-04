9e24c089f3afd81b60d0f4184870843486da16b7
Eddy Ashton <edashton@microsoft.com>
2020-06-04 10:42:53 +0100

    Reduce size of SmallVecs, make sure we're not double-packing

--- src/kv/serialised_entry.h
+++ src/kv/serialised_entry.h
@@ -8,49 +8,5 @@
 
 namespace kv::serialisers
 {
-  using SerialisedEntry = llvm_vecsmall::SmallVector<uint8_t, 64>;
-}
-
-namespace msgpack
-{
-  MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS)
-  {
-    namespace adaptor
-    {
-      template <>
-      struct pack<kv::serialisers::SerialisedEntry>
-      {
-        template <typename Stream>
-        msgpack::packer<Stream>& operator()(
-          msgpack::packer<Stream>& o,
-          const kv::serialisers::SerialisedEntry& entry) const
-        {
-          o.pack_bin(entry.size());
-          o.pack_bin_body(
-            reinterpret_cast<const char*>(entry.data()), entry.size());
-
-          return o;
-        }
-      };
-
-      template <>
-      struct convert<kv::serialisers::SerialisedEntry>
-      {
-        const msgpack::object& operator()(
-          const msgpack::object& o,
-          kv::serialisers::SerialisedEntry& entry) const
-        {
-          if ((o.type) != msgpack::type::BIN)
-          {
-            throw msgpack::type_error();
-          }
-
-          entry = kv::serialisers::SerialisedEntry(
-            o.via.bin.ptr, o.via.bin.ptr + o.via.bin.size);
-
-          return o;
-        }
-      };
-    }
-  }
+  using SerialisedEntry = llvm_vecsmall::SmallVector<uint8_t, 8>;
 }
