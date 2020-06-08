30fb05c8edc5a82759565a9d77d1011932088a66
Eddy Ashton <edashton@microsoft.com>
2020-06-08 17:31:15 +0100

    Expand unit test

--- src/ds/test/typed_messages.cpp
+++ src/ds/test/typed_messages.cpp
@@ -8,12 +8,24 @@ enum : ringbuffer::Message
 {
   DEFINE_RINGBUFFER_MSG_TYPE(large_block_message),
   DEFINE_RINGBUFFER_MSG_TYPE(large_compound_message),
+  DEFINE_RINGBUFFER_MSG_TYPE(large_complex_message),
   DEFINE_RINGBUFFER_MSG_TYPE(finish),
 };
 
 DECLARE_RINGBUFFER_MESSAGE_PAYLOAD(large_block_message, std::vector<uint8_t>);
 DECLARE_RINGBUFFER_MESSAGE_PAYLOAD(
   large_compound_message, size_t, std::vector<uint8_t>);
+DECLARE_RINGBUFFER_MESSAGE_PAYLOAD(
+  large_complex_message,
+  uint16_t,
+  bool,
+  std::vector<uint8_t>,
+  uint32_t,
+  std::string,
+  bool,
+  std::vector<uint8_t>,
+  uint16_t,
+  uint64_t);
 DECLARE_RINGBUFFER_MESSAGE_PAYLOAD(finish);
 
 TEST_CASE(
@@ -38,53 +50,92 @@ TEST_CASE(
 
   SUBCASE("block message")
   {
-    std::vector<uint8_t> received;
+    bool message_seen = false;
+
+    std::vector<uint8_t> sent(fragment_max * 2);
+    std::iota(sent.begin(), sent.end(), 0);
+
     DISPATCHER_SET_MESSAGE_HANDLER(
       bp,
       large_block_message,
-      [&bp, &received](const uint8_t* data, size_t size) {
+      [&](const uint8_t* data, size_t size) {
         auto [body] = ringbuffer::read_message<large_block_message>(data, size);
-        received = body;
-      });
+        REQUIRE(body == sent);
 
-    std::vector<uint8_t> sent(fragment_max * 2);
-    for (auto& b : sent)
-    {
-      b = rand();
-    }
+        REQUIRE(!message_seen);
+        message_seen = true;
+      });
 
     RINGBUFFER_WRITE_MESSAGE(large_block_message, writer_p, sent);
     RINGBUFFER_WRITE_MESSAGE(finish, writer_p);
     bp.run(rr);
-    REQUIRE(received == sent);
+    REQUIRE(message_seen);
   }
 
   SUBCASE("compound message")
   {
-    size_t received_n;
-    std::vector<uint8_t> received_body;
-    DISPATCHER_SET_MESSAGE_HANDLER(
-      bp,
-      large_compound_message,
-      [&bp, &received_n, &received_body](const uint8_t* data, size_t size) {
-        auto [n, body] =
-          ringbuffer::read_message<large_compound_message>(data, size);
-        received_n = n;
-        received_body = body;
-      });
+    bool message_seen = false;
 
     size_t sent_n = 42u;
     std::vector<uint8_t> sent_body(fragment_max * 2);
-    for (auto& b : sent_body)
-    {
-      b = rand();
-    }
+    std::iota(sent_body.begin(), sent_body.end(), 0);
+
+    DISPATCHER_SET_MESSAGE_HANDLER(
+      bp, large_compound_message, [&](const uint8_t* data, size_t size) {
+        auto [n, body] =
+          ringbuffer::read_message<large_compound_message>(data, size);
+
+        REQUIRE(n == sent_n);
+        REQUIRE(body == sent_body);
+
+        REQUIRE(!message_seen);
+        message_seen = true;
+      });
 
     RINGBUFFER_WRITE_MESSAGE(
       large_compound_message, writer_p, sent_n, sent_body);
     RINGBUFFER_WRITE_MESSAGE(finish, writer_p);
     bp.run(rr);
-    REQUIRE(received_n == sent_n);
-    REQUIRE(received_body == sent_body);
+    REQUIRE(message_seen);
   }
+
+  // SUBCASE("complex message")
+  // {
+  //   bool message_seen = false;
+
+  //   const uint16_t a = 16;
+  //   const bool b = true;
+  //   const std::vector<uint8_t> c{100, 101, 111};
+  //   const uint32_t d = 42;
+  //   const std::string e = "COMPLEX";
+  //   const bool f = false;
+  //   const std::vector<uint8_t> g{1, 2, 3, 4, 5};
+  //   const uint16_t h = 1661;
+  //   const uint64_t i = 0xdeadbeef;
+
+  //   DISPATCHER_SET_MESSAGE_HANDLER(
+  //     bp, large_complex_message, [&](const uint8_t* data, size_t size) {
+  //       auto [aa, bb, cc, dd, ee, ff, gg, hh, ii] =
+  //         ringbuffer::read_message<large_complex_message>(data, size);
+
+  //       REQUIRE(a == aa);
+  //       REQUIRE(b == bb);
+  //       REQUIRE(c == cc);
+  //       REQUIRE(d == dd);
+  //       REQUIRE(e == ee);
+  //       REQUIRE(f == ff);
+  //       REQUIRE(g == gg);
+  //       REQUIRE(h == hh);
+  //       REQUIRE(i == ii);
+
+  //       REQUIRE(!message_seen);
+  //       message_seen = true;
+  //     });
+
+  //   RINGBUFFER_WRITE_MESSAGE(
+  //     large_complex_message, writer_p, a, b, c, d, e, f, g, h, i);
+  //   RINGBUFFER_WRITE_MESSAGE(finish, writer_p);
+  //   bp.run(rr);
+  //   REQUIRE(message_seen);
+  // }
 }
