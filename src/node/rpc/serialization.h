45f617b946e51538d93399075e2fbf1841f77ab6
Lyndon Shi <lyshi@microsoft.com>
2020-06-23 05:31:36 -0700

    RPC to retrieve IDs assigned by CCF to a given node's RPC address (#1319)

--- src/node/rpc/serialization.h
+++ src/node/rpc/serialization.h
@@ -95,6 +95,13 @@ namespace ccf
   DECLARE_JSON_TYPE(GetNetworkInfo::Out)
   DECLARE_JSON_REQUIRED_FIELDS(GetNetworkInfo::Out, nodes, primary_id)
 
+  DECLARE_JSON_TYPE(GetNodesByRPCAddress::In)
+  DECLARE_JSON_REQUIRED_FIELDS(GetNodesByRPCAddress::In, host, port)
+  DECLARE_JSON_TYPE(GetNodesByRPCAddress::NodeInfo)
+  DECLARE_JSON_REQUIRED_FIELDS(GetNodesByRPCAddress::NodeInfo, node_id, status)
+  DECLARE_JSON_TYPE(GetNodesByRPCAddress::Out)
+  DECLARE_JSON_REQUIRED_FIELDS(GetNodesByRPCAddress::Out, nodes)
+
   DECLARE_JSON_TYPE(CallerInfo)
   DECLARE_JSON_REQUIRED_FIELDS(CallerInfo, caller_id)
 
