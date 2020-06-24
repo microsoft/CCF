470da33151ed7c03a5a463976bbfcd7b64068855
Eddy Ashton <edashton@microsoft.com>
2020-06-24 17:36:25 +0100

    Update logging app to use verbs for dispatch (#1332)

--- src/apps/logging/logging_js.lua
+++ src/apps/logging/logging_js.lua
@@ -2,7 +2,7 @@
 -- Licensed under the Apache 2.0 License.
 
 return {
-  LOG_get = [[
+  ["GET log/private"] = [[
     function get(query)
     {
       const elements = query.split("&");
@@ -24,7 +24,47 @@ return {
     get(query)
   ]],
 
-  LOG_remove = [[
+  ["GET log/public"] = [[
+    function get(query)
+    {
+      const elements = query.split("&");
+      for (kv of elements) {
+        const [k, v] = kv.split("=");
+        if (k == "id") {
+          try
+          {
+            return {msg: tables.data.get(JSON.parse(v).toString())};
+          }
+          catch (err)
+          {
+            return {error: err.message}
+          }
+        }
+      }
+      throw "Could not find 'id' in query";
+    }
+    get(query)
+  ]],
+
+  ["POST log/private"] = [[
+    function record(params)
+    {
+      tables.data.put(params.id.toString(), params.msg);
+      return true;
+    }
+    record(JSON.parse(body))
+  ]],
+
+  ["POST log/public"] = [[
+    function record(params)
+    {
+      tables.data.put(params.id.toString(), params.msg);
+      return true;
+    }
+    record(JSON.parse(body))
+  ]],
+
+  ["DELETE log/public"] = [[
     function remove(query)
     {
       const elements = query.split("&");
@@ -39,36 +79,18 @@ return {
     remove(query)
   ]],
 
-  LOG_get_pub = [[
-    function get(query)
+  ["DELETE log/private"] = [[
+    function remove(query)
     {
       const elements = query.split("&");
       for (kv of elements) {
         const [k, v] = kv.split("=");
         if (k == "id") {
-          return {msg: tables.data.get(JSON.parse(v).toString())};
+          return tables.data.remove(JSON.parse(v).toString());
         }
       }
       throw "Could not find 'id' in query";
     }
-    get(query)
-  ]],
-
-  LOG_record = [[
-    function record(params)
-    {
-      tables.data.put(params.id.toString(), params.msg);
-      return true;
-    }
-    record(JSON.parse(body))
-  ]],
-
-  LOG_record_pub = [[
-    function record(params)
-    {
-      tables.data.put(params.id.toString(), params.msg);
-      return true;
-    }
-    record(JSON.parse(body))
+    remove(query)
   ]]
 }
