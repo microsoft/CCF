d41978855e570baccf1a4d757ed55da13f753384
Amaury Chamayou <amchamay@microsoft.com>
2020-06-30 10:00:43 +0100

    Add fix mode

--- scripts/ci-checks.sh
+++ scripts/ci-checks.sh
@@ -10,22 +10,22 @@ fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"  
 
-# Shell scripts
+echo "Shell scripts"
 find . -type f -regex ".*\.sh$" | grep -E -v "^./3rdparty/" | xargs shellcheck -s bash -e SC2044,SC2002,SC1091,SC2181
 
-# TODOs
+echo "TODOs"
 "$SCRIPT_DIR"/check-todo.sh src
 
-# C/C++ Format
+echo "C/C++ format"
 "$SCRIPT_DIR"/check-format.sh src samples
 if [ $FIX -ne 0 ] && [ $? -ne 0 ]; then
     "$SCRIPT_DIR"/check-format.sh -f src samples
 fi
 
-# Copyright notice headers
+echo "Copyright notice headers"
 python3.7 "$SCRIPT_DIR"/notice-check.py
 
-# CMake format
+echo "CMake format"
 if [ $FIX -ne 0 ]; then
   "$SCRIPT_DIR"/check-cmake-format.sh -f cmake samples src tests CMakeLists.txt
 else
@@ -41,7 +41,7 @@ fi
 source scripts/env/bin/activate
 pip --disable-pip-version-check install black pylint 1>/dev/null
 
-# Python code format
+echo "Python format"
 if [ $FIX -ne 0 ]; then
   black tests/ scripts/*.py
 else
@@ -51,5 +51,5 @@ fi
 # Install test dependencies before linting
 pip --disable-pip-version-check install -U -r tests/requirements.txt 1>/dev/null
 
-# Python lint
+echo "Python lint"
 find tests/ -type f -name "*.py" -exec python -m pylint {} +
