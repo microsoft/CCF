f60f3214954e4c5117ed6ae2ce2c835cb5b0e4e8
Eddy Ashton <edashton@microsoft.com>
2020-07-09 14:50:11 +0100

    Construct (request-target) in bash

--- tests/scurl.sh
+++ tests/scurl.sh
@@ -4,20 +4,41 @@
 
 set -e
 
-# Loop through all arguments and find request data and private key
+# Loop through all arguments and find url, request data, command and private key
+# NB: For simplicity, assume url is the first argument or preceded by --url. curl is slightly more permissive
+next_is_url=false
 next_is_data=false
+next_is_command=false
 next_is_privk=false
+
+url=$1
+command="post"
+
 for item in "$@" ; do
+    if [ "$next_is_url" == true ]; then
+        url=$item
+        next_is_url=false
+    fi
     if [ "$next_is_data" == true ]; then
         request=$item
         next_is_data=false
+    fi
+    if [ "$next_is_command" == true ]; then
+        command=$item
+        next_is_command=false
     fi
     if [ "$next_is_privk" == true ]; then
         privk=$item
         next_is_privk=false
     fi
+    if [ "$item" == "--url" ]; then
+        next_is_url=true
+    fi
     if [ "$item" == "-d" ] || [ "$item" == "--data" ] || [ "$item" == "--data-binary" ]; then
         next_is_data=true
+    fi
+    if [ "$item" == "-X" ] || [ "$item" == "--request" ]; then
+        next_is_command=true
     fi
     if [ "$item" == "--key" ]; then
         next_is_privk=true
@@ -47,9 +68,15 @@ else
     content_length=${#request}
 fi
 
+# Trim URL to just the ":path" pseudo-header
+# https://tools.ietf.org/html/rfc7540#section-8.1.2.3
+url=${url#*//} # Remove protocol
+url=/${url#*/} # Remove domain name, restore leading slash
+url=${url%\#*} # Remove fragment
 
 # Construct string to sign
-string_to_sign="digest: SHA-256=$req_digest
+string_to_sign="(request-target): ${command,,} ${url}
+digest: SHA-256=$req_digest
 content-length: $content_length"
 
 # Compute signature
@@ -57,6 +84,6 @@ signed_raw=$(echo -n "$string_to_sign" | openssl dgst -sha256 -sign "$privk" | o
 
 curl \
 -H "Digest: SHA-256=$req_digest" \
--H "Authorization: Signature keyId=\"tls\",algorithm=\"ecdsa-sha256\",headers=\"digest content-length\",signature=\"$signed_raw\"" \
+-H "Authorization: Signature keyId=\"tls\",algorithm=\"ecdsa-sha256\",headers=\"(request-target) digest content-length\",signature=\"$signed_raw\"" \
 "${additional_curl_args[@]}" \
 "$@"
