6e0c3ce438b82570b108e2891c4acf42cbe7f1ae
Julien Maffre <42961061+jumaffre@users.noreply.github.com>
2020-07-21 18:08:02 +0100

    Update governance_history.py

--- tests/governance_history.py
+++ tests/governance_history.py
@@ -95,6 +95,7 @@ def run(args):
         assert response.body["state"] == ProposalState.Withdrawn.value
         withdrawals_issued += 1
 
+    ledger = ccf.ledger.Ledger(ledger_directory)
     (final_proposals, final_votes, final_withdrawals,) = count_governance_operations(
         ledger
     )
