d0cead3348f66755628bc33e80dd168aef576b56
Amaury Chamayou <amchamay@microsoft.com>
2020-07-24 15:39:59 +0100

    Initial endpoint metrics implementation (#1422)

--- src/http/ws_rpc_context.h
+++ src/http/ws_rpc_context.h
@@ -154,6 +154,11 @@ namespace ws
       response_status = (http_status)status;
     }
 
+    virtual int get_response_status() const override
+    {
+      return response_status;
+    }
+
     virtual void set_response_header(
       const std::string_view&, const std::string_view&) override
     {}
