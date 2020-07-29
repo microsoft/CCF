5584c5a2c320712da4d800e47e5c78a24d7a55c6
Maik Riechert <maik.riechert@microsoft.com>
2020-07-29 14:03:55 +0000

    black

--- tests/infra/network.py
+++ tests/infra/network.py
@@ -123,7 +123,7 @@ class Network:
         self.perf_nodes = perf_nodes
 
         try:
-            os.remove(os.path.join(self.binary_dir, 'vscode-gdb.sh'))
+            os.remove(os.path.join(self.binary_dir, "vscode-gdb.sh"))
         except FileNotFoundError:
             pass
         for host in hosts:
