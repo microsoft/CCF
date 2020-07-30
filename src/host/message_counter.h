8f803d53cc5270a4c238287f4a711f78e7c6010a
Eddy Ashton <edashton@microsoft.com>
2020-07-30 10:33:36 +0100

    Store duration rather than time_point

--- src/host/message_counter.h
+++ src/host/message_counter.h
@@ -10,7 +10,8 @@ namespace asynchost
   class MessageCounterImpl
   {
     using TClock = std::chrono::high_resolution_clock;
-    TClock::time_point last_update;
+    TClock::time_point start_time;
+    TClock::duration last_update = {};
 
     messaging::Dispatcher<ringbuffer::Message>& dispatcher;
 
@@ -21,13 +22,13 @@ namespace asynchost
       dispatcher(disp)
     {
       dispatcher.retrieve_message_counts();
-      last_update = TClock::now();
+      start_time = TClock::now();
     }
 
     void on_timer()
     {
       const auto message_counts = dispatcher.retrieve_message_counts();
-      const auto now = TClock::now();
+      const auto duration_now = TClock::now() - start_time;
 
       if (!message_counts.empty())
       {
@@ -44,12 +45,14 @@ namespace asynchost
         }
 
         LOG_INFO_FMT(
-          "Outbound messages between {} and {}: {}",
-          last_update.time_since_epoch().count(),
-          now.time_since_epoch().count(),
+          "Outbound messages from {} to {}: {}",
+          std::chrono::duration_cast<std::chrono::milliseconds>(last_update)
+            .count(),
+          std::chrono::duration_cast<std::chrono::milliseconds>(duration_now)
+            .count(),
           formatted);
 
-        last_update = now;
+        last_update = duration_now;
       }
     }
   };
