6535eb4d0f18288936546832294be66ba17c78cb
Maik Riechert <maik.riechert@microsoft.com>
2020-07-31 13:51:11 +0000

    add GOV_SCRIPT cmake arg

--- tests/governance_root_ca_certs.py
+++ tests/governance_root_ca_certs.py
@@ -18,6 +18,7 @@ from loguru import logger as LOG
 
 this_dir = os.path.dirname(__file__)
 
+
 @reqs.description("Add certificate with quote, query, update")
 def test_cert_store(network, args):
     # TODO: propose a cert update with valid cert but mismatching mrsigner
@@ -25,45 +26,62 @@ def test_cert_store(network, args):
     primary, _ = network.find_nodes()
 
     LOG.info("Member builds a root ca cert update proposal with malformed cert")
-    with tempfile.NamedTemporaryFile('w') as f:
-        f.write('foo')
+    with tempfile.NamedTemporaryFile("w") as f:
+        f.write("foo")
         f.flush()
         try:
-            proposal_body, _ = ccf.proposal_generator.update_root_ca_cert("mycert", f.name)
+            proposal_body, _ = ccf.proposal_generator.update_root_ca_cert(
+                "mycert", f.name
+            )
         except ValueError:
             pass
         else:
             assert False, "update_root_ca_cert should have raised an error"
 
     LOG.info("Member makes a root ca cert update proposal with malformed cert")
-    with tempfile.NamedTemporaryFile('w') as f:
-        f.write('foo')
+    with tempfile.NamedTemporaryFile("w") as f:
+        f.write("foo")
         f.flush()
-        proposal_body, _ = ccf.proposal_generator.update_root_ca_cert("mycert", f.name, skip_checks=True)
+        proposal_body, _ = ccf.proposal_generator.update_root_ca_cert(
+            "mycert", f.name, skip_checks=True
+        )
         try:
-            proposal = network.consortium.get_any_active_member().propose(primary, proposal_body)
+            proposal = network.consortium.get_any_active_member().propose(
+                primary, proposal_body
+            )
         except infra.proposal.ProposalNotCreated:
             pass
         else:
             assert False, "Proposal should not have been created"
 
     LOG.info("Member makes a root ca cert update proposal with valid cert")
-    ca_cert_path = os.path.join(this_dir, 'root_ca_cert.pem')
-    proposal_body, _ = ccf.proposal_generator.update_root_ca_cert("mycert", ca_cert_path)
-    proposal = network.consortium.get_any_active_member().propose(primary, proposal_body)
+    ca_cert_path = os.path.join(this_dir, "root_ca_cert.pem")
+    proposal_body, _ = ccf.proposal_generator.update_root_ca_cert(
+        "mycert", ca_cert_path
+    )
+    proposal = network.consortium.get_any_active_member().propose(
+        primary, proposal_body
+    )
     assert proposal.state == ProposalState.Accepted
-    
-    with primary.client(f"member{network.consortium.get_any_active_member().member_id}") as c:
-        r = c.post(
-            "/gov/read", {"table": "ccf.root_ca_cert_ders", "key": "mycert"}
-        )
+
+    with primary.client(
+        f"member{network.consortium.get_any_active_member().member_id}"
+    ) as c:
+        r = c.post("/gov/read", {"table": "ccf.root_ca_cert_ders", "key": "mycert"})
         assert r.status_code == 200, r.status_code
         cert_pem_str = open(ca_cert_path).read()
-        cert_ref = x509.load_pem_x509_certificate(cert_pem_str.encode(), crypto_backends.default_backend())
-        cert_kv = x509.load_der_x509_certificate(bytes(r.body), crypto_backends.default_backend())
-        assert cert_ref == cert_kv, f"stored cert not equal to input cert: {cert_ref} != {cert_kv}"
+        cert_ref = x509.load_pem_x509_certificate(
+            cert_pem_str.encode(), crypto_backends.default_backend()
+        )
+        cert_kv = x509.load_der_x509_certificate(
+            bytes(r.body), crypto_backends.default_backend()
+        )
+        assert (
+            cert_ref == cert_kv
+        ), f"stored cert not equal to input cert: {cert_ref} != {cert_kv}"
 
     return network
+
 
 def run(args):
     hosts = ["localhost"] * (3 if args.consensus == "pbft" else 2)
@@ -78,9 +96,5 @@ def run(args):
 if __name__ == "__main__":
 
     args = infra.e2e_args.cli_args()
-    
-    # temporary hack...
-    args.gov_script = "gov_root_ca_certs.lua"
-
     args.package = "liblogging"
     run(args)
