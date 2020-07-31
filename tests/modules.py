437c6bfbc921b926413d4703f52e600ea1598e60
Amaury Chamayou <amchamay@microsoft.com>
2020-07-31 15:07:01 +0100

    Pylint

--- tests/modules.py
+++ tests/modules.py
@@ -1,7 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
-import os
-import sys
 import tempfile
 import infra.network
 import infra.path
