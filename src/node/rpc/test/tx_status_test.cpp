a081e607bc1f27b3f9e405215bc7139b6f3d8a0b
Alex <shamis@gmail.com>
2020-08-27 15:24:20 +0100

    .

--- src/node/rpc/test/tx_status_test.cpp
+++ src/node/rpc/test/tx_status_test.cpp
@@ -65,18 +65,12 @@ TEST_CASE("edge cases")
     INFO("Unknown views");
     // Impossible: view for all global txs must be known
     // get_tx_status(a, N, 0, b, >=N)
-    CHECK_THROWS(
-      get_tx_status(3, 10, std::numeric_limits<int64_t>::min(), 1, 10));
-    CHECK_THROWS(
-      get_tx_status(3, 10, std::numeric_limits<int64_t>::min(), 1, 11));
-    CHECK_THROWS(
-      get_tx_status(3, 10, std::numeric_limits<int64_t>::min(), 3, 10));
-    CHECK_THROWS(
-      get_tx_status(3, 10, std::numeric_limits<int64_t>::min(), 3, 11));
-    CHECK_THROWS(
-      get_tx_status(3, 10, std::numeric_limits<int64_t>::min(), 4, 10));
-    CHECK_THROWS(
-      get_tx_status(3, 10, std::numeric_limits<int64_t>::min(), 4, 11));
+    CHECK_THROWS(get_tx_status(3, 10, VIEW_UNKNOWN, 1, 10));
+    CHECK_THROWS(get_tx_status(3, 10, VIEW_UNKNOWN, 1, 11));
+    CHECK_THROWS(get_tx_status(3, 10, VIEW_UNKNOWN, 3, 10));
+    CHECK_THROWS(get_tx_status(3, 10, VIEW_UNKNOWN, 3, 11));
+    CHECK_THROWS(get_tx_status(3, 10, VIEW_UNKNOWN, 4, 10));
+    CHECK_THROWS(get_tx_status(3, 10, VIEW_UNKNOWN, 4, 11));
   }
   {
     INFO("seqno is known locally in an old view");
