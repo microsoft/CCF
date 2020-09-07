e9fe0f36583d2ccad23f147a68f1a441b77bf353
Maik Riechert <maik.riechert@microsoft.com>
2020-09-07 15:07:12 +0000

    formatting

--- src/apps/js_generic/js_generic.cpp
+++ src/apps/js_generic/js_generic.cpp
@@ -525,22 +525,21 @@ namespace ccfapp
         std::vector<uint8_t> response_body;
         size_t buf_size;
         size_t buf_offset;
-        JSValue typed_array_buffer = JS_GetTypedArrayBuffer(ctx, val,
-                               &buf_offset,
-                               &buf_size,
-                               nullptr);
+        JSValue typed_array_buffer =
+          JS_GetTypedArrayBuffer(ctx, val, &buf_offset, &buf_size, nullptr);
         uint8_t* array_buffer;
         if (!JS_IsException(typed_array_buffer))
         {
           size_t buf_size_total;
-          array_buffer = JS_GetArrayBuffer(ctx, &buf_size_total, typed_array_buffer);
+          array_buffer =
+            JS_GetArrayBuffer(ctx, &buf_size_total, typed_array_buffer);
           array_buffer += buf_offset;
           JS_FreeValue(ctx, typed_array_buffer);
         }
         else
         {
           array_buffer = JS_GetArrayBuffer(ctx, &buf_size, val);
-        }          
+        }
         if (array_buffer)
         {
           response_content_type = http::headervalues::contenttype::OCTET_STREAM;
