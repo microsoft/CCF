9c86029b3e77d5ba856040e906eb635ee446eed0
Eddy Ashton <edashton@microsoft.com>
2020-09-08 16:01:21 +0100

    Benchmark an alternative raw serialisation format

--- samples/apps/smallbank/fbs/tests/small_bank_serdes_bench.cpp
+++ samples/apps/smallbank/fbs/tests/small_bank_serdes_bench.cpp
@@ -16,8 +16,8 @@ extern "C"
 #include <evercrypt/EverCrypt_AutoConfig2.h>
 }
 
-size_t account_name = 10;
-int transaction_value = 50;
+const std::string account_name = "10";
+const int transaction_value = 50;
 
 inline void clobber_memory()
 {
@@ -33,17 +33,31 @@ std::shared_ptr<ccf::LedgerSecrets> create_ledger_secrets()
   return secrets;
 }
 
+static std::vector<uint8_t> raw_tx_buffer()
+{
+  const auto size =
+    sizeof(size_t) + account_name.size() + sizeof(transaction_value);
+  std::vector<uint8_t> v(size);
+  auto data = v.data();
+  auto remaining = v.size();
+
+  serialized::write(data, remaining, account_name);
+  serialized::write(data, remaining, transaction_value);
+
+  return v;
+}
+
 static std::vector<uint8_t> packed_json_tx()
 {
   nlohmann::json j;
-  j["name"] = std::to_string(account_name);
+  j["name"] = account_name;
   j["value"] = transaction_value;
   return serdes::pack(j, serdes::Pack::MsgPack);
 }
 
 static std::unique_ptr<flatbuffers::DetachedBuffer> fb_tx_buffer()
 {
-  TransactionSerializer ts(std::to_string(account_name), transaction_value);
+  TransactionSerializer ts(account_name, transaction_value);
   return ts.get_detached_buffer();
 }
 
@@ -77,6 +91,35 @@ static std::vector<uint8_t> kv_serialized_data(std::vector<uint8_t>& data)
 // End Helper functions
 
 // Test functions
+static void raw_ser(picobench::state& s)
+{
+  s.start_timer();
+  for (int i = 0; i < s.iterations(); i++)
+  {
+    auto buf = raw_tx_buffer();
+    clobber_memory();
+  }
+  s.stop_timer();
+}
+
+static void raw_des(picobench::state& s)
+{
+  const auto buf = raw_tx_buffer();
+  s.start_timer();
+  for (int i = 0; i < s.iterations(); i++)
+  {
+    auto data = buf.data();
+    auto size = buf.size();
+
+    auto name =
+      serialized::read<std::remove_cv_t<typeof(account_name)>>(data, size);
+    auto value =
+      serialized::read<std::remove_cv_t<typeof(transaction_value)>>(data, size);
+    clobber_memory();
+  }
+  s.stop_timer();
+}
+
 static void flatbuffers_ser(picobench::state& s)
 {
   s.start_timer();
@@ -84,6 +127,7 @@ static void flatbuffers_ser(picobench::state& s)
   {
     auto buf = fb_tx_buffer();
     CBuffer b = {buf->data(), buf->size()};
+    clobber_memory();
   }
   s.stop_timer();
 }
@@ -97,6 +141,7 @@ static void flatbuffers_des(picobench::state& s)
     TransactionDeserializer td(buf->data());
     std::string name = td.name();
     int64_t value = td.value();
+    clobber_memory();
   }
   s.stop_timer();
 }
@@ -107,6 +152,7 @@ static void json_msgpack_ser(picobench::state& s)
   for (int i = 0; i < s.iterations(); i++)
   {
     auto p = packed_json_tx();
+    clobber_memory();
   }
   s.stop_timer();
 }
@@ -120,6 +166,7 @@ static void json_msgpack_des(picobench::state& s)
     auto params = serdes::unpack(p, serdes::Pack::MsgPack);
     std::string name = params["name"];
     int64_t value = params["value"];
+    clobber_memory();
   }
   s.stop_timer();
 }
@@ -137,6 +184,13 @@ static void run_mt_benchmark(picobench::state& s, std::vector<uint8_t> data)
   s.stop_timer();
 }
 
+static void raw_mt_append(picobench::state& s)
+{
+  auto data = raw_tx_buffer();
+  auto serialized_data = kv_serialized_data(data);
+  run_mt_benchmark(s, serialized_data);
+}
+
 static void flatbuffers_mt_append(picobench::state& s)
 {
   auto buf = fb_tx_buffer();
@@ -150,6 +204,14 @@ static void json_msgpack_mt_append(picobench::state& s)
   auto data = packed_json_tx();
   auto serialized_data = kv_serialized_data(data);
   run_mt_benchmark(s, serialized_data);
+}
+
+template <size_t S>
+static void raw_large_payload(picobench::state& s)
+{
+  auto payload = large_payload(S);
+  auto data = kv_serialized_data(payload);
+  run_mt_benchmark(s, data);
 }
 
 template <size_t S>
@@ -177,24 +239,31 @@ const std::vector<int> iters = {10, 100, 1000};
 const std::vector<int> iter = {100};
 
 PICOBENCH_SUITE("smallbank payload serialize");
+PICOBENCH(raw_ser).iterations(iters).samples(10);
 PICOBENCH(flatbuffers_ser).iterations(iters).samples(10);
 PICOBENCH(json_msgpack_ser).iterations(iters).samples(10);
 
 PICOBENCH_SUITE("smallbank payload deserialize");
+PICOBENCH(raw_des).iterations(iters).samples(10);
 PICOBENCH(flatbuffers_des).iterations(iters).samples(10);
 PICOBENCH(json_msgpack_des).iterations(iters).samples(10);
 
 PICOBENCH_SUITE("smallbank payload merkle tree bench");
+PICOBENCH(raw_mt_append).iterations(iters).samples(10);
 PICOBENCH(flatbuffers_mt_append).iterations(iters).samples(10);
 PICOBENCH(json_msgpack_mt_append).iterations(iters).samples(10);
 
 PICOBENCH_SUITE("large payload merkle tree bench");
+PICOBENCH(raw_large_payload<10>).iterations(iter).samples(10);
 PICOBENCH(fb_large_payload<10>).iterations(iter).samples(10);
 PICOBENCH(jm_large_payload<10>).iterations(iter).samples(10);
+PICOBENCH(raw_large_payload<100>).iterations(iter).samples(10);
 PICOBENCH(fb_large_payload<100>).iterations(iter).samples(10);
 PICOBENCH(jm_large_payload<100>).iterations(iter).samples(10);
+PICOBENCH(raw_large_payload<1000>).iterations(iter).samples(10);
 PICOBENCH(fb_large_payload<1000>).iterations(iter).samples(10);
 PICOBENCH(jm_large_payload<1000>).iterations(iter).samples(10);
+PICOBENCH(raw_large_payload<10000>).iterations(iter).samples(10);
 PICOBENCH(fb_large_payload<10000>).iterations(iter).samples(10);
 PICOBENCH(jm_large_payload<10000>).iterations(iter).samples(10);
 
