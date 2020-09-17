731ab758471cba1a9ebfb1cf159d9034282677a9
Amaury Chamayou <amchamay@microsoft.com>
2020-09-17 16:27:12 +0100

    fmt

--- tests/election.py
+++ tests/election.py
@@ -41,7 +41,9 @@ def wait_for_seqno_to_commit(seqno, view, nodes):
         for f in nodes:
             with f.client() as c:
                 logs[f.node_id] = []
-                r = c.get(f"/node/tx?view={view}&seqno={seqno}", log_capture=logs[f.node_id])
+                r = c.get(
+                    f"/node/tx?view={view}&seqno={seqno}", log_capture=logs[f.node_id]
+                )
                 assert (
                     r.status_code == http.HTTPStatus.OK
                 ), f"tx request returned HTTP status {r.status_code}"
