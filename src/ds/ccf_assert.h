b9b28eb40af2e494a0b2b8ae6b54ce74591ab3ac
Eddy Ashton <edashton@microsoft.com>
2020-09-18 17:28:58 +0100

    Flatten/combine definitions

--- src/ds/ccf_assert.h
+++ src/ds/ccf_assert.h
@@ -12,35 +12,17 @@
 #define CCF_ASSERT_FMT(expr, ...) \
   CCF_ASSERT(expr, fmt::format(__VA_ARGS__).c_str())
 
-#ifndef INSIDE_ENCLAVE
-#  ifndef NDEBUG
-#    define CCF_ASSERT(expr, msg) \
-      do \
+#ifndef NDEBUG
+#  define CCF_ASSERT(expr, msg) \
+    do \
+    { \
+      if ((expr) == 0) \
       { \
-        if ((expr) == 0) \
-        { \
-          LOG_FAIL_FMT("Assertion failed: {} {}", #expr, (msg)); \
-          stacktrace::print_stacktrace(); \
-          throw std::logic_error(msg); \
-        } \
-      } while (0)
-#  else
-#    define CCF_ASSERT(expr, msg) ((void)0)
-#  endif /* NDEBUG */
-
+        LOG_FAIL_FMT("Assertion failed: {} {}", #expr, (msg)); \
+        stacktrace::print_stacktrace(); \
+        throw std::logic_error(msg); \
+      } \
+    } while (0)
 #else
-#  ifndef NDEBUG
-#    define CCF_ASSERT(expr, msg) \
-      do \
-      { \
-        if ((expr) == 0) \
-        { \
-          LOG_FAIL_FMT("Assertion failed: {} {}", #expr, (msg)); \
-          throw std::logic_error(msg); \
-        } \
-      } while (0)
-#  else
-#    define CCF_ASSERT(expr, msg) ((void)0)
-#  endif /* NDEBUG */
-
-#endif
+#  define CCF_ASSERT(expr, msg) ((void)0)
+#endif /* NDEBUG */
