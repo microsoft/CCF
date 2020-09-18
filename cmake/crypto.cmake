b74f8d7c926de54f85d196bec98c7bfdc8750cdb
Christoph M. Wintersteiger <cwinter@microsoft.com>
2020-09-18 17:19:00 +0000

    Formatting

--- cmake/crypto.cmake
+++ cmake/crypto.cmake
@@ -17,9 +17,8 @@ file(GLOB_RECURSE EVERCRYPT_SRC "${EVERCRYPT_PREFIX}/*.[cS]")
 # We need two versions of EverCrypt, because it depends on libc
 
 if("sgx" IN_LIST COMPILE_TARGETS)
-  add_library(evercrypt.enclave STATIC
-    ${EVERCRYPT_SRC}
-    ${CCF_DIR}/3rdparty/stub/stub.c
+  add_library(
+    evercrypt.enclave STATIC ${EVERCRYPT_SRC} ${CCF_DIR}/3rdparty/stub/stub.c
   )
   target_compile_options(
     evercrypt.enclave PRIVATE -Wno-implicit-function-declaration
