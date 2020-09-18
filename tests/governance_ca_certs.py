2cb74ecc96b64428c8962de10a65fce4ff5759b0
Maik Riechert <maik.riechert@microsoft.com>
2020-09-18 10:53:09 +0000

    formatting

--- tests/governance_ca_certs.py
+++ tests/governance_ca_certs.py
@@ -70,7 +70,8 @@ def test_cert_store(network, args):
             # Note that /gov/read returns all data as JSON.
             # Here, the stored data is a uint8 array, therefore it
             # is returned as an array of integers.
-            bytes(r.body.json()), crypto_backends.default_backend()
+            bytes(r.body.json()),
+            crypto_backends.default_backend(),
         )
         assert (
             cert_ref == cert_kv
