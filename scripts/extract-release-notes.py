27a307237013d8bbc756584b3ccb8e90d552f5f1
Maik Riechert <maik.riechert@microsoft.com>
2020-09-21 14:25:30 +0000

    rewrite release note extraction script to be less insane

--- scripts/extract-release-notes.py
+++ scripts/extract-release-notes.py
@@ -2,23 +2,38 @@
 # Licensed under the Apache 2.0 License.
 
 import argparse
+import re
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("out_path")
 args = parser.parse_args()
 
+git_version = subprocess.run(['git', 'describe', '--tags'],
+    capture_output=True, universal_newlines=True).stdout.strip()
+git_version = git_version.replace("ccf-", "")
+
+version_header = re.compile(r'## \[(.+)\]')
+
+release_notes = ''
+found_version = False
 with open("CHANGELOG.md") as f:
-    changelog = f.read()
+    while line := f.readline():
+        if match := version_header.match(line):
+            if found_version:
+                # Next version starts, we're done.
+                break
+            log_version = match.group(1)
+            if log_version == git_version:
+                found_version = True
+        else:
+            if found_version:
+                release_notes += line
+if not found_version:
+    raise RuntimeError(f"git version {git_version} not found in CHANGELOG.md")
 
-marker = "\n## "
-
-idx_unreleased = changelog.find(marker)
-idx_newest = changelog.find(marker, idx_unreleased + len(marker))
-idx_newest_2nd_line = changelog.find("\n", idx_newest + len(marker))
-idx_end = changelog.find(marker, idx_newest + len(marker))
-
-rel_notes = changelog[idx_newest_2nd_line:idx_end].strip()
-print(rel_notes)
+release_notes = release_notes.strip()
+print(release_notes)
 
 with open(args.out_path, "w") as f:
-    f.write(rel_notes)
+    f.write(release_notes)
