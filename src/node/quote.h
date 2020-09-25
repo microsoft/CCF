f53cd6ddd1daa6be61444df076aabdad01db6b3b
Julien Maffre <jumaffre@microsoft.com>
2020-09-25 17:13:02 +0100

    WIP

--- src/node/quote.h
+++ src/node/quote.h
@@ -10,7 +10,7 @@
 #  include "network_tables.h"
 
 #  include <openenclave/attestation/attester.h>
-// #  include <openenclave/attestation/evidence.h>
+#  include <openenclave/attestation/sgx/evidence.h>
 // #  include <openenclave/bits/report.h>
 // #  include <openenclave/bits/result.h>
 #  include <optional>
@@ -34,20 +34,7 @@ namespace ccf
   class QuoteGenerator
   {
   private:
-    // For now, this is re-defined here as Open Enclave 0.10.0 does not expose
-    // this value. From Open Enclave > 0.10.0, this can go.
-    /**
-     * The SGX plugin UUID.
-     */
-#  define OE_FORMAT_UUID_SGX_ECDSA_P256 \
-    { \
-      0xa3, 0xa2, 0x1e, 0x87, 0x1b, 0x4d, 0x40, 0x14, 0xb7, 0x0a, 0xa1, 0x25, \
-        0xd2, 0xfb, 0xcd, 0x8c \
-    }
-
-    static constexpr oe_uuid_t ecdsa_uuid = {OE_FORMAT_UUID_SGX_ECDSA_P256};
-    // static const oe_uuid_t ecdsa_quote_uuid = {
-    //   OE_FORMAT_UUID_SGX_ECDSA_P256_QUOTE};
+    static constexpr oe_uuid_t sgx_remote_uuid = {OE_FORMAT_UUID_SGX_ECDSA};
 
   public:
     static std::optional<CodeDigest> get_code_id(
@@ -71,51 +58,47 @@ namespace ccf
       std::vector<uint8_t> raw_quote;
       crypto::Sha256Hash h{cert.raw()};
 
-      auto rc = oe_attester_initialize();
-      if (rc != OE_OK)
-      {
-        LOG_FAIL_FMT(
-          "Failed to initialise attester format: {}", oe_result_str(rc));
-        return std::nullopt;
-      }
+      // auto rc = oe_attester_initialize();
+      // if (rc != OE_OK)
+      // {
+      //   LOG_FAIL_FMT(
+      //     "Failed to initialise attester format: {}", oe_result_str(rc));
+      //   return std::nullopt;
+      // }
 
-      uint8_t* evidence = NULL;
-      size_t evidence_size = 0;
-      uint8_t* endorsements = NULL;
-      size_t endorsements_size = 0;
+      // uint8_t* evidence = NULL;
+      // size_t evidence_size = 0;
+      // uint8_t* endorsements = NULL;
+      // size_t endorsements_size = 0;
 
-      LOG_FAIL_FMT("Get quote");
-      std::cout << "Get quote" << std::endl;
+      // LOG_FAIL_FMT("Get quote");
 
-      oe_uuid_t selected_format;
-      rc = oe_attester_select_format(&ecdsa_uuid, 1, &selected_format);
-      if (rc != OE_OK)
-      {
-        LOG_FAIL_FMT("Failed to select attester format: {}", oe_result_str(rc));
-        return std::nullopt;
-      }
+      // // TODO: Add custom claims!!
 
-      rc = oe_get_evidence(
-        &selected_format,
-        NULL,
-        0,
-        NULL,
-        0,
-        &evidence,
-        &evidence_size,
-        &endorsements,
-        &endorsements_size);
-      if (rc != OE_OK)
-      {
-        LOG_FAIL_FMT("Failed to get evidence: {}", oe_result_str(rc));
-        oe_free_evidence(evidence);
-        oe_free_endorsements(endorsements);
-        return std::nullopt;
-      }
+      // rc = oe_get_evidence(
+      //   &sgx_remote_uuid,
+      //   0,
+      //   nullptr,
+      //   0,
+      //   nullptr,
+      //   0,
+      //   &evidence,
+      //   &evidence_size,
+      //   &endorsements,
+      //   &endorsements_size);
+      // if (rc != OE_OK)
+      // {
+      //   LOG_FAIL_FMT("Failed to get evidence: {}", oe_result_str(rc));
 
-      raw_quote.assign(evidence, evidence + evidence_size);
-      oe_free_report(evidence);
-      oe_free_endorsements(endorsements);
+      //   // TODO: Do we actually need to free this???
+      //   oe_free_evidence(evidence);
+      //   oe_free_endorsements(endorsements);
+      //   return std::nullopt;
+      // }
+
+      // raw_quote.assign(evidence, evidence + evidence_size);
+      // oe_free_report(evidence);
+      // oe_free_endorsements(endorsements);
 
       return raw_quote;
     }
--- tests/code_update.py
+++ tests/code_update.py
@@ -4,9 +4,13 @@ import infra.e2e_args
 import infra.network
 import infra.path
 import infra.proc
+import suite.test_requirements as reqs
+import ccf.clients
+
 import os
 import subprocess
 import sys
+
 from loguru import logger as LOG
 
 
@@ -23,127 +27,137 @@ def get_code_id(lib_path):
     return lines[0].split("=")[1]
 
 
+@reqs.description("Verify nodes quote")
+def test_verify_quotes(network, args):
+    primary, _ = network.find_primary()
+
+    with primary.client() as c:
+        r = c.get("/node/quotes")
+        # LOG.error(r.body.json())
+        for quote in r.body.json()["quotes"]:
+            LOG.success(quote["node_id"])
+
+
+@reqs.description("Update all nodes with new code version")
+def test_update_all_nodes(network, args):
+    first_code_id = get_code_id(
+        infra.path.build_lib_path(args.package, args.enclave_type)
+    )
+
+    primary, _ = network.find_nodes()
+    with primary.client() as uc:
+        r = uc.get("/node/code")
+        assert r.body.json() == {
+            "versions": [{"digest": first_code_id, "status": "ACCEPTED"}],
+        }, r.body
+
+    LOG.info("Adding a new node")
+    new_node = network.create_and_trust_node(args.package, "localhost", args)
+    assert new_node
+
+    new_code_id = get_code_id(
+        infra.path.build_lib_path(args.patched_file_name, args.enclave_type)
+    )
+
+    LOG.info(f"Adding a node with unsupported code id {new_code_id}")
+    code_not_found_exception = None
+    try:
+        network.create_and_add_pending_node(
+            args.patched_file_name, "localhost", args, timeout=3
+        )
+    except infra.network.CodeIdNotFound as err:
+        code_not_found_exception = err
+
+    assert (
+        code_not_found_exception is not None
+    ), f"Adding a node with unsupported code id {new_code_id} should fail"
+
+    # Slow quote verification means that any attempt to add a node may cause an election, so confirm primary after adding node
+    primary, _ = network.find_primary()
+
+    network.consortium.add_new_code(primary, new_code_id)
+
+    with primary.client() as uc:
+        r = uc.get("/node/code")
+        versions = sorted(r.body.json()["versions"], key=lambda x: x["digest"])
+        expected = sorted(
+            [
+                {"digest": first_code_id, "status": "ACCEPTED"},
+                {"digest": new_code_id, "status": "ACCEPTED"},
+            ],
+            key=lambda x: x["digest"],
+        )
+        assert versions == expected, versions
+
+    new_nodes = set()
+    old_nodes_count = len(network.nodes)
+    new_nodes_count = old_nodes_count + 1
+
+    LOG.info(
+        f"Adding more new nodes ({new_nodes_count}) than originally existed ({old_nodes_count})"
+    )
+    for _ in range(0, new_nodes_count):
+        new_node = network.create_and_trust_node(
+            args.patched_file_name, "localhost", args
+        )
+        assert new_node
+        new_nodes.add(new_node)
+
+    LOG.info("Stopping all original nodes")
+    old_nodes = set(network.nodes).difference(new_nodes)
+    for node in old_nodes:
+        LOG.debug(f"Stopping old node {node.node_id}")
+        node.stop()
+
+    new_primary, _ = network.wait_for_new_primary(primary.node_id)
+    LOG.info(f"New_primary is {new_primary.node_id}")
+
+    LOG.info("Adding another node to the network")
+    new_node = network.create_and_trust_node(args.patched_file_name, "localhost", args)
+    assert new_node
+    network.wait_for_node_commit_sync(args.consensus)
+
+    LOG.info("Remove first code id")
+    network.consortium.retire_code(new_node, first_code_id)
+
+    with new_node.client() as uc:
+        r = uc.get("/node/code")
+        versions = sorted(r.body.json()["versions"], key=lambda x: x["digest"])
+        expected = sorted(
+            [
+                {"digest": first_code_id, "status": "RETIRED"},
+                {"digest": new_code_id, "status": "ACCEPTED"},
+            ],
+            key=lambda x: x["digest"],
+        )
+        assert versions == expected, versions
+
+    LOG.info(f"Adding a node with retired code id {first_code_id}")
+    code_not_found_exception = None
+    try:
+        network.create_and_add_pending_node(args.package, "localhost", args, timeout=3)
+    except infra.network.CodeIdRetired as err:
+        code_not_found_exception = err
+
+    assert (
+        code_not_found_exception is not None
+    ), f"Adding a node with unsupported code id {new_code_id} should fail"
+
+    LOG.info("Adding another node with the new code to the network")
+    new_node = network.create_and_trust_node(args.patched_file_name, "localhost", args)
+    assert new_node
+    network.wait_for_node_commit_sync(args.consensus)
+
+
 def run(args):
-    hosts = ["localhost", "localhost"]
+    hosts = ["localhost"]
 
     with infra.network.network(
         hosts, args.binary_dir, args.debug_nodes, args.perf_nodes, pdb=args.pdb
     ) as network:
         network.start_and_join(args)
-        primary, _ = network.find_nodes()
-
-        first_code_id = get_code_id(
-            infra.path.build_lib_path(args.package, args.enclave_type)
-        )
-
-        with primary.client() as uc:
-            r = uc.get("/node/code")
-            assert r.body.json() == {
-                "versions": [{"digest": first_code_id, "status": "ACCEPTED"}],
-            }, r.body
-
-        LOG.info("Adding a new node")
-        new_node = network.create_and_trust_node(args.package, "localhost", args)
-        assert new_node
-
-        new_code_id = get_code_id(
-            infra.path.build_lib_path(args.patched_file_name, args.enclave_type)
-        )
-
-        LOG.info(f"Adding a node with unsupported code id {new_code_id}")
-        code_not_found_exception = None
-        try:
-            network.create_and_add_pending_node(
-                args.patched_file_name, "localhost", args, timeout=3
-            )
-        except infra.network.CodeIdNotFound as err:
-            code_not_found_exception = err
-
-        assert (
-            code_not_found_exception is not None
-        ), f"Adding a node with unsupported code id {new_code_id} should fail"
-
-        # Slow quote verification means that any attempt to add a node may cause an election, so confirm primary after adding node
-        primary, _ = network.find_primary()
-
-        network.consortium.add_new_code(primary, new_code_id)
-
-        with primary.client() as uc:
-            r = uc.get("/node/code")
-            versions = sorted(r.body.json()["versions"], key=lambda x: x["digest"])
-            expected = sorted(
-                [
-                    {"digest": first_code_id, "status": "ACCEPTED"},
-                    {"digest": new_code_id, "status": "ACCEPTED"},
-                ],
-                key=lambda x: x["digest"],
-            )
-            assert versions == expected, versions
-
-        new_nodes = set()
-        old_nodes_count = len(network.nodes)
-        new_nodes_count = old_nodes_count + 1
-
-        LOG.info(
-            f"Adding more new nodes ({new_nodes_count}) than originally existed ({old_nodes_count})"
-        )
-        for _ in range(0, new_nodes_count):
-            new_node = network.create_and_trust_node(
-                args.patched_file_name, "localhost", args
-            )
-            assert new_node
-            new_nodes.add(new_node)
-
-        LOG.info("Stopping all original nodes")
-        old_nodes = set(network.nodes).difference(new_nodes)
-        for node in old_nodes:
-            LOG.debug(f"Stopping old node {node.node_id}")
-            node.stop()
-
-        new_primary, _ = network.wait_for_new_primary(primary.node_id)
-        LOG.info(f"New_primary is {new_primary.node_id}")
-
-        LOG.info("Adding another node to the network")
-        new_node = network.create_and_trust_node(
-            args.patched_file_name, "localhost", args
-        )
-        assert new_node
-        network.wait_for_node_commit_sync(args.consensus)
-
-        LOG.info("Remove first code id")
-        network.consortium.retire_code(new_node, first_code_id)
-
-        with new_node.client() as uc:
-            r = uc.get("/node/code")
-            versions = sorted(r.body.json()["versions"], key=lambda x: x["digest"])
-            expected = sorted(
-                [
-                    {"digest": first_code_id, "status": "RETIRED"},
-                    {"digest": new_code_id, "status": "ACCEPTED"},
-                ],
-                key=lambda x: x["digest"],
-            )
-            assert versions == expected, versions
-
-        LOG.info(f"Adding a node with retired code id {first_code_id}")
-        code_not_found_exception = None
-        try:
-            network.create_and_add_pending_node(
-                args.package, "localhost", args, timeout=3
-            )
-        except infra.network.CodeIdRetired as err:
-            code_not_found_exception = err
-
-        assert (
-            code_not_found_exception is not None
-        ), f"Adding a node with unsupported code id {new_code_id} should fail"
-
-        LOG.info("Adding another node with the new code to the network")
-        new_node = network.create_and_trust_node(
-            args.patched_file_name, "localhost", args
-        )
-        assert new_node
-        network.wait_for_node_commit_sync(args.consensus)
+        test_verify_quotes(network, args)
+        # test_update_all_nodes(network, args)
 
 
 if __name__ == "__main__":
@@ -157,6 +171,9 @@ if __name__ == "__main__":
         )
         parser.add_argument(
             "--oesign", help="Path to oesign binary", type=str, required=True
+        )
+        parser.add_argument(
+            "--hostverify", help="Path to hostverify binary", type=str, required=True
         )
 
     args = infra.e2e_args.cli_args(add)
