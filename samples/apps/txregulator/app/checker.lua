e5b5d2321f77190f879ef64626b284c6826838ba
Amaury Chamayou <amchamay@microsoft.com>
2020-10-05 21:13:53 +0100

    Remove tx regulator demo

--- samples/apps/txregulator/app/checker.lua
+++
@@ -1,8 +0,0 @@
--- Copyright (c) Microsoft Corporation. All rights reserved.
--- Licensed under the Apache 2.0 License.
-
-if (tx_type == "TRANSFER" or tx_type == "CASH_OUT") and tonumber(amt) > 200000 then 
-	return true 
-else 
-	return false 
-end
