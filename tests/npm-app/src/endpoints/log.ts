15fab153068ffbebd2a1ccb791541cbe681564c7
Maik Riechert <maik.riechert@microsoft.com>
2020-10-06 15:43:29 +0000

    log json object

--- tests/npm-app/src/endpoints/log.ts
+++ tests/npm-app/src/endpoints/log.ts
@@ -4,20 +4,17 @@ interface LogItem {
     msg: string
 }
 
-const logMap = new ccf.TypedKVMap(ccf.kv.log, ccf.uint32, ccf.string);
+const logMap = new ccf.TypedKVMap(ccf.kv.log, ccf.uint32, ccf.json<LogItem>());
 
 export function getLogItem(request: ccf.Request): ccf.Response<LogItem> {
     const id = parseInt(request.query.split('=')[1])
     return {
-        body: {
-            msg: logMap.get(id)
-        }
+        body: logMap.get(id)
     }
 }
 
 export function setLogItem(request: ccf.Request<LogItem>): ccf.Response {
     const id = parseInt(request.query.split('=')[1])
-    const body = request.body.json();
-    logMap.set(id, body.msg);
+    logMap.set(id, request.body.json());
     return {};
 }
