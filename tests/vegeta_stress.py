c9d02c1ed7579017a1483a781411f5a55baa1279
Eddy Ashton <edashton@microsoft.com>
2020-10-14 16:52:41 +0100

    Parity with the bash script

--- tests/vegeta_stress.py
+++ tests/vegeta_stress.py
@@ -7,12 +7,31 @@ import json
 import base64
 from loguru import logger as LOG
 
+VEGETA_BIN = "/opt/vegeta/vegeta"
 
-def run(args):
+
+def build_vegeta_target(node, path, body=None, method="POST"):
+    target = {}
+    target["method"] = method
+    target["url"] = f"https://{node.pubhost}:{node.rpc_port}{path}"
+    target["header"] = {"Content-Type": ["application/json"]}
+    if body is not None:
+        # Bodies must be base64 encoded strings
+        target["body"] = base64.b64encode(json.dumps(body, indent=2).encode()).decode()
+    return target
+
+
+def write_vegeta_target_line(f, *args, **kwargs):
+    target = build_vegeta_target(*args, **kwargs)
+    f.write(json.dumps(target))
+    f.write("\n")
+
+
+def run(args, additional_attack_args):
     hosts = ["localhost", "localhost", "localhost"]
 
     # Test that vegeta is available
-    subprocess.run(["vegeta", "-version"], capture_output=True)
+    subprocess.run([VEGETA_BIN, "-version"], capture_output=True)
 
     with infra.network.network(
         hosts,
@@ -27,25 +46,33 @@ def run(args):
 
         vegeta_targets = "vegeta_targets"
         with open(vegeta_targets, "w") as f:
-            for i in range(5):
-                target = {}
-                target["method"] = "POST"
-                target[
-                    "url"
-                ] = f"https://{primary.pubhost}:{primary.rpc_port}/app/log/public"
-                target["header"] = {"Content-Type": ["application/json"]}
-                body = {}
-                body["id"] = i
-                body["msg"] = "Some logged message body"
-                # Bodies must be base64 encoded strings
-                target["body"] = base64.b64encode(
-                    json.dumps(body, indent=2).encode()
-                ).decode()
+            for i in range(10):
+                write_vegeta_target_line(
+                    f,
+                    primary,
+                    "/app/log/private",
+                    body={"id": i, "msg": f"Private message: {i}"},
+                )
 
-                json.dump(target, f)
-                f.write("\n")
+            for i in range(10):
+                write_vegeta_target_line(
+                    f, primary, f"/app/log/private?id={i}", method="GET"
+                )
 
-        attack_cmd = ["vegeta", "attack"]
+            for i in range(10):
+                write_vegeta_target_line(
+                    f,
+                    primary,
+                    "/app/log/public",
+                    body={"id": i, "msg": f"Public message: {i}"},
+                )
+
+            for i in range(10):
+                write_vegeta_target_line(
+                    f, primary, f"/app/log/public?id={i}", method="GET"
+                )
+
+        attack_cmd = [VEGETA_BIN, "attack"]
         attack_cmd += ["--targets", vegeta_targets]
         attack_cmd += ["--format", "json"]
         attack_cmd += ["--duration", "10s"]
@@ -53,16 +80,23 @@ def run(args):
         attack_cmd += ["--cert", certs["cert"]]
         attack_cmd += ["--key", certs["key"]]
         attack_cmd += ["--root-certs", certs["ca"]]
+        attack_cmd += additional_attack_args
 
-        attack_cmd_s = ' '.join(attack_cmd)
+        attack_cmd_s = " ".join(attack_cmd)
         LOG.warning(f"Starting: {attack_cmd_s}")
         vegeta_run = subprocess.Popen(attack_cmd, stdout=subprocess.PIPE)
 
-        report_cmd = ["vegeta", "report"]
-        report_cmd_s = ' '.join(report_cmd)
+        tee_split = subprocess.Popen(
+            ["tee", "vegeta_results.bin"],
+            stdin=vegeta_run.stdout,
+            stdout=subprocess.PIPE,
+        )
+
+        report_cmd = [VEGETA_BIN, "report"]
+        report_cmd_s = " ".join(report_cmd)
         vegeta_report = subprocess.Popen(
             report_cmd,
-            stdin=vegeta_run.stdout,
+            stdin=tee_split.stdout,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
@@ -73,11 +107,10 @@ def run(args):
         LOG.info(f"Report output:\n{report_out.decode()}")
         if report_err:
             LOG.error(f"Error running '{' '.join(attack_cmd)}':\n{report_err.decode()}")
-        LOG.info(f"Err was:\n{report_err}")
 
 
 if __name__ == "__main__":
 
-    args = infra.e2e_args.cli_args()
+    args, unknown_args = infra.e2e_args.cli_args(accept_unknown=True)
     args.package = args.app_script and "liblua_generic" or "liblogging"
-    run(args)
+    run(args, unknown_args)
