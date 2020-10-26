cbeb5cf419ecc67ec47414024522aface395f651
Maik Riechert <maik.riechert@microsoft.com>
2020-10-26 15:29:25 +0000

    better error handling

--- samples/apps/forum/src/error_handler.ts
+++ samples/apps/forum/src/error_handler.ts
@@ -11,14 +11,48 @@ import * as ccf from './types/ccf'
 // See https://tsoa-community.github.io/docs/error-handling.html#setting-up-error-handling
 // The code that imports and calls this handler is in tsoa-support/routes.ts.tmpl.
 
-export interface ValidateErrorResponse {
+export interface ErrorResponse {
+    message: string
+}
+
+export interface ValidateErrorResponse extends ErrorResponse {
     message: "Validation failed"
     details: FieldErrors
 }
 
 export const ValidateErrorStatus = 422
 
-export function errorHandler(err: unknown, req: ccf.Request): ccf.Response {
+class HttpError extends Error {
+    constructor(public statusCode: number, message: string) {
+        super(message)
+    }
+}
+
+export class BadRequestError extends HttpError {
+    static Status = 400
+
+    constructor(message: string) {
+        super(BadRequestError.Status, message)
+    }
+}
+
+export class ForbiddenError extends HttpError {
+    static Status = 403
+
+    constructor(message: string) {
+        super(ForbiddenError.Status, message)
+    }
+}
+
+export class NotFoundError extends HttpError {
+    static Status = 404
+
+    constructor(message: string) {
+        super(NotFoundError.Status, message)
+    }
+}
+
+export function errorHandler(err: unknown, req: ccf.Request): ccf.Response<ErrorResponse | ValidateErrorResponse> {
     if (err instanceof ValidateError) {
         return {
             body: {
@@ -26,6 +60,13 @@ export function errorHandler(err: unknown, req: ccf.Request): ccf.Response {
                 details: err.fields
             },
             statusCode: ValidateErrorStatus
+        }
+    } else if (err instanceof HttpError) {
+        return {
+            body: {
+                message: err.message
+            },
+            statusCode: err.statusCode
         }
     }
     // Let CCF turn all other errors into 500.
