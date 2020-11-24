80303f05f88f7abecddd2ecc644587115e297c32
Amaury Chamayou <amchamay@microsoft.com>
2020-11-24 16:13:36 +0000

    wip

---
+++ src/node/hooks.h
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache 2.0 License.
+#pragma once
+
+#include "ds/logger.h"
+#include "kv/change_set.h"
+
+namespace ccf
+{
+  class ConfigurationChangeHook : public kv::ConsensusHook
+  {
+  public:
+    void operator()(const kv::Consensus&) override
+    {
+      LOG_INFO_FMT("CONSENSUS HOOK");
+    }
+  };
+}
