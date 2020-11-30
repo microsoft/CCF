b788fd1751ba63932240cf0ab8d51f56bca70b20
Amaury Chamayou <amchamay@microsoft.com>
2020-11-30 09:13:17 +0000

    fix

--- tests/test_install_build.sh
+++ tests/test_install_build.sh
@@ -5,5 +5,5 @@ set -ex
 
 mkdir build_against_install
 cd build_against_install
-CC=`which clang-8` CXX=`which clang++-8` cmake -GNinja ../samples/apps/logging/
+CC=$(which clang-8) CXX=$(which clang++-8) cmake -GNinja ../samples/apps/logging/
 ninja
