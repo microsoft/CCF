faeebefcc4a19c22ed4add0f987e544a6ddeb140
Julien Maffre <jumaffre@microsoft.com>
2020-12-06 22:13:52 +0000

    Recovery default

--- tests/recovery.py
+++ tests/recovery.py
@@ -10,7 +10,7 @@ from loguru import logger as LOG
 
 
 @reqs.description("Recovering a network")
-@reqs.recover(number_txs=5)
+@reqs.recover(number_txs=2)
 def test(network, args, from_snapshot=False):
     old_primary, _ = network.find_primary()
 
