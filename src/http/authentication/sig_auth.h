75c8d1269d29e8220eb31788865bd1fd278bd330
Eddy Ashton <edashton@microsoft.com>
2021-01-06 14:48:27 +0000

    Add descriptions + comments

--- src/http/authentication/sig_auth.h
+++ src/http/authentication/sig_auth.h
@@ -116,7 +116,10 @@ namespace ccf
   inline const OpenAPISecuritySchema UserSignatureAuthnPolicy::security_schema =
     std::make_pair(
       "user_signature",
-      nlohmann::json{{"type", "http"}, {"scheme", "signature"}});
+      nlohmann::json{{"type", "http"}, {"scheme", "signature"},
+        {"description",
+         "Request must be signed according to the HTTP Signature scheme. The "
+         "signer must be a user identity registered with this service."}});
 
   struct MemberSignatureAuthnIdentity : public AuthnIdentity
   {
@@ -210,5 +213,10 @@ namespace ccf
   inline const OpenAPISecuritySchema
     MemberSignatureAuthnPolicy::security_schema = std::make_pair(
       "member_signature",
-      nlohmann::json{{"type", "http"}, {"scheme", "signature"}});
+      nlohmann::json{
+        {"type", "http"},
+        {"scheme", "signature"},
+        {"description",
+         "Request must be signed according to the HTTP Signature scheme. The "
+         "signer must be a member identity registered with this service."}});
 }
