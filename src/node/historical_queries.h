03cd273ad1b74f73aa02d7bae016659bdd8d4be8
Julien Maffre <jumaffre@microsoft.com>
2021-01-22 15:10:48 +0000

    Re-add hint for historical queries

--- src/node/historical_queries.h
+++ src/node/historical_queries.h
@@ -196,7 +196,7 @@ namespace ccf::historical
     void deserialise_ledger_entry(
       consensus::Index idx, const LedgerEntry& entry)
     {
-      StorePtr store = std::make_shared<kv::Store>(false);
+      StorePtr store = std::make_shared<kv::Store>(true);
 
       store->set_encryptor(source_store.get_encryptor());
       kv::ConsensusHookPtrs hooks;
