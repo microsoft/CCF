0bc3264e596e5dcd70b9de632dc89c89405a9d2f
Julien Maffre <jumaffre@microsoft.com>
2021-01-23 17:23:36 +0000

    WIP ledger verification

--- tests/infra/network.py
+++ tests/infra/network.py
@@ -13,6 +13,7 @@ import infra.consortium
 from ccf.tx_status import TxStatus
 import random
 from math import ceil
+from collections import defaultdict
 import http
 
 from loguru import logger as LOG
@@ -483,14 +484,92 @@ class Network:
     def ignore_errors_on_shutdown(self):
         self.ignoring_shutdown_errors = True
 
+    def compute_file_checksum(self, file_name):
+        import hashlib
+
+        h = hashlib.sha256()
+        with open(file_name, "rb") as f:
+            for b in iter(lambda: f.read(4096), b""):
+                h.update(b)
+        return h.hexdigest()
+
     def stop_all_nodes(self):
+
+        # TODO:
+        # 1. Replace node.stop() with network.stop(node)
+
+        primary, _ = self.find_primary()
+        primary_current_ledger_dir, primary_committed_ledger_dir = primary.get_ledger(
+            include_read_only_dirs=True
+        )
+
+        input("")
+
+        LOG.error(primary_current_ledger_dir)
+        LOG.error(primary_committed_ledger_dir)
+
+        primary_current_ledger_dir = None
+        primary_committed_ledger_dir = None
+        backups_current_ledger_dirs = []
+        backups_committed_ledger_dirs = []
+
         fatal_error_found = False
-        for node in self.nodes:
+        for node in self.nodes:  # TODO: What about nodes that are already stopped??
             _, fatal_errors = node.stop()
+
+            current_ledger, committed_ledger = node.get_ledger(
+                include_read_only_dirs=True
+            )
+            if node == primary:
+                primary_current_ledger_dir = current_ledger
+                primary_committed_ledger_dir = committed_ledger
+                LOG.error("Primary ledger!")
+            else:
+                backups_current_ledger_dirs.append(current_ledger)
+                backups_committed_ledger_dirs.append(committed_ledger)
+                LOG.error("Backup ledger!")
+
+            # Verify that:
+            # 1. Committed ledger files from are the same on another 2f nodes at least!
+            # 2.
+
+            delayed_backups_info = defaultdict(list)
+
+            primary_committed_ledgers = os.listdir(primary_committed_ledger_dir)
+            LOG.warning(primary_committed_ledgers)
+
+            for primary_ledger in sorted(os.listdir(primary_committed_ledger_dir)):
+                LOG.info(f"Checking {primary_ledger}...")
+                primary_ledger_digest = self.compute_file_checksum(
+                    os.path.join(primary_committed_ledger_dir, primary_ledger)
+                )
+                for backup_committed_ledger_dir in backups_committed_ledger_dirs:
+                    # for backup_ledger in os.listdir(backup_committed_ledger_dir):
+                    backup_ledger = os.path.join(
+                        backup_committed_ledger_dir, primary_ledger
+                    )
+                    if not os.path.exists(backup_ledger):
+                        delayed_backups_info[backup_committed_ledger_dir].append(
+                            primary_ledger
+                        )
+                        continue
+
+                    if (
+                        self.compute_file_checksum(backup_ledger)
+                        != primary_ledger_digest
+                    ):
+                        assert False
+
+            for key, value in delayed_backups_info.items():
+                LOG.error(key)
+                LOG.success(value)
+
             if fatal_errors:
                 fatal_error_found = True
 
-        LOG.info("All nodes stopped...")
+        LOG.info("All nodes stopped")
+
+        # TODO: Verify ledger files
 
         if fatal_error_found:
             if self.ignoring_shutdown_errors:
