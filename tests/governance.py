48fde62f6bb1c273f7940554782e7cdfb646b628
Eddy Ashton <edashton@microsoft.com>
2021-02-02 15:15:38 +0000

    Add proposals on service_principals, and test

--- tests/governance.py
+++ tests/governance.py
@@ -187,6 +187,63 @@ def test_node_ids(network, args):
         return network
 
 
+@reqs.description("Checking service principals proposals")
+def test_service_principals(network, args):
+    primary, _ = network.find_nodes()
+
+    principal_id = "0xdeadbeef"
+    ballot = {"ballot": {"text": "return true"}}
+
+    def read_service_principal():
+        with primary.client("member0") as mc:
+            return mc.post(
+                "/gov/read",
+                {"table": "public:ccf.gov.service_principals", "key": principal_id},
+            )
+
+    # Initially, there is nothing in this table
+    r = read_service_principal()
+    assert r.status_code == http.HTTPStatus.NOT_FOUND.value
+
+    # Create and accept a proposal which populates an entry in this table
+    principal_data = {"name": "Bob", "roles": ["Fireman", "Zookeeper"]}
+    proposal = {
+        "script": {
+            "text": 'tables, args = ...\nreturn Calls:call("set_service_principal", args)'
+        },
+        "parameter": {
+            "id": principal_id,
+            "data": principal_data,
+        },
+    }
+    proposal = network.consortium.get_any_active_member().propose(primary, proposal)
+    network.consortium.vote_using_majority(primary, proposal, ballot)
+
+    # Confirm it can be read
+    r = read_service_principal()
+    assert r.status_code == http.HTTPStatus.OK.value
+    j = r.body.json()
+    assert j == principal_data
+
+    # Create and accept a proposal which removes an entry from this table
+    proposal = {
+        "script": {
+            "text": 'tables, args = ...\nreturn Calls:call("remove_service_principal", args)'
+        },
+        "parameter": {
+            "id": principal_id,
+        },
+    }
+    proposal = network.consortium.get_any_active_member().propose(primary, proposal)
+    network.consortium.vote_using_majority(primary, proposal, ballot)
+
+    # Confirm it is gone
+    r = read_service_principal()
+    assert r.status_code == http.HTTPStatus.NOT_FOUND.value
+
+    return network
+
+
 def run(args):
     with infra.network.network(
         args.nodes, args.binary_dir, args.debug_nodes, args.perf_nodes, pdb=args.pdb
@@ -198,6 +255,7 @@ def run(args):
         network = test_user(network, args)
         network = test_no_quote(network, args)
         network = test_user_id(network, args)
+        network = test_service_principals(network, args)
 
 
 if __name__ == "__main__":
