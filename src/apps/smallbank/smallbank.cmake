a60d82ee439b2b8015cce7d9bdf8194045e2d9af
Amaury Chamayou <amchamay@microsoft.com>
2021-02-05 10:59:39 +0000

    .

--- src/apps/smallbank/smallbank.cmake
+++ src/apps/smallbank/smallbank.cmake
@@ -55,7 +55,7 @@ if(BUILD_TESTS)
 
   foreach(CONSENSUS ${CONSENSUSES})
     add_perf_test(
-      NAME small_bank_client_test_${CONSENSUS}
+      NAME sb
       PYTHON_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/tests/small_bank_client.py
       CLIENT_BIN ./small_bank_client
       VERIFICATION_FILE ${SMALL_BANK_VERIFICATION_FILE}
@@ -67,7 +67,7 @@ if(BUILD_TESTS)
   endforeach()
 
   add_perf_test(
-    NAME small_bank_client_ws_test_cft
+    NAME sb_ws
     PYTHON_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/tests/small_bank_client.py
     CLIENT_BIN ./small_bank_client
     VERIFICATION_FILE ${SMALL_BANK_VERIFICATION_FILE}
@@ -83,11 +83,10 @@ if(BUILD_TESTS)
   )
 
   add_perf_test(
-    NAME small_bank_sigs_client_test_cft
+    NAME sb_sig
     PYTHON_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/tests/small_bank_client.py
     CLIENT_BIN ./small_bank_client
     VERIFICATION_FILE ${SMALL_BANK_SIGNED_VERIFICATION_FILE}
-    LABEL "SB_sig"
     CONSENSUS cft
     ADDITIONAL_ARGS
       --transactions
