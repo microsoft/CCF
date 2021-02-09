45baae61b4d4dcbf98756c30a94764d0cbb9636c
Julien Maffre <jumaffre@microsoft.com>
2021-02-09 16:07:10 +0000

    Remove unsued static member

--- src/node/history.h
+++ src/node/history.h
@@ -470,9 +470,6 @@ namespace ccf
     size_t sig_tx_interval;
     size_t sig_ms_interval;
 
-    // Prevents multiple history instances to emit signatures
-    static bool has_signature_timer;
-
     SpinLock term_lock;
     kv::Term term = 0;
 
@@ -492,13 +489,7 @@ namespace ccf
     {
       if (signature_timer)
       {
-        // if (has_signature_timer)
-        {
-          // throw std::logic_error("Only one history can emit signatures");
-        }
-
         start_signature_emit_timer();
-        // has_signature_timer = true;
       }
     }
 
