acc4ad227777431e111d164d305b4f60bb35efad
Amaury Chamayou <amchamay@microsoft.com>
2021-02-10 09:29:24 +0000

    Consolidate some end to end testing envs (#2179)

--- tests/memberclient.py
+++ tests/memberclient.py
@@ -240,5 +240,5 @@ def run(args):
 if __name__ == "__main__":
     args = infra.e2e_args.cli_args()
     args.package = "liblogging"
-    args.nodes = infra.e2e_args.min_nodes(args, f=1)
+    args.nodes = infra.e2e_args.min_nodes(args, f=0)
     run(args)
