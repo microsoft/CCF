58350caf1c0fe3aae69d7917d48c6f3a6bf0c831
Syed Wali Hamza <syhamza@microsoft.com>
2021-02-12 15:13:58 -0800

    Update ledger_tutorial.py

--- python/ledger_tutorial.py
+++ python/ledger_tutorial.py
@@ -3,6 +3,7 @@
 
 import sys
 from loguru import logger as LOG
+import ccf.ledger
 
 # Note: It is safer to run the ledger tutorial when the service has stopped
 # as all ledger files will have been written to.
@@ -21,7 +22,6 @@ if len(sys.argv) < 2:
 ledger_dir = sys.argv[1]
 
 # SNIPPET: import_ledger
-import ccf.ledger
 
 # SNIPPET: create_ledger
 ledger = ccf.ledger.Ledger(ledger_dir)
@@ -41,4 +41,14 @@ for chunk in ledger:
         if target_table in public_tables:
             for key, value in public_tables[target_table].items():
                 target_table_changes += 1  # A key was changed
-# SNIPPET_END: iterate_over_ledger
+
+        # extracting transactions on a sample table
+        # ledger.ledger() does ledger verification implicitly
+        if "public:sample.logs" in public_tables:
+            public_tpal_table = public_tables["public:sample.logs"]
+            for key_value in public_tpal_table.items():
+                # knowledge of the serialization scheme is important to read the values from the table.
+                # if the table was serialized using msgpack, following code can be used to extract transaction key and value.
+                print(
+                    f"{ccf.ledger.extract_msgpacked_data(key_value[0])} = {ccf.ledger.extract_msgpacked_data(key_value[1]).decode()}")
+# snippet_end: iterate_over_ledger
