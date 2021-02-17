1a10f317e44b672f4551432bfccbbd8c9d2553cf
Amaury Chamayou <amchamay@microsoft.com>
2021-02-17 17:09:52 +0000

    .

--- src/runtime_config/constitution.js
+++ src/runtime_config/constitution.js
@@ -1,3 +1,26 @@
-export function validate(proposal) {
-  return { valid: true, description: "N/A" };
+const actions = new Map([
+  [
+    "set_recovery_threshold",
+    function (args) {
+      return Integer.isInteger(args.threshold) && args.threshold > 0 && args.threshold < 255 
+    }
+  ]
+])
+
+export function validate(input) {
+  let proposal = JSON.parse(input)
+  let errors = []
+  for (const action of actions)
+  {
+    const validator = actions.get(action.name);
+    if (validator && !validator(action.args))
+    {
+      errors.push(action.name + " failed validation")
+    }
+    else
+    {
+      errors.push(action.name + ": no such action")
+    }
+  }
+  return { valid: errors, description: errors.join("\n") }
 }
