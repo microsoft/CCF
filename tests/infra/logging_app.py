4bd26bb8ad78beee9e42716e0942c67e45e203b9
Julien Maffre <jumaffre@microsoft.com>
2021-02-17 17:02:27 +0000

    Check that the transaction is still marked as committed

--- tests/infra/logging_app.py
+++ tests/infra/logging_app.py
@@ -5,7 +5,9 @@ import infra.checker
 import time
 import http
 import ccf.clients
+import ccf.commit
 from collections import defaultdict
+
 
 from loguru import logger as LOG
 
@@ -142,6 +144,8 @@ class LoggingTxs:
         start_time = time.time()
         while time.time() < (start_time + timeout):
             with node.client(self.user) as c:
+                ccf.commit.wait_for_commit(c, seqno, view, timeout)
+
                 rep = c.get(f"{cmd}?id={idx}", headers=headers)
                 if rep.status_code == http.HTTPStatus.OK:
                     expected_result = {"msg": msg}
