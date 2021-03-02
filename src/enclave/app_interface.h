0af76539015dda731ac2743c5cbfe5cd8f64eab8
Amaury Chamayou <amchamay@microsoft.com>
2021-03-02 08:32:35 +0000

    Align src and install paths for new includes (#2263)

--- src/enclave/app_interface.h
+++ src/enclave/app_interface.h
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 License.
 #pragma once
 
-#include "ccf/historical_queries_interface.h"
+#include "historical_queries_interface.h"
 #include "node/rpc/node_interface.h"
 #include "node/rpc/user_frontend.h"
 
