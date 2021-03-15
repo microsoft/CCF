88c6e0da960e1a6b8f3409dea32ef2e2c09cea11
Alex <shamis@gmail.com>
2021-03-15 17:31:41 +0000

    .

--- src/node/rpc/frontend.h
+++ src/node/rpc/frontend.h
@@ -279,7 +279,7 @@ namespace ccf
 
         ++attempts;
 
-        // try
+        try
         {
           if (pre_exec)
           {
@@ -356,7 +356,6 @@ namespace ccf
             }
           }
         }
-        /*
         catch (const kv::CompactedVersionConflict& e)
         {
           // The executing transaction failed because of a conflicting
@@ -415,7 +414,6 @@ namespace ccf
           update_metrics(ctx, metrics);
           return ctx->serialise_response();
         }
-        */
       }
 
       ctx->set_error(
