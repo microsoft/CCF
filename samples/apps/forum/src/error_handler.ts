beb4e43fa04340f5a6a4714280f3ae8512f07b33
Maik Riechert <maik.riechert@microsoft.com>
2021-03-15 17:18:18 +0000

    Auto-generated JS API docs & TS cleanup (#2313)

--- samples/apps/forum/src/error_handler.ts
+++ samples/apps/forum/src/error_handler.ts
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 License.
 
 import { ValidateError as TsoaValidateError, FieldErrors } from "@tsoa/runtime";
-import * as ccf from "./types/ccf";
+import { Request, Response } from "./ccf/builtin";
 
 export interface ErrorResponse {
   message: string;
@@ -67,8 +67,8 @@ export class NotFoundError extends HttpError {
  */
 export function errorHandler(
   err: unknown,
-  req: ccf.Request
-): ccf.Response<ErrorResponse | ValidateErrorResponse> {
+  req: Request
+): Response<ErrorResponse | ValidateErrorResponse> {
   if (err instanceof TsoaValidateError) {
     return {
       body: {
