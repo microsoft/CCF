532002db77cf21dae256d71e69ecdce6e20473c2
Amaury Chamayou <amaury@xargs.fr>
2021-03-29 18:20:35 +0100

    Update python/ccf/proposal_generator.py
    Co-authored-by: Maik Riechert <maik.riechert@arcor.de>

--- python/ccf/proposal_generator.py
+++ python/ccf/proposal_generator.py
@@ -158,7 +158,7 @@ def build_proposal(
         vote_lines = []
         vote_lines.append("function vote (raw_proposal, proposer_id) {")
         vote_lines.append("  let proposal = JSON.parse(raw_proposal)")
-        vote_lines.append("  if (!'actions' in proposal) { return false }")
+        vote_lines.append("  if (!('actions' in proposal)) { return false }")
         vote_lines.append("  let actions = proposal['actions']")
         vote_lines.append("  if (actions.length !== 1) { return false }")
         vote_lines.append("  let action = actions[0]")
