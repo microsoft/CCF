79ad522be3fd0399514b514d29ccd143b433c400
Maik Riechert <maik.riechert@microsoft.com>
2021-03-30 09:28:45 +0000

    formatting

--- src/runtime_config/resolve_veto.js
+++ src/runtime_config/resolve_veto.js
@@ -1,24 +1,24 @@
 export function resolve(proposal, proposerId, votes) {
-    // Every member has the ability to veto a proposal.
-    // If they vote against it, it is rejected.
-    if (votes.some(v => !v.vote)) {
-        return "Rejected";
+  // Every member has the ability to veto a proposal.
+  // If they vote against it, it is rejected.
+  if (votes.some((v) => !v.vote)) {
+    return "Rejected";
+  }
+
+  const memberVoteCount = votes.length;
+
+  let activeMemberCount = 0;
+  ccf.kv["public:ccf.gov.members.info"].forEach((v) => {
+    const info = ccf.bufToJsonCompatible(v);
+    if (info.status === "Active") {
+      activeMemberCount++;
     }
-    
-    const memberVoteCount = votes.length;
+  });
 
-    let activeMemberCount = 0;
-    ccf.kv["public:ccf.gov.members.info"].forEach(v => {
-        const info = ccf.bufToJsonCompatible(v);
-        if (info.status === "Active") {
-            activeMemberCount++;
-        }
-    });
+  // A majority of members can accept a proposal.
+  if (memberVoteCount > Math.floor(activeMemberCount / 2)) {
+    return "Accepted";
+  }
 
-    // A majority of members can accept a proposal.
-    if (memberVoteCount > Math.floor(activeMemberCount / 2)) {
-        return "Accepted";
-    }
-
-    return "Open";
+  return "Open";
 }
