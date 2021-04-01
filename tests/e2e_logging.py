978dbe5710bc01718af8142860d0e516ea5b6f5e
Julien Maffre <jumaffre@microsoft.com>
2021-04-01 11:15:50 +0100

    WIP

--- tests/e2e_logging.py
+++ tests/e2e_logging.py
@@ -645,12 +645,13 @@ def test_user_data_ACL(network, args):
         user = network.users[0]
 
         # Give isAdmin permissions to a single user
-        proposal_body, careful_vote = ccf.proposal_generator.set_user_data(
-            user.service_id,
-            {"isAdmin": True},
-        )
-        proposal = proposing_member.propose(primary, proposal_body)
-        network.consortium.vote_using_majority(primary, proposal, careful_vote)
+        network.consortium.set_user_data(primary, user.service_id, user_data={"isAdmin": True})
+        # proposal_body, careful_vote = ccf.proposal_generator.set_user_data(
+        #     user.service_id,
+        #     {"isAdmin": True},
+        # )
+        # proposal = proposing_member.propose(primary, proposal_body)
+        # network.consortium.vote_using_majority(primary, proposal, careful_vote)
 
         # Confirm that user can now use this endpoint
         with primary.client(user.local_id) as c:
@@ -658,12 +659,13 @@ def test_user_data_ACL(network, args):
             assert r.status_code == http.HTTPStatus.OK.value, r.status_code
 
         # Remove permission
-        proposal_body, careful_vote = ccf.proposal_generator.set_user_data(
-            user.service_id,
-            {"isAdmin": False},
-        )
-        proposal = proposing_member.propose(primary, proposal_body)
-        network.consortium.vote_using_majority(primary, proposal, careful_vote)
+        # proposal_body, careful_vote = ccf.proposal_generator.set_user_data(
+        #     user.service_id,
+        #     {"isAdmin": False},
+        # )
+        # proposal = proposing_member.propose(primary, proposal_body)
+        # network.consortium.vote_using_majority(primary, proposal, careful_vote)
+        network.consortium.set_user_data(primary, user.service_id, user_data={"isAdmin": False})
 
         # Confirm that user is now forbidden on this endpoint
         with primary.client(user.local_id) as c:
