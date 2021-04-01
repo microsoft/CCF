6cd52e45618ae562b129d362c0a23c2e1f4e57eb
Eddy Ashton <edashton@microsoft.com>
2021-04-01 11:23:20 +0100

    Upgrade to OE 0.15.0~rc1

--- cmake/cpack_settings.cmake
+++ cmake/cpack_settings.cmake
@@ -11,7 +11,7 @@ set(CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
 
 # CPack variables for Debian packages
 set(CPACK_DEBIAN_PACKAGE_DEPENDS
-    "open-enclave (>=0.14.0), libuv1 (>= 1.18.0), libc++1-8, libc++abi1-8"
+    "open-enclave (>=0.15.0), libuv1 (>= 1.18.0), libc++1-8, libc++abi1-8"
 )
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 
