1e144f7dd6278ff86340232e61e3f021f3db8784
Christoph M. Wintersteiger <cwinter@microsoft.com>
2021-04-02 11:41:04 +0000

    Add RSA signature benchmarks

--- src/crypto/openssl/rsa_public_key.h
+++ src/crypto/openssl/rsa_public_key.h
@@ -22,19 +22,26 @@ namespace crypto
     RSAPublicKey_OpenSSL(const std::vector<uint8_t>& der);
     virtual ~RSAPublicKey_OpenSSL() = default;
 
-    virtual size_t key_size() const;
+    virtual size_t key_size() const override;
 
     virtual std::vector<uint8_t> rsa_oaep_wrap(
       const uint8_t* input,
       size_t input_size,
       const uint8_t* label = nullptr,
-      size_t label_size = 0);
+      size_t label_size = 0) override;
 
     virtual std::vector<uint8_t> rsa_oaep_wrap(
       const std::vector<uint8_t>& input,
-      std::optional<std::vector<std::uint8_t>> label = std::nullopt);
+      std::optional<std::vector<std::uint8_t>> label = std::nullopt) override;
 
-    virtual Pem public_key_pem() const;
-    virtual std::vector<uint8_t> public_key_der() const;
+    virtual Pem public_key_pem() const override;
+    virtual std::vector<uint8_t> public_key_der() const override;
+
+    virtual bool verify(
+      const uint8_t* contents,
+      size_t contents_size,
+      const uint8_t* signature,
+      size_t signature_size,
+      MDType md_type = MDType::NONE) override;
   };
 }
