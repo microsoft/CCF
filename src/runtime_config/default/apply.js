5fe8117e986a0418177e47db01612c2658f99a79
Eddy Ashton <edashton@microsoft.com>
2021-04-08 18:06:54 +0100

    Pass proposal ID through apply

--- src/runtime_config/default/apply.js
+++ src/runtime_config/default/apply.js
@@ -1,7 +1,7 @@
-export function apply(proposal) {
+export function apply(proposal, proposal_id) {
   const proposed_actions = JSON.parse(proposal)["actions"];
   for (const proposed_action of proposed_actions) {
     const definition = actions.get(proposed_action.name);
-    definition.apply(proposed_action.args);
+    definition.apply(proposed_action.args, proposal_id);
   }
 }
