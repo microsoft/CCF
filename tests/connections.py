b78441d907c780b9ec15eb8ede837d94d2077dcf
Eddy Ashton <edashton@microsoft.com>
2021-04-13 10:57:04 +0100

    WIP

--- tests/connections.py
+++ tests/connections.py
@@ -34,7 +34,7 @@ def run(args):
                 LOG.success(f"Creating {target} clients")
                 for i in range(target):
                     try:
-                        clients.append(es.enter_context(primary.client("user0")))
+                        clients.append(es.enter_context(primary.client("user0", connection_timeout=1)))
                         LOG.info(f"Created client {i}")
                     except OSError:
                         LOG.error(f"Failed to create client {i}")
@@ -61,6 +61,21 @@ def run(args):
                 LOG.success(
                     f"{primary_pid} has {num_fds}/{max_fds} open file descriptors"
                 )
+
+                clients.pop(-1)
+
+                LOG.info("Continuing to submit on existing connections")
+                while True:
+                    for client in clients:
+                        try:
+                            client.post("/app/log/private", {"id": 42, "msg": "foo"}, timeout=1)
+                        except Exception as e:
+                            LOG.error(e)
+
+                time.sleep(1)
+                num_fds = psutil.Process(primary_pid).num_fds()
+                LOG.success(f"{primary_pid} has {num_fds}/{max_fds} open file descriptors")
+
                 LOG.info("Disconnecting clients")
                 clients = []
 
@@ -72,13 +87,13 @@ def run(args):
         nb_conn = (max_fds - num_fds) * 2
         num_fds = create_connections_until_exhaustion(nb_conn)
 
-        to_create = max_fds - num_fds + 1
-        num_fds = create_connections_until_exhaustion(to_create)
+        # to_create = max_fds - num_fds + 1
+        # num_fds = create_connections_until_exhaustion(to_create)
 
 
 if __name__ == "__main__":
 
     args = infra.e2e_args.cli_args()
     args.package = "liblogging"
-    args.nodes = ["local://localhost"]
+    args.nodes = infra.e2e_args.min_nodes(args, f=1)
     run(args)
