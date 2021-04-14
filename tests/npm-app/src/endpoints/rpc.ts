0d525279f53746101fc8886020dfa92d9ff73c4d
Maik Riechert <maik.riechert@microsoft.com>
2021-04-14 17:12:53 +0000

    js: expose set_apply_writes

---
+++ tests/npm-app/src/endpoints/rpc.ts
@@ -0,0 +1,10 @@
+import * as ccfapp from "@microsoft/ccf-app";
+
+export function applyWrites(request: ccfapp.Request): ccfapp.Response {
+  const kv = ccfapp.typedKv("public:apply_writes", ccfapp.string, ccfapp.string);
+  kv.set("foo", "bar");
+  ccfapp.setApplyWrites(true);
+  return {
+    statusCode: 400
+  };
+}
