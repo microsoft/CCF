c92abf143d627f1d26e1d1e10745c49fe087c2d9
Eddy Ashton <edashton@microsoft.com>
2021-04-14 14:54:14 +0100

    Test with low fd limit, to destruction

--- tests/connections.py
+++ tests/connections.py
@@ -15,7 +15,11 @@ from loguru import logger as LOG
 
 
 def run(args):
+    # Set a relatively low cap on max open sessions, so we can saturate it in a reasonable amount of time
     args.max_open_sessions = 100
+
+    # Chunk often, so that new fds are regularly requested
+    args.ledger_chunk_bytes = "500B"
 
     with infra.network.network(
         args.nodes, args.binary_dir, args.debug_nodes, args.perf_nodes, pdb=args.pdb
@@ -26,17 +30,19 @@ def run(args):
 
         primary_pid = primary.remote.remote.proc.pid
 
-        num_fds = psutil.Process(primary_pid).num_fds()
-        max_fds = num_fds + 150
-        LOG.success(f"{primary_pid} has {num_fds} open file descriptors")
+        initial_fds = psutil.Process(primary_pid).num_fds()
+        assert (
+            initial_fds < args.max_open_sessions
+        ), f"Initial number of file descriptors has already reached session limit: {initial_fds} >= {args.max_open_sessions}"
 
-        resource.prlimit(primary_pid, resource.RLIMIT_NOFILE, (max_fds, max_fds))
-        LOG.success(f"set max fds to {max_fds} on {primary_pid}")
+        num_fds = initial_fds
+        LOG.success(f"{primary_pid} has {num_fds} open file descriptors")
 
         def create_connections_until_exhaustion(target):
             with contextlib.ExitStack() as es:
                 clients = []
                 LOG.success(f"Creating {target} clients")
+                consecutive_failures = 0
                 for i in range(target):
                     logs = []
                     try:
@@ -53,20 +59,33 @@ def run(args):
                             ),
                             result=True,
                         )
+                        consecutive_failures = 0
                     except Exception as e:
                         flush_info(logs)
                         LOG.warning(f"Hit exception at client {i}: {e}")
-                        break
+                        clients.pop(-1)
+                        if consecutive_failures < 5:
+                            # Maybe got unlucky and tried to create a session while many files were open - keep trying
+                            consecutive_failures += 1
+                            continue
+                        else:
+                            # Ok you've really hit a wall, stop trying to create clients
+                            break
+                else:
+                    raise RuntimeError(
+                        f"Successfully created {target} clients without exception - expected this to exhaust available connections"
+                    )
 
                 num_fds = psutil.Process(primary_pid).num_fds()
                 LOG.success(
                     f"{primary_pid} has {num_fds}/{max_fds} open file descriptors"
                 )
 
-                clients.pop(-1)
-
-                more_requests = len(clients) * 3
-                LOG.info(f"Submitting an additional {more_requests} requests from existing clients")
+                # Submit many requests, and at least enough to trigger additional snapshots
+                more_requests = max(len(clients) * 3, args.snapshot_tx_interval * 2)
+                LOG.info(
+                    f"Submitting an additional {more_requests} requests from existing clients"
+                )
                 for _ in range(more_requests):
                     client = random.choice(clients)
                     logs = []
@@ -96,11 +115,32 @@ def run(args):
             LOG.success(f"{primary_pid} has {num_fds}/{max_fds} open file descriptors")
             return num_fds
 
+        # For initial safe tests, we have many more fds than the maximum sessions, so file operations should still succeed even when network is saturated
+        max_fds = args.max_open_sessions + (initial_fds * 2)
+        resource.prlimit(primary_pid, resource.RLIMIT_NOFILE, (max_fds, max_fds))
+        LOG.success(f"Setting max fds to safe initial value {max_fds} on {primary_pid}")
+
         nb_conn = (max_fds - num_fds) * 2
         num_fds = create_connections_until_exhaustion(nb_conn)
 
         to_create = max_fds - num_fds + 1
         num_fds = create_connections_until_exhaustion(to_create)
+
+        # Now set a low fd limit, so network sessions completely exhaust them - expect this to cause failures
+        max_fds = args.max_open_sessions // 2
+        resource.prlimit(primary_pid, resource.RLIMIT_NOFILE, (max_fds, max_fds))
+        LOG.success(f"Setting max fds to dangerously low {max_fds} on {primary_pid}")
+
+        try:
+            num_fds = create_connections_until_exhaustion(to_create)
+        except Exception as e:
+            LOG.warning(
+                f"Node with only {max_fds} fds crashed when allowed to created {args.max_open_sessions} sessions, as expected"
+            )
+            LOG.warning(e)
+            network.ignore_errors_on_shutdown()
+        else:
+            raise RuntimeError("Expected a fatal crash and saw none!")
 
 
 if __name__ == "__main__":
