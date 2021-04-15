1d29c1cb39bdccdda226706d8acb9d442286d1bd
Maik Riechert <maik.riechert@microsoft.com>
2021-04-15 09:51:51 +0000

    formatting

--- tests/npm-app/src/endpoints/rpc.ts
+++ tests/npm-app/src/endpoints/rpc.ts
@@ -1,10 +1,14 @@
 import * as ccfapp from "@microsoft/ccf-app";
 
 export function applyWrites(request: ccfapp.Request): ccfapp.Response {
-  const kv = ccfapp.typedKv("public:apply_writes", ccfapp.string, ccfapp.string);
+  const kv = ccfapp.typedKv(
+    "public:apply_writes",
+    ccfapp.string,
+    ccfapp.string
+  );
   kv.set("foo", "bar");
   ccfapp.setApplyWrites(true);
   return {
-    statusCode: 400
+    statusCode: 400,
   };
 }
