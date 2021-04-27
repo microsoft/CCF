35cd0e650c734c9738169f79aa7f9eab6227da6e
Eddy Ashton <edashton@microsoft.com>
2021-04-27 14:23:47 +0100

    Reflow comment

--- src/host/main.cpp
+++ src/host/main.cpp
@@ -439,8 +439,7 @@ int main(int argc, char** argv)
       "--curve-id",
       curve_id,
       "Elliptic curve to use as for node and network identities (used for TLS "
-      "and ledger "
-      "signatures")
+      "and ledger signatures)")
     ->transform(CLI::CheckedTransformer(curve_id_map, CLI::ignore_case))
     ->capture_default_str();
 
