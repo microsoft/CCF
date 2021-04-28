a71cb731e516525e7decdb3dbf7a7c506c39a1b7
Eddy Ashton <edashton@microsoft.com>
2021-04-28 18:15:23 +0100

    Format

--- python/versionifier.py
+++ python/versionifier.py
@@ -1,15 +1,21 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-from setuptools.extern.packaging.version import Version, InvalidVersion
+from setuptools.extern.packaging.version import (
+    Version,
+    InvalidVersion,
+)
+
 
 def remove_prefix(s, prefix):
     if s.startswith(prefix):
         return s[len(prefix) :]
     return s
 
+
 def replace_char(s, n, c):
-    return s[:n] + str(c) + s[n+1:]
+    return s[:n] + str(c) + s[n + 1 :]
+
 
 def to_python_version(original):
     unprefixed = remove_prefix(original, "ccf-")
@@ -32,35 +38,35 @@ def to_python_version(original):
     raise ValueError(f"Cannot convert '{original}' to a Version")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Run some tests that expected versions are correctly parsed
     v = to_python_version("1")
     assert v.release == (1,)
 
     v = to_python_version("1.2.3")
-    assert v.release == (1,2,3)
+    assert v.release == (1, 2, 3)
 
     v = to_python_version("ccf-1.2.3")
-    assert v.release == (1,2,3)
-    
+    assert v.release == (1, 2, 3)
+
     v = to_python_version("ccf-1.2.3-a42")
-    assert v.release == (1,2,3)
+    assert v.release == (1, 2, 3)
     assert v.pre == ("a", 42)
-    
+
     v = to_python_version("ccf-1.2.3-rc1")
-    assert v.release == (1,2,3)
+    assert v.release == (1, 2, 3)
     assert v.pre == ("rc", 1)
-    
+
     v = to_python_version("ccf-1.2.3-dev2")
-    assert v.release == (1,2,3)
+    assert v.release == (1, 2, 3)
     assert v.dev == 2
-    
+
     v = to_python_version("ccf-1.2.3-dev3-5-deadbeef")
-    assert v.release == (1,2,3)
+    assert v.release == (1, 2, 3)
     assert v.dev == 3
     assert v.local == "5.deadbeef"
 
     v = to_python_version("ccf-1.2.3-42-deadbeef")
-    assert v.release == (1,2,3)
+    assert v.release == (1, 2, 3)
     assert v.post == 42
     assert v.local == "deadbeef"
