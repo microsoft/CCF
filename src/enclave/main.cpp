3ee34a9d3e9ad67fdad39b88ab605c7dc3674482
Julien Maffre <42961061+jumaffre@users.noreply.github.com>
2021-05-06 19:47:00 +0100

    Install `version.h` under `include/ccf` (#2562)

--- src/enclave/main.cpp
+++ src/enclave/main.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
+#include "ccf/version.h"
+#include "common/enclave_interface_types.h"
 #include "ds/json.h"
 #include "ds/logger.h"
 #include "ds/spin_lock.h"
--- tests/test_install_build.sh
+++ tests/test_install_build.sh
@@ -3,7 +3,7 @@
 # Licensed under the Apache 2.0 License.
 set -ex
 
-mkdir build_against_install
+mkdir -p build_against_install
 cd build_against_install
 CC=$(command -v clang-8) CXX=$(command -v clang++-8) cmake -GNinja ../samples/apps/logging/
 ninja
