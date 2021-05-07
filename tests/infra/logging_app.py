90648533ea1b864c768392c1f33136f69ec828e9
Eddy Ashton <edashton@microsoft.com>
2021-05-07 11:20:23 +0100

    Modify sample historical range query endpoint to better handle subranges (#2556)

--- tests/infra/logging_app.py
+++ tests/infra/logging_app.py
@@ -60,9 +60,10 @@ class LoggingTxs:
         repeat=False,
         idx=None,
         wait_for_sync=True,
+        log_capture=None,
     ):
         self.network = network
-        remote_node, _ = network.find_primary()
+        remote_node, _ = network.find_primary(log_capture=log_capture)
         if on_backup:
             remote_node = network.find_any_backup()
 
@@ -86,6 +87,7 @@ class LoggingTxs:
                         "id": target_idx,
                         "msg": priv_msg,
                     },
+                    log_capture=log_capture,
                 )
                 self.priv[target_idx].append(
                     {"msg": priv_msg, "seqno": rep_priv.seqno, "view": rep_priv.view}
@@ -100,6 +102,7 @@ class LoggingTxs:
                         "id": target_idx,
                         "msg": pub_msg,
                     },
+                    log_capture=log_capture,
                 )
                 self.pub[target_idx].append(
                     {"msg": pub_msg, "seqno": rep_pub.seqno, "view": rep_pub.view}
