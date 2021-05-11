84b4a9318d3fb17b1d35dd9d3072f78c2234500f
Julien Maffre <jumaffre@microsoft.com>
2021-05-11 11:15:42 +0100

    WIP

--- tests/lts_compatibility.py
+++ tests/lts_compatibility.py
@@ -2,11 +2,32 @@
 # Licensed under the Apache 2.0 License.
 import infra.network
 import infra.e2e_args
+import infra.proc
+import infra.logging_app as app
+
+
+def install_latest_lts():
+    download_cmd = [
+        "wget",
+        "https://github.com/microsoft/CCF/releases/download/ccf-1.0.0/ccf_1.0.0_amd64.deb",
+    ]
+    infra.proc.ccall(*download_cmd)
+
+    install_cmd = []
 
 
 def run(args):
-    pass
 
+    txs = app.LoggingTxs()
+    with infra.network.network(
+        args.nodes,
+        args.binary_dir,
+        args.debug_nodes,
+        args.perf_nodes,
+        pdb=args.pdb,
+        txs=txs,
+    ) as network:
+        pass
     # TODO:
     # 1. Download latest LTS, that has to be hardcoded somewhere as cannot be deduced from git tag
     # 2. Run service with cchost + libjsgeneric (enough for at least two chunks?)
