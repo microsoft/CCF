49419767af9f1dc3e2daa05a2f330eaaa97c8e1d
Amaury Chamayou <amchamay@microsoft.com>
2021-06-21 22:09:58 +0100

    Another go at fixing the LTS tests in the daily (#2706)

--- tests/lts_compatibility.py
+++ tests/lts_compatibility.py
@@ -181,7 +181,8 @@ def run_code_upgrade_from(
             # and retrieve new keys via auto refresh
             jwt_issuer.refresh_keys()
             # Note: /gov/jwt_keys/all endpoint was added in 2.x
-            if node.version and node.version > 1:
+            primary, _ = network.find_nodes()
+            if primary.version and primary.version > 1:
                 jwt_issuer.wait_for_refresh(network)
             else:
                 time.sleep(3)
@@ -344,7 +345,12 @@ def run_ledger_compatibility_since_first(args, local_branch, use_snapshot):
                 # Rollover JWKS so that new primary must read historical CA bundle table
                 # and retrieve new keys via auto refresh
                 jwt_issuer.refresh_keys()
-                jwt_issuer.wait_for_refresh(network)
+                # Note: /gov/jwt_keys/all endpoint was added in 2.x
+                primary, _ = network.find_nodes()
+                if primary.version and primary.version > 1:
+                    jwt_issuer.wait_for_refresh(network)
+                else:
+                    time.sleep(3)
 
                 issue_activity_on_live_service(network, args)
 
