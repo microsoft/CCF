3f4f08fdb6a5e65ba71e4c06a2187bcc986fa3b3
Amaury Chamayou <amchamay@microsoft.com>
2021-06-25 15:44:57 +0100

    compat_only

--- scripts/scan-build.sh
+++ scripts/scan-build.sh
@@ -2,10 +2,12 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-export CCC_CC=clang-10
-export CCC_CXX=clang++-10
+CLANG_VERSION=8
 
-SCAN="scan-build-10 --exclude 3rdparty --exclude test"
+export CCC_CC="clang-$CLANG_VERSION"
+export CCC_CXX="clang++-$CLANG_VERSION"
+
+SCAN="scan-build-$CLANG_VERSION --exclude 3rdparty --exclude test"
 
 $SCAN cmake -GNinja -DCMAKE_BUILD_TYPE=Debug ..
 $SCAN ninja
