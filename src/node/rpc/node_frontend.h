c8ffb9af502bc5130430f40c8636a137e4dbda21
Christoph M. Wintersteiger <cwinter@microsoft.com>
2021-06-28 15:34:56 +0000

    Correct OpenAPI version number

--- src/node/rpc/node_frontend.h
+++ src/node/rpc/node_frontend.h
@@ -216,7 +216,7 @@ namespace ccf
       openapi_info.description =
         "This API provides public, uncredentialed access to service and node "
         "state.";
-      openapi_info.document_version = "1.3.1";
+      openapi_info.document_version = "1.4.0";
     }
 
     void init_handlers() override
