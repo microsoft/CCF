3056add6ed0d1a7229536f6e5dcfe174d6c4b096
Julien Maffre <42961061+jumaffre@users.noreply.github.com>
2021-07-20 09:01:50 +0100

    Read SubjectAltName field from CSR (#2818)

--- src/crypto/san.h
+++ src/crypto/san.h
@@ -4,6 +4,8 @@
 
 #include "ds/json.h"
 
+#define FMT_HEADER_ONLY
+#include <fmt/format.h>
 #include <string>
 
 namespace crypto
@@ -16,3 +18,31 @@ namespace crypto
   DECLARE_JSON_TYPE(SubjectAltName);
   DECLARE_JSON_REQUIRED_FIELDS(SubjectAltName, san, is_ip);
 }
+
+FMT_BEGIN_NAMESPACE
+template <>
+struct formatter<crypto::SubjectAltName>
+{
+  template <typename ParseContext>
+  auto parse(ParseContext& ctx)
+  {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const crypto::SubjectAltName& san, FormatContext& ctx)
+    -> decltype(ctx.out())
+  {
+    std::string prefix;
+    if (san.is_ip)
+    {
+      prefix = "IP";
+    }
+    else
+    {
+      prefix = "DNS";
+    }
+    return format_to(ctx.out(), "{}:{}", prefix, san.san);
+  }
+};
+FMT_END_NAMESPACE
