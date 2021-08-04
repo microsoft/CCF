3f5433e73b04ffa1dcb5f811111750905572d9b1
Amaury Chamayou <amchamay@microsoft.com>
2021-08-04 13:58:34 +0100

    Perf test in 1ES pool

--- .nodes.sh
+++ .nodes.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-HN=$(hostname)
-
-if [ "$HN" == "ccf-perf-uk-0" ]; then
-    echo "-n ssh://10.1.0.7 -n ssh://10.1.0.4 -n ssh://10.1.0.5"
-fi
+echo "-n local://localhost -n local://localhost"
