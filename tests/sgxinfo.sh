8c380626e76ee1c730ece1bfa682f8916acd025b
Julien Maffre <jumaffre@microsoft.com>
2021-08-19 13:37:39 +0000

    Update `sgxinfo.sh` script for 5.11 kernel

--- tests/sgxinfo.sh
+++ tests/sgxinfo.sh
@@ -4,13 +4,22 @@
 
 set +e
 
-echo "DRIVER INFO:"
-modinfo intel_sgx
-echo ""
-echo ""
+kernel_version=$(uname -r)
+major_version=$(echo ${kernel_version} | cut -d '.' -f 1)
+minor_version=$(echo ${kernel_version} | cut -d '.' -f 2)
 
-echo "DRIVER LOADED:"
-lsmod | grep intel_sgx || echo "Not loaded"
+if [ ${major_version} -gt 5 ] || [[ ${major_version} -eq 5 && ${minor_version} -ge 11 ]]; then
+    echo "LINUX KERNEL WITH SGX SUPPORT (5.11+):"
+    echo "${kernel_version}"
+else
+    # Pre 5.11 kernel
+    echo "DRIVER INFO:"
+    modinfo intel_sgx 2>/dev/null
+    echo ""
+    echo ""
+    echo "DRIVER LOADED:"
+    lsmod | grep intel_sgx || echo "Not loaded"
+fi
 echo ""
 echo ""
 
