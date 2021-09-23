0fddf097fc13674c88e5d5f28cfb87d896bc66be
tpluck <tompluck@mail.com>
2021-09-23 15:37:23 +0100

    Correct script

--- getting_started/setup_vm/run.sh
+++ getting_started/setup_vm/run.sh
@@ -4,6 +4,10 @@
 
 set -ex
 
-sudo apt install python3-pip
+# Install ansible-base rather than ansible because service_facts
+# is broken on Ubuntu 20.04 with the default apt package.
+# See https://github.com/ansible/ansible/issues/68536 (fixed in ansible >= 2.10)
+sudo apt-get update
+sudo apt install -y python3-pip
 pip install ansible-base
 ansible-playbook "$@"
