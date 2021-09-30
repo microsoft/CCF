74970ad420969da995a5111c9d69bee193bb12e7
Eddy Ashton <edashton@microsoft.com>
2021-09-30 13:30:26 +0100

    Add perf test of historical query fetching (#3032)

--- samples/apps/logging/logging.cpp
+++ samples/apps/logging/logging.cpp
@@ -953,7 +953,7 @@ namespace loggingapp
         }
 
         // Set a maximum range, paginate larger requests
-        static constexpr size_t max_seqno_per_page = 20;
+        static constexpr size_t max_seqno_per_page = 2000;
         const auto range_begin = from_seqno;
         const auto range_end =
           std::min(to_seqno, range_begin + max_seqno_per_page);
