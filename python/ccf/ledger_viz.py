9002119be7a11d362e634d034ad21ed326bf49e2
Eddy Ashton <edashton@microsoft.com>
2021-10-22 15:32:38 +0100

    Draw term indicators in ledger_viz

--- python/ccf/ledger_viz.py
+++ python/ccf/ledger_viz.py
@@ -17,31 +17,54 @@ class Liner:
         self._line = ""
         self._len = 0
 
-    def append(self, c: str, colour: str, background_colour: str):
-        self._line += cs(c, colour, background_colour)
-        self._len += 1
+    def append(self, s: str, colour: str, background_colour: str = None):
+        self._line += cs(s, colour, background_colour)
+        self._len += len(s)
         if self._len >= self.MAX_LENGTH:
             self.flush()
 
 
 class DefaultLiner(Liner):
-    _mapping = {
-        "Signature": (" ", "White", "Green"),
-        "Governance": (" ", "White", "Red"),
-        "Internal": (" ", "White", "Orange"),
-        "User Public": (" ", "White", "Blue"),
-        "User Private": (" ", "White", "DarkBlue"),
+    _bg_colour_mapping = {
+        "Signature": "Green",
+        "Governance": "Red",
+        "Internal": "Orange",
+        "User Public": "Blue",
+        "User Private": "DarkBlue",
     }
+    _last_view = None
+    _fg_colour = "Black"
 
-    def entry(self, category):
-        self.append(*self._mapping[category])
+    @staticmethod
+    def view_to_char(view):
+        chars = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
+        return chars[view % len(chars)]
+
+    def entry(self, category, view):
+        if view != self._last_view:
+            char = DefaultLiner.view_to_char(view)
+            self._last_view = view
+            self.flush()
+            self.append(f"{view}: ", "White")
+        else:
+            char = "‾"
+        fg_colour = self._fg_colour
+        bg_colour = self._bg_colour_mapping[category]
+        self.append(char, fg_colour, bg_colour)
 
     def help(self):
         print(
             " ".join(
                 [
-                    f"{category}: {cs(*values)}"
-                    for category, values in self._mapping.items()
+                    f"{category}: {cs(' ', 'White', bg_colour)}"
+                    for category, bg_colour in self._bg_colour_mapping.items()
+                ]
+            )
+        )
+        print(
+            " ".join(
+                [
+                    f"Start of view 14: {cs(DefaultLiner.view_to_char(14), self._fg_colour, 'Grey')}"
                 ]
             )
         )
@@ -67,18 +90,20 @@ def main():
         for tx in chunk:
             public = tx.get_public_domain().get_tables()
             has_private = tx.get_private_domain_size()
+
+            view = tx.gcm_header.view
             if not has_private:
-                if "public:ccf.internal.signatures" in public:
-                    l.entry("Signature")
+                if ccf.ledger.SIGNATURE_TX_TABLE_NAME in public:
+                    l.entry("Signature", view)
                 else:
                     if all(table.startswith("public:ccf.gov.") for table in public):
-                        l.entry("Governance")
+                        l.entry("Governance", view)
                     elif all(table.startswith("public:ccf.") for table in public):
-                        l.entry("Internal")
+                        l.entry("Internal", view)
                     else:
-                        l.entry("User Public")
+                        l.entry("User Public", view)
             else:
-                l.entry("User Private")
+                l.entry("User Private", view)
 
     l.flush()
 
