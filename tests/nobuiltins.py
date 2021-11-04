47e58e1ed396f8d5d26ba991fc9b7f40a70075cb
Eddy Ashton <edashton@microsoft.com>
2021-11-04 11:48:22 +0000

    Use existing wait_for_commit pattern

--- tests/nobuiltins.py
+++ tests/nobuiltins.py
@@ -5,32 +5,25 @@ from http import HTTPStatus
 import openapi_spec_validator
 from datetime import datetime, timezone
 import time
+import ccf.commit
 
 
 def test_nobuiltins_endpoints(network, args):
     primary, backups = network.find_nodes()
     with primary.client() as c:
-        timeout = 3
-        end_time = time.time() + timeout
-        found_stable_commit = False
+        # The rest of this test assumes the network's commit index is stable. This may
+        # not be true yet, as the setup transactions may not have committed. Do a fresh
+        # read to find the latest TxID, and wait for that to commit, before proceeding.
         r = c.get("/node/network")
         assert r.status_code == HTTPStatus.OK
-        target_view = r.view
-        target_seqno = r.seqno
-        assert target_view is not None
-        assert target_seqno is not None
-        while time.time() < end_time:
-            r = c.get("/node/commit")
-            assert r.status_code == HTTPStatus.OK
-            body_j = r.body.json()
-            tx_id = TxID.from_str(body_j["transaction_id"])
-            if tx_id.view == target_view and tx_id.seqno == target_seqno:
-                found_stable_commit = True
-                break
-            else:
-                time.sleep(0.1)
+        assert r.view is not None
+        assert r.seqno is not None
+        ccf.commit.wait_for_commit(c, seqno=r.seqno, view=r.view, timeout=3)
 
-        assert found_stable_commit, f"Failed to reach stable commit after {timeout}s"
+        r = c.get("/app/commit")
+        assert r.status_code == HTTPStatus.OK
+        body_j = r.body.json()
+        tx_id = TxID.from_str(body_j["transaction_id"])
 
         r = c.get("/app/node_summary")
         assert r.status_code == HTTPStatus.OK
