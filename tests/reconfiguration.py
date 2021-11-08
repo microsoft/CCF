dd8adcb23b8789db17fe0a5ad39528c4e2210688
Christoph M. Wintersteiger <cwinter@microsoft.com>
2021-11-08 11:41:59 +0000

    Re-enable 2tx reconfig tests

--- tests/reconfiguration.py
+++ tests/reconfiguration.py
@@ -616,8 +616,7 @@ if __name__ == "__main__":
         reconfiguration_type="1tx",
     )
 
-    # Turned off while #3184 is in progress
-    if cr.args.include_2tx_reconfig and False:
+    if cr.args.include_2tx_reconfig:
         cr.add(
             "2tx_reconfig",
             run,
