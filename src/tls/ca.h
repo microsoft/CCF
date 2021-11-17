67204752ab75c879d338ef1a12d168382c719517
Renato Golin <rengolin@systemcall.eu>
2021-11-17 12:01:30 +0000

    Cleanup relative includes

--- src/tls/ca.h
+++ src/tls/ca.h
@@ -2,9 +2,9 @@
 // Licensed under the Apache 2.0 License.
 #pragma once
 
-#include "../crypto/mbedtls/mbedtls_wrappers.h"
-#include "../crypto/pem.h"
-#include "../ds/buffer.h"
+#include "crypto/mbedtls/mbedtls_wrappers.h"
+#include "crypto/pem.h"
+#include "ds/buffer.h"
 
 #include <exception>
 
