88e5fc7f1c295a0cd96ad2852137dfe353f53a05
Eddy Ashton <edashton@microsoft.com>
2021-11-22 10:01:26 +0000

    Move files

--- python/ccf/templates/ballot_script.js
+++ python/ccf/templates/ballot_script.js
@@ -1,12 +1,12 @@
 {%- from "macros.jinja" import json_encode as json_encode -%}
 export function vote (rawProposal, proposerId) {
   let proposal = JSON.parse(rawProposal);
-  if (!('actions' in proposal))
+  if (!("actions" in proposal))
   {
     return false;
   }
 
-  let actions = proposal['actions'];
+  let actions = proposal["actions"];
   if (actions.length !== {{ actions|length }} )
   {
     return false;
@@ -14,25 +14,25 @@ export function vote (rawProposal, proposerId) {
 {% for action in actions %}
   {
     let action = actions[{{ loop.index - 1}}];
-    if (!('name' in action) || !('args' in action))
+    if (!("name" in action) || !("args" in action))
     {
       return false;
     }
 
-    if (action.name !== '{{ action.name }}')
+    if (action.name !== "{{ action.name }}")
     {
       return false;
     }
 
     let args = action.args;
 {% if actions.args is defined and actions.args is mapping %}{% for arg_name, arg_value in action.args.items() %}    { 
-      if (!('{{ arg_name }}' in args))
+      if (!("{{ arg_name }}" in args))
       {
         return false;
       }
 
       const expected = {{ json_encode(arg_value) }};
-      if (args['{{ arg_name }}']) !== expected)
+      if (args["{{ arg_name }}"]) !== expected)
       {
         return false;
       }
