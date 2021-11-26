c23d996d371188bf4f2dbae8110866311742d080
Amaury Chamayou <amchamay@microsoft.com>
2021-11-26 17:03:50 +0000

    Missing file

---
+++ src/enclave/claims.h
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache 2.0 License.
+#pragma once
+
+#include "crypto/hash_provider.h"
+
+namespace ccf
+{
+    class ClaimsDigest
+    {
+        bool is_set = false;
+        crypto::Sha256Hash digest;
+
+        public:
+            ClaimsDigest() = default;
+
+            inline void set(crypto::Sha256Hash digest_)
+            {
+                is_set = true;
+                digest = digest_;
+            }
+
+            inline bool empty()
+            {
+                return is_set;
+            }
+
+            const crypto::Sha256Hash& value() const
+            {
+                return digest;
+            }
+    };
+
+    static ClaimsDigest no_claims() {
+        return ClaimsDigest();
+    }
+}
