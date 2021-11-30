edc60f5e7e26f62f58de8eb08d9d0667285b6173
Christoph M. Wintersteiger <cwinter@microsoft.com>
2021-11-30 14:43:51 +0000

    OpenAPI schema update

--- src/node/rpc/node_frontend.h
+++ src/node/rpc/node_frontend.h
@@ -320,7 +320,7 @@ namespace ccf
       openapi_info.description =
         "This API provides public, uncredentialed access to service and node "
         "state.";
-      openapi_info.document_version = "2.7.0";
+      openapi_info.document_version = "2.7.1";
     }
 
     void init_handlers() override
