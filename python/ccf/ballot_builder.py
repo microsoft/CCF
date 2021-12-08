98a958ef3ce1c035b2b44ab8062ac2986eabe561
Eddy Ashton <edashton@microsoft.com>
2021-12-08 17:11:25 +0000

    Simplify ballot_builder to write like everything else

--- python/ccf/ballot_builder.py
+++ python/ccf/ballot_builder.py
@@ -8,29 +8,8 @@ import sys
 import jinja2
 from typing import Optional
 
-from loguru import logger as LOG  # type: ignore
-
-
-DEFAULT_BALLOT_OUTPUT = "{proposal_name}_vote_for.json"
-
-
-def complete_ballot_output_path(
-    proposal_name: str, ballot_output_path: Optional[str] = None, common_dir: str = "."
-):
-    if ballot_output_path is None:
-        ballot_output_path = DEFAULT_BALLOT_OUTPUT.format(proposal_name=proposal_name)
-
-    if not ballot_output_path.endswith(".json"):
-        ballot_output_path += ".json"
-
-    ballot_output_path = os.path.normpath(os.path.join(common_dir, ballot_output_path))
-
-    return ballot_output_path
-
 
 def build_ballot_raw(proposal: dict):
-    LOG.trace(f"Generating ballot")
-
     template_loader = jinja2.PackageLoader("ccf", "templates")
     template_env = jinja2.Environment(
         loader=template_loader, undefined=jinja2.StrictUndefined
@@ -39,13 +18,10 @@ def build_ballot_raw(proposal: dict):
     ballot_template = template_env.get_template("ballots.json.jinja")
     ballot = ballot_template.render(proposal)
 
-    LOG.trace(f"Generated ballot:\n{ballot}")
-
     return ballot
 
 
 def build_ballot(proposal_path: str):
-    LOG.trace(f"Reading proposal from: {proposal_path}")
     with open(proposal_path, "r") as f:
         proposal = json.load(f)
 
@@ -55,29 +31,10 @@ def build_ballot(proposal_path: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("proposal", type=str, help="Path to proposal JSON file")
-    parser.add_argument(
-        "-bo",
-        "--ballot-output-file",
-        type=str,
-        help=f"Path where ballot JSON object will be written. Default is {DEFAULT_BALLOT_OUTPUT}",
-    )
     parser.add_argument("-v", "--verbose", action="store_true")
 
     args = parser.parse_args()
 
-    LOG.remove()
-    LOG.add(
-        sys.stdout,
-        format="<level>[{time:YYYY-MM-DD HH:mm:ss.SSS}] {level} | {message}</level>",
-        level="TRACE" if args.verbose else "INFO",
-    )
-
     ballot = build_ballot(args.proposal)
 
-    ballot_path = complete_ballot_output_path(
-        args.proposal.replace("_proposal", "").replace(".json", ""),
-        ballot_output_path=args.ballot_output_file,
-    )
-    LOG.success(f"Writing ballot to {ballot_path}")
-    with open(ballot_path, "w", encoding="utf-8") as f:
-        f.write(ballot)
+    print(json.dumps(ballot, indent=2))
--- tests/test_python_cli.sh
+++ tests/test_python_cli.sh
@@ -15,37 +15,37 @@ build_proposal.sh --help
 python -m ccf.ballot_builder --help
 
 build_proposal.sh --action set_member cert @bob_cert.pem > set_member_proposal_a.json
-python -m ccf.ballot_builder set_member_proposal_a.json
+python -m ccf.ballot_builder set_member_proposal_a.json > set_member_vote_for_a.json
 build_proposal.sh --action set_member cert @bob_cert.pem encryption_pub_key @bob_enc_pubk.pem > set_member_proposal_b.json
-python -m ccf.ballot_builder set_member_proposal_b.json
+python -m ccf.ballot_builder set_member_proposal_b.json > set_member_vote_for_b.json
 build_proposal.sh --action set_member cert @bob_cert.pem encryption_pub_key @bob_enc_pubk.pem member_data 'Arbitrary data' > set_member_proposal_c.json
-python -m ccf.ballot_builder set_member_proposal_c.json
+python -m ccf.ballot_builder set_member_proposal_c.json > set_member_vote_for_c.json
 build_proposal.sh \
     --action set_member \
         cert @bob_cert.pem \
         encryption_pub_key @bob_enc_pubk.pem \
         -j member_data '{"Interesting": {"nested": ["da", "ta"]}}' \
     > set_member_proposal_d.json
-python -m ccf.ballot_builder set_member_proposal_d.json
+python -m ccf.ballot_builder set_member_proposal_d.json > set_member_vote_for_d.json
 
 build_proposal.sh --action set_user cert @alice_cert.pem > set_user_proposal_a.json
-python -m ccf.ballot_builder set_user_proposal_a.json
+python -m ccf.ballot_builder set_user_proposal_a.json > set_user_vote_for_a.json
 build_proposal.sh --action set_user cert @alice_cert.pem user_data 'ADMIN' > set_user_proposal_b.json
-python -m ccf.ballot_builder set_user_proposal_b.json
+python -m ccf.ballot_builder set_user_proposal_b.json > set_user_vote_for_b.json
 build_proposal.sh --action set_user cert @alice_cert.pem -j user_data '{"type": "ADMIN", "friendlyName ": "Alice"}' > set_user_proposal_c.json
-python -m ccf.ballot_builder set_user_proposal_c.json
+python -m ccf.ballot_builder set_user_proposal_c.json > set_user_vote_for_c.json
 
 build_proposal.sh --action transition_service_to_open > transition_service_to_open_proposal.json
-python -m ccf.ballot_builder transition_service_to_open_proposal.json
+python -m ccf.ballot_builder transition_service_to_open_proposal.json > transition_service_to_open_vote_for.json
 
 build_proposal.sh --action transition_node_to_trusted node_id 42 valid_from 211019154318Z > transition_node_to_trusted_proposal.json
-python -m ccf.ballot_builder transition_node_to_trusted_proposal.json
+python -m ccf.ballot_builder transition_node_to_trusted_proposal.json > transition_node_to_trusted_vote_for.json
 
 build_proposal.sh --action add_node_code code_id 1234abcd > add_node_code_proposal.json
-python -m ccf.ballot_builder add_node_code_proposal.json
+python -m ccf.ballot_builder add_node_code_proposal.json > add_node_codde_vote_for.json
 
 CCF_ROOT_DIR="${1}"
 python -m ccf.bundle_js_app --help
 python -m ccf.bundle_js_app "${1}"/samples/apps/logging/js > bundle.json
 build_proposal.sh --action set_js_app bundle -j @bundle.json > set_js_app_proposal.json
-python -m ccf.ballot_builder set_js_app_proposal.json
+python -m ccf.ballot_builder set_js_app_proposal.json > set_js_app_vote_for.json
