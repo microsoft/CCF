8289123e283111ab3d2cfcee8e3951c6bb843ebd
Julien Maffre <42961061+jumaffre@users.noreply.github.com>
2021-12-09 10:44:02 +0000

    Always run run.sh non-interactively

--- getting_started/setup_vm/run.sh
+++ getting_started/setup_vm/run.sh
@@ -7,8 +7,8 @@ set -ex
 # Install ansible from ppa because service_facts
 # is broken on Ubuntu 20.04 with the default apt package.
 # See https://github.com/ansible/ansible/issues/68536 (fixed in ansible >= 2.10)
-sudo apt-get update
-sudo apt install software-properties-common
+sudo apt-get -y update
+sudo apt install software-properties-common -y
 sudo add-apt-repository -y --update ppa:ansible/ansible
 sudo apt install ansible -y
 ansible-playbook "$@"
