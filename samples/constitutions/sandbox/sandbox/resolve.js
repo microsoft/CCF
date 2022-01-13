b961018e8431b46b16ead1db1ae2cb8da51fe3ab
Eddy Ashton <edashton@microsoft.com>
2022-01-13 17:47:35 +0000

    Delete and comment tweak

--- samples/constitutions/sandbox/sandbox/resolve.js
+++
@@ -1,7 +0,0 @@
-export function resolve(proposal, proposerId, votes) {
-  // The constitution in a real CCF application deployment would at least
-  // count votes and compare to a threshold of members, but in the sandbox sample,
-  // all votes pass automatically.
-
-  return "Accepted";
-}
