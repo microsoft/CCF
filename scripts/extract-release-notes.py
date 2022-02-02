3536c538504069fa578d1e6fee2e122c093f49c9
Eddy Ashton <edashton@microsoft.com>
2022-02-02 16:35:42 +0000

    Check for release link

--- scripts/extract-release-notes.py
+++ scripts/extract-release-notes.py
@@ -12,23 +12,40 @@ def main():
     git_version = git_version.replace("ccf-", "")
 
     version_header = re.compile(r"## \[(.+)\]")
+    link_definition = re.compile(r"\[(.+)\]:")
 
     release_notes = ""
+
+    # Look for version_header line
     found_version = False
+    found_version_end = False
+    found_link = False
     with open("CHANGELOG.md") as f:
         while line := f.readline():
             if match := version_header.match(line):
                 if found_version:
-                    # Next version starts, we're done.
-                    break
+                    found_version_end = True
+                    if found_link:
+                        break
                 log_version = match.group(1)
                 if log_version == git_version:
                     found_version = True
+            elif match := link_definition.match(line):
+                link_version = match.group(1)
+                if link_version == git_version:
+                    found_link = True
+                    if found_version and found_version_end:
+                        break
             else:
-                if found_version:
+                if found_version and not found_version_end:
                     release_notes += line
+
     if not found_version:
         raise RuntimeError(f"git version {git_version} not found in CHANGELOG.md")
+
+    if not found_link:
+        expected_line = f"[{git_version}]: https://github.com/microsoft/CCF/releases/tag/ccf-{git_version}"
+        raise RuntimeError(f"Link to release for git version {git_version} not found in CHANGELOG.md. Add a link definition like:\n{expected_line}")
 
     release_notes = release_notes.strip()
     print(release_notes)
