932548fb2eca59e2ab518b81442d44c92c8db483
Eddy Ashton <edashton@microsoft.com>
2022-02-04 15:47:12 +0000

    Check all release notes, and add a fix option

--- scripts/extract-release-notes.py
+++ scripts/extract-release-notes.py
@@ -1,56 +1,94 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
+import argparse
 import re
-import subprocess
+import sys
 
 
 def main():
-    git_version = subprocess.run(
-        ["git", "describe", "--tags"], capture_output=True, universal_newlines=True
-    ).stdout.strip()
-    git_version = git_version.replace("ccf-", "")
+    parser = argparse.ArgumentParser(
+        description="Parses a CHANGELOG file and checks it meets some formatting expectations. "
+        "Will also extract release notes for targeted versions."
+    )
+    parser.add_argument(
+        "--target-version",
+        help="Add a version string which must be present in the CHANGELOG",
+        action="append",
+        default=[],
+    )
+    parser.add_argument(
+        "--changelog", help="Path to CHANGELOG file to parse", default="CHANGELOG.md"
+    )
+    parser.add_argument(
+        "-f",
+        "--fix",
+        help="Fix any automatically correctable errors",
+        action="store_true",
+    )
+    args = parser.parse_args()
 
     version_header = re.compile(r"## \[(.+)\]")
     link_definition = re.compile(r"\[(.+)\]:")
 
-    release_notes = ""
+    release_notes = {}
+    links_found = []
 
-    # Look for version_header line
-    found_version = False
-    found_version_end = False
-    found_link = False
-    with open("CHANGELOG.md") as f:
+    # Parse file, bucketing lines into each version's release notes
+    current_release_notes = None
+    with open(args.changelog) as f:
         while line := f.readline():
             if match := version_header.match(line):
-                if found_version:
-                    found_version_end = True
-                    if found_link:
-                        break
                 log_version = match.group(1)
-                if log_version == git_version:
-                    found_version = True
+                current_release_notes = []
+                release_notes[log_version] = current_release_notes
             elif match := link_definition.match(line):
                 link_version = match.group(1)
-                if link_version == git_version:
-                    found_link = True
-                    if found_version and found_version_end:
-                        break
+                links_found.append(link_version)
             else:
-                if found_version and not found_version_end:
-                    release_notes += line
+                if current_release_notes != None:
+                    current_release_notes.append(line.strip())
 
-    if not found_version:
-        raise RuntimeError(f"git version {git_version} not found in CHANGELOG.md")
+    documented_versions = set(release_notes.keys())
 
-    if not found_link:
-        expected_line = f"[{git_version}]: https://github.com/microsoft/CCF/releases/tag/ccf-{git_version}"
-        raise RuntimeError(
-            f"Link to release for git version {git_version} not found in CHANGELOG.md. Add a link definition like:\n{expected_line}"
-        )
+    # Check that each version has a link
+    versions_without_links = documented_versions - set(links_found)
+    if len(versions_without_links) > 0:
+        print("Missing links for following versions:")
+        for version in versions_without_links:
+            print(f"  {version}")
 
-    release_notes = release_notes.strip()
-    print(release_notes)
+    # Check that each target version is present
+    missing_target_versions = set(args.target_version) - documented_versions
+    if len(missing_target_versions) > 0:
+        print("Missing required versions:")
+        for version in missing_target_versions:
+            print(f"  {version}")
+
+    # If there were any problems, try to fix them and exit
+    if len(versions_without_links) > 0 or len(missing_target_versions) > 0:
+        if args.fix:
+            with open(args.changelog, "a") as f:
+                for version in versions_without_links:
+                    # Append presumed link
+                    f.write(
+                        f"[{version}]: https://github.com/microsoft/CCF/releases/tag/ccf-{version}\n"
+                    )
+        sys.exit(1)
+    else:
+        # File is valid.
+        if len(args.target_version) > 0:
+            # Print release notes for each target version.
+            # If multiple versions are requested, delimit and prefix each.
+            multiple = len(args.target_version) > 1
+            for i, version in enumerate(args.target_version):
+                if multiple:
+                    if i > 0:
+                        print("\n" + "-" * 80 + "\n")
+                    print(f"# {version}")
+                print("\n".join(release_notes[version]).strip())
+        else:
+            print("CHANGELOG is valid!")
 
 
 if __name__ == "__main__":
