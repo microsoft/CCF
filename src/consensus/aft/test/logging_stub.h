a1ef2a6ed84aaa5fdccc84daf1c784e6beae1c39
Julien Maffre <jumaffre@microsoft.com>
2022-02-09 11:04:34 +0000

    Fix build

--- src/consensus/aft/test/logging_stub.h
+++ src/consensus/aft/test/logging_stub.h
@@ -23,7 +23,7 @@ namespace aft
 
     LedgerStubProxy(const ccf::NodeId& id) : _id(id) {}
 
-    virtual void init(Index idx) {}
+    virtual void init(Index, Index) {}
 
     virtual void put_entry(
       const std::vector<uint8_t>& original,
