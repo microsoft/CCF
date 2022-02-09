fcf607da24ca9d19e85e346c0fa6f36e1d696afc
Julien Maffre <jumaffre@microsoft.com>
2022-02-09 13:14:12 +0000

    Completion on truncation unit test

--- src/host/test/ledger.cpp
+++ src/host/test/ledger.cpp
@@ -475,7 +475,7 @@ TEST_CASE("Truncation")
 
 TEST_CASE("Truncation and completion")
 {
-  // auto dir = AutoDeleteFolder(ledger_dir);
+  auto dir = AutoDeleteFolder(ledger_dir);
 
   size_t chunk_threshold = 30;
   Ledger ledger(ledger_dir, wf, chunk_threshold);
@@ -498,6 +498,7 @@ TEST_CASE("Truncation and completion")
     REQUIRE(number_of_files_in_ledger_dir() == chunk_count);
     REQUIRE(number_of_committed_files_in_ledger_dir() == 0);
 
+    // Truncation completed chunk so a new chunk is created on the next write
     entry_submitter.write(true);
 
     REQUIRE(number_of_files_in_ledger_dir() == chunk_count + 1);
@@ -512,11 +513,6 @@ TEST_CASE("Truncation and completion")
     // Completed file is committed
     REQUIRE(number_of_files_in_ledger_dir() == chunk_count + 1);
     REQUIRE(number_of_committed_files_in_ledger_dir() == chunk_count);
-  }
-
-  INFO("");
-  {
-    // TODO: Truncate and complete on full chunk
   }
 }
 
