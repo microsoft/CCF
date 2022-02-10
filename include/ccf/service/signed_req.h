fe68c3e895e236595ff8a03a1105a501c53b2182
Eddy Ashton <edashton@microsoft.com>
2022-02-10 16:35:33 +0000

    Remaining fixups

--- include/ccf/service/signed_req.h
+++ include/ccf/service/signed_req.h
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
 #pragma once
+
+#include "ccf/crypto.h"
 #include "ds/json.h"
 
 #include <vector>
