635432b9fd1724d2502bd6e56881e2fe003af8e0
Eddy Ashton <edashton@microsoft.com>
2022-02-11 16:53:15 +0000

    Make indexing strategy implementations private

--- include/ccf/indexing/strategies/seqnos_by_key_in_memory.h
+++ include/ccf/indexing/strategies/seqnos_by_key_in_memory.h
@@ -8,62 +8,37 @@
 namespace ccf::indexing::strategies
 {
   // A simple Strategy which stores one large map in-memory
-  template <typename M>
-  class SeqnosByKey_InMemory : public VisitEachEntryInMap
+  class SeqnosByKey_InMemory_Untyped : public VisitEachEntryInMap
   {
   protected:
     // Key is the raw value of a KV key.
     // Value is every SeqNo which talks about that key.
-    std::unordered_map<kv::untyped::SerialisedEntry, SeqNoCollection>
-      seqnos_by_key;
+    std::unordered_map<ccf::ByteVector, SeqNoCollection> seqnos_by_key;
 
     void visit_entry(
       const ccf::TxID& tx_id,
-      const kv::serialisers::SerialisedEntry& k,
-      const kv::serialisers::SerialisedEntry& v) override
-    {
-      seqnos_by_key[k].insert(tx_id.seqno);
-    }
+      const ccf::ByteVector& k,
+      const ccf::ByteVector& v) override;
 
     std::optional<SeqNoCollection> get_write_txs_impl(
-      const kv::serialisers::SerialisedEntry& serialised_key,
+      const ccf::ByteVector& serialised_key,
       ccf::SeqNo from,
       ccf::SeqNo to,
-      std::optional<size_t> max_seqnos = std::nullopt)
-    {
-      const auto it = seqnos_by_key.find(serialised_key);
-      if (it != seqnos_by_key.end())
-      {
-        SeqNoCollection& seqnos = it->second;
-        auto from_it = seqnos.lower_bound(from);
-        auto to_it = from_it;
-
-        if (
-          max_seqnos.has_value() &&
-          std::distance(from_it, seqnos.end()) > *max_seqnos)
-        {
-          std::advance(to_it, *max_seqnos);
-        }
-        else
-        {
-          to_it = seqnos.upper_bound(to);
-        }
-
-        SeqNoCollection sub_range(from_it, to_it);
-        return sub_range;
-      }
-
-      // In this case we have seen every tx in the requested range, but have not
-      // seen the target key at all
-      return SeqNoCollection();
-    }
+      std::optional<size_t> max_seqnos = std::nullopt);
 
   public:
-    SeqnosByKey_InMemory(const std::string& map_name_) :
+    SeqnosByKey_InMemory_Untyped(const std::string& map_name_) :
       VisitEachEntryInMap(map_name_, "SeqnosByKey")
     {}
+  };
 
-    SeqnosByKey_InMemory(const M& map) : SeqnosByKey_InMemory(map.get_name()) {}
+  template <typename M>
+  class SeqnosByKey_InMemory : public SeqnosByKey_InMemory_Untyped
+  {
+  public:
+    SeqnosByKey_InMemory(const M& map) :
+      SeqnosByKey_InMemory_Untyped(map.get_name())
+    {}
 
     std::optional<SeqNoCollection> get_write_txs_in_range(
       const typename M::Key& key,
--- include/ccf/indexing/strategies/visit_each_entry_in_map.h
+++ include/ccf/indexing/strategies/visit_each_entry_in_map.h
@@ -2,8 +2,8 @@
 // Licensed under the Apache 2.0 License.
 #pragma once
 
+#include "ccf/byte_vector.h"
 #include "ccf/indexing/strategy.h"
-#include "kv/store.h"
 
 namespace ccf::indexing::strategies
 {
@@ -18,40 +18,20 @@ namespace ccf::indexing::strategies
 
     virtual void visit_entry(
       const ccf::TxID& tx_id,
-      const kv::serialisers::SerialisedEntry& k,
-      const kv::serialisers::SerialisedEntry& v) = 0;
+      const ccf::ByteVector& k,
+      const ccf::ByteVector& v) = 0;
 
   public:
     VisitEachEntryInMap(
       const std::string& map_name_,
-      const std::string& strategy_prefix = "VisitEachEntryIn") :
-      Strategy(fmt::format("{} {}", strategy_prefix, map_name_)),
-      map_name(map_name_)
-    {}
+      const std::string& strategy_prefix = "VisitEachEntryIn");
+
+    virtual ~VisitEachEntryInMap() = default;
 
     void handle_committed_transaction(
-      const ccf::TxID& tx_id, const kv::StorePtr& store) override
-    {
-      // NB: Get an untyped view over the map with the same name. This saves
-      // deserialisation here, where we hand on the raw key and value.
-      auto tx = store->create_read_only_tx();
-      auto handle = tx.ro<kv::untyped::Map>(map_name);
+      const ccf::TxID& tx_id, const kv::StorePtr& store) override;
+    std::optional<ccf::SeqNo> next_requested() override;
 
-      handle->foreach([this, &tx_id](const auto& k, const auto& v) {
-        visit_entry(tx_id, k, v);
-        return true;
-      });
-      current_txid = tx_id;
-    }
-
-    std::optional<ccf::SeqNo> next_requested() override
-    {
-      return current_txid.seqno + 1;
-    }
-
-    ccf::TxID get_indexed_watermark() const
-    {
-      return current_txid;
-    }
+    ccf::TxID get_indexed_watermark() const;
   };
 }
