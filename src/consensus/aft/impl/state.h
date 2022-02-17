60e7be664d488080deb1cf1e4752c8febdeeca64
Eddy Ashton <edashton@microsoft.com>
2022-02-17 17:52:30 +0000

    Format

--- src/consensus/aft/impl/state.h
+++ src/consensus/aft/impl/state.h
@@ -3,10 +3,10 @@
 #pragma once
 
 #include "ccf/crypto/key_pair.h"
+#include "ccf/crypto/verifier.h"
 #include "ccf/ds/logger.h"
 #include "ccf/tx_status.h"
 #include "consensus/aft/raft_types.h"
-#include "ccf/crypto/verifier.h"
 #include "kv/kv_types.h"
 
 #include <map>
