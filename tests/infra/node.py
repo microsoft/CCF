18ff7af941c286f57c1fc428db19f457024fa668
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-03-11 14:41:14 +0000

    Add missing parameter

--- tests/infra/node.py
+++ tests/infra/node.py
@@ -290,7 +290,10 @@ class Node:
             self.remote.start()
 
         try:
-            self.remote.get_startup_files(self.common_dir)
+            file_timeout = kwargs.get("file_timeout")
+            self.remote.get_startup_files(
+                self.common_dir, timeout=file_timeout or infra.remote.FILE_TIMEOUT
+            )
         except Exception as e:
             LOG.exception(e)
             self.remote.get_logs(tail_lines_len=None)
