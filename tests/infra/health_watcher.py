bbe36a38fdecf91c1804c82d8ab60fe1358ec6f1
Julien Maffre <jumaffre@microsoft.com>
2022-03-15 15:36:40 +0000

    Refactor

--- tests/infra/health_watcher.py
+++ tests/infra/health_watcher.py
@@ -12,6 +12,26 @@ class HealthState(Enum):
     recovery = auto()
 
 
+def get_network_health(network, client_node_timeout_s=3):
+    primaries = {}
+    nodes = network.get_joined_nodes()
+    quorum = (len(nodes) + 1) // 2
+    LOG.success(f"Quorum is {quorum}")
+    for node in nodes:
+        with node.client() as c:
+            try:
+                r = c.get("/node/network", timeout=client_node_timeout_s).body.json()
+            except Exception as e:
+                LOG.warning(f"Could not connect to node {node.local_node_id}: {e}")
+            primaries[node.local_node_id] = (r["primary_id"], r["current_view"])
+
+    primaries_count = Counter(primaries.values())
+    most_common_primary, most_common_count = primaries_count.most_common()[0]
+    return (
+        HealthState.healthy if most_common_count < quorum else HealthState.election
+    )  # TODO: Wrong on purpose
+
+
 def watch_network_health(
     network,
     election_timeout_s=5,
@@ -19,42 +39,19 @@ def watch_network_health(
     poll_interval_s=1,
     client_node_timeout_s=3,
 ):
-    network_health_state = None
     primaries = {}
-    candidates = {}
     election_start_time = None
 
     while True:
-
-        nodes = network.get_joined_nodes()
-        quorum = (len(nodes) + 1) // 2
-        LOG.success(f"Quorum is {quorum}")
-        for node in nodes:
-            with node.client() as c:
-                try:
-                    r = c.get(
-                        "/node/network", timeout=client_node_timeout_s
-                    ).body.json()
-                except Exception as e:
-                    LOG.warning(f"Could not connect to node {node.local_node_id}: {e}")
-                primaries[node.local_node_id] = (r["primary_id"], r["current_view"])
-
-        LOG.error(primaries)
-        primaries_count = Counter(primaries.values())
-        LOG.info(primaries_count)
-        most_common_primary, most_common_count = primaries_count.most_common()[0]
-        LOG.success(most_common_primary)
-        if most_common_count < quorum:  # TODO: Wrong on purpose
-            LOG.success(
-                f"Healthy network: {most_common_count} nodes reported same primary {most_common_primary}"
-            )
-            network_health_state = HealthState.healthy
+        health_state = get_network_health(network, client_node_timeout_s)
+        if health_state == HealthState.healthy:
+            election_start_time = None
+            LOG.success(f"Healthy network")
         else:
             LOG.warning(f"Election network")
             if election_start_time is None:
                 election_start_time = time.time()
                 LOG.success(election_start_time)
-            network_health_state = HealthState.election
             LOG.info(election_timeout_factor * election_timeout_s)
             if (
                 time.time() - election_start_time
