87f9cf58ae85da4a64aaee1edd426c4ecaa7e9a1
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-03-28 12:48:01 +0000

    Nicer `or []`

--- python/ccf/receipt.py
+++ python/ccf/receipt.py
@@ -57,8 +57,7 @@ def check_endorsements(
     node_cert: Certificate, service_cert: Certificate, endorsements: List[Certificate]
 ):
     cert_i = node_cert
-    if endorsements:
-        for endorsement in endorsements:
-            check_endorsement(cert_i, endorsement)
-            cert_i = endorsement
+    for endorsement in endorsements or []:
+        check_endorsement(cert_i, endorsement)
+        cert_i = endorsement
     check_endorsement(cert_i, service_cert)
