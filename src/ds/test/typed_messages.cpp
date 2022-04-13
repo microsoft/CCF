f486bc00df94b29e85ab81bd5511edb7ae1ceef3
Eddy Ashton <edashton@microsoft.com>
2022-04-13 13:35:47 +0100

    Support variably sized elements in the middle of ringbuffer messages (#3764)

--- src/ds/test/typed_messages.cpp
+++ src/ds/test/typed_messages.cpp
@@ -9,6 +9,7 @@ enum : ringbuffer::Message
   DEFINE_RINGBUFFER_MSG_TYPE(large_block_message),
   DEFINE_RINGBUFFER_MSG_TYPE(large_compound_message),
   DEFINE_RINGBUFFER_MSG_TYPE(large_complex_message),
+  DEFINE_RINGBUFFER_MSG_TYPE(multi_variable_size_a),
   DEFINE_RINGBUFFER_MSG_TYPE(finish),
 };
 
@@ -25,12 +26,19 @@ DECLARE_RINGBUFFER_MESSAGE_PAYLOAD(
   uint16_t,
   uint64_t,
   std::vector<uint8_t>);
+DECLARE_RINGBUFFER_MESSAGE_PAYLOAD(
+  multi_variable_size_a,
+  serializer::ByteRange,
+  std::vector<uint8_t>,
+  unsigned short int,
+  serializer::ByteRange,
+  std::vector<uint8_t>);
 DECLARE_RINGBUFFER_MESSAGE_PAYLOAD(finish);
 
 TEST_CASE(
   "Large message reconstruction" * doctest::test_suite("typed_messages"))
 {
-  constexpr size_t buf_size = 1 << 8;
+  constexpr size_t buf_size = 1 << 10;
 
   auto buffer = std::make_unique<ringbuffer::TestBuffer>(buf_size);
   ringbuffer::Reader rr(buffer->bd);
@@ -143,5 +151,73 @@ TEST_CASE(
     RINGBUFFER_WRITE_MESSAGE(finish, writer_p);
     bp.run(rr);
     REQUIRE(message_seen);
+  }
+
+  SUBCASE("multiple variable sized args")
+  {
+    size_t messages_seen = 0;
+
+    std::vector<uint8_t> a{1, 2, 3};
+    std::vector<uint8_t> b{1, 2, 3, 4, 5, 6};
+    std::vector<uint8_t> c{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<uint8_t> d{
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    unsigned short int u = 42;
+
+    DISPATCHER_SET_MESSAGE_HANDLER(
+      bp, multi_variable_size_a, [&](const uint8_t* data, size_t size) {
+        auto [aa, bb, uu, cc, dd] =
+          ringbuffer::read_message<multi_variable_size_a>(data, size);
+
+        // To avoid "error: reference to local binding 'XX' declared in
+        // enclosing lambda expression", reassign all interesting result values
+        const auto aa_size = aa.size;
+        REQUIRE(aa_size == a.size());
+        const auto aa_data = aa.data;
+        REQUIRE(memcmp(a.data(), aa_data, a.size()) == 0);
+
+        const auto bb_size = bb.size();
+        REQUIRE(bb_size == b.size());
+        const auto bb_data = bb.data();
+        REQUIRE(memcmp(b.data(), bb_data, b.size()) == 0);
+
+        const auto uu_ = uu;
+        REQUIRE(u == uu_);
+
+        const auto cc_size = cc.size;
+        REQUIRE(cc_size == c.size());
+        const auto cc_data = cc.data;
+        REQUIRE(memcmp(c.data(), cc_data, c.size()) == 0);
+
+        const auto dd_size = dd.size();
+        REQUIRE(dd_size == d.size());
+        const auto dd_data = dd.data();
+        REQUIRE(memcmp(d.data(), dd_data, d.size()) == 0);
+
+        ++messages_seen;
+      });
+
+    RINGBUFFER_WRITE_MESSAGE(multi_variable_size_a, writer_p, a, b, u, c, d);
+    RINGBUFFER_WRITE_MESSAGE(finish, writer_p);
+    bp.run(rr);
+    REQUIRE(messages_seen == 1);
+
+    bp.set_finished(false);
+    std::swap(a, d);
+    std::swap(c, d);
+    ++u;
+    RINGBUFFER_WRITE_MESSAGE(multi_variable_size_a, writer_p, a, b, u, c, d);
+    RINGBUFFER_WRITE_MESSAGE(finish, writer_p);
+    bp.run(rr);
+    REQUIRE(messages_seen == 2);
+
+    bp.set_finished(false);
+    std::swap(a, b);
+    std::swap(b, c);
+    ++u;
+    RINGBUFFER_WRITE_MESSAGE(multi_variable_size_a, writer_p, a, b, u, c, d);
+    RINGBUFFER_WRITE_MESSAGE(finish, writer_p);
+    bp.run(rr);
+    REQUIRE(messages_seen == 3);
   }
 }
