241a5f0b97c15d6b7a155f9ccdba3d0d720edde7
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-05-24 09:46:40 +0000

    Copyright notices

--- src/node/acme_client.h
+++ src/node/acme_client.h
@@ -1,5 +1,5 @@
-// Licensed under the Apache 2.0 License.
 // Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache 2.0 License.
 #pragma once
 
 #include "ccf/crypto/base64.h"
