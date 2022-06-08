661966966192c2416b1a928af7b71f33c7a410c6
Eddy Ashton <edashton@microsoft.com>
2022-06-08 21:40:33 +0100

    Add endpoint to get previous service identity (#3880)

---
+++ src/service/tables/previous_service_identity.h
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache 2.0 License.
+#pragma once
+
+#include "ccf/crypto/pem.h"
+#include "ccf/kv/value.h"
+
+#include <string>
+#include <vector>
+
+namespace ccf
+{
+  using PreviousServiceIdentity = ServiceValue<crypto::Pem>;
+
+  namespace Tables
+  {
+    static constexpr auto PREVIOUS_SERVICE_IDENTITY =
+      "public:ccf.gov.service.previous_service_identity";
+  }
+}
