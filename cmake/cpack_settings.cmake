dfcb3b40218d4d01a97a1b0b0700e71f465467db
Amaury Chamayou <amchamay@microsoft.com>
2022-06-14 20:45:43 +0200

    [release/2.x] Cherry pick: Unsafe different project (#3894) (#3933)

--- cmake/cpack_settings.cmake
+++ cmake/cpack_settings.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-set(CPACK_PACKAGE_NAME "ccf")
+set(CPACK_PACKAGE_NAME ${CCF_PROJECT})
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Confidential Consortium Framework")
 set(CPACK_PACKAGE_CONTACT "https://github.com/Microsoft/CCF")
 set(CPACK_RESOURCE_FILE_LICENSE "${CCF_DIR}/LICENSE")
@@ -19,7 +19,7 @@ endif()
 
 # CPack variables for Debian packages
 set(CPACK_DEBIAN_PACKAGE_DEPENDS
-    "open-enclave (>=0.17.6), libuv1 (>= 1.34.2), libc++1-10, libc++abi1-10, openssl (>=1.1.1)"
+    "open-enclave (>=0.17.7), libuv1 (>= 1.34.2), libc++1-10, libc++abi1-10, openssl (>=1.1.1)"
 )
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 
