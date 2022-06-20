c5d9740d3f4370527130302992dee06c7baba156
Julien Maffre <jumaffre@microsoft.com>
2022-06-20 12:26:22 +0100

    Daily canary

--- tests/partitions_test.py
+++ tests/partitions_test.py
@@ -320,6 +320,7 @@ def test_learner_does_not_take_part(network, args):
                 primary,
                 new_node.node_id,
                 timeout=ceil(args.join_timer_s * 2),
+                valid_from=datetime.utcnow(),
             )
         except TimeoutError:
             LOG.info("Trust node proposal did not commit as expected")
