7ae9169366bb455619139aeaf4124b82adccaa88
Julien Maffre <42961061+jumaffre@users.noreply.github.com>
2022-06-30 16:34:59 +0100

    Report `TxID` at which current service was created (#3996) (#4002)
    (cherry picked from commit 7b2309047eb2877a40e4e59aed3642db76b64182)
    
    # Conflicts:
    #	CHANGELOG.md
    #	src/node/node_state.h

--- include/ccf/service/tables/service.h
+++ include/ccf/service/tables/service.h
@@ -5,6 +5,7 @@
 #include "ccf/crypto/pem.h"
 #include "ccf/ds/json.h"
 #include "ccf/service/map.h"
+#include "ccf/tx_id.h"
 
 namespace ccf
 {
@@ -29,15 +30,20 @@ namespace ccf
     crypto::Pem cert;
     /// Status of the service
     ServiceStatus status = ServiceStatus::OPENING;
-    /// Version of previous service identity (before the last recovery)
+    /// Version (seqno) of previous service identity (before the last recovery)
     std::optional<kv::Version> previous_service_identity_version = std::nullopt;
     /// Number of disaster recoveries performed on this service
     std::optional<size_t> recovery_count = std::nullopt;
+    /// TxID at which current service was created
+    std::optional<ccf::TxID> current_service_create_txid = std::nullopt;
   };
   DECLARE_JSON_TYPE_WITH_OPTIONAL_FIELDS(ServiceInfo);
   DECLARE_JSON_REQUIRED_FIELDS(ServiceInfo, cert, status);
   DECLARE_JSON_OPTIONAL_FIELDS(
-    ServiceInfo, previous_service_identity_version, recovery_count);
+    ServiceInfo,
+    previous_service_identity_version,
+    recovery_count,
+    current_service_create_txid);
 
   // As there is only one service active at a given time, it is stored in single
   // Value in the KV
