06a65b75e99526169a719e8fb0ee3439d8d8eb67
Julien Maffre <jumaffre@microsoft.com>
2022-07-19 09:00:53 +0000

    Drop async tasks at the end of test

--- src/consensus/aft/test/driver.h
+++ src/consensus/aft/test/driver.h
@@ -209,6 +209,11 @@ public:
     }
   }
 
+  ~RaftDriver()
+  {
+    threading::ThreadMessaging::thread_messaging.drop_tasks();
+  }
+
   void create_new_node(std::string node_id_s)
   {
     ccf::NodeId node_id(node_id_s);
