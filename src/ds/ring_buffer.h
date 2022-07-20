2b7752756ac8ed70f253d144124b3add7ba50c82
Eddy Ashton <ashton.eddy@gmail.com>
2022-07-20 09:28:58 +0100

    Update src/ds/ring_buffer.h
    Co-authored-by: Amaury Chamayou <amaury@xargs.fr>

--- src/ds/ring_buffer.h
+++ src/ds/ring_buffer.h
@@ -382,7 +382,7 @@ namespace ringbuffer
   private:
     // We use this to detect whether the head is ahead of the tail. In real
     // operation they should be close to each, relative to the total range of a
-    // uin64_t. To handle wrap-around (ie - when a has overflowed past the max
+    // uint64_t. To handle wrap-around (ie - when a write has overflowed past the max
     // value), we consider it larger if the distance between a and b is less
     // than half the total range (and positive).
     static bool greater_with_wraparound(size_t a, size_t b)
