006b1ea1f557727b40574e15dcdb5c0fca24db1e
Amaury Chamayou <amaury@xargs.fr>
2022-08-15 11:54:28 +0100

    Update src/kv/kv_types.h
    Co-authored-by: Eddy Ashton <ashton.eddy@gmail.com>

--- src/kv/kv_types.h
+++ src/kv/kv_types.h
@@ -798,7 +798,7 @@ struct formatter<kv::Configuration::Nodes>
 
   template <typename FormatContext>
   auto format(const kv::Configuration::Nodes& nodes, FormatContext& ctx) const
-    -> decltype(ctx.out()) const
+    -> decltype(ctx.out())
   {
     std::set<ccf::NodeId> node_ids;
     for (auto& [nid, _] : nodes)
