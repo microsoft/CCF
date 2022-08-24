6a3d4c544451edcad1f487ae948a01c4600208c8
Eddy Ashton <edashton@microsoft.com>
2022-08-24 13:10:29 +0000

    Add copyright notice

--- src/apps/external_executor/protobuf/build.sh
+++ src/apps/external_executor/protobuf/build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
 THIS_DIR=$( dirname "${BASH_SOURCE[0]}" )
 
 if [ ! -f "env/bin/activate" ]
