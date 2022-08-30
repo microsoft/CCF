78c0564fc7f662b9cb1d75ea134c416af3cef10d
Julien Maffre <jumaffre@microsoft.com>
2022-08-30 14:13:11 +0000

    fmt

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -1,5 +1,5 @@
-# Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the
-# MIT license.
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
 
 set(protobuf_BUILD_SHARED_LIBS_DEFAULT OFF)
 set(protobuf_BUILD_TESTS OFF)
@@ -7,7 +7,9 @@ set(protobuf_WITH_ZLIB OFF)
 set(protobuf_BUILD_PROTOC_BINARIES OFF)
 set(protobuf_INSTALL OFF)
 
-set(CMAKE_POLICY_DEFAULT_CMP0077 NEW) # Removes warnings when setting "protobuf_..." variable above
+set(CMAKE_POLICY_DEFAULT_CMP0077 NEW) # Removes warnings when setting
+
+# "protobuf_..." variable above
 add_subdirectory(${CCF_3RD_PARTY_INTERNAL_DIR}/protobuf EXCLUDE_FROM_ALL)
 
 add_custom_target(dummy ALL DEPENDS libprotobuf)
@@ -18,11 +20,17 @@ get_target_property(LIBPROTOBUF_INCLUDE_DIRS libprotobuf INCLUDE_DIRECTORIES)
 # enclave
 add_enclave_library(protobuf.enclave ${LIBPROTOBUF_SOURCES})
 target_include_directories(protobuf.enclave PUBLIC ${LIBPROTOBUF_INCLUDE_DIRS})
-target_compile_options(protobuf.enclave PUBLIC "-Wno-deprecated-enum-enum-conversion") # Remove warnings in generated_message_tctable_impl.h
-target_compile_options(protobuf.enclave PUBLIC "-Wno-invalid-noreturn") # https://github.com/protocolbuffers/protobuf/issues/9817
+target_compile_options(
+    protobuf.enclave PUBLIC "-Wno-deprecated-enum-enum-conversion"
+) # Remove warnings in generated_message_tctable_impl.h
+target_compile_options(protobuf.enclave PUBLIC "-Wno-invalid-noreturn"
+) # https://github.com/protocolbuffers/protobuf/issues/9817
 
 # virtual
 add_host_library(protobuf.virtual ${LIBPROTOBUF_SOURCES})
 target_include_directories(protobuf.virtual PUBLIC ${LIBPROTOBUF_INCLUDE_DIRS})
-target_compile_options(protobuf.virtual PUBLIC "-Wno-deprecated-enum-enum-conversion") # Remove warnings in generated_message_tctable_impl.h
-target_compile_options(protobuf.virtual PUBLIC "-Wno-invalid-noreturn") # https://github.com/protocolbuffers/protobuf/issues/9817
+target_compile_options(
+    protobuf.virtual PUBLIC "-Wno-deprecated-enum-enum-conversion"
+) # Remove warnings in generated_message_tctable_impl.h
+target_compile_options(protobuf.virtual PUBLIC "-Wno-invalid-noreturn"
+) # https://github.com/protocolbuffers/protobuf/issues/9817
