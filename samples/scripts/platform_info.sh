a7459ce8098f895ae7d914fd650ecd2fa9ae08bf
Julien Maffre <jumaffre@microsoft.com>
2022-09-01 10:39:42 +0000

    Rename `sgxinfo.sh` to `platform_info.sh` to cater for AMD SEV-SNP

---
+++ samples/scripts/platform_info.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
+set +e
+
+# SEV-SNP
+AMD_SEV_SNP_DEVICE="/dev/sev"
+if test -c "$AMD_SEV_SNP_DEVICE"; then
+    echo "AMD SEV-SNP:"
+    echo "$AMD_SEV_SNP_DEVICE detected."
+    exit 0
+fi
+
+# SGX
+if grep -q "^flags.*sgx.*" < /proc/cpuinfo ; then
+    echo "LINUX KERNEL WITH BUILT-IN SGX SUPPORT (5.11+):"
+    uname -r
+else
+    # Pre 5.11 kernel
+    echo "DRIVER INFO:"
+    modinfo intel_sgx
+    echo ""
+    echo ""
+    echo "DRIVER LOADED:"
+    lsmod | grep intel_sgx || echo "Not loaded"
+fi
+echo ""
+echo ""
+
+echo "AESM DAEMON:"
+systemctl --no-pager status aesmd 2>/dev/null || echo "Not running"
+echo ""
+echo ""
+
+echo "PSW INFO:"
+apt list --installed 2>/dev/null | grep libsgx
+echo ""
+echo ""
+
+echo "DCAP CLIENT INFO:"
+apt list --installed 2>/dev/null | grep az-dcap
+echo ""
+echo ""
+
+echo "SGX INFO:"
+/opt/openenclave/bin/oesgx
