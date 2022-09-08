0e7fce4a5a06a4ea0a01e4c0da22a91db2e4a83d
Amaury Chamayou <amchamay@microsoft.com>
2022-09-08 12:45:15 +0000

    wip

--- tests/signing.py
+++ tests/signing.py
@@ -11,12 +11,21 @@ from cose.keys.curves import P256, P384, P521
 from cose.keys.keyparam import EC2KpCurve, EC2KpX, EC2KpY, EC2KpD
 from cose.messages import Sign1Message
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey, EllipticCurvePublicKey
+from cryptography.hazmat.primitives.asymmetric.ec import (
+    EllipticCurvePrivateKey,
+    EllipticCurvePublicKey,
+)
 from cryptography.hazmat.backends import default_backend
 from cryptography.x509 import load_pem_x509_certificate
-from cryptography.hazmat.primitives.serialization import load_pem_private_key, Encoding, PublicFormat
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.serialization import (
+    load_pem_private_key,
+    Encoding,
+    PublicFormat,
+)
 
 Pem = str
+
 
 def from_cryptography_eckey_obj(ext_key) -> EC2Key:
     """
@@ -24,34 +33,39 @@ def from_cryptography_eckey_obj(ext_key) -> EC2Key:
     :param ext_key: Python cryptography key.
     :return: an initialized EC key
     """
-    if hasattr(ext_key, 'private_numbers'):
+    if hasattr(ext_key, "private_numbers"):
         priv_nums = ext_key.private_numbers()
         pub_nums = priv_nums.public_numbers
     else:
         priv_nums = None
         pub_nums = ext_key.public_numbers()
 
-    if pub_nums.curve.name == 'secp256r1':
+    if pub_nums.curve.name == "secp256r1":
         curve = P256
-    elif pub_nums.curve.name == 'secp384r1':
+    elif pub_nums.curve.name == "secp384r1":
         curve = P384
-    elif pub_nums.curve.name == 'secp521r1':
+    elif pub_nums.curve.name == "secp521r1":
         curve = P521
     else:
         raise NotImplementedError("unsupported curve")
 
     cose_key = {}
     if pub_nums:
-        cose_key.update({
-            EC2KpCurve: curve,
-            EC2KpX: pub_nums.x.to_bytes(curve.size, 'big'),
-            EC2KpY: pub_nums.y.to_bytes(curve.size, 'big'),
-        })
+        cose_key.update(
+            {
+                EC2KpCurve: curve,
+                EC2KpX: pub_nums.x.to_bytes(curve.size, "big"),
+                EC2KpY: pub_nums.y.to_bytes(curve.size, "big"),
+            }
+        )
     if priv_nums:
-        cose_key.update({
-            EC2KpD: priv_nums.private_value.to_bytes(curve.size, 'big'),
-        })
+        cose_key.update(
+            {
+                EC2KpD: priv_nums.private_value.to_bytes(curve.size, "big"),
+            }
+        )
     return EC2Key.from_dict(cose_key)
+
 
 def default_algorithm_for_key(key) -> str:
     """
@@ -68,38 +82,68 @@ def default_algorithm_for_key(key) -> str:
         else:
             raise NotImplementedError("unsupported curve")
     else:
-        raise NotImplementedError('unsupported key type')
+        raise NotImplementedError("unsupported key type")
+
 
 def get_priv_key_type(priv_pem: str) -> str:
     key = load_pem_private_key(priv_pem.encode("ascii"), None, default_backend())
     if isinstance(key, EllipticCurvePrivateKey):
-        return 'ec'
-    raise NotImplementedError('unsupported key type')
+        return "ec"
+    raise NotImplementedError("unsupported key type")
 
-def create_cose_sign1(payload: bytes, key_priv_pem: Pem, cert_pem: Pem, additional_headers: Optional[dict] = None) -> bytes:
+
+def cert_fingerprint(cert_pem: Pem):
+    cert = load_pem_x509_certificate(cert_pem.encode("ascii"), default_backend())
+    return cert.fingerprint(hashes.SHA256()).hex().encode("utf-8")
+
+
+def create_cose_sign1(
+    payload: bytes,
+    key_priv_pem: Pem,
+    cert_pem: Pem,
+    additional_headers: Optional[dict] = None,
+) -> bytes:
     key_type = get_priv_key_type(key_priv_pem)
 
     cert = load_pem_x509_certificate(cert_pem.encode("ascii"), default_backend())
-    pub_key = cert.public_key()
-    alg = default_algorithm_for_key(pub_key)
-    der = pub_key.public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
-    kid = hashlib.sha256(der).hexdigest()
+    alg = default_algorithm_for_key(cert.public_key())
+    kid = cert_fingerprint(cert_pem)
 
-    headers = {
-        cose.headers.Algorithm: alg,
-        cose.headers.KID: kid.encode('utf-8')
-    }
+    headers = {cose.headers.Algorithm: alg, cose.headers.KID: kid}
     headers.update(additional_headers or {})
     msg = Sign1Message(phdr=headers, payload=payload)
-    
+
     key = load_pem_private_key(key_priv_pem.encode("ascii"), None, default_backend())
-    if key_type == 'ec':
+    if key_type == "ec":
         cose_key = from_cryptography_eckey_obj(key)
     else:
-        raise NotImplementedError('unsupported key type')
+        raise NotImplementedError("unsupported key type")
     msg.key = cose_key
 
-    return msg.encode(tag=True) #TODO: what does tag=True do?
+    return msg.encode(tag=True)  # TODO: what does tag=True do?
+
+
+def get_cert_key_type(cert_pem: str) -> str:
+    cert = load_pem_x509_certificate(cert_pem.encode("ascii"), default_backend())
+    if isinstance(cert.public_key(), EllipticCurvePublicKey):
+        return "ec"
+    raise NotImplementedError("unsupported key type")
+
+
+def verify_cose_sign1(buf: bytes, cert_pem: str):
+    key_type = get_cert_key_type(cert_pem)
+    cert = load_pem_x509_certificate(cert_pem.encode("ascii"), default_backend())
+    key = cert.public_key()
+    if key_type == "ec":
+        cose_key = from_cryptography_eckey_obj(key)
+    else:
+        raise NotImplementedError("unsupported key type")
+    msg = Sign1Message.decode(buf)
+    msg.key = cose_key
+    if not msg.verify_signature():
+        raise ValueError("signature is invalid")
+    return msg
+
 
 PRIV = """-----BEGIN EC PARAMETERS-----
 BgUrgQQAIg==
@@ -128,5 +172,11 @@ b2H04E57XZmVdg==
 
 if __name__ == "__main__":
     signed_statement = create_cose_sign1(b"hello", PRIV, PUB)
-    print(signed_statement)
-    # TODO verify
+    msg = verify_cose_sign1(signed_statement, PUB)
+    assert msg.phdr[cose.headers.KID] == cert_fingerprint(PUB), (
+        msg.phdr[cose.headers.KID],
+        cert_fingerprint(PUB),
+    )
+    # TODO sample with governance tags
+    # TODO map to existing APIs
+    # TODO detached content
