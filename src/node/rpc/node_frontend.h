ac510b55d438695ab02c0e7014ae5133b0b7ed71
Dominic Ayre <domayre@outlook.com>
2022-10-05 15:12:44 +0100

    Update src/node/rpc/node_frontend.h
    Co-authored-by: Julien Maffre <42961061+jumaffre@users.noreply.github.com>

--- src/node/rpc/node_frontend.h
+++ src/node/rpc/node_frontend.h
@@ -156,7 +156,7 @@ namespace ccf
             "Quote does not contain a security policy digest");
         case QuoteVerificationResult::FailedInvalidSecurityPolicy:
           return std::make_pair(
-            HTTP_STATUS_UNAUTHORIZED, "Quote security policy isn't authorized");
+            HTTP_STATUS_UNAUTHORIZED, "Quote security policy is not authorised");
         default:
           return std::make_pair(
             HTTP_STATUS_INTERNAL_SERVER_ERROR,
