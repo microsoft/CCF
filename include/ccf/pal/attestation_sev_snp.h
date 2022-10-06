c7fbabafc6873b84a520c751afdb418e044e5720
Dominic Ayre <dominicayre@microsoft.com>
2022-10-06 10:45:01 +0000

    Fix ci-check for private header include

--- include/ccf/pal/attestation_sev_snp.h
+++ include/ccf/pal/attestation_sev_snp.h
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 License.
 #pragma once
 
-#include "attestation_sev_snp_endorsements.h"
+#include "ccf/pal/attestation_sev_snp_endorsements.h"
 
 #include <array>
 #include <map>
