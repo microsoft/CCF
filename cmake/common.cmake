2952d03219a20df70ade96b89b6e2a702a7cd3ec
Christoph M. Wintersteiger <cwinter@microsoft.com>
2022-10-07 17:24:39 +0000

    Add missing link library

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -279,6 +279,7 @@ target_link_libraries(
           ${CMAKE_THREAD_LIBS_INIT}
           ${LINK_LIBCXX}
           ccfcrypto.host
+          ravl.host
 )
 if("sgx" IN_LIST COMPILE_TARGETS)
   target_link_libraries(cchost PRIVATE openenclave::oehost)
