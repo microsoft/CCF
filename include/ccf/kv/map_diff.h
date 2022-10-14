cf4b0ae1279c6f3e27b733963002704a124e5f50
Andrew Jeffery <dev@jeffas.io>
2022-10-14 15:26:55 +0000

    Update comments on diff

--- include/ccf/kv/map_diff.h
+++ include/ccf/kv/map_diff.h
@@ -24,16 +24,10 @@ namespace kv
 
     /** Get value for key.
      *
-     * This returns the value for the key as seen by this transaction. If the
-     * key has been updated in the current transaction, that update will be
-     * reflected in the return of this call. Where the key has not been
-     * modified, this returns the state of a snapshot version from the start of
-     * the transaction's execution.
-     *
      * @param key Key to read
      *
-     * @return Optional containing associated value, or empty if the key doesn't
-     * exist
+     * @return nullopt if key does not exist, optional<nullopt> if key exists
+     * but was deleted and the value if it exists and was not deleted.
      */
     std::optional<std::optional<V>> get(const K& key)
     {
@@ -50,10 +44,7 @@ namespace kv
       return std::nullopt;
     }
 
-    /** Test if key is present.
-     *
-     * This obeys the same rules as @c get regarding key visibility, but is more
-     * efficient if you do not need the associated value.
+    /** Test if key is present and not deleted.
      *
      * @param key Key to read
      *
@@ -66,12 +57,9 @@ namespace kv
 
     /** Test if key is deleted in this diff.
      *
-     * This obeys the same rules as @c get regarding key visibility, but is more
-     * efficient if you do not need the associated value.
-     *
      * @param key Key to read
      *
-     * @return Boolean true iff key exists
+     * @return Boolean true iff key was deleted
      */
     bool is_deleted(const K& key)
     {
@@ -81,27 +69,14 @@ namespace kv
     /** Iterate over all entries in the map.
      *
      * The passed functor should have the signature
-     * `bool(const K& k, const V& v)`.
+     * `bool(const K& k, const std::optional<V>& v)`.
      * The iteration order is undefined.
      * Return true to continue iteration, or return false from any invocation to
      * terminate the iteration at that point - the functor will not be invoked
      * again after it returns false.
      *
-     * The set of key-value entries which will be iterated over is determined at
-     * the point foreach is called, and does not include any modifications made
-     * by the functor. This means:
-     * - If the functor sets a value V at a new key K', the functor will not be
-     * called for (K', V)
-     * - If the functor changes the value at key K from V to V', the functor
-     * will be called with the old value (K, V), not the new value (K, V')
-     * - If the functor removes K, the functor will still be called for (K, V)
-     *
-     * Calling @c get will always return the true latest state; the iterator
-     * visibility described above only applies to the keys and values passed to
-     * this functor.
-     *
      * @tparam F Functor type. Should usually be derived implicitly from f
-     * @param f Functor instance, taking (const K& k, const V& v) and returning
+     * @param f Functor instance, taking (const K& k, const std::optional<V>& v) and returning
      * a bool. Return value determines whether the iteration should continue
      * (true) or stop (false)
      */
@@ -131,9 +106,6 @@ namespace kv
 
     /** Iterate over all keys in the map.
      *
-     * Similar to @c foreach but the functor takes a single key argument rather
-     * than a key and value. Avoids deserialisation of values.
-     *
      * @tparam F Functor type. Should usually be derived implicitly from f
      * @param f Functor instance, taking (const K& k) and returning
      * a bool. Return value determines whether the iteration should continue
@@ -152,11 +124,8 @@ namespace kv
 
     /** Iterate over all values in the map.
      *
-     * Similar to @c foreach but the functor takes a single value argument
-     * rather than a key and value. Avoids deserialisation of keys.
-     *
      * @tparam F Functor type. Should usually be derived implicitly from f
-     * @param f Functor instance, taking (const V& v) and returning
+     * @param f Functor instance, taking (const std::optional<V>& v) and returning
      * a bool. Return value determines whether the iteration should continue
      * (true) or stop (false)
      */
@@ -179,10 +148,6 @@ namespace kv
     }
 
     /** Returns number of entries in this map.
-     *
-     * This is the count of all currently present keys, including both those
-     * which were already committed and any modifications (taking into account
-     * new additions or removals) that have been made during this transaction.
      *
      * @return Count of entries
      */
