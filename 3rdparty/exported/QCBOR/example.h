1bbf8886f3ef6dba0c446b597bb94a3ac505b768
Amaury Chamayou <amchamay@microsoft.com>
2022-10-18 11:01:54 +0100

    Cose signing authn (#4306)
    * .
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * tag=True does not do anything because it is default
    
    * attach, detach...
    
    * gov
    
    * deps
    
    * fix
    
    * wip
    
    * .
    
    * wip
    
    * wip
    
    * checks
    
    * wip
    
    * .
    
    * verifier
    
    * verifier
    
    * plug stuff in
    
    * fmt
    
    * .
    
    * wip
    
    * fix
    
    * rest
    
    * move test
    
    * fix
    
    * tweaks
    
    * cmake
    
    * cmake_fix
    
    * schema
    
    * Update include/ccf/common_auth_policies.h
    
    Co-authored-by: Eddy Ashton <ashton.eddy@gmail.com>
    
    * feedback
    
    * span span span
    
    * .
    
    * Turn off colours in the governance test
    
    Co-authored-by: Eddy Ashton <ashton.eddy@gmail.com>

---
+++ 3rdparty/exported/QCBOR/example.h
@@ -0,0 +1,20 @@
+/*==============================================================================
+ example.h -- QCBOR encode and decode example
+
+ Copyright (c) 2020, Laurence Lundblade. All rights reserved.
+
+ SPDX-License-Identifier: BSD-3-Clause
+
+ See BSD-3-Clause license in README.md
+
+ Created on 6/30/20
+ =============================================================================*/
+
+#ifndef qcborExample_h
+#define qcborExample_h
+
+#include <stdint.h>
+
+int32_t RunQCborExample(void);
+
+#endif /* qcborExample_h */
