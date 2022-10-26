2b8295019a489e01c1ad76036c4687728dabb7a1
Julien Maffre <jumaffre@microsoft.com>
2022-10-26 16:48:15 +0000

    .

--- tests/infra/node.py
+++ tests/infra/node.py
@@ -151,7 +151,7 @@ class Node:
         self.initial_node_data_json_file = node_data_json_file
         self.label = None
 
-        if os.getenv("CONTAINER_NODES") and not infra.is_snp.IS_SNP:
+        if os.getenv("CONTAINER_NODES"):
             self.remote_shim = infra.remote_shim.DockerShim
         else:
             self.remote_shim = infra.remote_shim.PassThroughShim
