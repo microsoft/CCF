2199cfe5e1ddc84dd13e2fe0e998f9855b5d8f42
Dominic Ayre <dominicayre@microsoft.com>
2022-10-31 17:24:55 +0000

    Copyrights

--- scripts/cleanup_snp_aci.py
+++ scripts/cleanup_snp_aci.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
 import os
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
