44d48112fb42e4d3cb642fc55551ef8307549c20
Fotis Kounelis <fotisss17@gmail.com>
2022-11-01 11:27:01 +0000

    update the default values in argparse

--- perf-system/generator/generate_packages.py
+++ perf-system/generator/generate_packages.py
@@ -10,48 +10,48 @@ def main():
     The function to receive the arguments
     from the command line
     """
-    arg_host = "https://127.0.0.1:8000"
-    arg_path = "/app/log/private"  # default path to request
-    arg_request_type = "HTTP/1.1"  # default type
-    arg_verb = "POST"  # default verb
-    arg_content_type = "application/json"
-    arg_iterations = 16
-    arg_path_to_parquet = "requests.parquet"
-    arg_data = '{"id": 1, "msg": "Send message with id 1"}'
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Generator for perf workloads",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     parser.add_argument(
         "-hs",
         "--host",
-        help="The host to submit the request. Default `https://127.0.0.1:8000`",
+        help="The host to submit the request.",
+        default="127.0.0.1:8000",
         type=str,
     )
     parser.add_argument(
         "-p",
         "--path",
-        help="The relative path to submit the request. Default `app/log/private`",
+        help="The relative path to submit the request.",
+        default="app/log/private",
         type=str,
     )
     parser.add_argument(
-        "-vr",
-        "--verb",
-        help="The request action. Default `POST`",
+        "-vr", "--verb", help="The request action.", default="POST", type=str
     )
     parser.add_argument(
-        "-r", "--rows", help="The number of requests to send. Default `16` ", type=int
+        "-r",
+        "--rows",
+        default=16,
+        help="The number of requests to send.",
+        type=int,
     )
     parser.add_argument(
         "-rt",
         "--request_type",
-        help="The transfer protocol for the request.\
-            Default type `HTTP/1.1`",
+        help="The transfer protocol for the request.",
+        default="HTTP/1.1",
         type=str,
     )
     parser.add_argument(
         "-pf",
         "--path_to_parquet",
         help="Path to the parquet file to store the\
-            generated requests. Default path `./requests.parquet`",
+            generated requests",
+        default="./requests.parquet",
         type=str,
     )
     parser.add_argument(
@@ -60,12 +60,14 @@ def main():
         help="he Content-Type representation header is used\
             to indicate the original media type of the resource.\
             Default `application-json`",
+        default="application-json",
         type=str,
     )
     parser.add_argument(
         "-d",
         "--data",
         help="A string with the data to be sent with a request",
+        default='{"id": 1, "msg": "Send message with id 1"}',
         type=str,
     )
 
@@ -73,16 +75,16 @@ def main():
 
     msg = Messages()
     msg.append(
-        args.host or arg_host,
-        args.path or arg_path,
-        args.verb or arg_verb,
-        args.request_type or arg_request_type,
-        args.content_type or arg_content_type,
-        args.data or arg_data,
-        args.rows or arg_iterations,
+        args.host,
+        args.path,
+        args.verb,
+        args.request_type,
+        args.content_type,
+        args.data,
+        args.rows,
     )
 
-    msg.to_parquet_file(args.path_to_parquet or arg_path_to_parquet)
+    msg.to_parquet_file(args.path_to_parquet)
 
 
 if __name__ == "__main__":
