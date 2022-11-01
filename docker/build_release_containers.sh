89ee25e0c97a8996cec47c018f3934f170885bf8
Julien Maffre <jumaffre@microsoft.com>
2022-11-01 17:02:35 +0000

    Cleanup containers GH action

--- docker/build_release_containers.sh
+++ docker/build_release_containers.sh
@@ -2,26 +2,26 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
+# Builds all release containers, for all target platforms
+
 set -ex
 
-REGISTRY="${1}"
-
-VERSION=${2:-"latest"} # TODO: defaults to latest
-
+ACR_REGISTRY="${1}"
+VERSION=${2:-"latest"}
 platforms="sgx snp virtual"
 
 for platform in $platforms; do
 
-    echo $platform
+    echo "** Building release containers for ${platform} platform"
 
     # Dev
-    docker build -f docker/app_dev . --build-arg="platform=${platform}" --build-arg="ansible_vars=ccf_ver=${VERSION}" -t $REGISTRY/public/ccf/app/dev:${VERSION}-${platform}
+    docker build -f docker/app_dev . --build-arg="platform=${platform}" --build-arg="ansible_vars=ccf_ver=${VERSION}" -t "$ACR_REGISTRY/public/ccf/app/dev:${VERSION}-${platform}"
 
     # Run
-    docker build -f docker/app_run . --build-arg="platform=${platform}" --build-arg="ansible_vars=ccf_ver=${VERSION}" -t $REGISTRY/public/ccf/app/run:${VERSION}-${platform}
+    docker build -f docker/app_run . --build-arg="platform=${platform}" --build-arg="ansible_vars=ccf_ver=${VERSION}" -t "$ACR_REGISTRY/public/ccf/app/run:${VERSION}-${platform}"
 
     # Run-JS
-    docker build -f docker/app_run . --build-arg="platform=sgx" --build-arg="ansible_vars=ccf_ver=${VERSION}  run_js=true" -t $REGISTRY/public/ccf/app/run-js:${VERSION}-${platform}
+    docker build -f docker/app_run . --build-arg="platform=sgx" --build-arg="ansible_vars=ccf_ver=${VERSION}  run_js=true" -t "$ACR_REGISTRY/public/ccf/app/run-js:${VERSION}-${platform}"
 
 done
 
