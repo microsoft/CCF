5b44f7c2130e63968fae3e04f3aa8e454319b1ba
Dominic Ayre <dominicayre@microsoft.com>
2022-11-02 11:10:00 +0000

    Fix cleanup to also delete containers

--- scripts/cleanup_snp_aci.py
+++ scripts/cleanup_snp_aci.py
@@ -5,6 +5,7 @@ import argparse
 
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.containerinstance import ContainerInstanceManagementClient
 
 parser = argparse.ArgumentParser(
     description="Deploy an Azure Container Instance",
@@ -27,8 +28,15 @@ args = parser.parse_args()
 RESOURCE_GROUP = "ccf-aci"
 
 resource_client = ResourceManagementClient(DefaultAzureCredential(), args.subscription_id)
+container_client = ContainerInstanceManagementClient(DefaultAzureCredential(), args.subscription_id)
 
 try:
+    # Delete the container groups
+    for resource in resource_client.deployments.get(RESOURCE_GROUP, args.deployment_name).properties.output_resources:
+        container_name = resource.id.split("/")[-1]
+        deletion = container_client.container_groups.begin_delete(RESOURCE_GROUP, container_name)
+        deletion.wait()
+    # Delete the deployment
     deletion = resource_client.deployments.begin_delete(RESOURCE_GROUP, args.deployment_name)
     deletion.wait()
 except Exception as e:
