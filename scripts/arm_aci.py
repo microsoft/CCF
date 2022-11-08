99e4a2308ad51d89b0574b1ac6e3b98edd03a615
Dominic Ayre <dominicayre@microsoft.com>
2022-11-08 14:05:07 +0000

    Refactor script to make it generic and the interface nicer

---
+++ scripts/arm_aci.py
@@ -0,0 +1,161 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
+from argparse import ArgumentParser, Namespace
+
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.resource.resources.models import (
+    Deployment,
+    DeploymentProperties,
+    DeploymentMode,
+)
+from azure.mgmt.containerinstance import ContainerInstanceManagementClient
+
+HOST_PUB_KEY = open("/root/.ssh/id_rsa.pub", "r").read().replace("\n", "")
+STARTUP_COMMANDS = {
+    "dynamic-agent": lambda args, i: [
+        "apt-get update",
+        "apt-get install -y openssh-server",
+        "sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config",
+        "sed -i 's/PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/sshd_config",
+        "service ssh restart",
+        "mkdir /root/.ssh",
+        f"echo {HOST_PUB_KEY} >> /root/.ssh/authorized_keys",
+    ],
+    "static-agent": lambda args, i: [
+        "wget https://gist.github.com/DomAyre/98d3a229870f4947fc99a2aa7ed995b47/raw",
+        "chmod 777 setup_agent.sh",
+        f"AGENT_NAME={args.deployment_name + i} PAT={args.aci_pat} ./setup_agent",
+    ],
+    "dev": lambda args, i: [
+        "wget https://gist.github.com/DomAyre/ea2c07a9cb790bf17da05d4ca1674c8c/raw",
+        "chmod 777 setup_dev.sh",
+        f"SSHUSER={args.deployment_name + i} GITHUBID={args.aci_github_id} ./setup_dev",
+    ],
+}
+
+
+def make_aci_deployment(parser: ArgumentParser) -> Deployment:
+
+    parser.add_argument(
+        "--aci-image",
+        help="The name of the image to deploy in the ACI",
+        type=str,
+        default="ccfmsrc.azurecr.io/ccf/ci:oe-0.18.2-snp",
+    )
+
+    parser.add_argument(
+        "--aci-type",
+        help="The type of ACI to deploy",
+        type=str,
+        choices=STARTUP_COMMANDS.keys(),
+    )
+
+    parser.add_argument(
+        "--aci-pat",
+        help="The PAT to deploy an ACI with",
+        type=str,
+    )
+
+    parser.add_argument(
+        "--aci-github-id",
+        help="The Github ID who owns a CCF clone to checkout",
+        type=str,
+    )
+
+    args = parser.parse_args()
+
+    return Deployment(
+        properties=DeploymentProperties(
+            mode=DeploymentMode.INCREMENTAL,
+            parameters={},
+            template={
+                "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+                "contentVersion": "1.0.0.0",
+                "parameters": {},
+                "variables": {},
+                "resources": [
+                    {
+                        "type": "Microsoft.ContainerInstance/containerGroups",
+                        "apiVersion": "2022-04-01-preview",
+                        "name": f"{args.deployment_name}-{i}",
+                        "location": "westeurope",
+                        "properties": {
+                            "sku": "Standard",
+                            "confidentialComputeProperties": {
+                                "isolationType": "SevSnp",
+                                "ccePolicy": "eyJhbGxvd19hbGwiOnRydWUsImNvbnRhaW5lcnMiOnsibGVuZ3RoIjowLCJlbGVtZW50cyI6bnVsbH19",
+                            },
+                            "containers": [
+                                {
+                                    "name": f"{args.deployment_name}-{i}",
+                                    "properties": {
+                                        "image": args.aci_image,
+                                        "command": [
+                                            "/bin/sh",
+                                            "-c",
+                                            " && ".join(
+                                                [
+                                                    *STARTUP_COMMANDS[args.aci_type](
+                                                        args, i
+                                                    ),
+                                                    "tail -f /dev/null",
+                                                ]
+                                            ),
+                                        ],
+                                        "ports": [
+                                            {"protocol": "TCP", "port": 8000},
+                                            {"protocol": "TCP", "port": 22},
+                                        ],
+                                        "environmentVariables": [],
+                                        "resources": {
+                                            "requests": {"memoryInGB": 16, "cpu": 4}
+                                        },
+                                    },
+                                }
+                            ],
+                            "initContainers": [],
+                            "restartPolicy": "Never",
+                            "ipAddress": {
+                                "ports": [
+                                    {"protocol": "TCP", "port": 8000},
+                                    {"protocol": "TCP", "port": 22},
+                                ],
+                                "type": "Public",
+                            },
+                            "osType": "Linux",
+                        },
+                    }
+                    for i in range(args.count)
+                ],
+            },
+        )
+    )
+
+
+def remove_aci_deployment(args: Namespace, deployment: Deployment):
+
+    container_client = ContainerInstanceManagementClient(
+        DefaultAzureCredential(), args.subscription_id
+    )
+
+    for resource in deployment.properties.output_resources:
+        container_name = resource.id.split("/")[-1]
+        container_client.container_groups.begin_delete(
+            args.resource_group, container_name
+        ).wait()
+
+
+def check_aci_deployment(args: Namespace, deployment: Deployment) -> str:
+
+    container_client = ContainerInstanceManagementClient(
+        DefaultAzureCredential(), args.subscription_id
+    )
+
+    for resource in deployment.properties.output_resources:
+        container_name = resource.id.split("/")[-1]
+        container_group = container_client.container_groups.get(
+            args.resource_group, container_name
+        )
+        print(container_group.ip_address.ip)
---
+++ scripts/arm_template.py
@@ -0,0 +1,125 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
+import argparse
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.resource.resources.models import (
+    Deployment,
+    DeploymentProperties,
+    DeploymentMode,
+)
+from scripts.arm_aci import (
+    check_aci_deployment,
+    make_aci_deployment,
+    remove_aci_deployment,
+)
+
+parser = argparse.ArgumentParser(
+    description="Python interface for Azure ARM deployments",
+)
+
+parser.add_argument(
+    "operation",
+    help="Whether to deploy or remove template",
+    type=str,
+    choices=["deploy", "remove", "check"],
+)
+
+parser.add_argument(
+    "deployment_type",
+    help="The type of Azure deployment to deploy",
+    type=str,
+    choices=[
+        "aci",
+    ],
+)
+
+parser.add_argument(
+    "--count",
+    help="The number of deployments",
+    type=int,
+    default=1,
+)
+
+parser.add_argument(
+    "--subscription-id",
+    help="The subscription ID used to deploy",
+    type=str,
+)
+
+parser.add_argument(
+    "--resource-group",
+    help="The resource group used to deploy",
+    default="ccf-aci",
+    type=str,
+)
+
+parser.add_argument(
+    "--deployment-name",
+    help="The name of the Azure deployment, used for agent names and cleanup",
+    type=str,
+)
+
+args, unknown_args = parser.parse_known_args()
+
+resource_client = ResourceManagementClient(
+    DefaultAzureCredential(), args.subscription_id
+)
+
+deployment_type_to_funcs = {
+    "aci": (make_aci_deployment, check_aci_deployment, remove_aci_deployment),
+}
+
+
+def deploy(make_template, print_status) -> str:
+
+    try:
+        resource_client.deployments.begin_create_or_update(
+            args.resource_group,
+            args.deployment_name,
+            make_template(parser),
+        ).wait()
+        print_status(
+            args,
+            resource_client.deployments.get(
+                args.resource_group,
+                args.deployment_name,
+            ),
+        )
+    except Exception as e:
+        print(e)
+
+
+def remove(args, remove_deployment_items):
+
+    try:
+        remove_deployment_items(
+            args,
+            resource_client.deployments.get(
+                args.resource_group,
+                args.deployment_name,
+            ),
+        )
+        resource_client.deployments.begin_delete(
+            args.resource_group,
+            args.deployment_name,
+        ).wait()
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+
+    if args.operation == "deploy":
+        deploy(*deployment_type_to_funcs[args.deployment_type][:2])
+    elif args.operation == "check":
+        deployment_type_to_funcs[args.deployment_type][1](
+            args,
+            resource_client.deployments.get(
+                args.resource_group,
+                args.deployment_name,
+            ),
+        )
+    elif args.operation == "remove":
+        remove(args, deployment_type_to_funcs[args.deployment_type][2])
