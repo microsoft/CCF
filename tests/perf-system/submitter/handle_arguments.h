8e4916db1febebc8e4b4bbbad6efed5bd4460c38
Fotis Kounelis <fotisss17@gmail.com>
2022-11-10 13:46:10 +0000

    update short names in arguments, add default for max inflight and pass filepaths as required options

--- tests/perf-system/submitter/handle_arguments.h
+++ tests/perf-system/submitter/handle_arguments.h
@@ -21,14 +21,14 @@ public:
   std::string send_filepath;
   std::string response_filepath;
   std::string generator_filepath;
-  int max_inflight_requests;
+  int max_inflight_requests = 0;
 
   ArgumentParser(const std::string& default_label, CLI::App& app) :
     label(default_label)
   {
     app
       .add_option(
-        "--cert",
+        "-c,--cert",
         cert,
         "Use the provided certificate file when working with a SSL-based "
         "protocol.")
@@ -36,7 +36,7 @@ public:
       ->check(CLI::ExistingFile);
     app
       .add_option(
-        "--key",
+        "-k,--key",
         key,
         "Specify the path to the file containing the private key.")
       ->required(false)
@@ -50,32 +50,40 @@ public:
       ->check(CLI::ExistingFile);
     app
       .add_option(
-        "--server-address",
+        "-a,--server-address",
         server_address,
         "Specify the address to submit requests.")
       ->capture_default_str();
-    app.add_option(
-      "--send-filepath",
-      send_filepath,
-      "Path to parquet file to store the submitted requests.");
-    app.add_option(
-      "--response-filepath",
-      response_filepath,
-      "Path to parquet file to store the responses from the submitted "
-      "requests.");
-    app.add_option(
-      "--generator-filepath",
-      generator_filepath,
-      "Path to parquet file with the generated requests to be submitted.");
-    app.add_option(
-      "--max-inflight-requests",
-      max_inflight_requests,
-      "Specifies the number of outstanding requests sent to the server while "
-      "waiting for response. When this options is set to 0 there will be no "
-      "pipelining. Any other value will enable pipelining. A positive value "
-      "will specify a window of outstanding requests on the server while "
-      "waiting for a response. -1 or a negative value will set the window of "
-      "outstanding requests to maximum i.e. submit requests without waiting "
-      "for a response");
+    app
+      .add_option(
+        "-s,--send-filepath",
+        send_filepath,
+        "Path to parquet file to store the submitted requests.")
+      ->required();
+    app
+      .add_option(
+        "-r,--response-filepath",
+        response_filepath,
+        "Path to parquet file to store the responses from the submitted "
+        "requests.")
+      ->required();
+    app
+      .add_option(
+        "-g,--generator-filepath",
+        generator_filepath,
+        "Path to parquet file with the generated requests to be submitted.")
+      ->required();
+    app
+      .add_option(
+        "-m,--max-inflight-requests",
+        max_inflight_requests,
+        "Specifies the number of outstanding requests sent to the server while "
+        "waiting for response. When this options is set to 0 there will be no "
+        "pipelining. Any other value will enable pipelining. A positive value "
+        "will specify a window of outstanding requests on the server while "
+        "waiting for a response. -1 or a negative value will set the window of "
+        "outstanding requests to maximum i.e. submit requests without waiting "
+        "for a response")
+      ->capture_default_str();
   }
 };
