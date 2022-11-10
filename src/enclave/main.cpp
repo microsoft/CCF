d8a4a5c15829dc846461d2b252664a4421eca4f3
Julien Maffre <jumaffre@microsoft.com>
2022-11-10 14:49:28 +0000

    .

--- src/enclave/main.cpp
+++ src/enclave/main.cpp
@@ -206,6 +206,7 @@ extern "C"
       ccf::host_time_us =
         static_cast<decltype(ccf::host_time_us)>(time_location);
 
+      oe_lfence();
     }
 
     if (!oe_is_outside_enclave(ccf_config, ccf_config_size))
