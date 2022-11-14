cf504f6e701ab07c0c134dcf3d1c7e41486d8d2f
Eddy Ashton <edashton@microsoft.com>
2022-11-14 13:39:47 +0000

    Add a comment and re-enable some tests

--- tests/partitions_test.py
+++ tests/partitions_test.py
@@ -734,14 +734,14 @@ def run(args):
     ) as network:
         network.start_and_open(args)
 
-        # test_invalid_partitions(network, args)
-        # test_partition_majority(network, args)
-        # test_isolate_primary_from_one_backup(network, args)
-        # test_new_joiner_helps_liveness(network, args)
-        # for n in range(5):
-        #     test_isolate_and_reconnect_primary(network, args, iteration=n)
-        # test_election_reconfiguration(network, args)
-        # test_forwarding_timeout(network, args)
+        test_invalid_partitions(network, args)
+        test_partition_majority(network, args)
+        test_isolate_primary_from_one_backup(network, args)
+        test_new_joiner_helps_liveness(network, args)
+        for n in range(5):
+            test_isolate_and_reconnect_primary(network, args, iteration=n)
+        test_election_reconfiguration(network, args)
+        test_forwarding_timeout(network, args)
         test_session_consistency(network, args)
 
 
@@ -758,7 +758,7 @@ if __name__ == "__main__":
     args = infra.e2e_args.cli_args(add)
     args.nodes = infra.e2e_args.min_nodes(args, f=1)
     args.package = "samples/apps/logging/liblogging"
-    args.snapshot_tx_interval = 20
+    args.snapshot_tx_interval = 20 # Increase snapshot frequency for faster reconfigurations
 
     run(args)
-    # run_2tx_reconfig_tests(args)
+    run_2tx_reconfig_tests(args)
