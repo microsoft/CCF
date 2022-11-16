868eebc5f95bd92502120454921e1d9f0a405995
Dominic Ayre <dominicayre@microsoft.com>
2022-11-16 17:30:28 +0000

    .

--- scripts/arm_aci.py
+++ scripts/arm_aci.py
@@ -17,8 +17,6 @@ try:
 except Exception as e:
     ...
 
-def make_passwd(password):
-    return repr(password + "\n" + password)
 
 STARTUP_COMMANDS = {
     "dynamic-agent": lambda args, i: [
@@ -27,8 +25,7 @@ STARTUP_COMMANDS = {
         "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config",
         "useradd -m agent",
         "echo \"agent ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers",
-        # f"echo {args.aci_dynamic_agent_password}\n{args.aci_dynamic_agent_password}",
-        f'echo -e {make_passwd(args.aci_dynamic_agent_password)} | passwd agent',
+        f'echo "agent:{args.aci_dynamic_agent_password}" | chpasswd',
         "service ssh restart",
         # "sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config",
         # "sed -i 's/PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/sshd_config",
