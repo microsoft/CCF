d6d1eadfba0144b4a39dcc84d3f482ac06e929f2
Fotis Kounelis <37985618+fotiskoun@users.noreply.github.com>
2022-11-21 17:18:14 +0000

    Update tests/infra/piccolo_driver.py
    Co-authored-by: Amaury Chamayou <amaury@xargs.fr>

--- tests/infra/piccolo_driver.py
+++ tests/infra/piccolo_driver.py
@@ -29,8 +29,6 @@ def minimum_number_of_local_nodes(args):
     """
     If we are using bft then we need to have 3 nodes. CFT will run with 1 nodes, unless it expects a backup
     """
-    if args.consensus == "BFT":
-        return 3
 
     if args.send_tx_to == "backups":
         return 2
