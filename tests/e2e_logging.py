929b9b947bae77406abbdbecb3d704badaa48460
Eddy Ashton <edashton@microsoft.com>
2022-11-24 14:20:39 +0000

    Oops, re-enable tests

--- tests/e2e_logging.py
+++ tests/e2e_logging.py
@@ -1639,39 +1639,38 @@ def run(args):
     ) as network:
         network.start_and_open(args)
 
-        # TODO: Re-enable
-        # test(network, args)
-        # test_remove(network, args)
-        # test_clear(network, args)
-        # test_record_count(network, args)
+        test(network, args)
+        test_remove(network, args)
+        test_clear(network, args)
+        test_record_count(network, args)
         test_forwarding_frontends(network, args)
         test_forwarding_frontends_without_app_prefix(network, args)
-        # test_signed_escapes(network, args)
-        # test_user_data_ACL(network, args)
-        # test_cert_prefix(network, args)
-        # test_anonymous_caller(network, args)
-        # test_multi_auth(network, args)
-        # test_custom_auth(network, args)
-        # test_custom_auth_safety(network, args)
-        # test_raw_text(network, args)
-        # test_historical_query(network, args)
-        # test_historical_query_range(network, args)
-        # test_view_history(network, args)
-        # test_metrics(network, args)
-        # test_empty_path(network, args)
-        # test_post_local_commit_failure(network, args)
-        # test_committed_index(network, args)
-        # # BFT does not handle re-keying yet
-        # if args.consensus == "CFT":
-        #     test_liveness(network, args)
-        #     test_rekey(network, args)
-        #     test_liveness(network, args)
-        #     test_random_receipts(network, args, False)
-        # if args.package == "samples/apps/logging/liblogging":
-        #     test_receipts(network, args)
-        #     test_historical_query_sparse(network, args)
-        # test_historical_receipts(network, args)
-        # test_historical_receipts_with_claims(network, args)
+        test_signed_escapes(network, args)
+        test_user_data_ACL(network, args)
+        test_cert_prefix(network, args)
+        test_anonymous_caller(network, args)
+        test_multi_auth(network, args)
+        test_custom_auth(network, args)
+        test_custom_auth_safety(network, args)
+        test_raw_text(network, args)
+        test_historical_query(network, args)
+        test_historical_query_range(network, args)
+        test_view_history(network, args)
+        test_metrics(network, args)
+        test_empty_path(network, args)
+        test_post_local_commit_failure(network, args)
+        test_committed_index(network, args)
+        # BFT does not handle re-keying yet
+        if args.consensus == "CFT":
+            test_liveness(network, args)
+            test_rekey(network, args)
+            test_liveness(network, args)
+            test_random_receipts(network, args, False)
+        if args.package == "samples/apps/logging/liblogging":
+            test_receipts(network, args)
+            test_historical_query_sparse(network, args)
+        test_historical_receipts(network, args)
+        test_historical_receipts_with_claims(network, args)
 
 
 def run_parsing_errors(args):
@@ -1693,15 +1692,14 @@ def run_parsing_errors(args):
 if __name__ == "__main__":
     cr = ConcurrentRunner()
 
-    # TODO: Re-enable
-    # cr.add(
-    #     "js",
-    #     run,
-    #     package="libjs_generic",
-    #     nodes=infra.e2e_args.max_nodes(cr.args, f=0),
-    #     initial_user_count=4,
-    #     initial_member_count=2,
-    # )
+    cr.add(
+        "js",
+        run,
+        package="libjs_generic",
+        nodes=infra.e2e_args.max_nodes(cr.args, f=0),
+        initial_user_count=4,
+        initial_member_count=2,
+    )
 
     cr.add(
         "cpp",
@@ -1713,35 +1711,35 @@ if __name__ == "__main__":
         initial_member_count=2,
     )
 
-    # cr.add(
-    #     "common",
-    #     e2e_common_endpoints.run,
-    #     package="samples/apps/logging/liblogging",
-    #     nodes=infra.e2e_args.max_nodes(cr.args, f=0),
-    # )
+    cr.add(
+        "common",
+        e2e_common_endpoints.run,
+        package="samples/apps/logging/liblogging",
+        nodes=infra.e2e_args.max_nodes(cr.args, f=0),
+    )
 
-    # # Run illegal traffic tests in separate runners, to reduce total serial runtime
-    # if not cr.args.http2:
-    #     cr.add(
-    #         "js_illegal",
-    #         run_parsing_errors,
-    #         package="libjs_generic",
-    #         nodes=infra.e2e_args.max_nodes(cr.args, f=0),
-    #     )
+    # Run illegal traffic tests in separate runners, to reduce total serial runtime
+    if not cr.args.http2:
+        cr.add(
+            "js_illegal",
+            run_parsing_errors,
+            package="libjs_generic",
+            nodes=infra.e2e_args.max_nodes(cr.args, f=0),
+        )
 
-    #     cr.add(
-    #         "cpp_illegal",
-    #         run_parsing_errors,
-    #         package="samples/apps/logging/liblogging",
-    #         nodes=infra.e2e_args.max_nodes(cr.args, f=0),
-    #     )
+        cr.add(
+            "cpp_illegal",
+            run_parsing_errors,
+            package="samples/apps/logging/liblogging",
+            nodes=infra.e2e_args.max_nodes(cr.args, f=0),
+        )
 
-    # # This is just for the UDP echo test for now
-    # cr.add(
-    #     "udp",
-    #     run_udp_tests,
-    #     package="samples/apps/logging/liblogging",
-    #     nodes=infra.e2e_args.max_nodes(cr.args, f=0),
-    # )
+    # This is just for the UDP echo test for now
+    cr.add(
+        "udp",
+        run_udp_tests,
+        package="samples/apps/logging/liblogging",
+        nodes=infra.e2e_args.max_nodes(cr.args, f=0),
+    )
 
     cr.run()
