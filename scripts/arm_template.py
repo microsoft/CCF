fee1c6caea901cd763816d0c56d8f74948454bd7
Amaury Chamayou <amaury@xargs.fr>
2022-11-28 17:17:53 +0000

    Update scripts/arm_template.py

--- scripts/arm_template.py
+++ scripts/arm_template.py
@@ -1,5 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
- # Licensed under the Apache 2.0 License.
+# Licensed under the Apache 2.0 License.
 
 import argparse
 from azure.identity import DefaultAzureCredential
