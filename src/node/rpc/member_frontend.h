7a53aa4a6fba1382cf3e2d538b9c751a674fe498
Eddy Ashton <edashton@microsoft.com>
2022-12-02 14:58:23 +0000

    Schema bump

--- src/node/rpc/member_frontend.h
+++ src/node/rpc/member_frontend.h
@@ -541,7 +541,7 @@ namespace ccf
       openapi_info.description =
         "This API is used to submit and query proposals which affect CCF's "
         "public governance tables.";
-      openapi_info.document_version = "2.13.0";
+      openapi_info.document_version = "2.14.0";
     }
 
     static std::optional<MemberId> get_caller_member_id(
