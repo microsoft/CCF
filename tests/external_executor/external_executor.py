996c730e438a496fd86b1efd19f0e845989d9474
Eddy Ashton <edashton@microsoft.com>
2022-12-08 14:44:29 +0000

    Remove direct kv tests

--- tests/external_executor/external_executor.py
+++ tests/external_executor/external_executor.py
@@ -43,27 +43,6 @@ from collections import defaultdict
 from loguru import logger as LOG
 
 
-# TODO: Rework anything using this
-@contextlib.contextmanager
-def wrap_tx(stub, primary, uri="/placeholder"):
-    with primary.client(connection_timeout=0.1) as c:
-        try:
-            # This wrapper is used to test the gRPC KV API directly. That is
-            # only possible when this executor is processing an active request
-            # (StartTx() returns a non-empty response). To trigger that, we do
-            # this placeholder GET request. It immediately times out and fails,
-            # but then the node we're speaking to will return a
-            # RequestDescription for us to operate over.
-            # This is a temporary hack to allow direct access to the KV API.
-            c.get(uri, timeout=0.1, log_capture=[])
-        except Exception as e:
-            LOG.trace(e)
-        rd = stub.StartTx(Empty())
-        assert rd.HasField("optional"), rd
-        yield stub
-        stub.EndTx(KV.ResponseDescription())
-
-
 def register_new_executor(node, network, message=None, supported_endpoints=None):
     # Generate a new executor identity
     key_priv_pem, _ = infra.crypto.generate_ec_keypair()
@@ -122,9 +101,6 @@ def test_executor_registration(network, args):
         open(os.path.join(network.common_dir, "service_cert.pem"), "rb").read()
     )
 
-    # TODO
-    return network
-
     # Confirm that these credentials (and NOT anoymous credentials) provide
     # access to the KV service on the target node, but no other nodes
     for node in (
@@ -141,132 +117,23 @@ def test_executor_registration(network, args):
             ) as channel:
                 should_pass = node == primary and credentials == executor_credentials
                 try:
-                    rd = Service.KVStub(channel).StartTx(Empty())
-                    assert should_pass, "Expected StartTx to fail"
-                    assert not rd.HasField("optional")
+                    stub = Service.KVStub(channel)
+                    for m in stub.Activate(Empty(), timeout=1):
+                        assert (
+                            False
+                        ), f"Did not expect to receive any work messages: {m}"
                 except grpc.RpcError as e:
-                    # NB: This failure will have printed errors like:
-                    # These are harmless and expected, and I haven't found a way to swallow them
-                    assert not should_pass
-                    # pylint: disable=no-member
-                    assert e.code() == grpc.StatusCode.UNAUTHENTICATED, e
-
-    return network
-
-
-@reqs.description("Test basic KV operations via external executor app")
-def test_kv(network, args):
-    primary, _ = network.find_primary()
-
-    supported_endpoints_a = [("GET", "/placeholder")]
-    supported_endpoints_b = [("GET", "/placeholderB")]
-    executor_a = register_new_executor(
-        primary, network, supported_endpoints=supported_endpoints_a
-    )
-    executor_b = register_new_executor(
-        primary, network, supported_endpoints=supported_endpoints_b
-    )
-
-    my_table = "public:my_table"
-    my_key = b"my_key"
-    my_value = b"my_value"
-
-    with grpc.secure_channel(
-        target=primary.get_public_rpc_address(),
-        credentials=executor_a,
-    ) as channel:
-        stub = Service.KVStub(channel)
-
-        with wrap_tx(stub, primary) as tx:
-            LOG.info(f"Put key {my_key} in table '{my_table}'")
-            tx.Put(KV.KVKeyValue(table=my_table, key=my_key, value=my_value))
-
-        with wrap_tx(stub, primary) as tx:
-            LOG.info(f"Get key {my_key} in table '{my_table}'")
-            r = tx.Get(KV.KVKey(table=my_table, key=my_key))
-            assert r.HasField("optional")
-            assert r.optional.value == my_value
-            LOG.success(f"Successfully read key {my_key} in table '{my_table}'")
-
-        unknown_key = b"unknown_key"
-        with wrap_tx(stub, primary) as tx:
-            LOG.info(f"Get unknown key {unknown_key} in table '{my_table}'")
-            r = tx.Get(KV.KVKey(table=my_table, key=unknown_key))
-            assert not r.HasField("optional")
-            LOG.success(f"Unable to read key {unknown_key} as expected")
-
-        tables = ("public:table_a", "public:table_b", "public:table_c")
-        writes = [
-            (
-                random.choice(tables),
-                f"Key{i}".encode(),
-                random.getrandbits(((i % 16) + 1) * 8).to_bytes(((i % 16) + 1), "big"),
-            )
-            for i in range(10)
-        ]
-
-        with wrap_tx(stub, primary) as tx:
-            LOG.info("Write multiple entries in single transaction")
-            for t, k, v in writes:
-                tx.Put(KV.KVKeyValue(table=t, key=k, value=v))
-
-            LOG.info("Read own writes")
-            for t, k, v in writes:
-                r = tx.Get(KV.KVKeyValue(table=t, key=k))
-                assert r.HasField("optional")
-                assert r.optional.value == v
-
-            LOG.info("Snapshot isolation")
-            with grpc.secure_channel(
-                target=primary.get_public_rpc_address(),
-                credentials=executor_b,
-            ) as channel_alt:
-                stub_alt = Service.KVStub(channel_alt)
-                with wrap_tx(stub_alt, primary, uri="/placeholderB") as tx2:
-                    for t, k, v in writes:
-                        r = tx2.Get(KV.KVKey(table=t, key=k))
-                        assert not r.HasField("optional")
-                        LOG.success(
-                            f"Unable to read key {k} from table {t} (in concurrent transaction) as expected"
-                        )
-
-        with wrap_tx(stub, primary) as tx3:
-            LOG.info("Read applied writes")
-            for t, k, v in writes:
-                r = tx3.Get(KV.KVKeyValue(table=t, key=k))
-                assert r.HasField("optional")
-                assert r.optional.value == v
-
-            writes_by_table = defaultdict(dict)
-            for t, k, v in writes:
-                writes_by_table[t][k] = v
-
-            for t, table_writes in writes_by_table.items():
-                LOG.info(f"Read all in {t}")
-                r = tx3.GetAll(KV.KVTable(table=t))
-                count = 0
-                for result in r:
-                    count += 1
-                    assert result.key in table_writes
-                    assert table_writes[result.key] == result.value
-                assert count == len(table_writes)
-
-            LOG.info("Clear one table")
-            t, cleared_writes = writes_by_table.popitem()
-            tx3.Clear(KV.KVTable(table=t))
-            for k, _ in cleared_writes.items():
-                r = tx3.Has(KV.KVKey(table=t, key=k))
-                assert not r.present
-
-                r = tx3.Get(KV.KVKey(table=t, key=k))
-                assert not r.HasField("optional")
-
-                r = tx3.GetAll(KV.KVTable(table=t))
-                try:
-                    next(r)
-                    raise AssertionError("Expected unreachable")
-                except StopIteration:
-                    pass
+                    if e.code() == grpc.StatusCode.DEADLINE_EXCEEDED:
+                        assert (
+                            should_pass
+                        ), "Expected Activate to fail with an auth error"
+                    else:
+                        # NB: This failure will have printed errors like:
+                        #  Error parsing metadata: error=invalid value key=content-type value=application/json
+                        # These are harmless and expected, and I haven't found a way to swallow them
+                        assert not should_pass
+                        # pylint: disable=no-member
+                        assert e.code() == grpc.StatusCode.UNAUTHENTICATED, e
 
     return network
 
@@ -468,6 +335,7 @@ def test_async_streaming(network, args):
 
         q = queue.Queue()
 
+        # TODO: Record event order, assert that it is interleaved
         def subscribe(event_name):
             credentials = grpc.ssl_channel_credentials(
                 open(os.path.join(network.common_dir, "service_cert.pem"), "rb").read()
@@ -622,7 +490,6 @@ def run(args):
             ), "Target node does not support HTTP/2"
 
         network = test_executor_registration(network, args)
-        # # # # network = test_kv(network, args)
         network = test_simple_executor(network, args)
         network = test_parallel_executors(network, args)
         network = test_streaming(network, args)
