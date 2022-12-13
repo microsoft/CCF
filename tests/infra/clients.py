1fc2a55dfcb160b7d3a0a2c3954f60e7653c104f
Eddy Ashton <edashton@microsoft.com>
2022-12-13 10:45:21 +0000

    Steps in the direction of an e2e test

--- tests/infra/clients.py
+++ tests/infra/clients.py
@@ -547,7 +547,8 @@ class HttpxClient:
         if "protocol" in kwargs:
             self.protocol = kwargs.get("protocol")
             kwargs.pop("protocol")
-        self.session = httpx.Client(verify=self.ca, cert=cert, **kwargs)
+        # TODO: Need to make this customisable, per-client
+        self.session = httpx.Client(verify=False, cert=cert, **kwargs)
         sig_auth = signing_auth or cose_signing_auth
         if sig_auth:
             with open(sig_auth.cert, encoding="utf-8") as cert_file:
