5ee261b7557b126557cc1a17328dee2e0e3cb8e0
Takuro Sato <79583855+takuro-sato@users.noreply.github.com>
2023-01-13 16:39:22 +0000

    Initial attestation container implementation (#4694)
    Co-authored-by: Eddy Ashton <ashton.eddy@gmail.com>
    Co-authored-by: Julien Maffre <42961061+jumaffre@users.noreply.github.com>

--- scripts/azure_deployment/arm_aci.py
+++ scripts/azure_deployment/arm_aci.py
@@ -11,6 +11,7 @@ from azure.mgmt.resource.resources.models import (
     Deployment,
     DeploymentProperties,
     DeploymentMode,
+    DeploymentPropertiesExtended,
 )
 from azure.mgmt.containerinstance import ContainerInstanceManagementClient
 
@@ -91,6 +92,13 @@ def make_aci_deployment(parser: ArgumentParser) -> Deployment:
     )
 
     parser.add_argument(
+        "--attestation-container-e2e",
+        help="Deploy attestation container for its E2E test if this flag is true. Default=False",
+        default=False,
+        type=bool,
+    )
+
+    parser.add_argument(
         "--aci-storage-account-key",
         help="The storage account key used to authorise access to the file share",
         type=str,
@@ -98,87 +106,144 @@ def make_aci_deployment(parser: ArgumentParser) -> Deployment:
 
     args = parser.parse_args()
 
-    return Deployment(
-        properties=DeploymentProperties(
-            mode=DeploymentMode.INCREMENTAL,
-            parameters={},
-            template={
-                "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
-                "contentVersion": "1.0.0.0",
-                "parameters": {},
-                "variables": {},
-                "resources": [
-                    {
-                        "type": "Microsoft.ContainerInstance/containerGroups",
-                        "apiVersion": "2022-04-01-preview",
-                        "name": f"{args.deployment_name}-{i}",
-                        "location": "eastus2euap",
-                        "properties": {
-                            "sku": "Standard",
-                            "confidentialComputeProperties": {
-                                "isolationType": "SevSnp",
-                                "ccePolicy": "cGFja2FnZSBwb2xpY3kKCmFwaV9zdm4gOj0gIjAuMTAuMCIKCm1vdW50X2RldmljZSA6PSB7ImFsbG93ZWQiOiB0cnVlfQptb3VudF9vdmVybGF5IDo9IHsiYWxsb3dlZCI6IHRydWV9CmNyZWF0ZV9jb250YWluZXIgOj0geyJhbGxvd2VkIjogdHJ1ZSwgImFsbG93X3N0ZGlvX2FjY2VzcyI6IHRydWV9CnVubW91bnRfZGV2aWNlIDo9IHsiYWxsb3dlZCI6IHRydWV9CnVubW91bnRfb3ZlcmxheSA6PSB7ImFsbG93ZWQiOiB0cnVlfQpleGVjX2luX2NvbnRhaW5lciA6PSB7ImFsbG93ZWQiOiB0cnVlfQpleGVjX2V4dGVybmFsIDo9IHsiYWxsb3dlZCI6IHRydWUsICJhbGxvd19zdGRpb19hY2Nlc3MiOiB0cnVlfQpzaHV0ZG93bl9jb250YWluZXIgOj0geyJhbGxvd2VkIjogdHJ1ZX0Kc2lnbmFsX2NvbnRhaW5lcl9wcm9jZXNzIDo9IHsiYWxsb3dlZCI6IHRydWV9CnBsYW45X21vdW50IDo9IHsiYWxsb3dlZCI6IHRydWV9CnBsYW45X3VubW91bnQgOj0geyJhbGxvd2VkIjogdHJ1ZX0KZ2V0X3Byb3BlcnRpZXMgOj0geyJhbGxvd2VkIjogdHJ1ZX0KZHVtcF9zdGFja3MgOj0geyJhbGxvd2VkIjogdHJ1ZX0KcnVudGltZV9sb2dnaW5nIDo9IHsiYWxsb3dlZCI6IHRydWV9CmxvYWRfZnJhZ21lbnQgOj0geyJhbGxvd2VkIjogdHJ1ZX0Kc2NyYXRjaF9tb3VudCA6PSB7ImFsbG93ZWQiOiB0cnVlfQpzY3JhdGNoX3VubW91bnQgOj0geyJhbGxvd2VkIjogdHJ1ZX0K",
-                            },
-                            "containers": [
-                                {
-                                    "name": f"{args.deployment_name}-{i}",
-                                    "properties": {
-                                        "image": args.aci_image,
-                                        "command": [
-                                            "/bin/sh",
-                                            "-c",
-                                            " && ".join(
-                                                [
-                                                    *STARTUP_COMMANDS[args.aci_type](
-                                                        args,
-                                                        i,
-                                                    ),
-                                                    "tail -f /dev/null",
-                                                ]
+    common_resource_attributes = {
+        "type": "Microsoft.ContainerInstance/containerGroups",
+        "apiVersion": "2022-04-01-preview",
+        "location": "eastus2euap",
+    }
+
+    common_resource_properties = {
+        "sku": "Standard",
+        "confidentialComputeProperties": {
+            "isolationType": "SevSnp",
+            "ccePolicy": "cGFja2FnZSBwb2xpY3kKCmFwaV9zdm4gOj0gIjAuMTAuMCIKCm1vdW50X2RldmljZSA6PSB7ImFsbG93ZWQiOiB0cnVlfQptb3VudF9vdmVybGF5IDo9IHsiYWxsb3dlZCI6IHRydWV9CmNyZWF0ZV9jb250YWluZXIgOj0geyJhbGxvd2VkIjogdHJ1ZSwgImFsbG93X3N0ZGlvX2FjY2VzcyI6IHRydWV9CnVubW91bnRfZGV2aWNlIDo9IHsiYWxsb3dlZCI6IHRydWV9CnVubW91bnRfb3ZlcmxheSA6PSB7ImFsbG93ZWQiOiB0cnVlfQpleGVjX2luX2NvbnRhaW5lciA6PSB7ImFsbG93ZWQiOiB0cnVlfQpleGVjX2V4dGVybmFsIDo9IHsiYWxsb3dlZCI6IHRydWUsICJhbGxvd19zdGRpb19hY2Nlc3MiOiB0cnVlfQpzaHV0ZG93bl9jb250YWluZXIgOj0geyJhbGxvd2VkIjogdHJ1ZX0Kc2lnbmFsX2NvbnRhaW5lcl9wcm9jZXNzIDo9IHsiYWxsb3dlZCI6IHRydWV9CnBsYW45X21vdW50IDo9IHsiYWxsb3dlZCI6IHRydWV9CnBsYW45X3VubW91bnQgOj0geyJhbGxvd2VkIjogdHJ1ZX0KZ2V0X3Byb3BlcnRpZXMgOj0geyJhbGxvd2VkIjogdHJ1ZX0KZHVtcF9zdGFja3MgOj0geyJhbGxvd2VkIjogdHJ1ZX0KcnVudGltZV9sb2dnaW5nIDo9IHsiYWxsb3dlZCI6IHRydWV9CmxvYWRfZnJhZ21lbnQgOj0geyJhbGxvd2VkIjogdHJ1ZX0Kc2NyYXRjaF9tb3VudCA6PSB7ImFsbG93ZWQiOiB0cnVlfQpzY3JhdGNoX3VubW91bnQgOj0geyJhbGxvd2VkIjogdHJ1ZX0K",
+        },
+        "initContainers": [],
+        "restartPolicy": "Never",
+        "osType": "Linux",
+    }
+
+    # Mount external volume only when account key is provided
+    volume_mounts = []
+    if args.aci_storage_account_key is not None:
+        common_resource_properties["volumes"] = [
+            {
+                "name": "ccfcivolume",
+                "azureFile": {
+                    "shareName": "ccfcishare",
+                    "storageAccountName": "ccfcistorage",
+                    "storageAccountKey": args.aci_storage_account_key,
+                },
+            }
+        ]
+        volume_mounts = [
+            {
+                "name": "ccfcivolume",
+                "mountPath": "/ccfci",
+            }
+        ]
+
+    template = {
+        "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+        "contentVersion": "1.0.0.0",
+        "parameters": {},
+        "variables": {},
+        "resources": [
+            {
+                **common_resource_attributes,
+                "name": f"{args.deployment_name}-{i}",
+                "properties": {
+                    **common_resource_properties,
+                    "containers": [
+                        {
+                            "name": f"{args.deployment_name}-{i}",
+                            "properties": {
+                                "image": args.aci_image,
+                                "command": [
+                                    "/bin/sh",
+                                    "-c",
+                                    " && ".join(
+                                        [
+                                            *STARTUP_COMMANDS[args.aci_type](
+                                                args,
+                                                i,
                                             ),
-                                        ],
-                                        "ports": [
-                                            {"protocol": "TCP", "port": 8000},
-                                            {"protocol": "TCP", "port": 22},
-                                        ],
-                                        "environmentVariables": [],
-                                        "resources": {
-                                            "requests": {"memoryInGB": 16, "cpu": 4}
-                                        },
-                                        "volumeMounts": [
-                                            {
-                                                "name": "ccfcivolume",
-                                                "mountPath": "/ccfci",
-                                            }
-                                        ],
-                                    },
-                                }
-                            ],
-                            "initContainers": [],
-                            "restartPolicy": "Never",
-                            "ipAddress": {
+                                            "tail -f /dev/null",
+                                        ]
+                                    ),
+                                ],
                                 "ports": [
                                     {"protocol": "TCP", "port": 8000},
                                     {"protocol": "TCP", "port": 22},
                                 ],
-                                "type": "Public",
+                                "environmentVariables": [],
+                                "resources": {"requests": {"memoryInGB": 16, "cpu": 4}},
+                                "volumeMounts": volume_mounts,
                             },
-                            "osType": "Linux",
-                            "volumes": [
-                                {
-                                    "name": "ccfcivolume",
-                                    "azureFile": {
-                                        "shareName": "ccfcishare",
-                                        "storageAccountName": "ccfcistorage",
-                                        "storageAccountKey": args.aci_storage_account_key,
-                                    },
-                                }
-                            ],
-                        },
-                    }
-                    for i in range(args.count)
-                ],
-            },
+                        }
+                    ],
+                    "ipAddress": {
+                        "ports": [
+                            {"protocol": "TCP", "port": 8000},
+                            {"protocol": "TCP", "port": 22},
+                        ],
+                        "type": "Public",
+                    },
+                },
+            }
+            for i in range(args.count)
+        ],
+    }
+
+    if args.attestation_container_e2e:
+        # NOTE: It currently exposes the attestation-container's port (50051) to outside of the container group for e2e testing purpose,
+        # but it shouldn't be done in actual CCF application.
+        template["resources"].append(
+            {
+                **common_resource_attributes,
+                "name": f"{args.deployment_name}-business-logic",
+                "properties": {
+                    **common_resource_properties,
+                    "containers": [
+                        {
+                            "name": f"{args.deployment_name}-attestation-container",
+                            "properties": {
+                                "image": f"attestationcontainerregistry.azurecr.io/attestation-container:{args.deployment_name}",
+                                "command": [
+                                    "/bin/sh",
+                                    "-c",
+                                    " && ".join(
+                                        [
+                                            *STARTUP_COMMANDS[args.aci_type](
+                                                args,
+                                                None,
+                                            ),
+                                            "app",
+                                        ]
+                                    ),
+                                ],
+                                "ports": [
+                                    {"protocol": "TCP", "port": 22},
+                                    {"protocol": "TCP", "port": 50051},
+                                ],
+                                "environmentVariables": [],
+                                "resources": {"requests": {"memoryInGB": 16, "cpu": 4}},
+                            },
+                        }
+                    ],
+                    "ipAddress": {
+                        "ports": [
+                            {"protocol": "TCP", "port": 22},
+                            {"protocol": "TCP", "port": 50051},
+                        ],
+                        "type": "Public",
+                    },
+                },
+            }
+        )
+    return Deployment(
+        properties=DeploymentProperties(
+            mode=DeploymentMode.INCREMENTAL, parameters={}, template=template
         )
     )
 
@@ -196,16 +261,26 @@ def remove_aci_deployment(args: Namespace, deployment: Deployment):
         ).wait()
 
 
-def check_aci_deployment(args: Namespace, deployment: Deployment) -> str:
+def check_aci_deployment(
+    args: Namespace, deployment: DeploymentPropertiesExtended
+) -> str:
+    """
+    Outputs the list of container group deployed to stdout.
+    The format of each line is `<container group name> <IP address>`.
+
+    example output:
+    container_group_a 10.10.10.10
+    container_group_b 10.10.10.11
+    """
 
     container_client = ContainerInstanceManagementClient(
         DefaultAzureCredential(), args.subscription_id
     )
 
     for resource in deployment.properties.output_resources:
-        container_name = resource.id.split("/")[-1]
+        container_group_name = resource.id.split("/")[-1]
         container_group = container_client.container_groups.get(
-            args.resource_group, container_name
+            args.resource_group, container_group_name
         )
 
         # Check that container commands have been completed
@@ -228,7 +303,7 @@ def check_aci_deployment(args: Namespace, deployment: Deployment) -> str:
                     )
                     == b"test\n"
                 )
-                print(container_group.ip_address.ip)
+                print(container_group_name, container_group.ip_address.ip)
                 break
             except Exception:
                 time.sleep(5)
