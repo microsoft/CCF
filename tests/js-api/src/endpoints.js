fa441f84a952fa96bdd8d1144a7f08be9a8e6674
Eddy Ashton <edashton@microsoft.com>
2023-01-13 13:15:27 +0000

    Add `enableUntrustedDateTime` to JS API (#4838)
    (cherry picked from commit 9bed464b09e03314b3dc3fef2f0331f992aa1b40)
    
    # Conflicts:
    #	CHANGELOG.md

--- tests/js-api/src/endpoints.js
+++ tests/js-api/src/endpoints.js
@@ -10,3 +10,25 @@ export function make_randoms(request) {
     },
   };
 }
+
+ccf.enableUntrustedDateTime(true);
+
+export function time_now(request) {
+  const original = new Date();
+
+  const prev_setting = ccf.enableUntrustedDateTime(false);
+  const definitely_1970 = new Date();
+
+  ccf.enableUntrustedDateTime(true);
+  const definitely_now = new Date();
+
+  ccf.enableUntrustedDateTime(prev_setting);
+
+  return {
+    body: {
+      default: original.toISOString(),
+      definitely_1970: definitely_1970.toISOString(),
+      definitely_now: definitely_now.toISOString(),
+    },
+  };
+}
