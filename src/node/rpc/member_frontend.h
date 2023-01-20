4fb084c700752eb65a3b6121867de3c4343563ad
Amaury Chamayou <amchamay@microsoft.com>
2023-01-20 16:22:35 +0000

    api

--- src/node/rpc/member_frontend.h
+++ src/node/rpc/member_frontend.h
@@ -579,7 +579,7 @@ namespace ccf
       openapi_info.description =
         "This API is used to submit and query proposals which affect CCF's "
         "public governance tables.";
-      openapi_info.document_version = "2.17.0";
+      openapi_info.document_version = "2.18.0";
     }
 
     static std::optional<MemberId> get_caller_member_id(
