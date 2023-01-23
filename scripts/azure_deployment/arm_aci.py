3ab08676633afb765bb52138cedbf07db5308d1a
Takuro Sato <takurosato@microsoft.com--username>
2023-01-23 15:49:03 +0000

    Update security policy

--- scripts/azure_deployment/arm_aci.py
+++ scripts/azure_deployment/arm_aci.py
@@ -83,7 +83,36 @@ scratch_mount := {"allowed": true}
 scratch_unmount := {"allowed": true}
 """
 
-ATTESTATION_CONTAINER_REGO_SECURITY_POLICY = DEFAULT_REGO_SECURITY_POLICY
+ATTESTATION_CONTAINER_REGO_SECURITY_POLICY = """package policy
+
+api_svn := "0.10.0"
+
+mount_device := {"allowed": true}
+mount_overlay := {"allowed": true}
+create_container := {
+    "allowed": true,
+    "allow_stdio_access": true,
+    "mounts": [{
+        destination": "/mnt/uds",
+        "options": ["rbind", "rshared", "rw"],
+        "type": "bind"
+    }]
+}
+unmount_device := {"allowed": true}
+unmount_overlay := {"allowed": true}
+exec_in_container := {"allowed": true}
+exec_external := {"allowed": true, "allow_stdio_access": true}
+shutdown_container := {"allowed": true}
+signal_container_process := {"allowed": true}
+plan9_mount := {"allowed": true}
+plan9_unmount := {"allowed": true}
+get_properties := {"allowed": true}
+dump_stacks := {"allowed": true}
+runtime_logging := {"allowed": true}
+load_fragment := {"allowed": true}
+scratch_mount := {"allowed": true}
+scratch_unmount := {"allowed": true}
+"""
 
 
 def make_dev_container_command(args):
