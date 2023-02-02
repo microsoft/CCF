139101e1fa0c507b08793b3ad4db780652899384
Mahati Chamarthy <mahati.chamarthy@gmail.com>
2023-02-02 15:32:24 +0000

    Call new endpoints in tests

--- tests/code_update.py
+++ tests/code_update.py
@@ -262,30 +262,24 @@ def test_update_all_nodes(network, args):
     network.consortium.add_new_code(primary, new_code_id)
     LOG.info("Check reported trusted measurements")
     with primary.client() as uc:
-        r = uc.get("/node/code")
-        expected = [
-            {"digest": first_code_id, "status": "AllowedToJoin"},
-            {"digest": new_code_id, "status": "AllowedToJoin"},
-        ]
+        r = uc.get("/gov/kv/nodes/code_ids")
+        expected = {first_code_id: "AllowedToJoin", new_code_id: "AllowedToJoin"}
         if args.enclave_platform == "virtual":
-            expected.insert(0, {"digest": VIRTUAL_CODE_ID, "status": "AllowedToJoin"})
+            expected[VIRTUAL_CODE_ID] = "AllowedToJoin"
 
-        versions = sorted(r.body.json()["versions"], key=lambda x: x["digest"])
-        expected.sort(key=lambda x: x["digest"])
+        versions = dict(sorted(r.body.json().items(), key=lambda x: x[0]))
+        expected = dict(sorted(expected.items(), key=lambda x: x[0]))
         assert versions == expected, f"{versions} != {expected}"
 
     LOG.info("Remove old code id")
     network.consortium.retire_code(primary, first_code_id)
     with primary.client() as uc:
-        r = uc.get("/node/code")
-        expected = [
-            {"digest": first_code_id, "status": "AllowedToJoin"},
-            {"digest": new_code_id, "status": "AllowedToJoin"},
-        ]
+        r = uc.get("/gov/kv/nodes/code_ids")
+        expected = {first_code_id: "AllowedToJoin", new_code_id: "AllowedToJoin"}
         if args.enclave_platform == "virtual":
-            expected.insert(0, {"digest": VIRTUAL_CODE_ID, "status": "AllowedToJoin"})
+            expected[VIRTUAL_CODE_ID] = "AllowedToJoin"
 
-        expected.sort(key=lambda x: x["digest"])
+        expected = dict(sorted(expected.items(), key=lambda x: x[0]))
         assert versions == expected, f"{versions} != {expected}"
 
     old_nodes = network.nodes.copy()
