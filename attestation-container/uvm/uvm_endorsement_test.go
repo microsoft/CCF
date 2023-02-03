234e67c17b11bd8f10bcaea8729850e3a7f94ad3
Takuro Sato <takurosato@microsoft.com>
2023-02-03 14:38:25 +0000

    Use dummy data for unit test

--- attestation-container/uvm/uvm_endorsement_test.go
+++ attestation-container/uvm/uvm_endorsement_test.go
@@ -2,17 +2,23 @@ package uvm
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
 func TestUVMEndorsement(t *testing.T) {
+	// Prepare for test
 	envVarName := DEFAULT_UVM_ENDORSEMENT_ENV_VAR_NAME
+	const testEnvVarValue = "test base64 string"
+	os.Setenv(envVarName, testEnvVarValue)
+
+	// Test
 	uvmEndorsement, err := FetchUVMEndorsement(envVarName)
 	if err != nil {
-		t.Fatalf("Couldn't get UVM endorsement: %s\nPlease make sure that %s environment variable is set before running test", err, envVarName)
+		t.Fatalf("Couldn't get UVM endorsement: %s", err)
 	}
-	if len(uvmEndorsement) == 0 {
-		t.Fatalf("uvmEndorsement should not be empty")
+	if uvmEndorsement != testEnvVarValue {
+		t.Fatalf("Value doesn't match: '%s' was expected but got '%s'", testEnvVarValue, uvmEndorsement)
 	}
 	fmt.Printf("UVM Endorsement: %s", uvmEndorsement)
 }
