462ee0b85a6f7a442f4bc0b5723dcd7b74a85462
Dominic Ayre <domayre@outlook.com>
2023-02-22 14:43:58 +0000

    Test mounted dirs

--- tests/external_executor/executors/utils/executor_container.py
+++ tests/external_executor/executors/utils/executor_container.py
@@ -37,9 +37,9 @@ class ExecutorContainer:
 
         # Create a container with external executor code loaded in a volume and
         # a command to run the executor
-        command = "pip install --upgrade pip &&"
-        command += " pip install -r /executor/external_executor/requirements.txt &&"
-        command += " python3 /executor/external_executor/run_executor.py"
+        # command = "pip install --upgrade pip &&"
+        # command += " pip install -r /executor/external_executor/requirements.txt &&"
+        command = "python3 /executor/external_executor/run_executor.py"
         command += f' --executor "{executor}"'
         command += f' --node-public-rpc-address "{node.get_public_rpc_address()}"'
         command += ' --network-common-dir "/ccf_network"'
