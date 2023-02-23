44b177f038e58c1f11b83548ab95d56c1a3178e9
Dominic Ayre <domayre@outlook.com>
2023-02-23 14:47:29 +0000

    Don't use requirements.txt

--- tests/external_executor/executors/utils/executor_container.py
+++ tests/external_executor/executors/utils/executor_container.py
@@ -44,7 +44,15 @@ class ExecutorContainer:
         # Create a container with external executor code loaded in a volume and
         # a command to run the executor
         command = "pip install --upgrade pip &&"
-        command += ' pip install -r "/executor/external_executor/requirements.txt" &&'
+        command += " pip install grpcio-tools &&"
+        command += " pip install loguru &&"
+        command += " pip install requests &&"
+        command += " pip install google &&"
+        command += " pip install protobuf &&"
+        command += " pip install pyasn1 &&"
+        command += " pip install cryptography &&"
+        command += " pip install jwt &&"
+        # command += ' pip install -r "/executor/external_executor/requirements.txt" &&'
         command += " python3 /executor/external_executor/run_executor.py"
         command += f' --executor "{executor}"'
         command += f' --node-public-rpc-address "{node.get_public_rpc_address()}"'
