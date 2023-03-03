a38eace2c973f4d699d23cc900b92312e955873c
Julien Maffre <jumaffre@microsoft.com>
2023-03-03 12:17:28 +0000

    .

--- tests/code_update.py
+++ tests/code_update.py
@@ -155,7 +155,7 @@ def test_host_data_table(network, args):
     primary, _ = network.find_nodes()
     with primary.client() as client:
         r = client.get("/gov/kv/nodes/snp/host_data").body.json()
-        host_data = sorted(r)
+        host_data = sorted(r.items())
 
     expected = {
         snp.get_container_group_security_policy_digest(): snp.get_container_group_security_policy(),
