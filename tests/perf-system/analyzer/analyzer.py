0fbc5c1445f4d23b9ab3737277dfc4f8ae3f1dd4
Heidi Howard <1835251+heidihoward@users.noreply.github.com>
2023-05-15 17:10:51 +0000

    adding analyzer.py

--- tests/perf-system/analyzer/analyzer.py
+++ tests/perf-system/analyzer/analyzer.py
@@ -13,8 +13,12 @@ from loguru import logger as LOG
 from email.parser import Parser
 import re
 import json
+from pathlib import Path
 
 SEC_MS = 1000
+VERBS = {"POST", "GET", "DELETE"}
+DEFAULT_FIGURE_SIZE = (8, 2.5)
+SMALL_FIGURE_SIZE = (4, 2)
 
 # Change default log format
 LOG.remove()
@@ -28,6 +32,8 @@ class Analyze:
     def __init__(self):
         self.latency_list = []
         self.ms_latency_list = []
+        self.request_verb = []
+        self.throughput = 0
 
     def get_req_type(self, df_responses: pd.DataFrame) -> str:
         return df_responses.iloc[0]["rawResponse"].split(b" ")[0].decode("ascii")
@@ -49,7 +55,7 @@ class Analyze:
         return 0
 
     def iter_for_success_and_latency(
-        self, df_sends: pd.DataFrame, df_responses: pd.DataFrame
+        self, df_input: pd.DataFrame, df_sends: pd.DataFrame, df_responses: pd.DataFrame
     ) -> float:
         successful_reqs = 0
 
@@ -59,6 +65,7 @@ class Analyze:
             latency_i = self.get_latency_at_i(df_sends, df_responses, i)
             self.latency_list.append(latency_i)
             self.ms_latency_list.append(latency_i * SEC_MS)
+            self.request_verb.append(df_input["request"][i].decode().split(" ")[0])
 
         return successful_reqs / len(df_sends.index) * 100
 
@@ -80,20 +87,19 @@ class Analyze:
         generic_output_table.field_names = [
             "Total Requests",
             "Total Time (s)",
-            "Pass (%)",
-            "Fail (%)",
+            "Success (%)",
             "Throughput (req/s)",
         ]
 
         time_spent = self.total_time_in_sec(df_sends, df_responses)
+        self.throughput = len(df_sends.index) / time_spent
 
         generic_output_table.add_row(
             [
                 len(df_sends.index),
                 round(time_spent, 3),
                 round(successful_percent, 1),
-                round(100 - successful_percent, 1),
-                round(len(df_sends.index) / time_spent, 1),
+                round(self.throughput, 1),
             ]
         )
         return generic_output_table
@@ -101,24 +107,19 @@ class Analyze:
     def latencies_table(
         self, df_sends: pd.DataFrame, df_responses: pd.DataFrame
     ) -> PrettyTable:
-        ms_time_spent_sum = self.total_time_in_sec(df_sends, df_responses) * SEC_MS
         latency_output_table = PrettyTable()
         latency_output_table.field_names = [
-            "Latency (50th) (ms)",
-            "Average Latency (ms)",
-            "Latency 80th (ms)",
+            "Mean Latency (ms)",
+            "Latency 50th (ms)",
             "Latency 90th (ms)",
-            "Latency 95th (ms)",
             "Latency 99th (ms)",
             "Latency 99.9th (ms)",
         ]
         latency_output_table.add_row(
             [
+                round(np.mean(self.ms_latency_list), 3),
                 round(np.percentile(self.ms_latency_list, 50), 3),
-                round(ms_time_spent_sum / len(df_sends.index), 3),
-                round(np.percentile(self.ms_latency_list, 80), 3),
                 round(np.percentile(self.ms_latency_list, 90), 3),
-                round(np.percentile(self.ms_latency_list, 95), 3),
                 round(np.percentile(self.ms_latency_list, 99), 3),
                 round(np.percentile(self.ms_latency_list, 99.9), 3),
             ]
@@ -132,7 +133,12 @@ class Analyze:
             custom_table.add_row(val_row)
         return custom_table
 
-    def plot_commits(self, df_responses: pd.DataFrame, df_generator: pd.DataFrame):
+    def plot_commits(
+        self,
+        df_responses: pd.DataFrame,
+        df_generator: pd.DataFrame,
+        results_directory: Path,
+    ):
         """
         For submitted requests of the type: N posts 1 commit, this function will
         plot the number of posted versus committed messages
@@ -190,31 +196,90 @@ class Analyze:
         plt.scatter(time_units, tx_ids, label="Txid", marker="o")
         plt.scatter(time_units, committed_ids, label="Commits", marker="+")
         plt.ylabel("Requests")
-        plt.xlabel("Time(s)")
+        plt.xlabel("Time (s)")
         plt.legend()
-        plt.savefig("posted_vs_committed.png")
+        plt.savefig(results_directory / "posted_vs_committed.pdf")
+        plt.close()
 
-    def plot_latency_by_id(self, df_sends: pd.DataFrame) -> None:
+    def plot_latency_by_id(
+        self,
+        df_sends: pd.DataFrame,
+        results_directory: Path,
+        y_limits=(None, None),
+        **kwargs,
+    ) -> None:
         id_unit = [x for x in range(0, len(df_sends.index))]
         lat_unit = self.ms_latency_list
-        plt.figure()
-        plt.scatter(id_unit, lat_unit, s=1)
-        plt.ylabel("Latency_ms")
-        plt.xlabel("ids")
-        plt.savefig("latency_per_id.png")
+        plt.figure(**kwargs)
+        plt.scatter(id_unit, lat_unit, s=1, c="black")
+        plt.ylabel("Latency (ms)")
+        plt.xlabel("Requests")
+        plt.xlim([0, len(df_sends.index)])
+        plt.ylim(y_limits)
+        if y_limits == (None, None):
+            range_string = "all"
+        else:
+            range_string = f"from_{y_limits[0]}ms_to_{y_limits[1]}ms"
+        filename = f"latency_per_id_{range_string}.pdf"
+        plt.savefig(results_directory / filename)
+        plt.close()
 
-    def plot_latency_across_time(self, df_responses) -> None:
+    def plot_latency_by_id_and_verb(
+        self, df_sends: pd.DataFrame, results_directory: Path, **kwargs
+    ) -> None:
+        id_unit = {}
+        lat_unit = {}
+        for verb in VERBS:
+            id_unit[verb] = []
+            lat_unit[verb] = []
+
+        for i, verb in enumerate(self.request_verb):
+            id_unit[verb].append(i)
+            lat_unit[verb].append(self.ms_latency_list[i])
+
+        plt.figure(**kwargs)
+        for verb in VERBS:
+            plt.scatter(id_unit[verb], lat_unit[verb], s=1, label=verb)
+        plt.legend()
+        plt.ylabel("Latency (ms)")
+        plt.xlabel("Requests")
+        plt.xlim([0, len(df_sends.index)])
+        plt.ylim(bottom=0)
+        plt.savefig(results_directory / "latency_per_id_and_verb.pdf")
+        plt.close()
+
+    def plot_latency_cdf(self, results_directory: Path, **kwargs) -> None:
+        latencies_sorted = np.sort(self.ms_latency_list)
+        proportion = (
+            1.0 * np.arange(len(self.ms_latency_list)) / (len(self.ms_latency_list) - 1)
+        )
+        plt.figure(**kwargs)
+        plt.plot(latencies_sorted, proportion)
+        plt.ylabel("$p$")
+        plt.ylim([0, 1])
+        plt.xlabel("Latency (ms)")
+        plt.savefig(results_directory / "latency_cdf.pdf")
+        plt.close()
+
+    def plot_latency_across_time(
+        self, df_responses, results_directory: Path, **kwargs
+    ) -> None:
         time_unit = [
-            x - df_responses["receiveTime"][0] + 1 for x in df_responses["receiveTime"]
+            x - df_responses["receiveTime"][0] for x in df_responses["receiveTime"]
         ]
-        plt.figure()
+        plt.figure(**kwargs)
         plt.scatter(time_unit, self.ms_latency_list, s=1)
-        plt.ylabel("Latency(ms)")
-        plt.xlabel("time(s)")
-        plt.savefig("latency_across_time.png")
+        plt.ylabel("Latency (ms)")
+        plt.xlabel("Time (s)")
+        plt.savefig(results_directory / "latency_across_time.pdf")
+        plt.close()
 
     def plot_throughput_per_block(
-        self, df_responses: pd.DataFrame, time_block: float
+        self,
+        df_responses: pd.DataFrame,
+        time_block: float,
+        results_directory: Path,
+        **kwargs,
     ) -> None:
         """
         It splits the dataset in buckets of time_block seconds difference
@@ -245,13 +310,19 @@ class Analyze:
         throughput_per_block = [
             x / time_block for x in req_per_block
         ]  # x/time_block comes from rule of three
-        plt.figure()
-        plt.plot(block_latency, throughput_per_block)
-        plt.ylabel("Throughput(req/s)")
-        plt.xlabel("time(ms)")
-        plt.savefig("throughput_across_time.png")
+        block_latency_sec = [x / SEC_MS for x in block_latency]
+        plt.figure(**kwargs)
+        plt.plot(block_latency_sec, throughput_per_block)
+        plt.ylabel("Throughput (req/s)")
+        plt.xlabel("Time (s)")
+        plt.ylim(bottom=0)
+        filename = f"throughput_across_time_{time_block}_buckets.pdf"
+        plt.savefig(results_directory / filename)
+        plt.close()
 
-    def plot_latency_distribution(self, ms_separator: float, highest_vals=15):
+    def plot_latency_distribution(
+        self, ms_separator: float, results_directory: Path, highest_vals=15
+    ):
         """
         Starting from minimum latency with ms_separator
         step split the ms latency list in buckets
@@ -310,26 +381,66 @@ class Analyze:
         ax.set_xticks(x_axis)
         ax.set_xticklabels(top_bins, rotation=25)
         plt.ylabel("Count")
-        plt.xlabel("Latency")
+        plt.xlabel("Latency (ms)")
         fig.subplots_adjust(bottom=0.2)
-        plt.savefig("latency_distribution.png")
+        plt.savefig(results_directory / "latency_distribution.pdf")
+        plt.close()
 
 
-def get_df_from_parquet_file(input_file: str):
+def get_df_from_parquet_file(input_file: Path):
     return pd.read_parquet(input_file)
 
 
-def default_analysis(send_file, response_file):
+def prettify_graphs():
+    fontsize = 9
+    params = {
+        "axes.labelsize": fontsize,
+        "font.size": fontsize,
+        "legend.fontsize": fontsize,
+        "xtick.labelsize": fontsize,
+        "ytick.labelsize": fontsize,
+        "figure.figsize": DEFAULT_FIGURE_SIZE,
+        "xtick.direction": "in",
+        "ytick.direction": "in",
+        "xtick.top": True,
+        "ytick.right": True,
+        "xtick.minor.visible": True,
+        "ytick.minor.visible": True,
+        "lines.linewidth": 2,
+        "legend.frameon": False,
+        "axes.grid": False,
+        "savefig.bbox": "tight",
+    }
+    plt.rcParams.update(params)
+
+
+def default_analysis(
+    input_file: Path,
+    send_file: Path,
+    response_file: Path,
+    results_directory: Path,
+    error_on_failure=False,
+    plots=True,
+):
     """
     Produce the analysis results
     """
     analysis = Analyze()
+    df_input = get_df_from_parquet_file(input_file)
     df_sends = get_df_from_parquet_file(send_file)
     df_responses = get_df_from_parquet_file(response_file)
 
-    successful_percent = analysis.iter_for_success_and_latency(df_sends, df_responses)
+    successful_percent = analysis.iter_for_success_and_latency(
+        df_input, df_sends, df_responses
+    )
+
+    if error_on_failure and successful_percent < 100:
+        assert False, "Not all requests were successful"
 
     LOG.info(f"The request type sent is {analysis.get_req_type(df_responses)}")
+    LOG.info(
+        f"Request verbs counts are: POST {analysis.request_verb.count('POST')}, GET {analysis.request_verb.count('GET')}, DELETE {analysis.request_verb.count('DELETE')}"
+    )
 
     print(
         analysis.time_success_throughput_table(
@@ -337,12 +448,37 @@ def default_analysis(send_file, response_file):
         )
     )
     print(analysis.latencies_table(df_sends, df_responses))
+    prettify_graphs()
+    results_directory = Path(results_directory)
 
-    x = "-" * 20
-    LOG.info(f'{"".join(x)} Start plotting  {"".join(x)}')
+    if plots:
+        x = "-" * 20
+        LOG.info(f'{"".join(x)} Start plotting  {"".join(x)}')
+        analysis.plot_latency_by_id(
+            df_sends, results_directory, figsize=SMALL_FIGURE_SIZE
+        )
+        analysis.plot_latency_by_id(
+            df_sends, results_directory, y_limits=(0, 10), figsize=SMALL_FIGURE_SIZE
+        )
+        analysis.plot_latency_by_id(
+            df_sends, results_directory, y_limits=(2, 4), figsize=SMALL_FIGURE_SIZE
+        )
+        analysis.plot_latency_across_time(df_responses, results_directory)
+        analysis.plot_throughput_per_block(df_responses, 0.1, results_directory)
+        analysis.plot_throughput_per_block(df_responses, 0.01, results_directory)
+        analysis.plot_latency_cdf(results_directory)
+        # analysis.plot_latency_distribution(0.1, results_directory)
+        analysis.plot_latency_by_id_and_verb(df_sends, results_directory)
+        LOG.info(f'{"".join(x)}Finished plotting{"".join(x)}')
 
-    analysis.plot_latency_by_id(df_sends)
-    analysis.plot_latency_across_time(df_responses)
-    analysis.plot_throughput_per_block(df_responses, 0.1)
+    return analysis
 
-    LOG.info(f'{"".join(x)}Finished plotting{"".join(x)}')
+
+def default_analysis_default_filenames(test_dir: Path, **kwargs):
+    return default_analysis(
+        test_dir / "input.parquet",
+        test_dir / "send.parquet",
+        test_dir / "responses.parquet",
+        test_dir,
+        **kwargs,
+    )
