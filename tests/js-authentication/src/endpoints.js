315663733fade0cfcc669a57bbaf01523f5b60d3
Eddy Ashton <edashton@microsoft.com>
2023-05-18 16:55:38 +0100

    Unskip JS e2e tests (#5269)

--- tests/js-authentication/src/endpoints.js
+++ tests/js-authentication/src/endpoints.js
@@ -10,44 +10,6 @@ export function jwt(request) {
   return { body: request.caller.jwt };
 }
 
-export function multi_auth(request) {
-  var lines = [];
-
-  if (request.caller.policy === "user_cert") {
-    lines.push("User TLS cert");
-    lines.push(`The caller is a user with ID: ${request.caller.id}`);
-    lines.push(
-      `The caller's user data is: ${JSON.stringify(request.caller.data)}`
-    );
-    lines.push(`The caller's cert is:\n${request.caller.cert}`);
-  } else if (request.caller.policy === "member_cert") {
-    lines.push("Member TLS cert");
-    lines.push(`The caller is a member with ID: ${request.caller.id}`);
-    lines.push(
-      `The caller's user data is: ${JSON.stringify(request.caller.data)}`
-    );
-    lines.push(`The caller's cert is:\n${request.caller.cert}`);
-  } else if (request.caller.policy === "jwt") {
-    lines.push("JWT");
-    lines.push(
-      `The caller is identified by a JWT issued by: ${request.caller.jwt.keyIssuer}`
-    );
-    lines.push(
-      `The JWT header is:\n${JSON.stringify(request.caller.jwt.header)}`
-    );
-    lines.push(
-      `The JWT payload is:\n${JSON.stringify(request.caller.jwt.payload)}`
-    );
-  } else if (request.caller.policy === "no_auth") {
-    lines.push("Unauthenticated");
-    lines.push("The caller did not provide any authenticated identity");
-  }
-
-  let s = lines.join("\n");
-  console.log(s);
-  return { body: s };
-}
-
 // There's only a single secret, stored at this special key (the empty array)
 const key_of_single_secret = new ArrayBuffer();
 
