cda62d56657b5bf5c20601efb35951567b6f7e4f
Eddy Ashton <edashton@microsoft.com>
2023-05-19 12:39:02 +0000

    Add api_version_adapter

--- src/node/gov/api_version.h
+++ src/node/gov/api_version.h
@@ -8,11 +8,61 @@ namespace ccf::gov::endpoints
 {
   enum class ApiVersion
   {
-    v0_0_1_preview
+    v0_0_1_preview,
   };
 
-  DECLARE_JSON_ENUM(
-    ApiVersion, {{ApiVersion::v0_0_1_preview, "0.0.1-preview"}});
+  static constexpr std::pair<ApiVersion, char const*> api_version_strings[] = {
+    {ApiVersion::v0_0_1_preview, "0.0.1-preview"}};
 
-  // TODO: Add a decorator to extract api-version?
+  template <typename Fn>
+  auto api_version_adapter(Fn&& f)
+  {
+    static std::string supported_suffix;
+    if (supported_suffix.empty())
+    {
+      supported_suffix = "Supported versions are:";
+      for (const auto& [_, s] : api_version_strings)
+      {
+        supported_suffix += fmt::format("\n  {}", s);
+      }
+    }
+
+    return [f](auto& ctx, nlohmann::json&& body) {
+      // TODO: Extract api-version, return error if it is missing
+      const auto param_name = "api-version";
+      const auto parsed_query =
+        http::parse_query(ctx.rpc_ctx->get_request_query());
+      const auto qit = parsed_query.find(param_name);
+      if (qit == parsed_query.end())
+      {
+        return make_error(
+          HTTP_STATUS_BAD_REQUEST,
+          ccf::errors::InvalidQueryParameterValue,
+          fmt::format(
+            "Missing required query parameter '{}'. {}",
+            param_name,
+            supported_suffix));
+      }
+
+      const auto it = std::find_if(
+        std::begin(api_version_strings),
+        std::end(api_version_strings),
+        [&qit](const auto& p) { return p.second == qit->second; });
+      if (it == std::end(api_version_strings))
+      {
+        return make_error(
+          HTTP_STATUS_BAD_REQUEST,
+          ccf::errors::InvalidQueryParameterValue,
+          fmt::format(
+            "Invalid value for query parameter '{}' - '{}' is not recognised "
+            "as a valid API version. {}",
+            param_name,
+            qit->second,
+            supported_suffix));
+      }
+
+      const ApiVersion api_version = it->first;
+      return f(ctx, std::move(body), api_version);
+    };
+  }
 }
--- src/node/gov/transactions_handlers.h
+++ src/node/gov/transactions_handlers.h
@@ -12,55 +12,70 @@ namespace ccf::gov::endpoints
 {
   void init_transactions_handlers(ccf::BaseEndpointRegistry& registry)
   {
-    auto get_transaction_status = [&](auto& ctx, nlohmann::json&&) {
-      std::string tx_id_str, error;
-      if (!ccf::endpoints::get_path_param(
-            ctx.rpc_ctx->get_request_path_params(),
-            "transactionId",
-            tx_id_str,
-            error))
+    auto get_transaction_status = [&](
+                                    auto& ctx,
+                                    nlohmann::json&& params,
+                                    ApiVersion api_version) {
+      switch (api_version)
       {
-        return make_error(
-          HTTP_STATUS_BAD_REQUEST, ccf::errors::InvalidResourceName, error);
-      }
+        case (ApiVersion::v0_0_1_preview):
+        default:
+        {
+          // Extract transaction ID from path parameter
+          std::string tx_id_str, error;
+          if (!ccf::endpoints::get_path_param(
+                ctx.rpc_ctx->get_request_path_params(),
+                "transactionId",
+                tx_id_str,
+                error))
+          {
+            return make_error(
+              HTTP_STATUS_BAD_REQUEST, ccf::errors::InvalidResourceName, error);
+          }
 
-      const auto tx_id = ccf::TxID::from_str(tx_id_str);
-      if (!tx_id.has_value())
-      {
-        return make_error(
-          HTTP_STATUS_BAD_REQUEST,
-          ccf::errors::InvalidQueryParameterValue,
-          fmt::format(
-            "The value '{}' passed as parameter 'transactionId' could not be "
-            "converted to a valid Transaction ID.",
-            tx_id_str));
-      }
+          // Parse transaction ID from string
+          const auto tx_id = ccf::TxID::from_str(tx_id_str);
+          if (!tx_id.has_value())
+          {
+            return make_error(
+              HTTP_STATUS_BAD_REQUEST,
+              ccf::errors::InvalidQueryParameterValue,
+              fmt::format(
+                "The value '{}' passed as parameter 'transactionId' could not "
+                "be converted to a valid Transaction ID.",
+                tx_id_str));
+          }
 
-      ccf::TxStatus status;
-      const auto result =
-        registry.get_status_for_txid_v1(tx_id->view, tx_id->seqno, status);
-      if (result == ccf::ApiResult::OK)
-      {
-        auto body = nlohmann::json::object();
+          // Lookup status
+          ccf::TxStatus status;
+          const auto result =
+            registry.get_status_for_txid_v1(tx_id->view, tx_id->seqno, status);
+          if (result == ccf::ApiResult::OK)
+          {
+            // Build response
+            auto body = nlohmann::json::object();
 
-        body["status"] = status;
-        body["transactionId"] = tx_id->to_str();
+            body["status"] = status;
+            body["transactionId"] = tx_id->to_str();
 
-        return make_success(body);
-      }
-      else
-      {
-        return make_error(
-          HTTP_STATUS_INTERNAL_SERVER_ERROR,
-          ccf::errors::InternalError,
-          fmt::format("Error code: {}", ccf::api_result_to_str(result)));
+            return make_success(body);
+          }
+          else
+          {
+            return make_error(
+              HTTP_STATUS_INTERNAL_SERVER_ERROR,
+              ccf::errors::InternalError,
+              fmt::format("Error code: {}", ccf::api_result_to_str(result)));
+          }
+          break;
+        }
       }
     };
     registry
       .make_endpoint(
         "/service/transactions/{transactionId}/status",
         HTTP_GET,
-        json_adapter(get_transaction_status),
+        json_adapter(api_version_adapter(get_transaction_status)),
         no_auth_required)
       .install();
   }
