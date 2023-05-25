6fc1ceff73e64303dce4e4557748eaa95d37a493
Julien Maffre <jumaffre@microsoft.com>
2023-05-25 14:28:12 +0000

    Better flag

--- cmake/open_enclave.cmake
+++ cmake/open_enclave.cmake
@@ -1,47 +1,54 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 
-if(NOT COMPILE_TARGET STREQUAL "sgx")
-  set(COMPONENT "OEHOSTVERIFY")
-endif()
+# We allow for Open Enclave (and Open Enclave HostVerify) to not be installed
+# if this specific flag is set.
+option(REQUIRE_OPENENCLAVE "Requires Open Enclave or HostVerify variant" ON)
 
-# Find OpenEnclave package
-find_package(OpenEnclave 0.19.0 CONFIG REQUIRED)
-# As well as pulling in openenclave:: targets, this sets variables which can be
-# used for our edge cases (eg - for virtual libraries). These do not follow the
-# standard naming patterns, for example use OE_INCLUDEDIR rather than
-# OpenEnclave_INCLUDE_DIRS
+if(REQUIRE_OPENENCLAVE)
+  if(NOT COMPILE_TARGET STREQUAL "sgx")
+    set(COMPONENT "OEHOSTVERIFY")
+  endif()
 
-if(COMPILE_TARGET STREQUAL "sgx")
-  set(OE_TARGET_LIBC openenclave::oelibc)
-  set(OE_TARGET_ENCLAVE_AND_STD
+  # Find OpenEnclave package
+  find_package(OpenEnclave 0.19.0 CONFIG REQUIRED)
+
+  # As well as pulling in openenclave:: targets, this sets variables which can be
+  # used for our edge cases (eg - for virtual libraries). These do not follow the
+  # standard naming patterns, for example use OE_INCLUDEDIR rather than
+  # OpenEnclave_INCLUDE_DIRS
+  if(COMPILE_TARGET STREQUAL "sgx")
+    set(OE_TARGET_LIBC openenclave::oelibc)
+    set(OE_TARGET_ENCLAVE_AND_STD
       openenclave::oeenclave openenclave::oelibcxx openenclave::oelibc
       openenclave::oecryptoopenssl
-  )
-  # These oe libraries must be linked in specific order
-  set(OE_TARGET_ENCLAVE_CORE_LIBS
+    )
+
+    # These oe libraries must be linked in specific order
+    set(OE_TARGET_ENCLAVE_CORE_LIBS
       openenclave::oeenclave openenclave::oesnmalloc openenclave::oecore
       openenclave::oesyscall
-  )
+    )
 
-  option(LVI_MITIGATIONS "Enable LVI mitigations" ON)
+    option(LVI_MITIGATIONS "Enable LVI mitigations" ON)
 
-  function(add_lvi_mitigations name)
-    if(LVI_MITIGATIONS)
-      # Enable clang-11 built-in LVI mitigation
-      target_compile_options(${name} PRIVATE -mlvi-cfi)
-    endif()
-  endfunction()
+    function(add_lvi_mitigations name)
+      if(LVI_MITIGATIONS)
+        # Enable clang-11 built-in LVI mitigation
+        target_compile_options(${name} PRIVATE -mlvi-cfi)
+      endif()
+    endfunction()
 
-  set(OE_HOST_LIBRARY openenclave::oehost)
-else()
-  option(SGX_ATTESTATION_VERIFICATION "Enable SGX attestation verification on non-SGX platforms" ON)
-
-  set(OE_HOST_LIBRARY openenclave::oehostverify)
+    set(OE_HOST_LIBRARY openenclave::oehost)
+  else()
+    set(OE_HOST_LIBRARY openenclave::oehostverify)
+  endif()
+elseif(COMPILE_TARGET STREQUAL "sgx")
+  message(FATAL_ERROR "Open Enclave is required for SGX target")
 endif()
 
 function(link_openenclave_host_verify name)
-  if (COMPILE_TARGET STREQUAL "sgx" OR SGX_ATTESTATION_VERIFICATION)
+  if(REQUIRE_OPENENCLAVE)
     target_link_libraries(${name} PRIVATE ${OE_HOST_LIBRARY})
     target_compile_definitions(${name} PUBLIC SGX_ATTESTATION_VERIFICATION)
   endif()
