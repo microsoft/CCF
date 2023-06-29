2fe5e38d7f775e61bb10d272a6fb8b932e634e7d
Amaury Chamayou <amchamay@microsoft.com>
2023-06-29 09:28:55 +0000

    note

--- tests/npm-app/src/endpoints/auth.ts
+++ tests/npm-app/src/endpoints/auth.ts
@@ -1,5 +1,7 @@
 import * as ccfapp from "@microsoft/ccf-app";
 
+// Note: this is also tested more generically on the multi_auth endpoint
+// of the logging application, but not with TypeScript types.
 export function checkUserCOSESign1Auth(
   request: ccfapp.Request
 ): ccfapp.Response {
