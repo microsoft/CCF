5d786cb0179e7f09c03607c1c3d0a0a285afecb7
Amaury Chamayou <amchamay@microsoft.com>
2023-08-10 17:38:54 +0000

    fmt

--- tests/external_executor/executors/ccf/executors/registration.py
+++ tests/external_executor/executors/ccf/executors/registration.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 import grpc
-import time
 
 import executor_registration_pb2 as ExecutorRegistration
 
