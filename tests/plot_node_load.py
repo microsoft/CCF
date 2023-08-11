ccdbbd9072344ff2c17ac982aee27efe798da36f
Eddy Ashton <edashton@microsoft.com>
2023-08-11 16:53:12 +0000

    Rewriting plotter

--- tests/plot_node_load.py
+++ tests/plot_node_load.py
@@ -38,6 +38,33 @@ LABELS_TO_COLOURS = {
     "OversizedMessage::fragment": "slategray",
 }
 
+_AVAIL_COLOURS = [
+    "#1f77b4",
+    "#ff7f0e",
+    "#2ca02c",
+    "#d62728",
+    "#9467bd",
+    "#8c564b",
+    "#e377c2",
+    "#7f7f7f",
+    "#bcbd22",
+    "#17becf",
+]
+COLOURS = {}
+
+
+def update_colour_mapping(labels):
+    colours = []
+    for label in labels:
+        if label not in COLOURS:
+            c = _AVAIL_COLOURS.pop(0)
+            COLOURS[label] = c
+            colours.append(c)
+        else:
+            colours.append(COLOURS[label])
+
+    return colours
+
 
 def num_to_bytes_formatter(n, _):
     suffixes = ("B", "KB", "MB", "GB")
@@ -103,50 +130,103 @@ def plot_stacked(jsons, key):
     plt.savefig(output_path, bbox_inches="tight")
 
 
+def stack_it(ax, df, y_label, y_is_bytes=False):
+    top, other = get_top(df)
+    df = df.with_columns(
+        other=sum(df.select(other).fill_null(0)),
+    )
+    labels = [*top, "other"]
+    colours = update_colour_mapping(labels)
+    ax.stackplot(
+        df["startTime"],
+        *(df[col] for col in top),
+        df["other"],
+        labels=[*top, "other"],
+        colors=colours,
+    )
+
+    ax.set_ylabel(y_label)
+    if y_is_bytes:
+        ax.yaxis.set_major_formatter(num_to_bytes_formatter)
+
+
+def get_top(df):
+    msg_types = [col.name for col in df if col.dtype is pl.Int64]
+    top_msgs = (
+        df.select(msg_types)
+        .sum()
+        .transpose(include_header=True, header_name="msg", column_names=["count"])
+        .top_k(4, by="count")
+    )["msg"]
+    other_msgs = set(msg_types) - set(top_msgs)
+    return top_msgs, other_msgs
+
+
+def parse_load(line):
+    j = json.loads(line.split("|")[1])
+    counts = {
+        "startTime": datetime.datetime.fromtimestamp(j["start_time_ms"] / 1000),
+        "endTime": datetime.datetime.fromtimestamp(j["end_time_ms"] / 1000),
+    }
+    sizes = counts.copy()
+    # Note: Purely working with message counts for now, ignoring bytes
+    for k, v in j["ringbuffer_messages"].items():
+        counts[k] = v["count"]
+        sizes[k] = v["bytes"]
+    return counts, sizes
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
         "load_file",
         type=argparse.FileType("r"),
-        help="Path to load log file to be parsed",
+        help="Path to node output file to be parsed",
     )
 
     args = parser.parse_args()
 
-    lines = args.load_file.readlines()
-    jsons = [json.loads(line) for line in lines]
+    host_counts = []
+    host_sizes = []
+    enclave_counts = []
+    enclave_sizes = []
+    ENCLAVE_FIND = "load_monitor.h:91"
+    HOST_FIND = "load_monitor.h:84"
+    for line in args.load_file.readlines():
+        if ENCLAVE_FIND in line:
+            c, s = parse_load(line)
+            enclave_counts.append(c)
+            enclave_sizes.append(s)
+        elif HOST_FIND in line:
+            c, s = parse_load(line)
+            host_counts.append(c)
+            host_sizes.append(s)
 
-    df = pl.DataFrame(
-        {
-            "startTime": datetime.datetime.fromtimestamp(j["start_time_ms"] / 1000),
-            "endTime": datetime.datetime.fromtimestamp(j["end_time_ms"] / 1000),
-            **{
-                f"{vk}({k})": vv
-                for k, v in j["ringbuffer_messages"].items()
-                for vk, vv in v.items()
-            },
-        }
-        for j in jsons
-    )
-    df = df.with_columns(
-        total_bytes=sum(
-            df.select(col for col in df.columns if col.startswith("bytes(")).fill_null(
-                0
-            )
-        ),
-        total_count=sum(
-            df.select(col for col in df.columns if col.startswith("count(")).fill_null(
-                0
-            )
-        ),
-    )
-    top_by_count = (
-        df.select(col for col in df.columns if col.startswith("count("))
-        .sum()
-        .transpose(include_header=True, header_name="msg", column_names=["count"])
-        .top_k(5, by="count")
-    )["msg"]
+    _, axs = plt.subplots(4, sharex="all", figsize=(6, 15))
 
-    plot_stacked(jsons, "count")
-    plot_stacked(jsons, "bytes")
+    df = pl.DataFrame(host_counts)
+    stack_it(axs[0], df, "Host counts")
+
+    df = pl.DataFrame(host_sizes)
+    stack_it(axs[1], df, "Host bytes", y_is_bytes=True)
+
+    df = pl.DataFrame(enclave_counts)
+    stack_it(axs[2], df, "Enclave counts")
+
+    df = pl.DataFrame(enclave_sizes)
+    stack_it(axs[3], df, "Enclave bytes", y_is_bytes=True)
+
+    legend = {}
+    for ax in axs:
+        handles, labels = ax.get_legend_handles_labels()
+        for h, l in zip(handles, labels):
+            if l not in legend:
+                legend[l] = h
+    
+    plt.figlegend(handles=legend.values(), labels=legend.keys())
+
+    path_without_ext, _ = os.path.splitext(args.load_file.name)
+    output_path = f"{path_without_ext}.png"
+    print(f"Saving plot to {output_path}")
+    plt.savefig(output_path, bbox_inches="tight")
