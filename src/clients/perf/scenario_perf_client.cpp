9a12d2666aa9994ecc84c7e416b5a33fe227eef6
Julien Maffre <jumaffre@microsoft.com>
2023-08-15 14:59:14 +0000

    Cleanup

--- src/clients/perf/scenario_perf_client.cpp
+++ src/clients/perf/scenario_perf_client.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
-#include "crypto/openssl/hash.h"
 #include "ds/files.h"
 #include "perf_client.h"
 
--- src/crypto/test/bench.cpp
+++ src/crypto/test/bench.cpp
@@ -113,8 +113,6 @@ static void benchmark_hmac(picobench::state& s)
 template <typename P, MDType M, size_t NContents>
 static void benchmark_hash(picobench::state& s)
 {
-  logger::config::default_init();
-
   const auto contents = make_contents<NContents>();
 
   s.start_timer();
@@ -356,7 +354,6 @@ namespace Hashes
 template <size_t size>
 static void sha256_bench(picobench::state& s)
 {
-  logger::config::default_init();
   crypto::openssl_sha256_init();
 
   std::vector<uint8_t> v(size);
