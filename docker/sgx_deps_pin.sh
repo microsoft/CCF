2c8bfa4b68c6279dbe56a090f073e6fd170dbe5f
Amaury Chamayou <amchamay@microsoft.com>
2023-09-05 13:21:29 +0000

    Trailing slash

--- docker/sgx_deps_pin.sh
+++ docker/sgx_deps_pin.sh
@@ -23,5 +23,5 @@ apt-get update && apt-get install -y wget gnupg
 # Reference https://manpages.debian.org/buster/apt/apt_preferences.5.en.html
 # Download the pref file from https://download.01.org/intel-sgx/sgx_repo/ubuntu/apt_preference_files/
 # Assuming file name to follow *sgx_<PSW_VERSION>_${UBUNTU}_custom_version.cfg convention
-wget -r -l1 --no-parent -nd -A "*sgx_${PSW_VERSION//./_}_${UBUNTU}_custom_version.cfg" "https://download.01.org/intel-sgx/sgx_repo/ubuntu/apt_preference_files"
+wget -r -l1 --no-parent -nd -A "*sgx_${PSW_VERSION//./_}_${UBUNTU}_custom_version.cfg" "https://download.01.org/intel-sgx/sgx_repo/ubuntu/apt_preference_files/"
 mv ./*"sgx_${PSW_VERSION//./_}_${UBUNTU}_custom_version.cfg" "/etc/apt/preferences.d/intel-sgx.pref"
