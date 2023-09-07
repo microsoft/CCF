50c6ffa8dfce103011274bd6fbfe00ec458fbe86
Amaury Chamayou <amchamay@microsoft.com>
2023-09-07 14:21:22 +0000

    schema, schema, schema

--- src/node/rpc/node_frontend.h
+++ src/node/rpc/node_frontend.h
@@ -375,7 +375,7 @@ namespace ccf
       openapi_info.description =
         "This API provides public, uncredentialed access to service and node "
         "state.";
-      openapi_info.document_version = "4.4.0";
+      openapi_info.document_version = "4.5.0";
     }
 
     void init_handlers() override
