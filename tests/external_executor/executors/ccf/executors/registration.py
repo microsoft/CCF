b803fe55a026233629231f26df323b8443748a6a
Amaury Chamayou <amchamay@microsoft.com>
2023-09-15 11:55:00 +0100

    [release/4.x] Cherry pick: Remove our copy of attestation container now that it has moved repos (#5533) (#5648)

--- tests/external_executor/executors/ccf/executors/registration.py
+++ tests/external_executor/executors/ccf/executors/registration.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the Apache 2.0 License.
 import grpc
-import time
 
 # pylint: disable=import-error
 import executor_registration_pb2 as ExecutorRegistration
@@ -9,11 +8,6 @@ import executor_registration_pb2 as ExecutorRegistration
 # pylint: disable=import-error
 import executor_registration_pb2_grpc as RegistrationService
 
-# pylint: disable=import-error
-import attestation_container_pb2_grpc as AttestationContainerService
-
-# pylint: disable=import-error
-import attestation_container_pb2 as AttestationContainer
 
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography import x509
@@ -32,7 +26,6 @@ import datetime
 from loguru import logger as LOG
 
 DEFAULT_VALIDITY_PERIOD_DAYS = 90
-ATTESTATION_CONTAINER_UNIX_DOMAIN_SOCKET = "unix:///tmp/attestation-container.sock"
 
 
 def generate_ec_keypair(curve=ec.SECP256R1):
@@ -90,46 +83,16 @@ def register_new_executor(
     node_public_rpc_address,
     service_certificate_bytes,
     supported_endpoints=None,
-    with_attestation_container=True,  # Note: remove when all executors are containerised
+    with_attestation_container=False,  # Note: remove when all executors are containerised
     timeout=3,
 ):
     # Generate a new executor identity
     key_priv_pem, _ = generate_ec_keypair()
     cert = generate_self_signed_cert(key_priv_pem)
 
-    # Retrieve attestation report and endorsements from attestation container
-    # Note: As containers (in the same container group) may startup at different speeds,
-    # wait a reasonable timeout until the attestation container is up.
-    if with_attestation_container:
-        end_time = time.time() + timeout
-        while True:
-            with grpc.insecure_channel(
-                target=ATTESTATION_CONTAINER_UNIX_DOMAIN_SOCKET,
-            ) as channel:
-                message = AttestationContainer.FetchAttestationRequest()
-                message.report_data = b""  # Note: unset for now
-                stub = AttestationContainerService.AttestationContainerStub(channel)
-
-                try:
-                    reply = stub.FetchAttestation(message)
-                except grpc.RpcError as e:
-                    if time.time() > end_time:
-                        raise TimeoutError(
-                            f"Attestation container could not be reached after {timeout}s. Stopping."
-                        ) from e
-                    LOG.trace("Attestation container starting up, retrying...")
-                    time.sleep(0.1)
-                    continue
-                else:
-                    break
-
     # Create a default NewExecutor message
     message = ExecutorRegistration.NewExecutor()
     message.attestation.format = ExecutorRegistration.Attestation.AMD_SEV_SNP_V1
-    if with_attestation_container:
-        message.attestation.attestation = reply.attestation
-        message.attestation.attestation_endorsements = reply.attestation_endorsements
-        message.attestation.uvm_endorsements = reply.uvm_endorsements
 
     if supported_endpoints:
         for method, uri in supported_endpoints:
