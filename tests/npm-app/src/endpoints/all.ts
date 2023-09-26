6e7caf0098979f4900f877a1b7963fe5dd4272ff
Dominic Ayre <dominicayre@microsoft.com>
2023-09-26 08:39:56 +0100

    Expose SNP Attestation validation in TS (#5653)

--- tests/npm-app/src/endpoints/all.ts
+++ tests/npm-app/src/endpoints/all.ts
@@ -1,6 +1,7 @@
 export * from "./jwt";
 export * from "./crypto";
 export * from "./oe";
+export * from "./snp_attestation";
 export * from "./partition";
 export * from "./proto";
 export * from "./log";
