acbab47a44e7402ce6be33f37ac358619a3aada1
Eddy Ashton <edashton@microsoft.com>
2023-10-10 10:46:06 +0000

    Shim TypedKvMap to do lazy lookup of handles

--- js/ccf-app/src/kv.ts
+++ js/ccf-app/src/kv.ts
@@ -41,43 +41,51 @@ import { KvMap, ccf } from "./global.js";
 import { DataConverter } from "./converters.js";
 
 export class TypedKvMap<K, V> {
+  private _kv() {
+    const kvMap =
+      typeof this.nameOrMap === "string"
+        ? ccf.kv[this.nameOrMap]
+        : this.nameOrMap;
+    return kvMap;
+  }
+
   constructor(
-    private kv: KvMap,
+    private nameOrMap: string | KvMap,
     private kt: DataConverter<K>,
     private vt: DataConverter<V>,
   ) {}
 
   has(key: K): boolean {
-    return this.kv.has(this.kt.encode(key));
+    return this._kv().has(this.kt.encode(key));
   }
 
   get(key: K): V | undefined {
-    const v = this.kv.get(this.kt.encode(key));
+    const v = this._kv().get(this.kt.encode(key));
     return v === undefined ? undefined : this.vt.decode(v);
   }
 
   getVersionOfPreviousWrite(key: K): number | undefined {
-    return this.kv.getVersionOfPreviousWrite(this.kt.encode(key));
+    return this._kv().getVersionOfPreviousWrite(this.kt.encode(key));
   }
 
   set(key: K, value: V): TypedKvMap<K, V> {
-    this.kv.set(this.kt.encode(key), this.vt.encode(value));
+    this._kv().set(this.kt.encode(key), this.vt.encode(value));
     return this;
   }
 
   delete(key: K): void {
-    this.kv.delete(this.kt.encode(key));
+    this._kv().delete(this.kt.encode(key));
   }
 
   clear(): void {
-    this.kv.clear();
+    this._kv().clear();
   }
 
   forEach(callback: (value: V, key: K, table: TypedKvMap<K, V>) => void): void {
     let kt = this.kt;
     let vt = this.vt;
     let typedMap = this;
-    this.kv.forEach(function (
+    this._kv().forEach(function (
       raw_v: ArrayBuffer,
       raw_k: ArrayBuffer,
       table: KvMap,
@@ -87,7 +95,7 @@ export class TypedKvMap<K, V> {
   }
 
   get size(): number {
-    return this.kv.size;
+    return this._kv().size;
   }
 }
 
@@ -109,8 +117,7 @@ export function typedKv<K, V>(
   kt: DataConverter<K>,
   vt: DataConverter<V>,
 ) {
-  const kvMap = typeof nameOrMap === "string" ? ccf.kv[nameOrMap] : nameOrMap;
-  return new TypedKvMap(kvMap, kt, vt);
+  return new TypedKvMap(nameOrMap, kt, vt);
 }
 
 /**
--- tests/js-interpreter-reuse/src/global_handle.ts
+++ tests/js-interpreter-reuse/src/global_handle.ts
@@ -1,27 +1,24 @@
 import * as ccfapp from "@microsoft/ccf-app";
 
 function increment() {
-  if (!('kvHandle' in globalThis)) {
+  if (!("kvHandle" in globalThis)) {
     globalThis.kvHandle = ccfapp.typedKv(
       "public:cached_handle_table",
       ccfapp.string,
-      ccfapp.uint32
+      ccfapp.uint32,
     );
   }
 
   const k = "single_key";
-  if (!globalThis.kvHandle.has(k))
-  {
+  if (!globalThis.kvHandle.has(k)) {
     globalThis.kvHandle.set(k, 0);
-  }
-  else
-  {
+  } else {
     const v = globalThis.kvHandle.get(k);
     globalThis.kvHandle.set(k, v + 1);
   }
 
   const v = globalThis.kvHandle.get(k);
-  
+
   return { body: { value: v } };
 }
 
