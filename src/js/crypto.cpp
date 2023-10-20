9ffed73d34ca8b95b94c63f2a6843993ad2718b8
Amaury Chamayou <amchamay@microsoft.com>
2023-10-20 10:31:19 +0000

    x509

--- src/js/crypto.cpp
+++ src/js/crypto.cpp
@@ -229,28 +229,31 @@ namespace ccf::js
     auto digest_algo_name_str = jsctx.to_str(argv[0]);
     if (!digest_algo_name_str)
     {
-      js::js_dump_error(ctx);
       return ccf::js::constants::Exception;
     }
 
     if (*digest_algo_name_str != "SHA-256")
     {
-      auto e = JS_ThrowRangeError(
+      return JS_ThrowRangeError(
         ctx, "unsupported digest algorithm, supported: SHA-256");
-      js::js_dump_error(ctx);
-      return e;
     }
 
     size_t data_size;
     uint8_t* data = JS_GetArrayBuffer(ctx, &data_size, argv[1]);
     if (!data)
     {
-      js::js_dump_error(ctx);
       return ccf::js::constants::Exception;
     }
 
-    auto h = crypto::sha256(data, data_size);
-    return JS_NewArrayBufferCopy(ctx, h.data(), h.size());
+    try
+    {
+      auto h = crypto::sha256(data, data_size);
+      return JS_NewArrayBufferCopy(ctx, h.data(), h.size());
+    }
+    catch (const std::exception& exc)
+    {
+      return JS_ThrowInternalError(ctx, "Failed to digest: %s", exc.what());
+    }
   }
 
   static JSValue js_is_valid_x509_cert_bundle(
@@ -265,7 +268,6 @@ namespace ccf::js
     auto pem = jsctx.to_str(argv[0]);
     if (!pem)
     {
-      js::js_dump_error(ctx);
       return ccf::js::constants::Exception;
     }
 
@@ -273,10 +275,15 @@ namespace ccf::js
     {
       tls::CA ca(pem.value());
     }
-    catch (const std::logic_error& e)
+    catch (const std::runtime_error& e)
     {
       LOG_DEBUG_FMT("isValidX509Bundle: {}", e.what());
       return ccf::js::constants::False;
+    }
+    catch (const std::logic_error& e)
+    {
+      return JS_ThrowInternalError(
+        ctx, "isValidX509Bundle failed: %s", e.what());
     }
 
     return ccf::js::constants::True;
@@ -299,13 +306,11 @@ namespace ccf::js
     auto chain_str = jsctx.to_str(chain_js);
     if (!chain_str)
     {
-      js::js_dump_error(ctx);
       return ccf::js::constants::Exception;
     }
     auto trusted_str = jsctx.to_str(trusted_js);
     if (!trusted_str)
     {
-      js::js_dump_error(ctx);
       return ccf::js::constants::Exception;
     }
 
@@ -314,30 +319,38 @@ namespace ccf::js
       auto chain_vec = crypto::split_x509_cert_bundle(*chain_str);
       auto trusted_vec = crypto::split_x509_cert_bundle(*trusted_str);
       if (chain_vec.empty() || trusted_vec.empty())
-        throw std::logic_error(
+      {
+        throw std::runtime_error(
           "chain/trusted arguments must contain at least one certificate");
+      }
 
       auto& target_pem = chain_vec[0];
       std::vector<const crypto::Pem*> chain_ptr;
       for (auto it = chain_vec.begin() + 1; it != chain_vec.end(); it++)
+      {
         chain_ptr.push_back(&*it);
+      }
       std::vector<const crypto::Pem*> trusted_ptr;
       for (auto& pem : trusted_vec)
+      {
         trusted_ptr.push_back(&pem);
+      }
 
       auto verifier = crypto::make_unique_verifier(target_pem);
       if (!verifier->verify_certificate(trusted_ptr, chain_ptr))
-        throw std::logic_error("certificate chain is invalid");
-    }
-    catch (const std::logic_error& e)
-    {
-      LOG_DEBUG_FMT("isValidX509Chain: {}", e.what());
-      return ccf::js::constants::False;
+      {
+        throw std::runtime_error("certificate chain is invalid");
+      }
     }
     catch (const std::runtime_error& e)
     {
       LOG_DEBUG_FMT("isValidX509Chain: {}", e.what());
       return ccf::js::constants::False;
+    }
+    catch (const std::logic_error& e)
+    {
+      return JS_ThrowInternalError(
+        ctx, "isValidX509Chain failed: %s", e.what());
     }
 
     return ccf::js::constants::True;
@@ -537,7 +550,6 @@ namespace ccf::js
     uint8_t* key = JS_GetArrayBuffer(ctx, &key_size, argv[0]);
     if (!key)
     {
-      js::js_dump_error(ctx);
       return ccf::js::constants::Exception;
     }
 
@@ -545,20 +557,18 @@ namespace ccf::js
     uint8_t* wrapping_key = JS_GetArrayBuffer(ctx, &wrapping_key_size, argv[1]);
     if (!wrapping_key)
     {
-      js::js_dump_error(ctx);
       return ccf::js::constants::Exception;
     }
 
     js::Context& jsctx = *(js::Context*)JS_GetContextOpaque(ctx);
 
     auto parameters = argv[2];
-    JSValue wrap_algo_name_val =
-      jsctx(JS_GetPropertyStr(ctx, parameters, "name"));
+    auto wrap_algo_name_val = jsctx(JS_GetPropertyStr(ctx, parameters, "name"));
+    JS_CHECK_EXC(wrap_algo_name_val);
 
     auto wrap_algo_name_str = jsctx.to_str(wrap_algo_name_val);
     if (!wrap_algo_name_str)
     {
-      js::js_dump_error(ctx);
       return ccf::js::constants::Exception;
     }
 
@@ -571,11 +581,13 @@ namespace ccf::js
         // in rsa_key_pair.h). wrapping_key is a public RSA key.
 
         auto label_val = jsctx(JS_GetPropertyStr(ctx, parameters, "label"));
+        JS_CHECK_EXC(label_val);
+
         size_t label_buf_size = 0;
         uint8_t* label_buf = JS_GetArrayBuffer(ctx, &label_buf_size, label_val);
 
         std::optional<std::vector<uint8_t>> label_opt = std::nullopt;
-        if (label_buf_size > 0)
+        if (label_buf && label_buf_size > 0)
         {
           label_opt = {label_buf, label_buf + label_buf_size};
         }
@@ -601,19 +613,22 @@ namespace ccf::js
       {
         auto aes_key_size_value =
           jsctx(JS_GetPropertyStr(ctx, parameters, "aesKeySize"));
+        JS_CHECK_EXC(aes_key_size_value);
+
         int32_t aes_key_size = 0;
         if (JS_ToInt32(ctx, &aes_key_size, aes_key_size_value) < 0)
         {
-          js::js_dump_error(ctx);
           return ccf::js::constants::Exception;
         }
 
         auto label_val = jsctx(JS_GetPropertyStr(ctx, parameters, "label"));
+        JS_CHECK_EXC(label_val);
+
         size_t label_buf_size = 0;
         uint8_t* label_buf = JS_GetArrayBuffer(ctx, &label_buf_size, label_val);
 
         std::optional<std::vector<uint8_t>> label_opt = std::nullopt;
-        if (label_buf_size > 0)
+        if (label_buf && label_buf_size > 0)
         {
           label_opt = {label_buf, label_buf + label_buf_size};
         }
@@ -629,25 +644,19 @@ namespace ccf::js
       }
       else
       {
-        auto e = JS_ThrowRangeError(
+        return JS_ThrowRangeError(
           ctx,
           "unsupported key wrapping algorithm, supported: RSA-OAEP, AES-KWP, "
           "RSA-OAEP-AES-KWP");
-        js::js_dump_error(ctx);
-        return e;
       }
     }
     catch (std::exception& ex)
     {
-      auto e = JS_ThrowRangeError(ctx, "%s", ex.what());
-      js::js_dump_error(ctx);
-      return e;
+      return JS_ThrowInternalError(ctx, "Failed to wrap key: %s", ex.what());
     }
     catch (...)
     {
-      auto e = JS_ThrowRangeError(ctx, "caught unknown exception");
-      js::js_dump_error(ctx);
-      return e;
+      return JS_ThrowRangeError(ctx, "caught unknown exception");
     }
   }
 
