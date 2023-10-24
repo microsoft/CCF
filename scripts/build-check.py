02dc6541269d37c7091bbcc7d6a9d721748cac27
Amaury Chamayou <amchamay@microsoft.com>
2023-10-24 10:14:38 +0000

    SNP only, run from CI

--- scripts/build-check.py
+++ scripts/build-check.py
@@ -1,18 +1,19 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
 import re
 import sys
 
 PLAUSIBLE_CLANG = re.compile(r"^.*/clang((\+\+)?-\d+)?$")
-NOT_ENCLAVE = re.compile(".*/(tests?|perf|host)/.*")  
+NOT_IN_TCB = re.compile(".*/(tests?|perf)/.*")
+
 
 class Checker:
-    platform = None
+    def __init__(self, platform):
+        assert platform == "SNP"
+        self.platform = platform
 
     def check_line(self, line):
-        if "Compile target platform:" in line:
-            assert self.platform is None, f"Compile target platform already set to {self.platform}, but found {line}"
-            self.platform = line.split(":")[1].strip()
-            return
-
         words = line.split(" ")
         for index, word in enumerate(words):
             if PLAUSIBLE_CLANG.match(word):
@@ -22,27 +23,24 @@ class Checker:
         if index == len(words) - 1:
             return
 
-        # Need to know what the target is to check the flags
-        assert self.platform, self.__dict__
-        
-        options = words[index+1:]
+        options = words[index + 1 :]
 
-        # Don't worry about testcases, tools etc
-        if any(NOT_ENCLAVE.match(option) for option in options):
+        if any(NOT_IN_TCB.match(option) for option in options):
             return
 
         if "-shared" in options:
             pass
         elif any(option for option in options if option.startswith("-l")):
-            if self.platform == "sgx":
-                assert "-mlvi-cfg" in options, f"Missing -mlvi-cfi in {line}"
-            # TODO: check -lvi-cfg oe libraries for sgx
+            pass
         else:
-            # Build line
+            # Compile line
             if self.platform == "snp":
-                assert "-x86-speculative-load-hardening" in options, f"Missing -x86-speculative-load-hardening in {line}"
+                assert (
+                    "-x86-speculative-load-hardening" in options
+                ), f"Missing -x86-speculative-load-hardening in {line}"
+
 
 if __name__ == "__main__":
-    checker = Checker()
+    checker = Checker(sys.argv[1])
     for line in sys.stdin:
         checker.check_line(line)
