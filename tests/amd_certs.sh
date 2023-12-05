7b364ad3949cf1b4993ef8a483ab1ee277c8a6a6
Amaury Chamayou <amchamay@microsoft.com>
2023-12-05 09:44:43 +0000

    Test THIM on ACI

---
+++ tests/amd_certs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
+set -ex
+
+# Our linter, shellcheck, expects (reasonably) that env vars are all caps. Alas.
+# shellcheck disable=SC2154
+if [ -z "${Fabric_NodeIPOrFQDN}" ]; then
+    URL="http://169.254.254.169"
+else
+    URL="http://${Fabric_NodeIPOrFQDN}:2377"
+fi
+
+curl -s "${URL}/metadata/THIM/amd/certification" -H "Metadata: true" | jq
