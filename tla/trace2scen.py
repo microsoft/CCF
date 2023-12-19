8eeb3e3b05e2ddd3924797db02437965e39e3cfb
Amaury Chamayou <amchamay@microsoft.com>
2023-12-19 17:13:48 +0000

    driver wants the term

--- tla/trace2scen.py
+++ tla/trace2scen.py
@@ -8,32 +8,35 @@ import os
 def comment(action):
     return f"# {action['name']} {action['location']['module']}:{action['location']['beginLine']}"
 
+def term(ctx, pre):
+    return str(pre["currentTerm"][ctx['i']])
+
 MAP = {
-    "ClientRequest": lambda ctx: ["replicate", ctx['i'], "42"],
-    "MCClientRequest": lambda ctx: ["replicate", ctx['i'], "42"],
-    "CheckQuorum": lambda ctx: ["periodic_one", ctx['i'], "110"],
-    "Timeout": lambda ctx: ["periodic_one",  ctx['i'], "110"],
-    "MCTimeout": lambda ctx: ["periodic_one",  ctx['i'], "110"],
-    "RequestVote": lambda _: ["# Noop"],
-    "AppendEntries": lambda _: ["dispatch_all"],
-    "BecomeLeader": lambda _: ["# Noop"],
-    "SignCommittableMessages": lambda ctx: ["emit_signature", ctx['i']],
-    "MCSignCommittableMessages": lambda ctx: ["emit_signature", ctx['i']],
-    "ChangeConfigurationInt": lambda ctx: ["replicate_new_configuration", ctx['i'], *ctx['newConfiguration']],
-    "MCChangeConfigurationInt": lambda ctx: ["replicate_new_configuration", ctx['i'], *ctx['newConfiguration']],
-    "ChangeConfiguration": lambda _: ["# Noop"],
-    "AdvanceCommitIndex": lambda _: ["# Noop"],
-    "HandleRequestVoteRequest": lambda _: ["dispatch_all"],
-    "HandleRequestVoteResponse": lambda _: ["# Noop"],
-    "RejectAppendEntriesRequest": lambda _: ["# Noop"],
-    "ReturnToFollowerState": lambda _: ["# Noop"],
-    "AppendEntriesAlreadyDone": lambda _: ["# Noop"],
+    "ClientRequest": lambda ctx, pre: ["replicate", term(ctx, pre), "42"],
+    "MCClientRequest": lambda ctx, pre: ["replicate", term(ctx, pre), "42"],
+    "CheckQuorum": lambda ctx, pre: ["periodic_one", ctx['i'], "110"],
+    "Timeout": lambda ctx, pre: ["periodic_one",  ctx['i'], "110"],
+    "MCTimeout": lambda ctx, pre: ["periodic_one",  ctx['i'], "110"],
+    "RequestVote": lambda _, __: ["# Noop"],
+    "AppendEntries": lambda _, __: ["dispatch_all"],
+    "BecomeLeader": lambda _, __: ["# Noop"],
+    "SignCommittableMessages": lambda ctx, pre: ["emit_signature", term(ctx, pre)],
+    "MCSignCommittableMessages": lambda ctx, pre: ["emit_signature", term(ctx, pre)],
+    "ChangeConfigurationInt": lambda ctx, pre: ["replicate_new_configuration", term(ctx, pre), *ctx['newConfiguration']],
+    "MCChangeConfigurationInt": lambda ctx, pre: ["replicate_new_configuration", term(ctx, pre), *ctx['newConfiguration']],
+    "ChangeConfiguration": lambda _, __: ["# Noop"],
+    "AdvanceCommitIndex": lambda _, __: ["# Noop"],
+    "HandleRequestVoteRequest": lambda _, __: ["dispatch_all"],
+    "HandleRequestVoteResponse": lambda _, __: ["# Noop"],
+    "RejectAppendEntriesRequest": lambda _, __: ["# Noop"],
+    "ReturnToFollowerState": lambda _, __: ["# Noop"],
+    "AppendEntriesAlreadyDone": lambda _, __: ["# Noop"],
 }
 
 def step_to_action(pre_state, action, post_state):
     return os.linesep.join([
         comment(action),
-        ','.join(MAP[action['name']](action['context']))])
+        ','.join(MAP[action['name']](action['context'], pre_state[1]))])
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as trace:
