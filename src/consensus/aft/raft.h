fc67f444d0b66672e53c65714fab539356fb03a3
Amaury Chamayou <amaury@xargs.fr>
2024-02-13 16:15:24 +0000

    Update src/consensus/aft/raft.h
    Co-authored-by: Eddy Ashton <ashton.eddy@gmail.com>

--- src/consensus/aft/raft.h
+++ src/consensus/aft/raft.h
@@ -309,13 +309,13 @@ namespace aft
           CCF_ASSERT(
             state->retirement_phase == kv::RetirementPhase::Completed,
             "Node is not retired, cannot become retired committed");
-          state->retirement_phase = kv::RetirementPhase::RetiredCommitted;
           CCF_ASSERT_FMT(
             state->retired_committed_idx == state->commit_idx,
             "Retired "
             "committed index {} does not match current commit index {}",
             state->retired_committed_idx.value_or(0),
             state->commit_idx);
+          state->retirement_phase = kv::RetirementPhase::RetiredCommitted;
           state->retired_committed_idx = seqno;
         }
       }
