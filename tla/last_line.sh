db7c2ac4faa5a86a8dbc953c10ad254abc830ba0
Amaury Chamayou <amchamay@microsoft.com>
2024-02-13 11:44:33 +0000

    Add details of last matching line in TV CI

---
+++ tla/last_line.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
+# Extract a summary of the last line in a trace, useful when finding out where a scenario has mismatched
+
+if [ -e "$1" ]; then
+    cat "$1" | jq '.action | last | .[2][1]._logline | "Last matched [" + .h_ts + "] " + .msg.function + " (" + .cmd + ")"'
+fi
