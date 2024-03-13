98f120a999147b3d0b5e145b4025dd0515494bfa
Amaury Chamayou <amchamay@microsoft.com>
2024-03-13 16:03:36 +0000

    fmt

--- samples/apps/logging/create_tx_claims_digest.cpp
+++ samples/apps/logging/create_tx_claims_digest.cpp
@@ -1,15 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
-#include <optional>
-#include <ccf/tx.h>
 #include <ccf/receipt.h>
 #include <ccf/service/tables/constitution.h>
+#include <ccf/tx.h>
+#include <optional>
 
 namespace ccfapp
 {
-  std::optional<ccf::ClaimsDigest::Digest> get_create_tx_claims_digest(kv::ReadOnlyTx& tx)
+  std::optional<ccf::ClaimsDigest::Digest> get_create_tx_claims_digest(
+    kv::ReadOnlyTx& tx)
   {
-    auto constitution = tx.ro<ccf::Constitution>(ccf::Tables::CONSTITUTION)->get();
+    auto constitution =
+      tx.ro<ccf::Constitution>(ccf::Tables::CONSTITUTION)->get();
     if (!constitution.has_value())
     {
       throw std::logic_error("Constitution is missing");
