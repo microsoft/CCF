bb1fd7f859589dde58e486da31c33d722cdf6378
Amaury Chamayou <amchamay@microsoft.com>
2024-03-20 11:02:04 +0000

    Plot distinct states

--- tla/actions.py
+++ tla/actions.py
@@ -6,14 +6,19 @@ import sys
 from collections import defaultdict
 import hvplot
 import hvplot.pandas
+import json
 
 """
 A little script to plot TLC action count
 
-Run TLC with -coverage 1 (or higher), filter the output through
-egrep "<.*\sline.*>:" | grep -v Inv
-To only get action states lines, and remove invariants, and then
-pipe the output to this script.
+Run TLC with -coverage 1 (or higher), and pip the input in, e.g.:
+cat <spec>_coverage_*.json | python3 actions.py
+
+The spec needs to have constraint like:
+
+SerialiseCoverageConstraint ==
+    LET interval == 100000
+    IN IF TLCGet("distinct") % interval = 0 THEN ndJsonSerialize("MCccfraftAtomicReconfig_coverage_" \o ToString(TLCGet("distinct") \div interval) \o ".json", <<TLCGet("spec")>>) ELSE TRUE
 """
 
 TOP=5
@@ -21,16 +26,22 @@ TOP=5
 if __name__ == "__main__":
     ts = defaultdict(list)
     for line in sys.stdin:
-        words = line.split(" ")
-        action = words[0][1:]
-        unique = words[-1].split(":")[0]
-        ts[action].append(int(unique))
+        cov_dump = json.loads(line)
+        actions = {}
+        # De-duplicate actions by name
+        for action in cov_dump["actions"]:
+            actions[action["name"]] = action["coverage"]["distinct"]
+        for action, count in actions.items():
+            ts[action].append(count)
     df = pl.DataFrame(ts)
     # There's surely a nice way to do this in polars, but I did not find it.
     # Just get the top TOP columns by last action count.
     col_max = sorted([(col, df[-1][col][-1]) for col in df.columns], key=lambda x: x[1])
-    top_TOP = [x[0] for x in col_max[-TOP:]] # + ["AppendRetiredCommitted"]
+    top_TOP = [x[0] for x in col_max[-TOP:]]
     df_TOP = df[top_TOP]
     print(df_TOP)
-    plot = df_TOP.plot.line(y=df_TOP.columns, width=1200, height=600, title=f"Top {TOP} actions")
-    hvplot.show(plot)
+    plot = df_TOP.plot.line(y=df_TOP.columns, width=1200, height=600, title=f"Top {TOP} distinct actions")
+    if len(sys.argv) > 1:
+        hvplot.save(plot, sys.argv[1])
+    else:
+        hvplot.show(plot)
