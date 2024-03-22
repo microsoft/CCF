cfeac208dbae2efcbfb1a8140dc7cb09bb4b83b6
Eddy Ashton <edashton@microsoft.com>
2024-03-22 16:28:10 +0000

    Uniform handling of allOf and if

--- doc/generate_config_rst.py
+++ doc/generate_config_rst.py
@@ -7,7 +7,8 @@ import json
 import tempfile
 import filecmp
 
-HEADER_DEPTH=1
+HEADER_DEPTH = 1
+
 
 class SchemaRstGenerator:
     def __init__(self):
@@ -26,7 +27,7 @@ class SchemaRstGenerator:
         self._lines.append(text)
         self._lines.append(depth_to_char[self._depth] * len(text))
 
-    def _start_property_section(self, header):
+    def _start_definition_section(self, header):
         if self._lines and self._lines[-1] != "|":
             self._lines.append("|")
         self.add_line(header)
@@ -37,7 +38,7 @@ class SchemaRstGenerator:
             self._start_header_section(prefix + header)
         else:
             header = f":configproperty:`{prefix}{header}`"
-            self._start_property_section(header)
+            self._start_definition_section(header)
         self._depth += 1
 
     def end_section(self):
@@ -50,43 +51,28 @@ class SchemaRstGenerator:
         return "\n".join(self._prefix) + "\n".join(self._lines)
 
 
-def dump_object(output: SchemaRstGenerator, obj: dict, path: list = []):
-    required = obj.get("required", [])
-    properties = obj.get("properties", {})
+def dump_property(
+    output: SchemaRstGenerator,
+    property_name: str,
+    obj: dict,
+    required: bool = False,
+    path_to_property: list = [],
+    conditions=[],
+):
+    prefix = "".join(path_to_property)
 
-    def prefix():
-        _prefix = "".join(f"{e}" for e in path)
-        return _prefix
+    output.start_section(property_name, prefix=prefix)
 
-    for k, v in properties.items():
-        output.start_section(k, prefix=prefix())
-        dump(output, v, path + [f"{k}."], required=k in required)
-        output.end_section()
+    for condition in conditions:
+        output.add_line(f"`{condition}`")
 
-    additional = obj.get("additionalProperties", None)
-    if additional:
-        assert isinstance(additional, dict)
-
-        k = "[name]"
-        output.start_section(k, prefix=prefix())
-        dump(output, additional, path + [f"{k}."])
-        output.end_section()
-
-
-def monospace_literal(v):
-    return f"``{json.dumps(v)}``"
-
-
-def dump(output: SchemaRstGenerator, obj: dict, path=[], required=False):
-    desc = obj.get("description", None)
-    if desc:
-        # Insert a trailing full-stop, but only if not present in original string
-        if desc[-1] != ".":
-            desc = desc + "."
-        output.add_line(desc)
+    t = obj.get("type")
+    if isinstance(t, list):
+        t = " | ".join(t)
+    output.add_kv_line("Type", t)
 
     if required:
-        output.add_line("*Required*")
+        output.add_kv_line("Required", "true")
 
     if "enum" in obj:
         output.add_kv_line(
@@ -105,13 +91,73 @@ def dump(output: SchemaRstGenerator, obj: dict, path=[], required=False):
     if maximum:
         output.add_kv_line("Maximum", monospace_literal(maximum))
 
-    t = obj.get("type")
+    desc = obj.get("description", None)
+    if desc:
+        # Insert a trailing full-stop, but only if not present in original string
+        if desc[-1] != ".":
+            desc = desc + "."
+        output.add_line(desc)
+
     if t == "object":
-        dump_object(output, obj, path)
-    else:
-        if isinstance(t, list):
-            t = " | ".join(t)
-        output.add_kv_line("Type", t)
+        dump_object(output, obj, path_to_property)
+
+    output.end_section()
+
+
+def dump_object(output: SchemaRstGenerator, obj: dict, path: list = [], conditions=[]):
+    props = []
+
+    def add_prop(name, obj, required=False, **kwargs):
+        props.append(
+            {
+                "property_name": name,
+                "obj": obj,
+                "required": required,
+                "path_to_property": kwargs.pop("path_to_property", path),
+                "conditions": kwargs.pop("conditions", conditions),
+            }
+        )
+
+    def gather_properties(obj, **kwargs):
+        required = obj.get("required", [])
+        properties = obj.get("properties", {})
+
+        for k, v in properties.items():
+            add_prop(k, v, k in required, **kwargs)
+
+        additional = obj.get("additionalProperties", None)
+        if additional:
+            assert isinstance(additional, dict)
+            add_prop("[name]", additional, **kwargs)
+
+        all_of = obj.get("allOf", None)
+        if all_of:
+            for schema in all_of:
+                gather_properties(schema)
+
+        if_el = obj.get("if", None)
+        if if_el:
+            assert "then" in obj, "Missing 'then' clause from JSON schema"
+            assert not "else" in obj, "'else' clause from JSON schema currently unsupported"
+
+            extra_conditions = []
+            for k, cond in if_el["properties"].items():
+                assert "const" in cond, "Only 'const' conditions supported"
+                extra_conditions.append(f"(Only if {k} is {cond['const']})")
+
+            gather_properties(obj["then"], conditions=conditions + extra_conditions)
+
+    gather_properties(obj)
+
+    for p in props:
+        dump_property(
+            output=output,
+            **p,
+        )
+
+
+def monospace_literal(v):
+    return f"``{json.dumps(v)}``"
 
 
 def generate_configuration_docs(input_file_path, output_file_path):
