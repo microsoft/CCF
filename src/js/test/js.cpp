63f063c7d38f55d4d6ffa72c4e9bff06f90b2f7c
Eddy Ashton <edashton@microsoft.com>
2024-06-06 15:38:57 +0000

    Unit test the free function

--- src/js/test/js.cpp
+++ src/js/test/js.cpp
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "js/map_access_permissions.h"
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
+#include <random>
 
 TEST_CASE("Check KV Map access")
 {
@@ -165,6 +166,162 @@ TEST_CASE("Check KV Map access")
       REQUIRE(
         check_kv_map_access(TxAccess::GOV_RW, private_app_table_name) ==
         MapAccessPermissions::ILLEGAL);
+    }
+  }
+}
+
+TEST_CASE("Check KV Map namespace restrictions")
+{
+  using namespace ccf::js;
+
+  constexpr auto RW = MapAccessPermissions::READ_WRITE;
+  constexpr auto RO = MapAccessPermissions::READ_ONLY;
+  constexpr auto ILL = MapAccessPermissions::ILLEGAL;
+
+  NamespaceRestriction ro_nr;
+  ro_nr.regex = std::regex(".*bar.*");
+  ro_nr.permission = RO;
+
+  NamespaceRestriction ill_nr;
+  ill_nr.regex = std::regex(".*baz.*");
+  ill_nr.permission = ILL;
+
+  {
+    INFO("With a single read-only namespace restriction");
+    NamespaceRestrictions restrictions{ro_nr};
+
+    {
+      INFO("Non-matching tables are unaffected");
+      REQUIRE(
+        calculate_namespace_restrictions(RW, restrictions, "any.other.table") ==
+        RW);
+    }
+
+    {
+      INFO("Matching tables may become more restricted");
+      REQUIRE(calculate_namespace_restrictions(RW, restrictions, "bar") == RO);
+      REQUIRE(
+        calculate_namespace_restrictions(RW, restrictions, "foobar") == RO);
+      REQUIRE(
+        calculate_namespace_restrictions(RW, restrictions, "barfoo") == RO);
+      REQUIRE(
+        calculate_namespace_restrictions(RW, restrictions, "foobarfoo") == RO);
+    }
+
+    {
+      INFO("Tables cannot become less restricted");
+      REQUIRE(
+        calculate_namespace_restrictions(RO, restrictions, "any.other.table") ==
+        RO);
+
+      REQUIRE(
+        calculate_namespace_restrictions(ILL, restrictions, "bar") == ILL);
+    }
+  }
+
+  {
+    INFO("With a single illegal namespace restriction");
+    NamespaceRestrictions restrictions{ill_nr};
+
+    {
+      INFO("Non-matching tables are unaffected");
+      REQUIRE(
+        calculate_namespace_restrictions(RW, restrictions, "any.other.table") ==
+        RW);
+    }
+
+    {
+      INFO("Matching tables may become more restricted");
+      for (auto current : {RW, RO})
+      {
+        REQUIRE(
+          calculate_namespace_restrictions(current, restrictions, "baz") ==
+          ILL);
+        REQUIRE(
+          calculate_namespace_restrictions(current, restrictions, "foobaz") ==
+          ILL);
+        REQUIRE(
+          calculate_namespace_restrictions(current, restrictions, "bazfoo") ==
+          ILL);
+        REQUIRE(
+          calculate_namespace_restrictions(
+            current, restrictions, "foobazfoo") == ILL);
+      }
+    }
+
+    {
+      INFO("Tables cannot become less restricted");
+      REQUIRE(
+        calculate_namespace_restrictions(RO, restrictions, "any.other.table") ==
+        RO);
+
+      REQUIRE(
+        calculate_namespace_restrictions(ILL, restrictions, "baz") == ILL);
+    }
+  }
+
+  {
+    INFO("With multiple namespace restrictions");
+
+    NamespaceRestriction ro_nr2;
+    ro_nr2.regex = std::regex(".*bur.*");
+    ro_nr2.permission = RO;
+
+    NamespaceRestriction ill_nr2;
+    ill_nr2.regex = std::regex(".*buz.*");
+    ill_nr2.permission = ILL;
+
+    NamespaceRestrictions restrictions{ro_nr, ro_nr2, ill_nr, ill_nr2};
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    for (size_t i = 0; i < 10; ++i)
+    {
+      INFO("Regardless of restriction order");
+      std::shuffle(restrictions.begin(), restrictions.end(), g);
+
+      {
+        INFO("Non-matching tables are unaffected");
+        REQUIRE(
+          calculate_namespace_restrictions(
+            RW, restrictions, "any.other.table") == RW);
+      }
+
+      {
+        INFO("Matching tables may become more restricted");
+        REQUIRE(
+          calculate_namespace_restrictions(RW, restrictions, "bar") == RO);
+        REQUIRE(
+          calculate_namespace_restrictions(RW, restrictions, "bur") == RO);
+        REQUIRE(
+          calculate_namespace_restrictions(RW, restrictions, "baz") == ILL);
+        REQUIRE(
+          calculate_namespace_restrictions(RW, restrictions, "buz") == ILL);
+
+        REQUIRE(
+          calculate_namespace_restrictions(RW, restrictions, "fooburfoo") ==
+          RO);
+        REQUIRE(
+          calculate_namespace_restrictions(RW, restrictions, "foobarburfoo") ==
+          RO);
+        REQUIRE(
+          calculate_namespace_restrictions(RW, restrictions, "foobuzfoo") ==
+          ILL);
+        REQUIRE(
+          calculate_namespace_restrictions(RW, restrictions, "foobarbuzfoo") ==
+          ILL);
+      }
+
+      {
+        INFO("Tables cannot become less restricted");
+        REQUIRE(
+          calculate_namespace_restrictions(
+            RO, restrictions, "any.other.table") == RO);
+        REQUIRE(
+          calculate_namespace_restrictions(
+            ILL, restrictions, "any.other.table") == ILL);
+      }
     }
   }
 }
