7751c0ea74319537f5279981a6953f59010576aa
Amaury Chamayou <amchamay@microsoft.com>
2024-06-06 12:52:30 +0000

    Multi-role sample

--- tests/programmability.py
+++ tests/programmability.py
@@ -21,6 +21,35 @@ export function content(request) {
 }
 """
 
+TESTJS_ROLE = """
+export function content(request) {
+  let raw_id = ccf.strToBuf(request.caller.id);
+  let user_info = ccf.kv["public:ccf.gov.users.info"].get(raw_id);
+  if (user_info !== undefined) {
+    user_info = ccf.bufToJsonCompatible(user_info);
+    let roles = user_info?.user_data?.roles || [];
+
+    for (const [_, role] of roles.entries()) {
+        let role_map = ccf.kv[`public:ccf.gov.roles.${role}`];
+        let endpoint_name = request.url.split("/")[2];
+        if (role_map?.has(ccf.strToBuf(`/${endpoint_name}/read`)))
+        {
+            return {
+                statusCode: 200,
+                body: {
+                    payload: "Test content",
+                },
+            };
+        }
+    }
+  }
+
+  return {
+    statusCode: 403
+  };
+}
+"""
+
 
 def test_custom_endpoints(network, args):
     primary, _ = network.find_primary()
@@ -84,6 +113,36 @@ def test_custom_role_definitions(network, args):
     primary, _ = network.find_primary()
     member = network.consortium.get_any_active_member()
 
+    # Assign a role to user0
+    user = network.users[0]
+    network.consortium.set_user_data(
+        primary,
+        user.service_id,
+        user_data={"isAdmin": True, "roles": ["ContentGetter"]},
+    )
+
+    content_endpoint_def = {
+        "get": {
+            "js_module": "test.js",
+            "js_function": "content",
+            "forwarding_required": "never",
+            "redirection_strategy": "none",
+            "authn_policies": ["user_cert"],
+            "mode": "readonly",
+            "openapi": {},
+        }
+    }
+
+    bundle_with_auth = {
+        "metadata": {"endpoints": {"/content": content_endpoint_def}},
+        "modules": {"test.js": TESTJS_ROLE},
+    }
+
+    # Install app with auth/role support
+    with primary.client(None, None, user.local_id) as c:
+        r = c.put("/app/custom_endpoints", body={"bundle": bundle_with_auth})
+        assert r.status_code == http.HTTPStatus.NO_CONTENT.value, r.status_code
+
     # Add role definition
     prop = member.propose(
         primary,
@@ -95,12 +154,33 @@ def test_custom_role_definitions(network, args):
     )
     member.vote(primary, prop, ballot_yes)
 
+    # user0 has "ContentGetter" role, which has "/content/read" should be able to access "/content"
+    with primary.client("user0") as c:
+        r = c.get("/app/content")
+        assert r.status_code == http.HTTPStatus.OK, r.status_code
+        assert r.body.json()["payload"] == "Test content", r.body.json()
+
+    # But user1 does not
+    with primary.client("user1") as c:
+        r = c.get("/app/content")
+        assert r.status_code == http.HTTPStatus.FORBIDDEN, r.status_code
+
+    # And unauthenticated users definitely don't
+    with primary.client() as c:
+        r = c.get("/app/content")
+        assert r.status_code == http.HTTPStatus.UNAUTHORIZED, r.status_code
+
     # Delete role definition
     prop = member.propose(
         primary,
         proposal(action("set_role_definition", role="ContentGetter", actions=[])),
     )
     member.vote(primary, prop, ballot_yes)
+
+    # Now user0 can't access /content anymore
+    with primary.client("user0") as c:
+        r = c.get("/app/content")
+        assert r.status_code == http.HTTPStatus.FORBIDDEN, r.status_code
 
     # Multiple definitions
     prop = member.propose(
@@ -118,6 +198,38 @@ def test_custom_role_definitions(network, args):
     )
     member.vote(primary, prop, ballot_yes)
 
+    bundle_with_auth_both = {
+        "metadata": {
+            "endpoints": {
+                "/content": content_endpoint_def,
+                "/other_content": content_endpoint_def,
+            }
+        },
+        "modules": {"test.js": TESTJS_ROLE},
+    }
+
+    # Install two endpoints with role auth
+    with primary.client(None, None, user.local_id) as c:
+        r = c.put("/app/custom_endpoints", body={"bundle": bundle_with_auth_both})
+        assert r.status_code == http.HTTPStatus.NO_CONTENT.value, r.status_code
+
+    # Assign the new role to user0
+    user = network.users[0]
+    network.consortium.set_user_data(
+        primary,
+        user.service_id,
+        user_data={"isAdmin": True, "roles": ["ContentGetter", "AllContentGetter"]},
+    )
+
+    # user0 has access both now
+    with primary.client("user0") as c:
+        r = c.get("/app/content")
+        assert r.status_code == http.HTTPStatus.OK, r.status_code
+        assert r.body.json()["payload"] == "Test content", r.body.json()
+        r = c.get("/app/other_content")
+        assert r.status_code == http.HTTPStatus.OK, r.status_code
+        assert r.body.json()["payload"] == "Test content", r.body.json()
+
     return network
 
 
@@ -130,9 +242,7 @@ def run(args):
         pdb=args.pdb,
     ) as network:
         network.start_and_open(args)
-
         test_custom_endpoints(network, args)
-
         test_custom_role_definitions(network, args)
 
 
