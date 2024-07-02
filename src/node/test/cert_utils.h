a039dde2fa8a61f1be1bdf1965fe9787b8b7a554
Eddy Ashton <edashton@microsoft.com>
2024-07-02 14:12:39 +0000

    Fixup

--- src/node/test/cert_utils.h
+++ src/node/test/cert_utils.h
@@ -4,14 +4,14 @@
 
 #include "crypto/certs.h"
 
-crypto::Pem make_self_signed_cert(crypto::KeyPairPtr kp)
+ccf::crypto::Pem make_self_signed_cert(ccf::crypto::KeyPairPtr kp)
 {
   constexpr size_t certificate_validity_period_days = 365;
   using namespace std::literals;
   const auto valid_from =
-    ds::to_x509_time_string(std::chrono::system_clock::now() - 24h);
+    ::ds::to_x509_time_string(std::chrono::system_clock::now() - 24h);
 
-  const auto valid_to = crypto::compute_cert_valid_to_string(
+  const auto valid_to = ccf::crypto::compute_cert_valid_to_string(
     valid_from, certificate_validity_period_days);
 
   return kp->self_sign("CN=Node", valid_from, valid_to);
