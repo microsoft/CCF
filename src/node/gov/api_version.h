d400322255c788dead363f40f934036028ca119f
Eddy Ashton <edashton@microsoft.com>
2024-07-02 09:47:41 +0000

    Return new API from gov/api, and nicer errors there

--- src/node/gov/api_version.h
+++ src/node/gov/api_version.h
@@ -20,7 +20,9 @@ namespace ccf::gov::endpoints
     {ApiVersion::v1, "2024-07-01"}};
 
   std::optional<ApiVersion> get_api_version(
-    ccf::endpoints::CommandEndpointContext& ctx)
+    ccf::endpoints::CommandEndpointContext& ctx,
+    // Optional out-parameter indicating why API version wasn't found
+    const char** error_code = nullptr)
   {
     static std::string accepted_versions_suffix = "";
     if (accepted_versions_suffix.empty())
@@ -55,6 +57,10 @@ namespace ccf::gov::endpoints
           "requests. {}",
           param_name,
           accepted_versions_suffix));
+      if (error_code != nullptr)
+      {
+        *error_code = ccf::errors::MissingApiVersionParameter;
+      }
       return std::nullopt;
     }
 
@@ -72,6 +78,10 @@ namespace ccf::gov::endpoints
         HTTP_STATUS_BAD_REQUEST,
         ccf::errors::UnsupportedApiVersionValue,
         std::move(message));
+      if (error_code != nullptr)
+      {
+        *error_code = ccf::errors::UnsupportedApiVersionValue;
+      }
       return std::nullopt;
     }
 
