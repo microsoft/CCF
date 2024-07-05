c36ea23f8ece0d3726df0ce06ed1c43e30c2ffb6
Amaury Chamayou <amchamay@microsoft.com>
2024-07-05 14:47:54 +0000

    Remove duplicate header

--- include/ccf/endpoints/authentication/js.h
+++ include/ccf/endpoints/authentication/js.h
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
 
+#include "ccf/common_auth_policies.h"
 #include "ccf/endpoint.h"
 #include "ccf/endpoints/authentication/all_of_auth.h"
 
