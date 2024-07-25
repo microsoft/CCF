5157ee1e6029f9f1078a6637aa189f3d58a1e9ab
Eddy Ashton <edashton@microsoft.com>
2024-07-25 10:59:46 +0000

    Add missing notices and pragmas

--- .cmake-format.py
+++ .cmake-format.py
@@ -1,4 +1,6 @@
-# ----------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+# # ----------------------------------
 # Options affecting listfile parsing
 # ----------------------------------
 with section("parse"):
