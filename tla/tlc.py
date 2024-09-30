3723fde20f5454c32b34ec2a6b9384d833e65530
Amaury Chamayou <amchamay@microsoft.com>
2024-09-30 14:34:49 +0000

    Expand flags, format Python under tlc/ too

--- tla/tlc.py
+++ tla/tlc.py
@@ -13,36 +13,84 @@
 import os
 import argparse
 import pprint
+import pathlib
 
 DEFAULT_JVM_ARGS = [
-    "-XX:+UseParallelGC", # Use parallel garbage collection, for performance
+    "-XX:+UseParallelGC",  # Use parallel garbage collection, for performance
 ]
 
 
-DEFAULT_CLASSPATH_ARGS = [
-    '-cp', 'tla2tools.jar:CommunityModules-deps.jar'
-]
+DEFAULT_CLASSPATH_ARGS = ["-cp", "tla2tools.jar:CommunityModules-deps.jar"]
 
 
 def cli():
-    parser = argparse.ArgumentParser(description="TLC model checker wrapper for the CCF project")
-    parser.add_argument("--disable-cdot", action="store_true", help="Disable \\\cdot support")
-    parser.add_argument("--jmx", action="store_true", help="Enable JMX to allow monitoring, use echo 'get -b tlc2.tool:type=ModelChecker CurrentState' | jmxterm -l localhost:55449 -i to query")
-    parser.add_argument("-v", action="store_true", help="Print out command and environment before running")
-    parser.add_argument("--workers", type=str, default="auto", help="Number of workers to use, default is 'auto'")
-    parser.add_argument("--checkpoint", type=int, default=0, help="Checkpoint interval, default is 0")
-    parser.add_argument("--lncheck", type=str, choices=["final", "default"], default="final", help="Liveness check, set to 'default' to run periodically or 'final' to run once at the end, default is final")
-    parser.add_argument("spec", help="Path to the TLA+ specification")
+    parser = argparse.ArgumentParser(
+        description="TLC model checker wrapper for the CCF project"
+    )
+    parser.add_argument(
+        "--disable-cdot", action="store_true", help="Disable \\\cdot support"
+    )
+    parser.add_argument(
+        "--jmx",
+        action="store_true",
+        help="Enable JMX to allow monitoring, use echo 'get -b tlc2.tool:type=ModelChecker CurrentState' | jmxterm -l localhost:55449 -i to query",
+    )
+    parser.add_argument(
+        "-v",
+        action="store_true",
+        help="Print out command and environment before running",
+    )
+    parser.add_argument(
+        "--workers",
+        type=str,
+        default="auto",
+        help="Number of workers to use, default is 'auto'",
+    )
+    parser.add_argument(
+        "--checkpoint", type=int, default=0, help="Checkpoint interval, default is 0"
+    )
+    parser.add_argument(
+        "--lncheck",
+        type=str,
+        choices=["final", "default"],
+        default="final",
+        help="Liveness check, set to 'default' to run periodically or 'final' to run once at the end, default is final",
+    )
+    parser.add_argument(
+        "--config",
+        type=pathlib.Path,
+        default=None,
+        help="Path to the TLA+ configuration",
+    )
+    parser.add_argument(
+        "spec", type=pathlib.Path, help="Path to the TLA+ specification"
+    )
     return parser
+
 
 if __name__ == "__main__":
     env = os.environ.copy()
     args = cli().parse_args()
     jvm_args = DEFAULT_JVM_ARGS
+    cp_args = DEFAULT_CLASSPATH_ARGS
+    tlc_args = [
+        "-workers",
+        args.workers,
+        "-checkpoint",
+        str(args.checkpoint),
+        "-lncheck",
+        args.lncheck,
+    ]
+    trace_name = os.path.basename(args.spec).replace(".tla", "")
+
     if "CI" in env:
         # When run in CI, format output for GitHub, and participate in statistics collection
         jvm_args.append("-Dtlc2.TLC.ide=Github")
-        jvm_args.append("-Dutil.ExecutionStatisticsCollector.id=be29f6283abeed2fb1fd0be898bc6601")
+        jvm_args.append(
+            "-Dutil.ExecutionStatisticsCollector.id=be29f6283abeed2fb1fd0be898bc6601"
+        )
+        # When run in CI, always dump a JSON trace
+        tlc_args.extend(["-dumpTrace", "json", f"{trace_name}.json"])
     if args.jmx:
         jvm_args.append("-Dcom.sun.management.jmxremote")
         jvm_args.append("-Dcom.sun.management.jmxremote.port=55449")
@@ -50,14 +98,11 @@ if __name__ == "__main__":
         jvm_args.append("-Dcom.sun.management.jmxremote.authenticate=false")
     if not args.disable_cdot:
         jvm_args.append("-Dtlc2.tool.impl.Tool.cdot=true")
-    cp_args = DEFAULT_CLASSPATH_ARGS
-    tlc_args = [
-        "-workers", args.workers,
-        "-checkpoint", str(args.checkpoint),
-        "-lncheck", args.lncheck,
-    ]
-    cmd = ['java'] + jvm_args +  cp_args + ['tlc2.TLC'] + tlc_args + [args.spec]
+    if args.config:
+        tlc_args.extend(["-config", args.config])
+
+    cmd = ["java"] + jvm_args + cp_args + ["tlc2.TLC"] + tlc_args + [args.spec]
     if args.v:
         pprint.pprint(env)
         pprint.pprint(cmd)
-    os.execvpe('java', cmd, env)
+    os.execvpe("java", cmd, env)
--- tla/trace2scen.py
+++ tla/trace2scen.py
@@ -5,27 +5,41 @@ import sys
 import json
 import os
 
+
 def comment(action):
     return f"# {action['name']} {action['location']['module']}:{action['location']['beginLine']}"
 
+
 def term(ctx, pre):
-    return str(pre["currentTerm"][ctx['i']])
+    return str(pre["currentTerm"][ctx["i"]])
+
 
 def noop(ctx, pre, post):
     return ["# Noop"]
 
+
 MAP = {
     "ClientRequest": lambda ctx, pre, post: ["replicate", term(ctx, pre), "42"],
     "MCClientRequest": lambda ctx, pre, post: ["replicate", term(ctx, pre), "42"],
-    "CheckQuorum": lambda ctx, pre, post: ["periodic_one", ctx['i'], "110"],
-    "Timeout": lambda ctx, pre, post: ["periodic_one",  ctx['i'], "110"],
-    "MCTimeout": lambda ctx, pre, post: ["periodic_one",  ctx['i'], "110"],
+    "CheckQuorum": lambda ctx, pre, post: ["periodic_one", ctx["i"], "110"],
+    "Timeout": lambda ctx, pre, post: ["periodic_one", ctx["i"], "110"],
+    "MCTimeout": lambda ctx, pre, post: ["periodic_one", ctx["i"], "110"],
     "RequestVote": noop,
     "AppendEntries": lambda _, __, ___: ["dispatch_all"],
     "BecomeLeader": noop,
-    "SignCommittableMessages": lambda ctx, pre, post: ["emit_signature", term(ctx, pre)],
-    "MCSignCommittableMessages": lambda ctx, pre, post: ["emit_signature", term(ctx, pre)],
-    "ChangeConfigurationInt": lambda ctx, pre, post: ["replicate_new_configuration", term(ctx, pre), *ctx["newConfiguration"]],
+    "SignCommittableMessages": lambda ctx, pre, post: [
+        "emit_signature",
+        term(ctx, pre),
+    ],
+    "MCSignCommittableMessages": lambda ctx, pre, post: [
+        "emit_signature",
+        term(ctx, pre),
+    ],
+    "ChangeConfigurationInt": lambda ctx, pre, post: [
+        "replicate_new_configuration",
+        term(ctx, pre),
+        *ctx["newConfiguration"],
+    ],
     "AdvanceCommitIndex": noop,
     "HandleRequestVoteRequest": lambda _, __, ___: ["dispatch_all"],
     "HandleRequestVoteResponse": noop,
@@ -38,28 +52,43 @@ MAP = {
     "RcvRequestVoteResponse": noop,
 }
 
+
 def post_commit(post):
-    return [["assert_commit_idx", node, str(idx)] for node, idx in post["commitIndex"].items()]
+    return [
+        ["assert_commit_idx", node, str(idx)]
+        for node, idx in post["commitIndex"].items()
+    ]
+
 
 def post_state(post):
     entries = []
     for node, state in post["state"].items():
-            entries.append(["assert_detail", "leadership_state", node, state])
+        entries.append(["assert_detail", "leadership_state", node, state])
     return entries
 
+
 def step_to_action(pre_state, action, post_state):
-    return os.linesep.join([
-        comment(action),
-        ','.join(MAP[action['name']](action['context'], pre_state[1], post_state[1]))])
+    return os.linesep.join(
+        [
+            comment(action),
+            ",".join(
+                MAP[action["name"]](action["context"], pre_state[1], post_state[1])
+            ),
+        ]
+    )
+
 
 def asserts(pre_state, action, post_state, assert_gen):
-    return os.linesep.join([','.join(assertion) for assertion in assert_gen(post_state[1])])
+    return os.linesep.join(
+        [",".join(assertion) for assertion in assert_gen(post_state[1])]
+    )
+
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as trace:
         steps = json.load(trace)["action"]
         initial_state = steps[0][0][1]
-        initial_node, = [node for node, log in initial_state["log"].items() if log]
+        (initial_node,) = [node for node, log in initial_state["log"].items() if log]
         print(f"start_node,{initial_node}")
         print(f"emit_signature,2")
         for step in steps:
