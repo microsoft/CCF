85e39abac419519561e743b22dbbc731b3462cd9
Max Tropets <maxtropets@gmail.com>
2024-10-18 11:50:09 +0000

    CDDL schema e2e test for cose sig

--- samples/apps/logging/logging.cpp
+++ samples/apps/logging/logging.cpp
@@ -2002,6 +2002,42 @@ namespace loggingapp
         .set_auto_schema<void, LoggingGetCoseEndorsements::Out>()
         .set_forwarding_required(ccf::endpoints::ForwardingRequired::Never)
         .install();
+
+      auto get_cose_signature = [this](
+                                  ccf::endpoints::ReadOnlyEndpointContext& ctx,
+                                  ccf::historical::StatePtr historical_state) {
+        auto historical_tx = historical_state->store->create_read_only_tx();
+
+        assert(historical_state->receipt);
+        auto signature = describe_cose_signature_v1(*historical_state->receipt);
+        if (!signature.has_value())
+        {
+          ctx.rpc_ctx->set_error(
+            HTTP_STATUS_NOT_FOUND,
+            ccf::errors::ResourceNotFound,
+            "No COSE signature available for this transaction");
+          return;
+        }
+
+        LoggingGetCoseSignature::Out response{
+          .cose_signature = signature.value()};
+
+        nlohmann::json j_response = response;
+        ctx.rpc_ctx->set_response_status(HTTP_STATUS_OK);
+        ctx.rpc_ctx->set_response_header(
+          ccf::http::headers::CONTENT_TYPE,
+          ccf::http::headervalues::contenttype::JSON);
+        ctx.rpc_ctx->set_response_body(j_response.dump());
+      };
+      make_read_only_endpoint(
+        "/log/public/cose_signature",
+        HTTP_GET,
+        ccf::historical::read_only_adapter_v4(
+          get_cose_signature, context, is_tx_committed),
+        auth_policies)
+        .set_auto_schema<void, LoggingGetCoseSignature::Out>()
+        .set_forwarding_required(ccf::endpoints::ForwardingRequired::Never)
+        .install();
     }
   };
 }
