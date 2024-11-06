a0d3c5b66f84b7f2f79081cf13ee4bf99540e1a6
Amaury Chamayou <amchamay@microsoft.com>
2024-11-06 15:08:37 +0000

    Any cert authn policy (#6608)
    Co-authored-by: Eddy Ashton <ashton.eddy@gmail.com>
    (cherry picked from commit 75732648c15c2ed5349634cac9198929996d3816)
    
    # Conflicts:
    #	CHANGELOG.md
    #	python/pyproject.toml

--- samples/apps/logging/logging.cpp
+++ samples/apps/logging/logging.cpp
@@ -289,6 +289,17 @@ namespace loggingapp
         return response;
       }
       else if (
+        auto any_cert_ident =
+          dynamic_cast<const ccf::AnyCertAuthnIdentity*>(caller.get()))
+      {
+        auto response = std::string("Any TLS cert");
+        auto caller_cert = ccf::crypto::cert_der_to_pem(any_cert_ident->cert);
+
+        response +=
+          fmt::format("\nThe caller's cert is:\n{}", caller_cert.str());
+        return response;
+      }
+      else if (
         auto jwt_ident =
           dynamic_cast<const ccf::JwtAuthnIdentity*>(caller.get()))
       {
@@ -1167,6 +1178,7 @@ namespace loggingapp
          user_cert_jwt_and_sig_auth_policy,
          ccf::user_cert_auth_policy,
          ccf::member_cert_auth_policy,
+         ccf::any_cert_auth_policy,
          ccf::jwt_auth_policy,
          ccf::user_cose_sign1_auth_policy,
          ccf::empty_auth_policy})
