99b0b1034bde09be81debbf6d42e8714d2406b0a
Max <maxtropets@microsoft.com>
2024-11-09 17:49:26 +0000

    Update UVM endorsements test with fresh endorsements from C-AKS (#6621)

--- src/node/uvm_endorsements.h
+++ src/node/uvm_endorsements.h
@@ -63,7 +63,7 @@ namespace ccf
     {"did:x509:0:sha256:I__iuL25oXEVFdTP_aBLx_eT1RPHbCQ_ECBQfYZpt9s::eku:1.3.6."
      "1.4.1.311.76.59.1.5",
      "ConfAKS-AMD-UVM",
-     "0"}};
+     "1"}};
 
   bool inline matches_uvm_roots_of_trust(
     const UVMEndorsements& endorsements,
