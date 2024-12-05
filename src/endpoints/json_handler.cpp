caebf6ad0f42d92383537593d4f731ba1d44e80e
Eddy Ashton <edashton@microsoft.com>
2024-12-05 10:12:29 +0000

    Includes considered harmful

--- src/endpoints/json_handler.cpp
+++ src/endpoints/json_handler.cpp
@@ -2,6 +2,7 @@
 // Licensed under the Apache 2.0 License.
 #include "ccf/json_handler.h"
 
+#include "ccf/ds/logger.h"
 #include "ccf/http_consts.h"
 #include "ccf/odata_error.h"
 #include "ccf/redirect.h"
