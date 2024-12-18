ef254c7a9f335db4719d881731a772c2b62bc1b8
Eddy Ashton <edashton@microsoft.com>
2024-12-18 14:39:07 +0000

    Add GovernanceDrivenJSRegistry as a (public) sample

--- src/apps/js_generic/js_generic.cpp
+++ src/apps/js_generic/js_generic.cpp
@@ -1,32 +1,15 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
-#include "ccf/js/registry.h"
-#include "ccf/service/tables/jsengine.h"
-#include "ccf/service/tables/modules.h"
+
+#include "ccf/app_interface.h"
+#include "ccf/js/samples/governance_driven_registry.h"
 
 namespace ccf
 {
-  class JSHandlers : public ccf::js::BaseDynamicJSEndpointRegistry
-  {
-  public:
-    JSHandlers(AbstractNodeContext& context) :
-      // Note: We do not pass a kv_prefix here, instead we explicitly, manually
-      // construct each map name to match previously used values
-      ccf::js::BaseDynamicJSEndpointRegistry(context)
-    {
-      modules_map = ccf::Tables::MODULES;
-      metadata_map = ccf::endpoints::Tables::ENDPOINTS;
-      interpreter_flush_map = ccf::Tables::INTERPRETER_FLUSH;
-      modules_quickjs_version_map = ccf::Tables::MODULES_QUICKJS_VERSION;
-      modules_quickjs_bytecode_map = ccf::Tables::MODULES_QUICKJS_BYTECODE;
-      runtime_options_map = ccf::Tables::JSENGINE;
-    }
-  };
-
   std::unique_ptr<ccf::endpoints::EndpointRegistry> make_user_endpoints(
     ccf::AbstractNodeContext& context)
   {
-    return std::make_unique<JSHandlers>(context);
+    return std::make_unique<ccf::js::GovernanceDrivenJSRegistry>(context);
   }
 
 } // namespace ccf
