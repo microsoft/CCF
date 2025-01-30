926299ef381b37f9cd7f0ec6f3badd6861ece9d5
Max Tropets <maxtropets@gmail.com>
2025-01-30 12:54:54 +0000

    Update - review

--- tests/test_install_build.sh
+++ tests/test_install_build.sh
@@ -14,11 +14,6 @@ if [ "$CC" = "" ] || [ "$CXX" = "" ]; then
     CXX=$(command -v clang++-15 || true)
 fi
 
-if [ "$CC" = "" ] || [ "$CXX" = "" ]; then
-    CC=$(command -v clang-11)
-    CXX=$(command -v clang++-11)
-fi
-
 CC=$CC CXX=$CXX cmake -GNinja "$@" ../samples/apps/logging/
 
 ninja
