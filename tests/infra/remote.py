9bc4e5411fff2ac08025caacd2b18106359acf6b
Eddy Ashton <edashton@microsoft.com>
2025-02-19 14:35:14 +0000

    my_thim_shim, and talk to it

--- tests/infra/remote.py
+++ tests/infra/remote.py
@@ -461,8 +461,8 @@ class CCFRemote(object):
             snp_endorsements_servers_list.append(s)
         snp_endorsements_servers_list.append(
             {
-                "type": "Azure",
-                "url": "global.acccache.azure.net:443",
+                "type": "THIM",
+                "url": "127.2.3.4:8989",
                 "max_retries_count": 5,
             }
         )
