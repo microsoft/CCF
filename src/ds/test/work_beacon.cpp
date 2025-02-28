910eb0c61c184fc1fd6fbf7e18a4817b9c8bf9d9
Eddy Ashton <edashton@microsoft.com>
2025-02-28 10:48:04 +0000

    100x more headroom

--- src/ds/test/work_beacon.cpp
+++ src/ds/test/work_beacon.cpp
@@ -198,7 +198,7 @@ TEST_CASE("WorkBeacon" * doctest::test_suite("workbeacon"))
 
           // Then try to be a little more precise, allowing head-room for
           // different build configs and cosmic rays
-          REQUIRE(wakes_with_waits * 1000 < wakes_with_spinloop);
+          REQUIRE(wakes_with_waits * 10 < wakes_with_spinloop);
         }
       }
     }
