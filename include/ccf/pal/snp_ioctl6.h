1f4a673da0a458237b5ce555d3bac9cb23cfadcc
Max <maxtropets@gmail.com>
2025-04-09 13:21:00 +0100

    Remote TODO comment

--- include/ccf/pal/snp_ioctl6.h
+++ include/ccf/pal/snp_ioctl6.h
@@ -195,8 +195,6 @@ namespace ccf::pal::snp::ioctl6
 
   // This 4000 comes from the definition of snp_report_resp in
   // https://github.com/torvalds/linux/blob/master/include/uapi/linux/sev-guest.h
-  // EDA: Temporarily decreased this, to confirm that tests now fail loudly. DO
-  // NOT MERGE
   using PaddedAttestationResp = PaddedTo<AttestationResp, 4000>;
   using PaddedDerivedKeyResp = PaddedTo<DerivedKeyResp, 4000>;
 
