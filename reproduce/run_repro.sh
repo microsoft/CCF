5fc63d65690eaa701503c427f9e648191f77a3b5
Chinenye <okaforchinenyelilian@gmail.com>
2025-06-20 18:16:45 +0000

    Add reproducible build support

--- reproduce/run_repro.sh
+++ reproduce/run_repro.sh
@@ -9,9 +9,7 @@ usage() {
 
 setup_env() {
   IMAGE=$(jq -r '.build_container_image' "$REPRO_JSON")
-  COMMIT_ID=$(jq -r '.commit_sha' "$REPRO_JSON")
   export SOURCE_DATE_EPOCH=$(jq -r '.tdnf_snapshottime' "$REPRO_JSON")
-  echo "Reproducing using SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH"
 }
 
 if [ "$#" -ne 1 ]; then
@@ -22,26 +20,22 @@ REPRO_JSON="$1"
 setup_env
 
 docker run --rm -it \
-  --network host \
-  -e COMMIT_ID="$COMMIT_ID" \
   -e SOURCE_DATE_EPOCH="$SOURCE_DATE_EPOCH" \
   -v "$(pwd)/reproduced":/tmp/reproduced \
-  -v $REPRO_JSON:/tmp/reproduce.json \
-  -v /home/barbie/CCF:/home/CCF \
+  -v $REPRO_JSON:/reproduce.json \
   "$IMAGE" \
   bash -c '
     set -ex
-    tdnf install --snapshottime=$SOURCE_DATE_EPOCH -y git
+    tdnf install --snapshottime=$SOURCE_DATE_EPOCH -y git jq
+    COMMIT_ID=$(jq -r '.commit_sha' "/reproduce.json")
     echo "Cloning repo and checking out commit $COMMIT_ID"
-    REPO_URL="/home/CCF"
-    git config --global --add safe.directory /home/CCF/.git #deleteme when repourl is updated
-    git clone -b "reproducibility-tests" "$REPO_URL" /CCF #deleteme
-    # git clone "$REPO_URL" /CCF
+    REPO_URL="https://github.com/microsoft/CCF"
+    git clone "$REPO_URL" /CCF
     git config --global --add safe.directory /CCF
     cd /CCF
     git checkout "$COMMIT_ID"
     
     echo "Running reproduce script..."
     chmod +x ./reproduce/reproduce_rpm.sh
-    ./reproduce/reproduce_rpm.sh /tmp/reproduce.json
+    ./reproduce/reproduce_rpm.sh /reproduce.json
   '
