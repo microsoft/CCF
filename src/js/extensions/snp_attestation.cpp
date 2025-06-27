a7c58a66a14ee3cff0018aaed1b9ceec62536a91
cjen1-msft <chrisjensen@microsoft.com>
2025-06-27 17:38:27 +0100

    Fix js parsing

--- src/js/extensions/snp_attestation.cpp
+++ src/js/extensions/snp_attestation.cpp
@@ -20,14 +20,9 @@ namespace ccf::js::extensions
   static JSValue make_js_tcb_version(
     js::core::Context& jsctx, pal::snp::TcbVersion tcb)
   {
-    auto js_tcb = jsctx.new_obj();
-    JS_CHECK_EXC(js_tcb);
-
-    JS_CHECK_SET(js_tcb.set_uint32("boot_loader", tcb.boot_loader));
-    JS_CHECK_SET(js_tcb.set_uint32("tee", tcb.tee));
-    JS_CHECK_SET(js_tcb.set_uint32("snp", tcb.snp));
-    JS_CHECK_SET(js_tcb.set_uint32("microcode", tcb.microcode));
-    return js_tcb.take();
+    auto data_hex = jsctx.new_string(ds::to_hex(tcb.data));
+    JS_CHECK_EXC(data_hex);
+    return data_hex.take();
   }
 
   static JSValue JS_NewArrayBuffer2(
