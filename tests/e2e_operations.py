2004279de3796a99cccfdd01771747d44b390f5c
Eddy Ashton <edashton@microsoft.com>
2025-07-03 20:03:23 +0000

    Revert more temp-disabled tests

--- tests/e2e_operations.py
+++ tests/e2e_operations.py
@@ -1468,9 +1468,6 @@ def run_read_ledger_on_testdata(args):
                     )
 
 
-import itertools
-
-
 def run_ledger_chunk_bytes_check(const_args):
     LOG.info("Confirm that ledger chunks are determined by the primary")
     args = copy.deepcopy(const_args)
