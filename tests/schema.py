6ca99c7ff8d08e36087c2f5e8be5d96e55e5e63b
Eddy Ashton <ashton.eddy@gmail.com>
2025-07-08 11:09:33 +0100

    Apply suggestions from code review
    Co-authored-by: Amaury Chamayou <amaury@xargs.fr>

--- tests/schema.py
+++ tests/schema.py
@@ -209,28 +209,28 @@ if __name__ == "__main__":
         nodes=infra.e2e_args.nodes(cr.args, 1),
     )
 
-    # cr.add(
-    #     "nobuiltins",
-    #     run_nobuiltins,
-    #     package="samples/apps/nobuiltins/libnobuiltins",
-    #     nodes=infra.e2e_args.min_nodes(cr.args, f=1),
-    # )
+    cr.add(
+        "nobuiltins",
+        run_nobuiltins,
+        package="samples/apps/nobuiltins/libnobuiltins",
+        nodes=infra.e2e_args.min_nodes(cr.args, f=1),
+    )
 
-    # cr.add(
-    #     "tutorial",
-    #     e2e_tutorial.run,
-    #     package="samples/apps/logging/liblogging",
-    #     nodes=["local://127.0.0.1:8000"],
-    #     initial_member_count=1,
-    # )
+    cr.add(
+        "tutorial",
+        e2e_tutorial.run,
+        package="samples/apps/logging/liblogging",
+        nodes=["local://127.0.0.1:8000"],
+        initial_member_count=1,
+    )
 
-    # cr.add(
-    #     "operations",
-    #     e2e_operations.run,
-    #     package="samples/apps/logging/liblogging",
-    #     nodes=infra.e2e_args.min_nodes(cr.args, f=0),
-    #     initial_user_count=1,
-    #     ledger_chunk_bytes="1B",  # Chunk ledger at every signature transaction
-    # )
+    cr.add(
+        "operations",
+        e2e_operations.run,
+        package="samples/apps/logging/liblogging",
+        nodes=infra.e2e_args.min_nodes(cr.args, f=0),
+        initial_user_count=1,
+        ledger_chunk_bytes="1B",  # Chunk ledger at every signature transaction
+    )
 
     cr.run()
