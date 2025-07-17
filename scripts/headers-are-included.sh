535d1372a0f63ee9965806e4ea67d76fcdafb4d0
Amaury Chamayou <amaury@xargs.fr>
2025-07-17 08:31:05 +0100

    Update scripts/headers-are-included.sh

--- scripts/headers-are-included.sh
+++ scripts/headers-are-included.sh
@@ -5,7 +5,7 @@
 # Very crude and incomplete check, but should catch at least egregiously
 # uncompiled but exported headers.
 
-find src/ include/ -type f -print0 | xargs -0 grep -h "#include" | grep -E "include .?ccf/" | cut -d " " -f 2 | jq -r . | sort -u  > /tmp/CCF_INCLUDED
+find src/ include/ -type f -print0 | xargs -0 grep -h "#include" | grep -E "include .?ccf/" | cut -d " " -f 2 | jq -r . | grep -v "version\.h" | sort -u  > /tmp/CCF_INCLUDED
 
 pushd include/ || exit 1
 find ccf -type f -name "*.h" | sort -u > /tmp/CCF_HEADERS
