46656f10458d06b0e571da187f759a33c6951388
Amaury Chamayou <amaury@xargs.fr>
2025-07-22 10:40:20 +0000

    Make headers-are-included.sh work post-build

--- scripts/headers-are-included.sh
+++ scripts/headers-are-included.sh
@@ -8,7 +8,7 @@
 find src/ include/ -type f -print0 | xargs -0 grep -h "#include" | grep -E "include .?ccf/" | cut -d " " -f 2 | jq -r . | grep -v "ccf/version.h" | sort -u  > /tmp/CCF_INCLUDED
 
 pushd include/ || exit 1
-find ccf -type f -name "*.h" | sort -u > /tmp/CCF_HEADERS
+find ccf -type f -name "*.h" | grep -v "ccf/version.h" | sort -u > /tmp/CCF_HEADERS
 popd || exit 1
 
 diff -y --suppress-common-lines /tmp/CCF_HEADERS /tmp/CCF_INCLUDED
