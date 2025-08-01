c77dbdc4468cafc45aa49a4b88add13f224bc1bb
cjen1-msft <chrisjensen@microsoft.com>
2025-08-01 15:57:46 +0100

    add back in tests

--- tests/schema.py
+++ tests/schema.py
@@ -12,7 +12,6 @@ from packaging import version
 from infra.runner import ConcurrentRunner
 import nobuiltins
 import packaging.version
-import e2e_tutorial
 import e2e_operations
 
 from loguru import logger as LOG
@@ -204,27 +203,27 @@ if __name__ == "__main__":
 
     cr = ConcurrentRunner(add)
 
-    #cr.add(
-    #    "schema",
-    #    run,
-    #    package="samples/apps/logging/logging",
-    #    nodes=infra.e2e_args.nodes(cr.args, 1),
-    #)
+    cr.add(
+        "schema",
+        run,
+        package="samples/apps/logging/logging",
+        nodes=infra.e2e_args.nodes(cr.args, 1),
+    )
 
-    #cr.add(
-    #    "nobuiltins",
-    #    run_nobuiltins,
-    #    package="samples/apps/nobuiltins/nobuiltins",
-    #    nodes=infra.e2e_args.min_nodes(cr.args, f=1),
-    #)
+    cr.add(
+        "nobuiltins",
+        run_nobuiltins,
+        package="samples/apps/nobuiltins/nobuiltins",
+        nodes=infra.e2e_args.min_nodes(cr.args, f=1),
+    )
 
-    #cr.add(
-    #    "tutorial",
-    #    e2e_tutorial.run,
-    #    package="samples/apps/logging/logging",
-    #    nodes=["local://127.0.0.1:8000"],
-    #    initial_member_count=1,
-    #)
+    cr.add(
+        "tutorial",
+        e2e_tutorial.run,
+        package="samples/apps/logging/logging",
+        nodes=["local://127.0.0.1:8000"],
+        initial_member_count=1,
+    )
 
     cr.add(
         "operations",
