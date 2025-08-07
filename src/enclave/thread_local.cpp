6f855b8ea9665a81f4b467faade9942e45096150
cjen1-msft <chrisjensen@microsoft.com>
2025-08-07 16:15:40 +0100

    Update src/enclave/thread_local.cpp
    Co-authored-by: Amaury Chamayou <amaury@xargs.fr>

--- src/enclave/thread_local.cpp
+++ src/enclave/thread_local.cpp
@@ -6,7 +6,7 @@ namespace ccf::threading
 {
   static std::atomic<ThreadID> next_thread_id = MAIN_THREAD_ID;
 
-  uint16_t& get_current_thread_id_unsafe()
+  uint16_t& current_thread_id()
   {
     thread_local ThreadID this_thread_id = next_thread_id.fetch_add(1);
     return this_thread_id;
