9bbd9bd2c65913e4b9fb9ebfb31c760daf9e001d
Eddy Ashton <edashton@microsoft.com>
2025-08-20 14:45:08 +0000

    Missed diff

--- tests/e2e_operations.py
+++ tests/e2e_operations.py
@@ -1627,7 +1627,7 @@ def run_ledger_chunk_bytes_check(const_args):
                 chunk_size = chunk_ends_to_expected_size[end]
                 num_transactions = 1 + end - start
                 min_expected = chunk_size + overhead(num_transactions, num_signatures=0)
-                max_expected = chunk_size + overhead(num_transactions, num_signatures=2)
+                max_expected = chunk_size + overhead(num_transactions, num_signatures=3)
 
                 r = range(min_expected, max_expected)
                 assert (
