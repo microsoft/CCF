7f55f13a8d6c779a62d17b598b3950e19f1ed969
cjen1-msft <chrisjensen@microsoft.com>
2025-08-21 13:48:10 +0100

    Snagging

--- src/common/configuration.h
+++ src/common/configuration.h
@@ -16,7 +16,6 @@
 #include "common/enclave_interface_types.h"
 #include "consensus/consensus_types.h"
 #include "ds/oversized.h"
-#include "http/curl.h"
 #include "service/tables/config.h"
 
 #include <optional>
