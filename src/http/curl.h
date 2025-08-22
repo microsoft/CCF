6fa1587a93393578dbc900b508cb18365b750cde
cjen1-msft <chrisjensen@microsoft.com>
2025-08-22 17:41:09 +0100

    Ensure attaching request  check curl_request_curlm

--- src/http/curl.h
+++ src/http/curl.h
@@ -812,6 +812,10 @@ namespace ccf::curl
     void attach_request(std::unique_ptr<CurlRequest>& request)
     {
       std::lock_guard<std::recursive_mutex> lock(curlm_lock);
+      if (curl_request_curlm == nullptr) {
+        LOG_FAIL_FMT("CurlmLibuvContext already closed, cannot attach request");
+        return;
+      }
       curl_request_curlm.attach_curl_request(request);
     }
 
