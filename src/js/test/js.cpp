b0a803bce6bf1bc8a0c44baf83feeac79b7e52d8
Eddy Ashton <edashton@microsoft.com>
2025-08-26 15:47:00 +0000

    Add unit test validating new API

--- src/js/test/js.cpp
+++ src/js/test/js.cpp
@@ -1,14 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the Apache 2.0 License.
+#include "ccf/js/core/wrapped_value.h"
+#include "ccf/js/extensions/ccf/gov.h"
+#include "js/global_class_ids.h"
 #include "js/permissions_checks.h"
 
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
 #include <random>
 
+using namespace ccf::js;
+
 TEST_CASE("Check KV Map access")
 {
-  using namespace ccf::js;
   constexpr auto public_internal_table_name = "public:ccf.internal.table";
   constexpr auto private_internal_table_name = "ccf.internal.table";
 
@@ -168,4 +172,130 @@ TEST_CASE("Check KV Map access")
         KVAccessPermissions::ILLEGAL);
     }
   }
+}
+
+bool str_contains(const std::string& s, std::string_view sv)
+{
+  return s.find(sv) != std::string::npos;
+}
+
+bool str_contains(const std::optional<std::string>& s, std::string_view sv)
+{
+  return str_contains(s.value_or(""), sv);
+}
+
+// Returns error string, or nullopt if validation succeeded
+std::optional<std::string> call_validate_constitution(
+  const std::string& constitution)
+{
+  ccf::js::core::Context ctx(TxAccess::GOV_RO);
+
+  ctx.add_extension(std::make_shared<ccf::js::extensions::GovExtension>());
+
+  const auto path = "/path/to/constitution";
+
+  auto module = fmt::format(
+    "export function call_validate () {{\n"
+    "  let constitution = {};\n"
+    "  return ccf.gov.validateConstitution(constitution);\n"
+    "}}",
+    constitution);
+
+  auto func = ctx.get_exported_function(module, "call_validate", path);
+
+  const auto result = ctx.inner_call(func, {});
+  if (result.is_true())
+  {
+    return std::nullopt;
+  }
+
+  auto [reason, trace] = ctx.error_message();
+  return reason;
+}
+
+TEST_CASE("Constitution validation")
+{
+  {
+    INFO("not a string");
+    for (const auto& c : {"1", "1 + 2", "{}", "true", "null"})
+    {
+      const auto error = call_validate_constitution(c);
+      REQUIRE(error.has_value());
+      REQUIRE(str_contains(error, "not a string"));
+    }
+  }
+
+  {
+    INFO("empty");
+    for (const auto& c : {"``", "\"\""})
+    {
+      const auto error = call_validate_constitution(c);
+      REQUIRE(error.has_value());
+      REQUIRE(str_contains(error, "empty"));
+    }
+  }
+
+  {
+    INFO("missing validate");
+    for (const auto& c : {R"!!!(`
+export function apply() {}
+export function resolve() {}
+`)!!!"})
+    {
+      const auto error = call_validate_constitution(c);
+      REQUIRE(error.has_value());
+      REQUIRE(str_contains(error, "not find function validate"));
+    }
+  }
+
+  {
+    INFO("missing apply");
+    for (const auto& c : {R"!!!(`
+export function validate() {}
+export function resolve() {}
+`)!!!"})
+    {
+      const auto error = call_validate_constitution(c);
+      REQUIRE(error.has_value());
+      REQUIRE(str_contains(error, "not find function apply"));
+    }
+  }
+
+  {
+    INFO("missing resolve");
+    for (const auto& c : {R"!!!(`
+export function validate() {}
+export function apply() {}
+`)!!!"})
+    {
+      const auto error = call_validate_constitution(c);
+      REQUIRE(error.has_value());
+      REQUIRE(str_contains(error, "not find function resolve"));
+    }
+  }
+
+  {
+    INFO("good");
+    for (const auto& c : {R"!!!(`
+export function validate() {}
+export function apply() {}
+export function resolve() {}
+`)!!!"})
+    {
+      const auto error = call_validate_constitution(c);
+      REQUIRE(!error.has_value());
+    }
+  }
+}
+
+int main(int argc, char** argv)
+{
+  ccf::js::register_class_ids();
+
+  doctest::Context context;
+  context.applyCommandLine(argc, argv);
+  int res = context.run();
+  if (context.shouldExit())
+    return res;
+  return res;
 }
