286d94f2523dc20aaed7a6b33f89a71b1eb18b9f
cjen1-msft <chrisjensen@microsoft.com>
2025-09-05 16:22:03 +0100

    pass fmc

--- include/ccf/pal/attestation_sev_snp_endorsements.h
+++ include/ccf/pal/attestation_sev_snp_endorsements.h
@@ -132,13 +132,19 @@ namespace ccf::pal::snp
     const std::string& microcode,
     const ProductName& product_name,
     size_t max_retries_count,
-    size_t max_client_response_size)
+    size_t max_client_response_size,
+    const std::optional<std::string>& fmc_version = std::nullopt
+  )
   {
     std::map<std::string, std::string> params;
     params["blSPL"] = boot_loader;
     params["teeSPL"] = tee;
     params["snpSPL"] = snp;
     params["ucodeSPL"] = microcode;
+    if (fmc_version.has_value())
+    {
+      params["fmcSPL"] = fmc_version.value();
+    }
 
     EndorsementEndpointsConfiguration::Server server;
     EndorsementEndpointsConfiguration::EndpointInfo leaf{
