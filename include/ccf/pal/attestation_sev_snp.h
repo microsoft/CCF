f8cedfb37d7233b81105ed7c297f076313787b9d
cjen1-msft <chrisjensen@microsoft.com>
2025-09-08 22:18:16 +0100

    Fix broken turin endorsements fetching (#7264)
    Co-authored-by: Amaury Chamayou <amchamay@microsoft.com>

--- include/ccf/pal/attestation_sev_snp.h
+++ include/ccf/pal/attestation_sev_snp.h
@@ -523,6 +523,7 @@ pRb21iI1NlNCfOGUPIhVpWECAwEAAQ==
           std::string tee;
           std::string snp;
           std::string microcode;
+          std::optional<std::string> fmc = std::nullopt;
           switch (product)
           {
             case ProductName::Milan:
@@ -542,6 +543,7 @@ pRb21iI1NlNCfOGUPIhVpWECAwEAAQ==
               tee = fmt::format("{}", tcb.tee);
               snp = fmt::format("{}", tcb.snp);
               microcode = fmt::format("{}", tcb.microcode);
+              fmc = fmt::format("{}", tcb.fmc);
               break;
             }
             default:
@@ -562,7 +564,8 @@ pRb21iI1NlNCfOGUPIhVpWECAwEAAQ==
             microcode,
             product,
             max_retries_count,
-            max_client_response_size));
+            max_client_response_size,
+            fmc));
           break;
         }
         case EndorsementsEndpointType::THIM:
