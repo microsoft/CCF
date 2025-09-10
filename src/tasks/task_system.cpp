1d4b2091c2fa45956835769c50bb3645bac565ab
Eddy Ashton <edashton@microsoft.com>
2025-09-10 10:50:21 +0000

    Revert previous debugging commits

--- src/tasks/task_system.cpp
+++ src/tasks/task_system.cpp
@@ -106,11 +106,6 @@ namespace ccf::tasks
 
   void tick(std::chrono::milliseconds elapsed)
   {
-    LOG_INFO_FMT(
-      "!!! Advancing elapsed time by {}, from {}",
-      elapsed,
-      total_elapsed.load());
-
     elapsed += total_elapsed.load();
 
     {
@@ -130,10 +125,6 @@ namespace ccf::tasks
           {
             continue;
           }
-
-          LOG_INFO_FMT(
-            "!!! Found ready, non-cancelled task named {} - scheduling it now",
-            dt.task->get_name());
 
           add_task(dt.task);
           if (dt.repeat.has_value())
