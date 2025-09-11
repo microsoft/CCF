0e8f0630a423164132fcaf93621618ef064e93aa
cjen1-msft <chrisjensen@microsoft.com>
2025-09-11 15:38:25 +0100

    Improve condition rename

--- src/pal/test/snp_attestation_validation.cpp
+++ src/pal/test/snp_attestation_validation.cpp
@@ -145,96 +145,117 @@ struct QuoteEndorsemenstTestCase
 
 TEST_CASE("Quote endorsements url generation")
 {
+  constexpr size_t max_retries_count = 10;
   std::vector<QuoteEndorsemenstTestCase> test_cases{
     {.attestation = ccf::pal::snp::testing::genoa_attestation,
      .servers = {{
        ccf::pal::snp::EndorsementsEndpointType::Azure,
-       "invalid.azure.com:443",
+       "invalid.azure.com:12345",
      }},
      .expected_urls =
        {.servers = {{{
-          "invalid.azure.com",
-          "443",
-          "/SevSnpVM/certificates/"
-          "b1e24a27bbc3a4d58090d8b89851dce3b8031544be249b9ac17132bb222b02762234"
-          "7"
-          "ee4d0fe4f689efdfc47a68cefc686cbb448d01436506ee1e28010cab7c0/"
-          "541700000000000a",
-          {
-            {"api-version", "2020-10-15-preview"},
-          },
-          true, // DER
+          .host = "invalid.azure.com",
+          .port = "12345",
+          .uri = "/SevSnpVM/certificates/"
+                 "b1e24a27bbc3a4d58090d8b89851dce3b8031544be249b9ac17132bb2"
+                 "22b027622347ee4d0fe4f689efdfc47a68cefc686cbb448d01436506e"
+                 "e1e28010cab7c0/541700000000000a",
+          .params =
+            {
+              {"api-version", "2020-10-15-preview"},
+            },
+          .max_retries_count = max_retries_count,
+          .max_client_response_size = ccf::ds::SizeString("100mb"),
         }}}}},
     {.attestation = ccf::pal::snp::testing::milan_attestation,
      .servers = {{
        ccf::pal::snp::EndorsementsEndpointType::AMD,
-       "kdsintf.amd.com:443",
+       "invalid.amd.com:12345",
      }},
      .expected_urls =
        {.servers =
           {
             {{{
-                "kdsintf.amd.com",
-                "443",
-                "/vcek/v1/Milan/"
-                "4ffb5cb4fd594f3fee6528fc3fb10370bb38abe89dcd5ba2cf0ab6a11df2ca"
-                "282add516bef45a890a8c9f9732bdca68f9f3f16c42e846030a800295dbeb1"
-                "9ba5",
-                {{"blSPL", "4"},
-                 {"teeSPL", "0"},
-                 {"snpSPL", "24"},
-                 {"ucodeSPL", "219"}},
-                true, // DER
+                .host = "invalid.amd.com",
+                .port = "12345",
+                .uri = "/vcek/v1/Milan/"
+                       "4ffb5cb4fd594f3fee6528fc3fb10370bb38abe89dcd5ba2cf0ab6a"
+                       "11df2ca282add516bef45a890a8c9f9732bdca68f9f3f16c42e8460"
+                       "30a800295dbeb19ba5",
+                .params =
+                  {{"blSPL", "4"},
+                   {"teeSPL", "0"},
+                   {"snpSPL", "24"},
+                   {"ucodeSPL", "219"}},
+                .response_is_der = true, // DER response
+                .max_retries_count = max_retries_count,
+                .max_client_response_size = ccf::ds::SizeString("100mb"),
               },
               {
-                "kdsintf.amd.com",
-                "443",
-                "/vcek/v1/Milan/cert_chain",
-                {},
-                true, // DER
+                .host = "invalid.amd.com",
+                .port = "12345",
+                .uri = "/vcek/v1/Milan/cert_chain",
+                .params = {},
+                .response_is_der = false, // Not DER response
+                .max_retries_count = max_retries_count,
+                .max_client_response_size = ccf::ds::SizeString("100mb"),
               }}}}}},
-    {.attestation = ccf::pal::snp::testing::genoa_attestation,
-     .servers = {{
-       ccf::pal::snp::EndorsementsEndpointType::AMD,
-       "kdsintf.amd.com:443",
-     }},
-     .expected_urls =
-       {.servers =
-          {{{
-              "kdsintf.amd.com",
-              "443",
-              "/vcek/v1/Genoa/"
-              "b1e24a27bbc3a4d58090d8b89851dce3b8031544be249b9ac17132bb222b0276"
-              "22347ee4d0fe4f689efdfc47a68cefc686cbb448d01436506ee1e28010cab7c"
-              "0",
-              {{"blSPL", "10"},
-               {"teeSPL", "0"},
-               {"snpSPL", "23"},
-               {"ucodeSPL", "84"}},
-              true, // DER
-            },
-            {
-              "kdsintf.amd.com",
-              "443",
-              "/vcek/v1/Genoa/cert_chain",
-              {},
-              true, // DER
-            }}}}},
+    {
+      .attestation = ccf::pal::snp::testing::genoa_attestation,
+      .servers = {{
+        ccf::pal::snp::EndorsementsEndpointType::AMD,
+        "invalid.amd.com:12345",
+      }},
+      .expected_urls =
+        {.servers =
+           {
+             {{{
+                 .host = "invalid.amd.com",
+                 .port = "12345",
+                 .uri =
+                   "/vcek/v1/Genoa/"
+                   "b1e24a27bbc3a4d58090d8b89851dce3b8031544be249b9ac17132b"
+                   "b222b027622347ee4d0fe4f689efdfc47a68cefc686cbb448d01436"
+                   "506ee1e28010cab7c0",
+                 .params =
+                   {{"blSPL", "10"},
+                    {"teeSPL", "0"},
+                    {"snpSPL", "23"},
+                    {"ucodeSPL", "84"}},
+                 .response_is_der = true, // DER response
+                 .max_retries_count = max_retries_count,
+                 .max_client_response_size = ccf::ds::SizeString("100mb"),
+               },
+               {
+                 .host = "invalid.amd.com",
+                 .port = "12345",
+                 .uri = "/vcek/v1/Genoa/cert_chain",
+                 .params = {},
+                 .response_is_der = false, // Not DER response
+                 .max_retries_count = max_retries_count,
+                 .max_client_response_size = ccf::ds::SizeString("100mb"),
+               }}}}},
+    },
     {.attestation = ccf::pal::snp::testing::genoa_attestation,
      .servers = {{
        ccf::pal::snp::EndorsementsEndpointType::THIM,
-       "invalid.azure.com:443",
+       "invalid.thim.azure.com:12345",
      }},
      .expected_urls = {
        .servers = {{{
-         "invalid.azure.com",
-         "443",
-         "/metadata/THIM/amd/certification",
-         {{"platformId",
-           "b1e24a27bbc3a4d58090d8b89851dce3b8031544be249b9ac17132bb222b0276223"
-           "47ee4d0fe4f689efdfc47a68cefc686cbb448d01436506ee1e28010cab7c0"},
-          {"tcbVersion", "541700000000000a"}},
-         true, // DER
+         .host = "invalid.thim.azure.com",
+         .port = "12345",
+         .uri = "/metadata/THIM/amd/certification",
+         .params =
+           {{"platformId",
+             "b1e24a27bbc3a4d58090d8b89851dce3b8031544be249b9ac17132bb222b02762"
+             "2347ee4d0fe4f689efdfc47a68cefc686cbb448d01436506ee1e28010cab7c0"},
+            {"tcbVersion", "541700000000000a"}},
+         .response_is_thim_json = true,
+         .headers = {{"Metadata", "true"}},
+         .tls = false,
+         .max_retries_count = max_retries_count,
+         .max_client_response_size = ccf::ds::SizeString("100mb"),
        }}}}}};
 
   for (auto [attestation, servers, expected_url] : test_cases)
@@ -243,53 +264,24 @@ TEST_CASE("Quote endorsements url generation")
       *reinterpret_cast<const ccf::pal::snp::Attestation*>(attestation.data());
     auto config =
       ccf::pal::snp::make_endorsement_endpoint_configuration(quote, servers);
-    REQUIRE_EQ(config.servers.size(), expected_url.servers.size());
-    for (int i = 0; i < config.servers.size(); i++)
-    {
-      auto endpoints = config.servers.begin();
-      std::advance(endpoints, i);
-      auto expected_endpoints = expected_url.servers.begin();
-      std::advance(endpoints, i);
 
-      REQUIRE_EQ(endpoints->size(), expected_endpoints->size());
-      for (int j = 0; j < endpoints->size(); j++)
-      {
-        auto endpoint = endpoints->begin();
-        std::advance(endpoint, j);
-        auto expected_endpoint = expected_endpoints->begin();
-        std::advance(expected_endpoint, j);
-
-        CHECK_EQ(endpoint->host, expected_endpoint->host);
-        CHECK_EQ(endpoint->port, expected_endpoint->port);
-        CHECK_EQ(endpoint->uri, expected_endpoint->uri);
-        REQUIRE_EQ(endpoint->params.size(), expected_endpoint->params.size());
-        for (int k = 0; k < endpoint->params.size(); k++)
-        {
-          auto it = endpoint->params.begin();
-          std::advance(it, k);
-          auto expected_it = expected_endpoint->params.begin();
-          std::advance(expected_it, k);
-          CHECK_EQ(it->first, expected_it->first);
-          CHECK_EQ(it->second, expected_it->second);
-        }
-      }
-    }
+    CHECK_EQ(nlohmann::json(config), nlohmann::json(expected_url));
   }
 }
 
-TEST_CASE("Quote endorsements generation for old tcb version fails")
+TEST_CASE("Quote endorsements generation for v2 attestation version fails")
 {
-  auto old_milan_attestation =
+  auto v2_milan_attestation =
     *reinterpret_cast<const ccf::pal::snp::Attestation*>(
-      ccf::pal::snp::testing::old_milan_attestation.data());
+      ccf::pal::snp::testing::v2_milan_attestation.data());
 
-  CHECK_EQ(old_milan_attestation.version, 2);
-  CHECK_EQ(old_milan_attestation.cpuid_fam_id, 0x0);
-  CHECK_EQ(old_milan_attestation.cpuid_mod_id, 0x0);
+  CHECK_EQ(v2_milan_attestation.version, 2);
+  CHECK_EQ(v2_milan_attestation.cpuid_fam_id, 0x0);
+  CHECK_EQ(v2_milan_attestation.cpuid_mod_id, 0x0);
 
   CHECK_THROWS_WITH(
     ccf::pal::snp::make_endorsement_endpoint_configuration(
-      (old_milan_attestation),
+      (v2_milan_attestation),
       {{
         ccf::pal::snp::EndorsementsEndpointType::AMD,
         "kdsintf.amd.com:443",
