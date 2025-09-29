0a376d457c31ba634e92e141a559aec56363e37b
cjen1-msft <chrisjensen@microsoft.com>
2025-09-29 18:06:59 +0100

    [release/6.x] Backport logging of digests of snapshot files #7300 (#7313)
    Co-authored-by: Amaury Chamayou <amaury@xargs.fr>
    Co-authored-by: Amaury Chamayou <amchamay@microsoft.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [6.0.14]
+## [6.0.15]
+
+[6.0.15]: https://github.com/microsoft/CCF/releases/tag/6.0.15
+
+### Added
+
+- Improved logging of snapshot digests (#7300)
 
 [6.0.14]: https://github.com/microsoft/CCF/releases/tag/ccf-6.0.14
 
--- src/host/main.cpp
+++ src/host/main.cpp
@@ -438,6 +438,8 @@ int main(int argc, char** argv) // NOLINT(bugprone-exception-escape)
   asynchost::ProcessLauncher process_launcher;
   process_launcher.register_message_handlers(buffer_processor.get_dispatcher());
 
+  ccf::crypto::openssl_sha256_init();
+
   {
     // provide regular ticks to the enclave
     const asynchost::Ticker ticker(config.tick_interval, writer_factory);
@@ -857,6 +859,12 @@ int main(int argc, char** argv) // NOLINT(bugprone-exception-escape)
       config.command.type == StartType::Recover)
     {
       auto latest_local_snapshot = snapshots.find_latest_committed_snapshot();
+      auto sha = ccf::crypto::Sha256Hash(startup_snapshot);
+      LOG_INFO_FMT(
+        "Found latest snapshot file: {} (size: {}, sha256: {})",
+        latest_local_snapshot->first / latest_local_snapshot->second,
+        startup_snapshot.size(),
+        sha.hex_str());
 
       if (
         config.command.type == StartType::Join &&
@@ -874,11 +882,14 @@ int main(int argc, char** argv) // NOLINT(bugprone-exception-escape)
 
         if (latest_peer_snapshot.has_value())
         {
+          auto peer_sha =
+            ccf::crypto::Sha256Hash(latest_peer_snapshot->snapshot_data);
           LOG_INFO_FMT(
-            "Received snapshot {} from peer (size: {}) - writing this to disk "
-            "and using for join startup",
+            "Received snapshot {} from peer (size: {}, sha256: {}) - writing "
+            "this to disk and using for join startup",
             latest_peer_snapshot->snapshot_name,
-            latest_peer_snapshot->snapshot_data.size());
+            latest_peer_snapshot->snapshot_data.size(),
+            peer_sha.hex_str());
 
           const auto dst_path = fs::path(config.snapshots.directory) /
             fs::path(latest_peer_snapshot->snapshot_name);
