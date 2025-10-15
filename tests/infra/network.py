ee225fe649733a317766a3099e3fedf22b477b06
cjen1-msft <chrisjensen@microsoft.com>
2025-10-15 16:55:55 +0100

    Try to get a stack trace when an exception is thrown during network unwind

--- tests/infra/network.py
+++ tests/infra/network.py
@@ -551,6 +551,10 @@ class Network:
             infra.proc.ccall(*cmd).returncode == 0
         ), f"Could not symlink {self.KEY_GEN} to {self.common_dir}"
 
+    def print_stack_traces(self):
+        for node in self.nodes:
+            node.print_stack_trace()
+
     def start(self, args, **kwargs):
         """
         Starts a CCF network.
@@ -1816,6 +1820,8 @@ def network(
     except Exception:
         # Don't try to verify txs on Exception path
         net.txs = None
+
+        net.print_stack_traces()
 
         if pdb:
             import pdb
