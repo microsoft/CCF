11245c3e194a8db72239a80de685c3baa63944bd
Eddy Ashton <edashton@microsoft.com>
2025-10-16 10:42:14 +0000

    Docs and renames and ownership fiddling, and one critical bug

--- src/tasks/job_board.cpp
+++ src/tasks/job_board.cpp
@@ -7,10 +7,30 @@
 
 namespace ccf::tasks
 {
-  struct Consumer
+  // Temporary struct used by idle worker threads when no tasks are available.
+  // See wait_for_task and add_task for useage.
+  struct WaitingWorkerThread
   {
-    Task task;
+    // Ownership of a condition variable that a single thread will wait on
     std::condition_variable cv;
+
+    // Output variable to assign that thread a task
+    Task& assigned_task;
+
+    WaitingWorkerThread(Task& at_) : assigned_task(at_) {}
+
+    WaitingWorkerThread(const WaitingWorkerThread&) = delete;
+    WaitingWorkerThread& operator=(const WaitingWorkerThread&) = delete;
+
+    WaitingWorkerThread(WaitingWorkerThread&&) = delete;
+    WaitingWorkerThread& operator=(WaitingWorkerThread&&) = delete;
+
+    ~WaitingWorkerThread()
+    {
+      // It is only safe to destruct a condition_variable if all threads have
+      // been notified
+      cv.notify_all();
+    }
   };
 
   struct Delayed
@@ -35,32 +55,44 @@ namespace ccf::tasks
 
   struct JobBoard::PImpl
   {
+    // Mutex protects access to both pending_tasks and waiting_worker_threads
     std::mutex mutex;
-    std::queue<Task> pending_tasks;
-    std::vector<Consumer*> waiting_consumers;
 
+    // Collection of tasks that are ready for execution
+    std::queue<Task> pending_tasks;
+
+    // Collection describing idle worker threads. Owns structs used to assign
+    // them incoming tasks
+    std::vector<std::unique_ptr<WaitingWorkerThread>> waiting_worker_threads;
+
+    // Collection of delayed tasks, that may be ready for execution on a future
+    // tick
     Delayed delayed;
 
     void add_task(Task&& task)
     {
+      // Under lock
       std::unique_lock<std::mutex> lock(mutex);
 
-      for (Consumer* consumer : waiting_consumers)
+      // First check if there is an idle worker waiting for a task
+      auto it = waiting_worker_threads.begin();
+      if (it != waiting_worker_threads.end())
       {
-        // NB: Although waiting_consumers is modified under lock, it's possible
-        // that another call to add_task arrives the notified thread wakes up
-        // and removes itself from this collection. In this case we must avoid
-        // overwriting a previously-assigned task.
-        if (consumer->task == nullptr)
-        {
-          consumer->task = std::move(task);
-          consumer->cv.notify_one();
-          return;
-        }
-      }
+        std::unique_ptr<WaitingWorkerThread>& worker = *it;
 
-      // There are no waiting_consumers currently, or none waiting for a task
-      pending_tasks.emplace(std::move(task));
+        // Assign this worker the incoming task, notify them, and then remove
+        // their waiting state
+        worker->assigned_task = std::move(task);
+        worker->cv.notify_one();
+
+        it = waiting_worker_threads.erase(it);
+      }
+      else
+      {
+        // There are no waiting_worker_threads currently, or none waiting for a
+        // task, so enqueue this task for later execution
+        pending_tasks.emplace(std::move(task));
+      }
     }
 
     Task get_task()
@@ -74,27 +106,47 @@ namespace ccf::tasks
       Task to_return = nullptr;
 
       {
+        // Under lock
         std::unique_lock<std::mutex> lock(mutex);
 
+        // Check if there are pending tasks to be executed
         if (pending_tasks.empty())
         {
-          auto consumer = std::make_unique<Consumer>();
-          waiting_consumers.push_back(consumer.get());
+          // When the task queue is empty, append this thread to
+          // waiting_worker_threads and wait on a condition_variable. It will be
+          // notified when a new Task is available
+          auto waiting_worker =
+            std::make_unique<WaitingWorkerThread>(to_return);
 
-          consumer->cv.wait_for(lock, timeout);
+          // Maintain reference to a field in a unique ptr we're about to move.
+          // This is only safe because we're under a mutex, and the only thing
+          // that could destroy this object must first acquire the same mutex
+          std::condition_variable& cv = waiting_worker->cv;
 
-          auto it = std::find(
-            waiting_consumers.begin(), waiting_consumers.end(), consumer.get());
-          waiting_consumers.erase(it);
+          // Transfer ownership to the central collection
+          waiting_worker_threads.push_back(std::move(waiting_worker));
 
-          to_return = std::move(consumer->task);
+          // NOLINTBEGIN(bugprone-spuriously-wake-up-functions)
+          // Spurious wakeup is acceptable, treated equivalently to timeout
+          // elapsing
+          cv.wait_for(lock, timeout);
+          // NOLINTEND(bugprone-spuriously-wake-up-functions)
+
+          // We reach here either because the condition_variable was notified,
+          // or the timeout expired. In either case, return the (potentially
+          // still null) assigned task
+
+          // TODO: This doesn't work because we still need to cleanup if we
+          // timed out/were spuriously woken!
         }
         else
         {
+          // When the task queue is non-empty, take the first element from it
           to_return = pending_tasks.front();
           pending_tasks.pop();
         }
       }
+
       return to_return;
     }
 
@@ -191,7 +243,7 @@ namespace ccf::tasks
     {
       std::lock_guard<std::mutex> lock(pimpl->mutex);
       summary.pending_tasks = pimpl->pending_tasks.size();
-      summary.idle_workers = pimpl->waiting_consumers.size();
+      summary.idle_workers = pimpl->waiting_worker_threads.size();
     }
     return summary;
   }
