03823fbedcf56d1dcfd02c3456ae86a7e6c20e3e
Max Tropets <maxtropets@gmail.com>
2025-10-20 12:28:48 +0000

    Git alias

--- .devcontainer/post_create_setup.sh
+++ .devcontainer/post_create_setup.sh
@@ -8,3 +8,8 @@ set -ex
 ./scripts/setup-dev.sh
 
 git config --global --add safe.directory /workspaces/CCF
+
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.co checkout
