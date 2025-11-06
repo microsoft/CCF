67b20466303badc6c419ff4c63ad88159f4cedb9
Amaury Chamayou <amaury@xargs.fr>
2025-11-06 08:58:44 +0000

    .

--- include/ccf/ds/hex.h
+++ include/ccf/ds/hex.h
@@ -11,6 +11,7 @@
 
 namespace ccf::ds
 {
+  constexpr size_t ascii_offset = 10;
   static uint8_t hex_char_to_int(char c)
   {
     if (c <= '9')
@@ -20,12 +21,12 @@ namespace ccf::ds
 
     if (c <= 'F')
     {
-      return c - 'A' + 10;
+      return c - 'A' + ascii_offset;
     }
 
     if (c <= 'f')
     {
-      return c - 'a' + 10;
+      return c - 'a' + ascii_offset;
     }
 
     return c;
@@ -54,6 +55,8 @@ namespace ccf::ds
     return r;
   }
 
+  constexpr size_t hex_base = 16;
+
   template <typename Iter>
   static void from_hex(const std::string& str, Iter begin, Iter end)
   {
@@ -74,7 +77,7 @@ namespace ccf::ds
     auto it = begin;
     for (size_t i = 0; i < str.size(); i += 2, ++it)
     {
-      *it = 16 * hex_char_to_int(str[i]) + hex_char_to_int(str[i + 1]);
+      *it = hex_base * hex_char_to_int(str[i]) + hex_char_to_int(str[i + 1]);
     }
   }
 
