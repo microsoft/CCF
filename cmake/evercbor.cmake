b1b50893fd4de9a4a7629025f6d51ef6bbd33d92
Tahina Ramananandro <taramana@microsoft.com>
2025-12-10 18:11:45 +0000

    CMake format

--- cmake/evercbor.cmake
+++ cmake/evercbor.cmake
@@ -3,15 +3,13 @@
 
 # Build EverCBOR
 set(EVERCBOR_DIR "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/exported/evercbor")
-set(EVERCBOR_SRCS
-    "${EVERCBOR_DIR}/CBORNondet.c"
-)
+set(EVERCBOR_SRCS "${EVERCBOR_DIR}/CBORNondet.c")
 
 add_library(evercbor STATIC ${EVERCBOR_SRCS})
 
 target_include_directories(
   evercbor PUBLIC $<BUILD_INTERFACE:${CCF_3RD_PARTY_EXPORTED_DIR}/evercbor>
-               $<INSTALL_INTERFACE:include/3rdparty/evercbor>
+                  $<INSTALL_INTERFACE:include/3rdparty/evercbor>
 )
 set_property(TARGET evercbor PROPERTY POSITION_INDEPENDENT_CODE ON)
 add_san(evercbor)
