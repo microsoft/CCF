32a744b4c0848039ccbb5a2de7c768a39c60f1da
Max <maxtropets@microsoft.com>
2026-01-23 20:20:23 +0000

    Take t_cose and QCBOR private (#7616)
    Co-authored-by: Amaury Chamayou <amchamay@microsoft.com>

--- cmake/t_cose.cmake
+++ cmake/t_cose.cmake
@@ -2,7 +2,7 @@
 # Licensed under the Apache 2.0 License.
 
 # Build t_cose
-set(T_COSE_DIR "${CCF_3RD_PARTY_EXPORTED_DIR}/t_cose")
+set(T_COSE_DIR "${CCF_3RD_PARTY_INTERNAL_DIR}/t_cose")
 set(T_COSE_SRC "${T_COSE_DIR}/src")
 set(T_COSE_DEFS -DT_COSE_USE_OPENSSL_CRYPTO=1
                 -DT_COSE_DISABLE_SHORT_CIRCUIT_SIGN=1
@@ -19,13 +19,11 @@ target_compile_definitions(t_cose PRIVATE ${T_COSE_DEFS})
 target_compile_options(t_cose INTERFACE ${T_COSE_OPTS_INTERFACE})
 
 target_include_directories(t_cose PRIVATE "${T_COSE_SRC}")
-
 target_include_directories(
-  t_cose PUBLIC $<BUILD_INTERFACE:${CCF_3RD_PARTY_EXPORTED_DIR}/t_cose/inc>
-                $<INSTALL_INTERFACE:include/3rdparty/t_cose/inc>
+  t_cose PUBLIC $<BUILD_INTERFACE:${CCF_3RD_PARTY_INTERNAL_DIR}/t_cose/inc>
 )
 
-target_link_libraries(t_cose PUBLIC qcbor crypto)
+target_link_libraries(t_cose PUBLIC qcbor)
 set_property(TARGET t_cose PROPERTY POSITION_INDEPENDENT_CODE ON)
 add_san(t_cose)
 
