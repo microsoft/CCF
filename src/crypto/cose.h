4c5ee51e11832d2b2b47f25f3b044af8b166c3d4
Max Tropets <maxtropets@gmail.com>
2026-01-26 16:48:06 +0000

    Tidy

--- src/crypto/cose.h
+++ src/crypto/cose.h
@@ -46,31 +46,28 @@ namespace ccf
         "application/octet-stream";
     }
   }
-  namespace cwt
+  namespace cwt::header
   {
-    namespace header
+    namespace iana // https://www.iana.org/assignments/cwt/cwt.xhtml
     {
-      namespace iana // https://www.iana.org/assignments/cwt/cwt.xhtml
-      {
-        static constexpr int64_t ISS = 1;
-        static constexpr int64_t SUB = 2;
+      static constexpr int64_t ISS = 1;
+      static constexpr int64_t SUB = 2;
 
-        /* Value is **PLAIN INTEGER**, as per
-         * https://www.rfc-editor.org/rfc/rfc8392#section-2. Quote:
-         *
-         * The "NumericDate" term in this specification has the same meaning and
-         * processing rules as the JWT "NumericDate" term defined in Section 2
-         * of [RFC7519], except that it is represented as a CBOR numericdate
-         * (from Section 2.4.1 of [RFC7049]) instead of a JSON number.  The
-         * encoding is modified so that the leading tag 1 (epoch-based
-         * date/time) MUST  be omitted.
-         */
-        static constexpr int64_t IAT = 6;
-      }
-      namespace custom
-      {
-        static constexpr std::string_view SVN = "svn";
-      }
+      /* Value is **PLAIN INTEGER**, as per
+       * https://www.rfc-editor.org/rfc/rfc8392#section-2. Quote:
+       *
+       * The "NumericDate" term in this specification has the same meaning and
+       * processing rules as the JWT "NumericDate" term defined in Section 2 of
+       * [RFC7519], except that it is represented as a CBOR numericdate (from
+       * Section 2.4.1 of [RFC7049]) instead of a JSON number. The encoding is
+       * modified so that the leading tag 1 (epoch-based date/time) MUST be
+       * omitted.
+       */
+      static constexpr int64_t IAT = 6;
+    }
+    namespace custom
+    {
+      static constexpr std::string_view SVN = "svn";
     }
   }
 }
