8f8ed243e3e1df8fbe1ef1c84c11dde5e0461ae3
Amaury Chamayou <amaury@xargs.fr>
2026-01-28 17:42:57 +0000

    schema

--- src/node/rpc/node_frontend.h
+++ src/node/rpc/node_frontend.h
@@ -397,7 +397,7 @@ namespace ccf
       openapi_info.description =
         "This API provides public, uncredentialed access to service and node "
         "state.";
-      openapi_info.document_version = "5.0.1";
+      openapi_info.document_version = "5.0.2";
     }
 
     void init_handlers() override
