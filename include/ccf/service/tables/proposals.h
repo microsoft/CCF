65891801e655e2c7d8da55ccedf725058adb186b
cjen1-msft <chrisjensen@microsoft.com>
2026-03-16 16:02:23 +0000

    Also for proposalState

--- include/ccf/service/tables/proposals.h
+++ include/ccf/service/tables/proposals.h
@@ -82,7 +82,9 @@ struct formatter<ccf::ProposalState>
       }
       default:
       {
-        throw std::logic_error(fmt::format("Unknown proposal state {}", state));
+        throw std::logic_error(fmt::format(
+          "Unknown proposal state {}",
+          static_cast<uint8_t>(state)));
       }
     }
   }
