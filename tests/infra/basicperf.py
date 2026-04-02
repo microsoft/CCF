666c9fee428753a1c0217fe79892101dc37de951
Amaury Chamayou <amaury@xargs.fr>
2026-04-02 19:59:47 +0000

    Longer runs, three nodes

--- tests/infra/basicperf.py
+++ tests/infra/basicperf.py
@@ -261,7 +261,7 @@ def replace_primary(network, host, old_primary, snapshots_dir, statistics):
 
 
 def run(args):
-    hosts = args.nodes or infra.e2e_args.nodes(args, 1)
+    hosts = args.nodes or infra.e2e_args.nodes(args, 3)
 
     if args.stop_primary_after_s:
         assert (
