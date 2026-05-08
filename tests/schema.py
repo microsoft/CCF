127312447b158cfa75e219721ae48d57d5e5564a
Max Tropets <maxtropets@gmail.com>
2026-05-08 12:59:25 +0000

    Tests back

--- tests/schema.py
+++ tests/schema.py
@@ -204,27 +204,27 @@ if __name__ == "__main__":
 
     cr = ConcurrentRunner(add)
 
-    # cr.add(
-    #     "schema",
-    #     run,
-    #     package="samples/apps/logging/logging",
-    #     nodes=infra.e2e_args.nodes(cr.args, 1),
-    # )
+    cr.add(
+        "schema",
+        run,
+        package="samples/apps/logging/logging",
+        nodes=infra.e2e_args.nodes(cr.args, 1),
+    )
 
-    # cr.add(
-    #     "nobuiltins",
-    #     run_nobuiltins,
-    #     package="samples/apps/nobuiltins/nobuiltins",
-    #     nodes=infra.e2e_args.min_nodes(cr.args, f=1),
-    # )
+    cr.add(
+        "nobuiltins",
+        run_nobuiltins,
+        package="samples/apps/nobuiltins/nobuiltins",
+        nodes=infra.e2e_args.min_nodes(cr.args, f=1),
+    )
 
-    # cr.add(
-    #     "tutorial",
-    #     e2e_tutorial.run,
-    #     package="samples/apps/logging/logging",
-    #     nodes=["local://127.0.0.1:8000"],
-    #     initial_member_count=1,
-    # )
+    cr.add(
+        "tutorial",
+        e2e_tutorial.run,
+        package="samples/apps/logging/logging",
+        nodes=["local://127.0.0.1:8000"],
+        initial_member_count=1,
+    )
 
     cr.add(
         "operations",
@@ -235,21 +235,21 @@ if __name__ == "__main__":
         ledger_chunk_bytes="1B",  # Chunk ledger at every signature transaction
     )
 
-    # cr.add(
-    #     "download",
-    #     e2e_operations.run_ledger_chunk_download,
-    #     package="samples/apps/logging/logging",
-    #     nodes=infra.e2e_args.max_nodes(cr.args, f=0),
-    #     initial_user_count=1,
-    #     ledger_chunk_bytes="1B",  # Chunk ledger at every signature transaction
-    # )
+    cr.add(
+        "download",
+        e2e_operations.run_ledger_chunk_download,
+        package="samples/apps/logging/logging",
+        nodes=infra.e2e_args.max_nodes(cr.args, f=0),
+        initial_user_count=1,
+        ledger_chunk_bytes="1B",  # Chunk ledger at every signature transaction
+    )
 
-    # cr.add(
-    #     "download-snapshot",
-    #     e2e_operations.run_backup_snapshot_download,
-    #     package="samples/apps/logging/logging",
-    #     nodes=infra.e2e_args.max_nodes(cr.args, f=0),
-    #     initial_user_count=1,
-    # )
+    cr.add(
+        "download-snapshot",
+        e2e_operations.run_backup_snapshot_download,
+        package="samples/apps/logging/logging",
+        nodes=infra.e2e_args.max_nodes(cr.args, f=0),
+        initial_user_count=1,
+    )
 
     cr.run()
