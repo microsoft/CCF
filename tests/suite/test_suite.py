5569ec151e307979470e724ca78f64277d1b23c7
cjen1-msft <chrisjensen@microsoft.com>
2026-05-15 10:34:52 +0100

    Add recovery equiv test

--- tests/suite/test_suite.py
+++ tests/suite/test_suite.py
@@ -110,6 +110,7 @@ all_tests_suite = [
     # recovery:
     recovery.test_recover_service,
     recovery.test_recover_service_aborted,
+    recovery.test_recover_service_snapshot_ledger_offset,
     # rekey:
     e2e_logging.test_rekey,
     # election:
