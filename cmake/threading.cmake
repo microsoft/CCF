b2dc363465f6532baf58dec85e8296f1a2635770
Amaury Chamayou <amchamay@microsoft.com>
2026-07-13 18:13:56 +0100

    Apply suggestions from code review
    Co-authored-by: Amaury Chamayou <amaury@xargs.fr>

--- cmake/threading.cmake
+++ cmake/threading.cmake
@@ -5,10 +5,7 @@
 # headers depend on (ccf::threading::get_current_thread_id and friends).
 #
 # Keeping this in its own library lets every logger user resolve the symbol by
-# depending on ccf_threading, without having to link libccf. This is what
-# avoids the circular dependency tracked in
-# https://github.com/microsoft/CCF/issues/7596: ccfcrypto (and the other
-# low-level libraries) use the logger, but are themselves dependencies of ccf.
+# depending on ccf_threading, without having to link libccf.
 add_ccf_static_library(
   ccf_threading
   SRCS ${CCF_DIR}/src/threading/thread_ids.cpp
